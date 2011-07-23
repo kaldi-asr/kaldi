1880343b40b4b5724ffd33f3f704bde1a63526c8
Dan Povey <dpovey@gmail.com>
2011-07-23 03:05:54 +0000

    Changed path.sh in wsj recipe.
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@170 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/wsj/s1/path.sh
+++ egs/wsj/s1/path.sh
@@ -1,2 +1,2 @@
-export PATH=$PATH:../../../src/bin:../../../tools/openfst/bin:../../../src/fstbin/:../../../src/gmmbin/:../../../src/featbin/:../../../src/lm/:../../../src/sgmmbin/:../../../src/fgmmbin/
+export PATH=../../../src/bin:../../../tools/openfst/bin:../../../src/fstbin/:../../../src/gmmbin/:../../../src/featbin/:../../../src/lm/:../../../src/sgmmbin/:../../../src/fgmmbin/:$PATH
 export LC_ALL=C
