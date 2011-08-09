5e74113b236b46066ddc89fc76b680dc36b203b5
Dan Povey <dpovey@gmail.com>
2011-08-09 11:49:46 +0000

    Fix to install.sh script.
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/sandbox/thang-project@236 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- tools/install.sh
+++ tools/install.sh
@@ -72,7 +72,7 @@ fi
   done
   cp ../ATLAS/include/cblas.h . || exit 1
   
-  if [ ! -e clapack.h ] || [ ! -e f2c.h ] || [ ! -e cblas.h]; then
+  if [ ! -e clapack.h ] || [ ! -e f2c.h ] || [ ! -e cblas.h ]; then
     echo "****download clapack.h or f2c.h failed."
     cd ..
     exit 1
