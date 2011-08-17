f4d33f9732694597f0c795f0a874d392aa737467
Gilles Boulianne <gilles.boulianne@crim.ca>
2011-08-17 19:01:16 +0000

    Code for large LM decoding.
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@323 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ src/fstext/deterministic-fst-inl.h
@@ -0,0 +1,252 @@
+// fstext/deterministic-fst-inl.h
+
+// Copyright 2011 Gilles Boulianne
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef KALDI_FSTEXT_DETERMINISTIC_FST_INL_H_
+#define KALDI_FSTEXT_DETERMINISTIC_FST_INL_H_
+#include "base/kaldi-common.h"
+#include "fstext/fstext-utils.h"
+
+// Do not include this file directly.  It is included by deterministic-fst.h.
+
+// NOTES: normally only the composition case should use the CacheImpl. In the
+//        single FST case we could just return values from the FST without
+//        recording anything to the cache. However the cache is used anyway
+//        to make it clearer what will be needed for the composition implementation.
+
+
+namespace fst {
+
+/// \addtogroup deterministic_fst_group
+/// @{
+
+// in addition to CacheImpl, there is a cache for GetArc() results
+template<class Arc>
+bool DeterministicOnDemandFstImpl<Arc>::HasArc(StateId s, Label l) {
+  cache_calls_++;
+  StateLabelPair sl = make_pair(s,l);
+  scm_it_ = state_cache_map_.find(sl);
+  if (scm_it_ != state_cache_map_.end()){
+    cache_hits_++;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+template<class Arc>
+void DeterministicOnDemandFstImpl<Arc>::AddSingleArc(StateId s, Label l, Arc& a) {
+  StateLabelPair sl = make_pair(s,l);
+  state_cache_map_[sl] = a;
+}
+
+template<class Arc>
+void DeterministicOnDemandFstImpl<Arc>::SetArc(StateId s, Label l) {
+  // add an invalid arc with a kNoStateId nextstate
+  Arc a(0,0,0,kNoStateId);
+  DeterministicOnDemandFstImpl<Arc>::AddSingleArc(s,l,a);
+}
+
+template<class Arc>
+bool DeterministicOnDemandFstImpl<Arc>::GetCachedArc(StateId s, Label l, Arc *oarc) {
+  // assume HasArc() was just called and return true, thus scm_it_ is properly set
+  *oarc = scm_it_->second;
+  return (oarc->nextstate != kNoStateId);  // return true if valid arc
+}
+
+template<class Arc>
+typename DeterministicOnDemandFstImpl<Arc>::StateId DeterministicOnDemandFstImpl<Arc>::Start() {
+  if (! CacheImpl<Arc>::HasStart()) {
+    StateId s = 0;
+    if (fst2_) {
+      // composition
+      StateId s1 = fst1_->Start(), s2 = fst2_->Start();
+      if (s1 == kNoStateId || s2 == kNoStateId) return kNoStateId;
+      if (composedState_.size()==0) AddComposedState(s1,s2);
+    } else {
+      // single FST case
+      s = fst1_->Start();
+    }
+    assert(s == 0);
+    SetStart(s);
+    return s;
+  }
+  return CacheImpl<Arc>::Start();
+}
+
+// Constructor from single FST
+template<class Arc>
+  DeterministicOnDemandFstImpl<Arc>::DeterministicOnDemandFstImpl(const Fst<Arc>&fst): 
+                                      fst1_(&fst), fst2_(NULL), cache_calls_(0), cache_hits_(0) {
+  if (!fst1_->Properties(kILabelSorted,true))
+	KALDI_ERR << "DeterministicOnDemandFst: error: input FST must be input label sorted";
+  SetType("deterministic");
+}
+
+// Constructor from 2 DODF
+template<class Arc>
+DeterministicOnDemandFstImpl<Arc>::DeterministicOnDemandFstImpl(const Fst<Arc> &fst1,const Fst<Arc> &fst2):
+                                      fst1_(&fst1), fst2_(&fst2), cache_calls_(0), cache_hits_(0) {
+  if (!fst1_->Properties(kILabelSorted,true) || !fst2_->Properties(kILabelSorted,true))
+	KALDI_ERR << "DeterministicOnDemandFst: error: input FST's must be input label sorted";
+  SetType("deterministic");
+}
+
+template<class Arc>
+DeterministicOnDemandFstImpl<Arc>::DeterministicOnDemandFstImpl(const DeterministicOnDemandFstImpl &other) {
+  /* to be implemented */
+  KALDI_ERR << "DeterministicOnDemandFst copying not yet supported.";
+}
+
+template<class Arc>
+typename DeterministicOnDemandFstImpl<Arc>::Weight DeterministicOnDemandFstImpl<Arc>::Final(StateId s) {
+  if (!HasFinal(s)) {  
+    // Work out final-state weight.
+    Weight w;
+    if (fst2_){
+      StatePair sp = composedState_[s];
+      w = Times(fst1_->Final(sp.first),fst2_->Final(sp.second));
+    } else {
+      w = fst1_->Final(s);
+    }
+    SetFinal(s, w);
+    return w;
+  }
+  return CacheImpl<Arc>::Final(s);
+}
+
+template<class Arc>
+size_t DeterministicOnDemandFstImpl<Arc>::NumArcs(StateId s) {
+  if (!HasArcs(s))
+    Expand(s);
+  return CacheImpl<Arc>::NumArcs(s);
+}
+
+template<class Arc>
+size_t DeterministicOnDemandFstImpl<Arc>::NumOutputEpsilons(StateId s) {
+  if (!HasArcs(s))
+    Expand(s);
+  return CacheImpl<Arc>::NumOutputEpsilons(s);
+}
+
+template<class Arc>
+void DeterministicOnDemandFstImpl<Arc>::InitArcIterator(StateId s, ArcIteratorData<Arc> *data) {
+  if (!HasArcs(s))
+    Expand(s);
+  CacheImpl<Arc>::InitArcIterator(s, data);
+}
+
+// helper method for GetArc()
+template<class Arc>
+bool  DeterministicOnDemandFstImpl<Arc>::GetArcFromNonDetFst(const Fst<Arc>* fst, StateId s, Label ilabel, Arc *oarc, DeterministicOnDemandFstImpl<Arc>::Weight iweight = Weight::One()){
+
+  // use a SortedMatcher
+  // fst should already have been tested for correct "sortedness"
+  SortedMatcher<Fst<Arc> > sm(*fst,MATCH_INPUT,0); // enable matching epsilons as well
+  sm.SetState(s);
+  if (sm.Find(ilabel)) {
+    const Arc &arc = sm.Value();
+    *oarc = Arc(ilabel, arc.olabel, Times(arc.weight, iweight), arc.nextstate);
+    return true;
+  }
+  // didn't find matching ilabel, look for an epsilon transition and take it
+  if (sm.Find(kNoLabel)) {
+    const Arc &arc = sm.Value();
+    return DeterministicOnDemandFstImpl<Arc>::GetArcFromNonDetFst(
+                      fst, arc.nextstate, ilabel, oarc, Times(arc.weight,iweight));
+
+  }    
+  return false;  // otherwise, no match is possible
+}
+
+// This function is specific to DeterministicOnDemandFst.
+template<class Arc>
+bool DeterministicOnDemandFstImpl<Arc>::GetArc(StateId s, Label ilabel, Arc *oarc) {
+
+  // Check first if we already have the result in cache
+  if (HasArc(s, ilabel)) {
+    return(GetCachedArc(s, ilabel, oarc));
+  } 
+  // otherwise compute the arc and set cache info
+  if (fst2_) {
+    // composition case
+    StatePair sp = composedState_[s];
+    Arc arc1, arc2;
+    bool r1 = GetArcFromNonDetFst(fst1_,sp.first, ilabel,     &arc1); 
+    if (!r1) {
+      SetArc(s,ilabel); // set without arc added
+      return false;
+    }
+    //cerr << "Matched "<<ilabel     <<" in fst1, cost "<<arc1.weight<<" olabel "<<arc1.olabel<<endl;
+    if (arc1.olabel==0) {
+      // don't move in fst2_
+      *oarc = Arc(ilabel, 0, arc1.weight, AddComposedState(arc1.nextstate,sp.second));
+      AddSingleArc(s, ilabel, *oarc); // add 1 arc and set
+      return true;
+    }
+    bool r2 = GetArcFromNonDetFst(fst2_,sp.second,arc1.olabel,&arc2);
+    if (!r2) {
+      SetArc(s,ilabel); // set without arc added
+      return false;
+    }
+    // we have matching arcs from both FSTs, compute composed arc
+    *oarc = Arc(ilabel, arc2.olabel,
+				Times(arc1.weight, arc2.weight), 
+				AddComposedState(arc1.nextstate,arc2.nextstate));
+    AddSingleArc(s, ilabel, *oarc); // add 1 arc and set
+        return true;
+  } else {
+    // single FST case: not cached
+    return GetArcFromNonDetFst(fst1_, s, ilabel, oarc);
+  }
+  return false;
+}
+
+// Note that Expand is not called if we do the composition using
+// GetArc(), which is the normal case.
+template<class Arc>
+void DeterministicOnDemandFstImpl<Arc>::Expand(StateId s) {  // expands arcs only [not final state weight].
+
+  if (fst2_) {
+    KALDI_ERR << "DeterministicOnDemandFstImpl<Arc>::Expand() not implemented for the composition case.";
+  }
+  // Add all the arcs to the cache. Just for clarity, not efficiency.
+  for (ArcIterator<Fst<Arc> > aiter(*fst1_, s); !aiter.Done(); aiter.Next()) {
+    const Arc &arc = aiter.Value();
+    AddArc(s, arc);
+  }  
+  SetArcs(s);  // mark the arcs as "done". [so HasArcs returns true].
+}
+
+template<class Arc>
+DeterministicOnDemandFst<Arc>::DeterministicOnDemandFst(const DeterministicOnDemandFst<Arc> &fst, bool reset) {
+  if (reset) {
+    impl_ = new DeterministicOnDemandFstImpl<Arc>(*(fst.impl_));
+    // Copy constructor of DeterministicOnDemandFstImpl.
+    // Main use of calling with reset = true is to free up memory
+    // (e.g. then you could delete original one).
+  } else {
+    impl_ = fst.impl_;
+    impl_->IncrRefCount();
+  }
+}
+
+///
+
+} // end namespace fst
+
+
+#endif
---
+++ src/fstext/deterministic-fst-test.cc
@@ -0,0 +1,224 @@
+// fstext/deterministic-fst-test.cc
+
+// Copyright 2009-2011  Gilles Boulianne
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+
+#include "fstext/deterministic-fst.h"
+#include "fstext/fst-test-utils.h"
+#include "util/kaldi-io.h"
+
+#include <sys/stat.h> 
+
+bool FileExists(string strFilename) { 
+  struct stat stFileInfo; 
+  bool blnReturn; 
+  int intStat; 
+
+  // Attempt to get the file attributes 
+  intStat = stat(strFilename.c_str(),&stFileInfo); 
+  if(intStat == 0) { 
+    // We were able to get the file attributes 
+    // so the file obviously exists. 
+    blnReturn = true; 
+  } else { 
+    // We were not able to get the file attributes. 
+    // This may mean that we don't have permission to 
+    // access the folder which contains this file. If you 
+    // need to do that level of checking, lookup the 
+    // return values of stat which will give you 
+    // more details on why stat failed. 
+    blnReturn = false; 
+  } 
+   
+  return(blnReturn); 
+}
+
+// Simplify writing
+typedef fst::StdArc          StdArc;
+typedef fst::StdArc::Label   Label;
+typedef fst::StdArc::StateId StateId;
+typedef fst::StdVectorFst    StdVectorFst;
+typedef fst::DeterministicOnDemandFst<StdArc> StdDeterministicOnDemandFst;
+typedef fst::StdArc::Weight  Weight;
+
+
+// something that looks like a language model FST with epsilon backoffs
+StdVectorFst* CreateBackoffFst() {
+  StdVectorFst *fst = new StdVectorFst();
+  fst->AddState();   // state 0
+  fst->SetStart(0);
+  fst->AddArc(0, StdArc(10,10,0.0,1));
+
+  fst->AddState();    // state 1
+  fst->AddArc(1, StdArc(12,12,0.0,4));
+  fst->AddArc(1, StdArc(0,0,  0.1,2));  // backoff from 1 to 2
+
+  fst->AddState();    // state 2
+  fst->AddArc(2, StdArc(13,13,0.2,4));
+  fst->AddArc(2, StdArc(0,0,  0.3,3));  // backoff from 2 to 3
+
+  fst->AddState();     // state 3
+  fst->AddArc(3, StdArc(14,14,0.4,4));
+
+  fst->AddState();    // state 4
+  fst->AddArc(4, StdArc(15,15,0.5,5));
+
+  fst->AddState();     // state 5
+  fst->SetFinal(5,0.6);
+
+  return(fst);
+}
+
+// what the resulting DeterministicOnDemand FST should be like
+StdVectorFst* CreateResultFst() {
+  StdVectorFst *fst = new StdVectorFst();
+  fst->AddState();   // state 0
+  fst->SetStart(0);
+  fst->AddArc(0, StdArc(10,10,0.0,1));
+
+  fst->AddState();    // state 1
+  fst->AddArc(1, StdArc(12,12,0.0,4));
+  fst->AddArc(1, StdArc(13,13,0.3,4));  // went through 1 backoff
+  fst->AddArc(1, StdArc(14,14,0.8,4));  // went through 2 backoffs
+
+  fst->AddState();    // state 2
+  fst->AddState();    // state 3
+
+  fst->AddState();    // state 4
+  fst->AddArc(4, StdArc(15,15,0.5,5));
+
+  fst->AddState();     // state 5 
+  fst->SetFinal(5,0.6);
+  
+  return(fst);
+}
+
+void DeleteTestFst(StdVectorFst *fst) {
+  if (fst) delete fst;
+}
+
+// Follow paths from an input fst representing a string
+// (poor man's composition)
+Weight WalkSinglePath(StdVectorFst* ifst, StdDeterministicOnDemandFst* dfst) {
+  StdArc* oarc =  new StdArc();
+  StateId isrc=ifst->Start();
+  StateId dsrc=dfst->Start();
+  Weight totalCost = Weight::One();
+  
+  while (ifst->Final(isrc) == Weight::Zero()) { // while not final
+	fst::ArcIterator<StdVectorFst> aiter(*ifst,isrc);
+	const StdArc &iarc = aiter.Value();
+	if (dfst->GetArc(dsrc,iarc.olabel,oarc)) {
+	  Weight cost = Times(iarc.weight, oarc->weight);
+	  //cout << "  Matched label "<<iarc.olabel<<" at summed cost "<<cost<<endl;	  
+	  totalCost = Times(totalCost, cost);
+	} else {
+	  cout << "  Can't match arc ["<<iarc.ilabel<<","<<iarc.olabel<<","<<iarc.weight<<"] from "<<isrc<<endl;
+	  exit(1);
+	}
+	isrc = iarc.nextstate;
+	dsrc = oarc->nextstate;
+  }
+  cout << "  Total cost: " << totalCost << endl;
+  return totalCost;
+}
+
+int main() {
+
+  //--------------- Test without files ---------------------
+
+  // Build from existing fst
+  cout << "Test with single generated backoff FST" << endl;
+  StdVectorFst* nfst = CreateBackoffFst();
+  StdVectorFst* rfst = CreateResultFst();
+
+  // before using, make sure that it is input sorted
+  fst::ArcSort(nfst, fst::StdILabelCompare());
+  StdDeterministicOnDemandFst* dfst1 = new StdDeterministicOnDemandFst(*nfst);
+  
+  // Compare all arcs in dfst1 with expected result
+  for (fst::StateIterator<StdVectorFst> riter(*rfst); !riter.Done(); riter.Next()) {
+	StateId rsrc = riter.Value();
+	// verify that states have same weight (or final status)
+	assert(fst::ApproxEqual(rfst->Final(rsrc),dfst1->Final(rsrc)));
+	for (fst::ArcIterator<StdVectorFst> aiter(*rfst,rsrc); !aiter.Done(); aiter.Next()) {
+	  StdArc rarc = aiter.Value();
+	  StdArc darc;
+	  if (dfst1->GetArc(rsrc, rarc.ilabel, &darc)) {
+		assert(fst::ApproxEqual(rarc.weight, darc.weight,0.001));
+		assert(rarc.ilabel==darc.ilabel);
+		assert(rarc.olabel==darc.olabel);
+		assert(rarc.nextstate == darc.nextstate);
+		cerr << "  Got same arc at state "<<rsrc<<": "<<rarc.ilabel<<" "<<darc.ilabel<<endl;
+	  } else {
+		cerr << "Couldn't find arc "<<rarc.ilabel<<" for state "<<rsrc<<endl;
+		exit(1);
+	  }
+	}
+  }
+
+  delete nfst;
+  delete rfst;
+  delete dfst1;
+
+  // Tests with files Gm.fst, Gp.fst, str.fst, strbg.fst -------
+  // Skipped if these files are absent
+  if (!FileExists("Gm.fst") || !FileExists("Gp.fst") || !FileExists("str.fst") ||!FileExists("strbg.fst")) {
+	cout << "Skipped test with external files." << endl;
+	cout << "Test succeeded." << endl;
+	exit(0);
+  }
+  
+
+  //----------------- Test with files, single FST case --------------
+  Weight cost2;
+  cout << "Single FST case: string with G'" <<  endl;
+  cout << "  Reading Gp.fst" << endl << std::flush;
+  StdVectorFst* gpfst = StdVectorFst::Read("Gp.fst");
+  cout << "  Creating SDODF from Gp.fst" << endl << std::flush;
+  StdDeterministicOnDemandFst* dfst2 = new StdDeterministicOnDemandFst(*gpfst);
+  StdVectorFst* strfst = StdVectorFst::Read("str.fst");
+  cost2 = WalkSinglePath(strfst, dfst2);
+  delete dfst2; delete strfst;
+
+  //----------------- Test with files, composition case (with string transducer)
+  Weight cost3;
+  cout << "Composition case: string*bg with G^-1 * G'" << endl;
+  cout << "  Reading Gm.fst" << endl << std::flush;
+  StdVectorFst *gmfst = StdVectorFst::Read("Gm.fst");
+  StdDeterministicOnDemandFst* dfst3 = new StdDeterministicOnDemandFst(*gmfst,*gpfst);
+  StdVectorFst *strbgfst = StdVectorFst::Read("strbg.fst");
+  cost3 = WalkSinglePath(strbgfst,dfst3);
+  cout << "Cache had "<<dfst3->CacheCalls()<<" calls and "<<dfst3->CacheHits()<<" hits"<< endl;
+  delete gmfst; delete gpfst; 
+
+  if ( !ApproxEqual(cost2,cost3)) {
+	cout << "Test failed." << endl;
+	exit(1);
+  }
+
+  //----------------- Exercise cache, composition case (with string transducer)
+  Weight cost4;
+  cout << "Exercise cache: string*bg with G^-1 * G'" << endl;
+  cost4 = WalkSinglePath(strbgfst,dfst3);
+  if ( !ApproxEqual(cost2,cost4)) {
+	cout << "Test failed." << endl;
+	exit(1);
+  }
+  cout << "Cache had "<<dfst3->CacheCalls()<<" calls and "<<dfst3->CacheHits()<<" hits"<< endl;
+  delete strbgfst;
+
+  cout << "Test succeeded." << endl;
+}
