74ab02e2fbe0550e9121c7f4ad916c8591776cfe
Stefan Kombrink <katakombi@users.sf.net>
2011-08-22 21:26:05 +0000

    wip
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@369 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/rnn/rnn-rescore.cc
+++ src/rnn/rnn-rescore.cc
@@ -15,7 +15,7 @@
 // See the Apache 2 License for the specific language governing permissions and
 // limitations under the License.
 
-#include <Eigen/Dense>
+#include <rnn/Eigen/Dense>
 #include <iostream>
 #include <fstream>
 #include <string>
