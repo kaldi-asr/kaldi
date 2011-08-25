51c22b64f5deba6f19f01e26edfe4fe1fdb89e2d
Stefan Kombrink <katakombi@users.sf.net>
2011-08-25 18:14:14 +0000

    adding rnn rescore (version independent of Eigen) to the top level Makefile since it can be build safely now!
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@402 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/rnn/rnndebug.sh
+++ src/rnn/rnndebug.sh
@@ -4,7 +4,7 @@ cd /mnt/matylda5/kombrink/src/kaldi/trunk/egs/wsj/s1/
 . path.sh
 cd $olddir
 
-N=1000
+N=10
 S=0.0625
 
 for L in 1.0 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1 0.0; do
@@ -13,7 +13,7 @@ for L in 1.0 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1 0.0; do
 #compute-wer --text --mode=present ark:/mnt/matylda5/kombrink/src/kaldi/trunk/egs/wsj/s1/exp/decode_tri1ckn_latgen_tgpr_eval92/test_trans.filt ark:1best.kn5.txt
 
 echo "RESCORE WITH RNN"
-time ./rnn-rescore --lambda=$L --acoustic-scale=$S --n=$N words.txt ark,t:"gunzip -c /mnt/matylda5/kombrink/src/kaldi/trunk/egs/wsj/s1/lmrescore/lats.newlm.gz|" WSJ.35M.200cl.350h.kaldi.rnn ark,t:rnn.92.lats
+time ./rnn-rescore-kaldi --lambda=$L --acoustic-scale=$S --n=$N words.txt ark,t:"gunzip -c /mnt/matylda5/kombrink/src/kaldi/trunk/egs/wsj/s1/lmrescore/lats.newlm.gz|" WSJ.35M.200cl.350h.kaldi.rnn ark,t:rnn.92.lats
 
 echo "EXTRACT NEW ONEBEST"
 lattice-best-path --acoustic-scale=$S ark:rnn.92.lats ark,t:- 2> /dev/null | /mnt/matylda5/kombrink/src/kaldi/trunk/egs/wsj/s1/scripts/int2sym.pl --ignore-first-field words.txt | sed 's/ <s>//' | sed 's/ <\/s>//' > rnntest 
