0dada5191c71c994d46ab33a0e22d772027e1ef3
Milos Janda <ijanda@users.sf.net>
2011-08-26 08:21:44 +0000

    Committing data-preparation scripts
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@406 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ egs/swbd/s1/path.sh
@@ -0,0 +1,8 @@
+
+root=`pwd`/../../..
+
+export PATH=${root}/src/bin:${root}/tools/openfst/bin:${root}/src/fstbin/:${root}/src/gmmbin/:${root}/src/featbin/:${root}/src/fgmmbin:${root}/src/sgmmbin:${root}/src/lm:${root}/src/latbin:$PATH  
+
+export LC_ALL=C
+export LC_LOCALE_ALL=C
+
---
+++ egs/swbd/s1/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Copyright 2010-2011 Microsoft Corporation
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+# WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+# MERCHANTABLITY OR NON-INFRINGEMENT.
+# See the Apache 2 License for the specific language governing permissions and
+# limitations under the License.
+
+exit 1;
+# This is a shell script, but it's recommended that you run the commands one by
+# one by copying and pasting into the shell.
+# Caution: some of the graph creation steps use quite a bit of memory, so you
+
+# might want to run this script on a machine that has plenty of memory.
+
+# To get the CMU dictionary, do:
+svn co https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/cmudict/
+# got this at revision 10966 in the last tests done before releasing v1.0.
+# can add -r 10966 for strict compatibility.
+
+
+# Data prep
+local/SWBD_data_prep.sh /mnt/matylda2/data/SWITCHBOARD_1R2
+
+local/SWBD_format_data.sh
+
