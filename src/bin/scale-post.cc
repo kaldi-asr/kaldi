93612966f0d92a0a65624704b8d30043748e52da
Chao Weng <cweng6@gmail.com>
2011-12-07 03:20:16 +0000

    split  scale-post.cc into scale-post.cc and compute-mce-scale.cc
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/sandbox/discrim@643 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/bin/scale-post.cc
+++ src/bin/scale-post.cc
@@ -26,58 +26,46 @@ int main(int argc, char *argv[]) {
     typedef kaldi::int32 int32;  
 
     const char *usage =
-        "scale the posterior according to num&den score for MCE\n"
-        "Usage: score-post [option] numscore-rspecifier denscore-rspecifier\n"
-        "unscaled-post-rspecifier scaled-post-wspecifier\n";
-     
-    ParseOptions po(usage);
-    kaldi::BaseFloat mce_alpha = 1.0, mce_beta = 0.0; 
-    po.Register("mce-alpha", &mce_alpha, "alpha parameter for sigmoid");
-    po.Register("mce-beta", &mce_beta, "beta parameter for sigmoid");
+        "scale the posterior \n"
+        "Usage: score-post scale-rspecifier post-rspecifier post-wspecifier\n";
+    
+    ParseOptions po(usage); 
     po.Read(argc, argv);
 
-    if (po.NumArgs() != 4) {
+    if (po.NumArgs() != 3) {
       po.PrintUsage();
       exit(1);
     }
       
-    std::string num_score_rspecifier = po.GetArg(1);
-    std::string den_score_rspecifier = po.GetArg(2);
-    std::string post_rspecifier = po.GetArg(3);
-    std::string post_wspecifier = po.GetArg(4);
+    std::string scale_rspecifier = po.GetArg(1);
+    std::string post_rspecifier = po.GetArg(2);
+    std::string post_wspecifier = po.GetArg(3);
   
-    kaldi::RandomAccessBaseFloatReader num_score_reader(num_score_rspecifier);
-    kaldi::RandomAccessBaseFloatReader den_score_reader(den_score_rspecifier); 
+    kaldi::RandomAccessBaseFloatReader scale_reader(scale_rspecifier);
     kaldi::SequentialPosteriorReader posterior_reader(post_rspecifier);
     kaldi::PosteriorWriter posterior_writer(post_wspecifier); 
 
-    int32 num_scaled = 0, num_no_score = 0;  
+    int32 num_scaled = 0, num_no_scale = 0;  
    
-    for(; !posterior_reader.Done(); posterior_reader.Next()) {
+    for (; !posterior_reader.Done(); posterior_reader.Next()) {
       std::string key = posterior_reader.Key();
       kaldi::Posterior posterior = posterior_reader.Value();
       posterior_reader.FreeCurrent();
-      if (!num_score_reader.HasKey(key) || !den_score_reader.HasKey(key)) {
-        num_no_score++;
+      if (!scale_reader.HasKey(key)) {
+        num_no_scale++;
       } else {
-        //calculate the sigmoid scaling factor for MCE
-        // = sigmoid(num_score - den_score)
-        BaseFloat num_score = num_score_reader.Value(key);
-        BaseFloat den_score = den_score_reader.Value(key);
-        BaseFloat score_difference = mce_alpha * (num_score - den_score) + mce_beta;
-        BaseFloat sigmoid_difference = 1.0 / (1.0 + exp(score_difference)); 
-        BaseFloat post_scale = mce_alpha * sigmoid_difference * (1 - sigmoid_difference);
+        BaseFloat post_scale = scale_reader.Value(key);
         for (size_t i = 0; i < posterior.size(); i++) {
           for (size_t j = 0; j < posterior[i].size(); j++) {
             posterior[i][j].second = posterior[i][j].second * post_scale;  	 
           }
         }
         num_scaled++; 
+        posterior_writer.Write(key, posterior);
       }
-      posterior_writer.Write(key, posterior);
     }
-    KALDI_LOG << "Scale " << num_scaled << " posteriors;  " << num_no_score
-              << " had no scores.";
+    KALDI_LOG << "Scale " << num_scaled << " posteriors;  " << num_no_scale
+              << " had no scales.";
     return (num_scaled != 0 ? 0 : 1);
   } catch(const std::exception& e) {
     std::cerr << e.what();
