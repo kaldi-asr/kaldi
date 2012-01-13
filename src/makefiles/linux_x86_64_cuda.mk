cc7763529be8cbb86b4fdb4d3625d87c6a98643d
Karel Vesely <vesis84@gmail.com>
2012-01-13 15:48:38 +0000

    fixing the build process
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/sandbox/karel@696 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/makefiles/linux_x86_64_cuda.mk
+++ src/makefiles/linux_x86_64_cuda.mk
@@ -3,6 +3,6 @@ CUDA_INCLUDE= -I$(CUDATKDIR)/include
 CUDA_FLAGS = -g -Xcompiler -fPIC --verbose --machine 64 -DHAVE_CUDA
 
 CXXFLAGS += -DHAVE_CUDA -I$(CUDATKDIR)/include 
-LDFLAGS += -L$(CUDATKDIR)/lib -Wl,-rpath,$(CUDATKDIR)/lib64
+LDFLAGS += -L$(CUDATKDIR)/lib64 -Wl,-rpath,$(CUDATKDIR)/lib64
 LDFLAGS += -lcublas -lcudart -lcuda
 
