cd4a6454665a0d39698a1fb71fa78c64d27b3cfc
Arnab Ghoshal <arnab13@gmail.com>
2012-02-15 15:46:53 +0000

    Added GlobalPhone recipe in trunk/egs/gp; removed sandbox/discrim; fixed sandbox/karel/src/configure to use CUDA with MKL.
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@758 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ egs/gp/README.txt
@@ -0,0 +1,17 @@
+
+About the GlobalPhone corpus:
+    This is a corpus of read sentences from the newspapers in 19
+    different languages recorded under varying degrees of "clean" 
+    conditions. There is roughly 15-20 hours of training data for
+    each language, as well as DEV and EVAL sets of roughly 2 hours 
+    each.
+
+Each subdirectory of this directory contains the
+scripts for a sequence of experiments.  
+Note: s1 is the "default" set of scripts at the moment.
+
+  s1: This setup is experiments with GMM-based systems with various 
+      Maximum Likelihood 
+      techniques including global and speaker-specific transforms.
+      See a parallel setup in ../wsj/s3
+
---
+++ egs/gp/s1/path.sh
@@ -0,0 +1,34 @@
+# This contains the locations of the tools and data required for running
+# the GlobalPhone experiments.
+
+KALDISRC=/exports/home/aghoshal/kaldi/trunk/src
+KALDIBIN=$KALDISRC/bin:$KALDISRC/featbin:$KALDISRC/fgmmbin:$KALDISRC/fstbin  
+KALDIBIN=$KALDIBIN:$KALDISRC/gmmbin:$KALDISRC/latbin:$KALDISRC/nnetbin
+KALDIBIN=$KALDIBIN:$KALDISRC/sgmmbin:$KALDISRC/tiedbin:$KALDISRC/lm
+
+FSTBIN=/exports/home/aghoshal/kaldi/trunk/tools/openfst/bin
+LMBIN=/exports/home/aghoshal/kaldi/trunk/tools/irstlm/bin
+
+[ -d $PWD/local ] || { echo "Expecting 'local' subdirectory"; exit 1; }
+[ -d $PWD/utils ] || { echo "Expecting 'utils' subdirectory"; exit 1; }
+[ -d $PWD/steps ] || { echo "Expecting 'steps' subdirectory"; exit 1; }
+
+LOCALUTILS=$PWD/local
+KALDIUTILS=$PWD/utils
+KALDISTEPS=$PWD/steps
+SCRIPTS=$LOCALUTILS:$KALDIUTILS:$KALDISTEPS
+
+# If you already have shorten and sox on your path, comment the following out.
+# Else use install.sh to install them first in the specified locations.
+SHORTEN=$PWD/tools/shorten-3.6.1/bin
+SOX=$PWD/tools/sox-14.3.2/bin
+[ -x $SHORTEN/shorten ] || { echo "Cannot find shorten executable"; }
+[ -x $SOX/sox ] || { echo "Cannot find sox executable"; }
+TOOLS=$SHORTEN:$SOX
+
+export PATH=$PATH:$KALDIBIN:$FSTBIN:$LMBIN:$SCRIPTS:$TOOLS
+export LC_ALL=C
+
+# Site-specific configs:
+[ `hostname -y` == ecdf ] && \
+  { . /etc/profile.d/modules.sh; module add intel/mkl; }
