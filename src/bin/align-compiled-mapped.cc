386be34699c7ad02c99f593bb1bff180149d9d96
Karel Vesely <vesis84@gmail.com>
2012-04-19 04:48:16 +0000

    updated documentation headers
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@849 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/bin/align-compiled-mapped.cc
+++ src/bin/align-compiled-mapped.cc
@@ -1,7 +1,7 @@
 // gmmbin/gmm-align-compiled.cc
 
-// Copyright 2009-2011  Microsoft Corporation
-
+// Copyright 2009-2012  Microsoft Corporation, Karel Vesely
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
