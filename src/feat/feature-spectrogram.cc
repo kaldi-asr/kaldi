e25ae4f0aeff38e69be1bad340764af54fe0aea0
Navdeep Jaitly <ndjaitly@gmail.com>
2012-06-19 20:37:39 +0000

    Added code to compute spectrogram features. Based on feature-fbank
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@1061 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ src/feat/feature-spectrogram.cc
@@ -0,0 +1,96 @@
+// feat/feature-fbank.cc
+
+// Copyright 2009-2012  Karel Vesely
+// Copyright 2012  Navdeep Jaitly
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include "feat/feature-spectrogram.h"
+
+
+namespace kaldi {
+
+Spectrogram::Spectrogram(const SpectrogramOptions &opts):
+    opts_(opts),
+    feature_window_function_(opts.frame_opts),
+    srfft_(NULL) {
+  if (opts.energy_floor != 0.0)
+    log_energy_floor_ = log(opts.energy_floor);
+
+  int32 padded_window_size = opts.frame_opts.PaddedWindowSize();
+  if ((padded_window_size & (padded_window_size-1)) == 0)  // Is a power of two...
+    srfft_ = new SplitRadixRealFft<BaseFloat>(padded_window_size);
+}
+
+Spectrogram::~Spectrogram() {
+  if (srfft_)
+    delete srfft_;
+}
+
+void Spectrogram::Compute(const VectorBase<BaseFloat> &wave,
+                   Matrix<BaseFloat> *output,
+                   Vector<BaseFloat> *wave_remainder) {
+  assert(output != NULL);
+
+  // Get dimensions of output features
+  int32 rows_out = NumFrames(wave.Dim(), opts_.frame_opts);
+  int32 cols_out =  opts_.frame_opts.PaddedWindowSize()/2 +1;
+  if (rows_out == 0)
+    KALDI_ERR << "Spectrogram::Compute, no frames fit in file (#samples is " << wave.Dim() << ")";
+  // Prepare the output buffer
+  output->Resize(rows_out, cols_out);
+  
+  // Optionally extract the remainder for further processing
+  if (wave_remainder != NULL)
+    ExtractWaveformRemainder(wave, opts_.frame_opts, wave_remainder);
+
+  // Buffers
+  Vector<BaseFloat> window;  // windowed waveform.
+  BaseFloat log_energy;
+  
+  // Compute all the freames, r is frame index..
+  for (int32 r = 0; r < rows_out; r++) {  
+    // Cut the window, apply window function
+    ExtractWindow(wave, r, opts_.frame_opts, 
+                  feature_window_function_, &window, 
+                  (opts_.raw_energy ? 
+                     &log_energy : NULL));
+    
+    // Compute energy after window function (not the raw one)
+    if (!opts_.raw_energy)
+      log_energy = VecVec(window, window);
+    
+    // Compute FFT using split-radix algorithm.
+    if (srfft_) srfft_->Compute(window.Data(), true); 
+    // An alternative algorithm that works for non-powers-of-two. 
+    else RealFft(&window, true);  
+
+    // Convert the FFT into a power spectrum.
+    ComputePowerSpectrum(&window);
+    SubVector<BaseFloat> power_spectrum(window, 0, window.Dim()/2 + 1);
+
+    power_spectrum.ApplyLog();  // take the log.
+
+    // Output buffers
+    SubVector<BaseFloat> this_output(output->Row(r));
+    this_output.CopyFromVec(power_spectrum);
+    if (opts_.energy_floor != 0.0 && log_energy < log_energy_floor_) {
+        log_energy = log_energy_floor_;
+    }
+    this_output(0) = log_energy;
+
+  }
+}
+} // namespace
---
+++ src/feat/feature-spectrogram.h
@@ -0,0 +1,76 @@
+// feat/feature-spectrogram.h
+
+// Copyright 2009-2012  Karel Vesely
+// Copyright 2012  Navdeep Jaitly
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef KALDI_FEAT_FEATURE_SPECTROGRAM_H_
+#define KALDI_FEAT_FEATURE_SPECTROGRAM_H_
+
+
+#include <string>
+
+#include "feat/feature-functions.h"
+
+namespace kaldi {
+/// @addtogroup  feat FeatureExtraction
+/// @{
+
+
+/// SpectrogramOptions contains basic options for computing SPECTROGRAM features
+/// It only includes things that can be done in a "stateless" way, i.e.
+/// it does not include energy max-normalization.
+/// It does not include delta computation.
+struct SpectrogramOptions {
+  FrameExtractionOptions frame_opts;
+  BaseFloat energy_floor;
+  bool raw_energy;  // compute energy before preemphasis and hamming window (else after)
+
+  SpectrogramOptions(): 
+                 energy_floor(0.0),  // not in log scale: a small value e.g. 1.0e-10
+                 raw_energy(true) {}
+  void Register(ParseOptions *po) {
+    frame_opts.Register(po);
+    po->Register("energy-floor", &energy_floor, "Floor on energy (absolute, not relative) in Spectrogram computation");
+    po->Register("raw-energy", &raw_energy, "If true, compute energy before Hamming window and preemphasis");
+  }
+};
+
+/// Class for computing SPECTROGRAM features; see \ref feat_mfcc for more information.
+class Spectrogram {
+ public:
+  Spectrogram(const SpectrogramOptions &opts);
+  ~Spectrogram();
+
+  /// Will throw exception on failure (e.g. if file too short for
+  /// even one frame).
+  void Compute(const VectorBase<BaseFloat> &wave,
+               Matrix<BaseFloat> *output,
+               Vector<BaseFloat> *wave_remainder = NULL);
+
+ private:
+  SpectrogramOptions opts_;
+  BaseFloat log_energy_floor_;
+  FeatureWindowFunction feature_window_function_;
+  SplitRadixRealFft<BaseFloat> *srfft_;
+  KALDI_DISALLOW_COPY_AND_ASSIGN(Spectrogram);
+};
+
+
+/// @} End of "addtogroup feat"
+}// namespace kaldi
+
+
+#endif
