fc87cf220237f212e9182a4f12ef0cc8d388c9fb
Karel Vesely <vesis84@gmail.com>
2012-08-20 19:28:31 +0000

    update
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@1254 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/wsj/s2/run.sh
+++ egs/wsj/s2/run.sh
@@ -4,20 +4,20 @@
 
 ######################################################################
 # Prepare the data, train the baseline GMM-based systems
-bash run.A.gmm.sh
+bash run.A.gmm.sh || exit 1
 
 
 ######################################################################
 # Train the pure hybdir systems (GMM replaced by ANN)
-bash run.B.hybrid.sh
+bash run.B.hybrid.sh || exit 1
 
 
 ######################################################################
 # Train the tandem systems (ANN as feature extraction for GMM)
-bash run.C.tandem.sh
+bash run.C.tandem.sh || exit 1
 
 
 ######################################################################
 # Train deep hybrid system (ANN pretrained by RBMs and Xentropy opt.)
-bash run.D.deep.sh
+bash run.D.deep.sh || exit 1
 
