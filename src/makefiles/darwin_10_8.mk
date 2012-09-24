43703748366831a5f1648db5d2459cf97cbd6383
Korbinian Riedhammer <korbinianr@gmail.com>
2012-09-24 10:40:17 +0000

    Added MacOS X 10.8 compile support (equal to 10.7)
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@1386 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ src/makefiles/darwin_10_8.mk
@@ -0,0 +1,21 @@
+# makefiles/darwin_10_6.mk contains Darwin-specific rules for OS X 10.6.*
+
+ifndef FSTROOT
+$(error FSTROOT not defined.)
+endif
+
+CXXFLAGS = -msse -msse2 -Wall -I.. \
+      -DKALDI_DOUBLEPRECISION=0 -DHAVE_POSIX_MEMALIGN \
+      -Wno-sign-compare -Winit-self \
+      -DHAVE_EXECINFO_H=1 -DHAVE_CXXABI_H -rdynamic \
+      -DHAVE_CLAPACK \
+      -I$(FSTROOT)/include \
+      $(EXTRA_CXXFLAGS) \
+      -g # -O0 -DKALDI_PARANOID
+
+LDFLAGS = -g -rdynamic
+LDLIBS = $(EXTRA_LDLIBS) $(FSTROOT)/lib/libfst.a -ldl -lm -lpthread -framework Accelerate
+CXX = g++
+CC = g++
+RANLIB = ranlib
+AR = ar
