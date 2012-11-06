43e2189dde9ec03fe8f76b5547e6afb84b3fcd3e
Dan Povey <dpovey@gmail.com>
2012-11-06 03:48:23 +0000

    Fix to makefile for cuda
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@1559 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/makefiles/linux_cuda.mk
+++ src/makefiles/linux_cuda.mk
@@ -4,6 +4,6 @@ CUDA_FLAGS = -g -Xcompiler -fPIC --verbose --machine 32 -DHAVE_CUDA
 CUDA_FLAGS += --gpu-architecture compute_13 --gpu-code sm_13
 
 CXXFLAGS += -DHAVE_CUDA -I$(CUDATKDIR)/include 
-LDFLAGS += -L$(CUDATKDIR)/lib -Wl,-rpath,$(CUDATKDIR)/lib
+LDFLAGS += -L$(CUDATKDIR)/lib -Wl,-rpath=$(CUDATKDIR)/lib
 LDFLAGS += -lcublas -lcudart -lcuda
 
