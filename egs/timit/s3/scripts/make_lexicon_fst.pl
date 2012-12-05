2ecf78eff662b384602a24a00664c8300bcad97f
Navdeep Jaitly <ndjaitly@gmail.com>
2012-12-05 03:22:51 +0000

    Bug in L.fst creation.
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@1641 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/timit/s3/scripts/make_lexicon_fst.pl
+++ egs/timit/s3/scripts/make_lexicon_fst.pl
@@ -42,7 +42,7 @@ open(L, "<$lexfn") || die "Error opening lexicon $lexfn";
 
 if( $silprob == 0.0 ) { # No optional silences: just have one (loop+final) state which is numbered zero.
     $loopstate = 0;
-    $nexststate = 1; # next unallocated state.
+    $nextstate = 1; # next unallocated state.
     while(<L>) {
         @A = split(" ", $_);
         $w = shift @A;
