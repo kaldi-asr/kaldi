add8809ff9640dd43ef5b36e8b1b3e8e71155e61
Jan Trmal <jtrmal@gmail.com>
2013-01-28 23:57:10 +0000

    Check the parameters and exit, if the parameters are not valid
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@1954 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/babel/s5/local/kws_setup.sh
+++ egs/babel/s5/local/kws_setup.sh
@@ -10,21 +10,27 @@ case_insensitive=true
 
 echo "$0 $@"  # Print the command line for logging
 
-help_message="$0: Initialize and setup the KWS task directory\n
-              Example:
-                $0 <ecf_file> <kwlist-file> <rttm-file> <lang-dir> <data-dir>"
+help_message="$0: Initialize and setup the KWS task directory
+Example:
+              $0 <ecf_file> <kwlist-file> <rttm-file> <lang-dir> <data-dir>"
 
 [ -f ./path.sh ] && . ./path.sh; # source the path.
 . parse_options.sh || exit 1;
+
+if [[ "$#" -ne "5" ]] ; then
+    printf "FATAL: invalid number of arguments.\n\n"
+    printf "$help_message\n"
+    exit 1
+fi
 
 ecf_file=$1
 kwlist_file=$2
 rttm_file=$3
 langdir=$4
 datadir=$5
-kwsdatadir=$datadir/kws
 
 for filename in "$ecf_file" "$kwlist_file" "$rttm_file" ; do
+    echo $filename
     if [ ! -f $filename ] ; then
         printf "FATAL: filename \'$filename\' does not refer to a valid file\n"
         printf "$help_message\n"
@@ -32,14 +38,15 @@ for filename in "$ecf_file" "$kwlist_file" "$rttm_file" ; do
     fi
 done
 for dirname in "$langdir" "$datadir" ; do
-    if [ ! -f $dirname ] ; then
+    if [ ! -d $dirname ] ; then
         printf "FATAL: dirname \'$dirname\' does not refer to a valid directory\n"
         printf "$help_message\n"
         exit 1;
     fi
-do
+done
 
 
+kwsdatadir=$datadir/kws
 mkdir -p $kwsdatadir
 
 cp `readlink -f $ecf_file` $kwsdatadir/ecf.xml || exit 1
