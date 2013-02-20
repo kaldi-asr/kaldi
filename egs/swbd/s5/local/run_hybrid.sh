51321957decff46c2da7cf6c640f4cf7c30d3625
Karel Vesely <vesis84@gmail.com>
2013-02-20 15:31:43 +0000

    adding the MMI example
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2041 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/swbd/s5/local/run_hybrid.sh
+++ egs/swbd/s5/local/run_hybrid.sh
@@ -8,7 +8,7 @@
 
 
 false && \
-(
+{
 
 ###
 ### First we need to dump the fMLLR features, so we can train on them easily
@@ -41,7 +41,7 @@ dir=data-fmllr/train_100k_nodup
 steps/make_fmllr_feats.sh --nj 40 --cmd "$train_cmd" \
    --transform-dir exp/tri5a_ali_100k_nodup \
    $dir data/train_100k_nodup $gmmdir $dir/_log $dir/_data || exit 1
-)
+}
 
 
 ###
@@ -53,7 +53,8 @@ steps/make_fmllr_feats.sh --nj 40 --cmd "$train_cmd" \
 ###   -rescaled by CMVN over dataset
 ###
 
-( # Train the MLP
+#false && \
+{ # Train the MLP
 dir=exp/tri5a_dnn
 ali=exp/tri5a_ali
 $cuda_cmd $dir/_train_nnet.log \
@@ -68,7 +69,63 @@ steps/decode_nnet.sh --nj 20 --cmd "$decode_cmd" --acwt 0.10 \
   $dir/graph data-fmllr/train_dev $dir/decode_train_dev &&
 steps/decode_nnet.sh --nj 20 --cmd "$decode_cmd" --acwt 0.10 \
   $dir/graph data-fmllr/eval2000 $dir/decode_eval2000
-)
+}
+
+
+
+###
+### As the next step we apply MMI training to the hybrid system,
+### using Stochastic-GD with per-utterance updates. 
+###
+### - During the training, part of the training frames is dropped.
+###   These are the frames, where the alignment has zero support from the den-lattice
+###   (ie. for given state and time from alignment, 
+###    there is hard zero in FW-BW posterior matrix)
+### - Originally I thought, these frames are there due to search errors when generating 
+###   den-lattices, but after implementing tracking of the reference path, the number
+###   of dropped frames decreased only by ~10%
+### - By inspecting the sph files and annotations, I found out that at least part of
+###   such frames are wrongly annotated, so it is reasonable thing to exclude those.
+###   Anyway by disabling the frame dropping, the WER improvement was smaller.
+###
+### - I also experimented with lattice boosting, but no further improvement was observed
+###
+
+
+dir=exp/tri5a_dnn_mmi
+srcdir=exp/tri5a_dnn
+
+# MMI starting from system in tri5a_dnn.  Use the same data (100k_nodup)
+
+# First we need to generate lattices and alignments:
+#false && \
+{
+steps/align_nnet.sh --nj 40 --cmd "$train_cmd" \
+  data-fmllr/train_100k_nodup data/lang $srcdir exp/tri5a_dnn_ali_100k_nodup || exit 1;
+steps/make_denlats_nnet.sh --nj 40 --cmd "$decode_cmd" --config conf/decode.config \
+  data-fmllr/train_100k_nodup data/lang $srcdir exp/tri5a_dnn_denlats_100k_nodup  || exit 1;
+}
+
+# Now we can re-train the hybrid by the sequence criterion (MMI)
+#false && \
+{
+steps/train_nnet_mmi.sh --cmd "$cuda_cmd" \
+  data-fmllr/train_100k_nodup data/lang $srcdir \
+  exp/tri5a_dnn_ali_100k_nodup \
+  exp/tri5a_dnn_denlats_100k_nodup \
+  $dir 
+}
+
+# Finally let's decode
+#false && \
+{
+for ITER in 1 2 3 4; do
+  steps/decode_nnet.sh --nj 30 --cmd "$decode_cmd" --config conf/decode.config \
+    --nnet $dir/${ITER}.nnet \
+    exp/tri5a/graph data-fmllr/eval2000 $dir/decode_eval2000_it${ITER} 
+done 
+}
+
 
 
 
