b8e6e24aeb4467389bfb1a0503d980080abb0f60
Yajie Miao <yajiemiao@gmail.com>
2013-02-25 04:51:48 +0000

    step scripts for BNF system building
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2077 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ egs/babel/s5/steps_BNF/align_sgmm2.sh
@@ -0,0 +1,188 @@
+#!/bin/bash
+
+# Copyright 2012  Johns Hopkins University (Author: Daniel Povey)
+#                 Carnegie Mellon University (Author: Yajie Miao)
+# Apache 2.0.
+
+# This is a modified version by Yajie for BNF front-end.
+
+# Computes training alignments and (if needed) speaker-vectors, given an 
+# SGMM system.  If the system is built on top of SAT, you should supply
+# transforms with the --transform-dir option.
+
+# If you supply the --use-graphs option, it will use the training
+# graphs from the source directory.
+
+# Begin configuration section.  
+stage=0
+nj=4
+cmd=run.pl
+use_graphs=false # use graphs from srcdir
+use_gselect=false # use gselect info from srcdir [regardless, we use
+   # Gaussian-selection info, we might have to compute it though.]
+gselect=15  # Number of Gaussian-selection indices for SGMMs.
+# Begin configuration.
+scale_opts="--transition-scale=1.0 --acoustic-scale=0.1 --self-loop-scale=0.1"
+beam=10
+retry_beam=40
+transform_dir=  # directory to find fMLLR transforms in.
+# End configuration options.
+
+echo "$0 $@"  # Print the command line for logging
+
+[ -f path.sh ] && . ./path.sh # source the path.
+. parse_options.sh || exit 1;
+
+if [ $# != 4 ]; then
+   echo "usage: steps/align_sgmm.sh <data-dir> <lang-dir> <src-dir> <align-dir>"
+   echo "e.g.:  steps/align_sgmm.sh --transform-dir exp/tri3b data/train data/lang \\"
+   echo "           exp/sgmm4a exp/sgmm5a_ali"
+   echo "main options (for others, see top of script file)"
+   echo "  --config <config-file>                           # config containing options"
+   echo "  --nj <nj>                                        # number of parallel jobs"
+   echo "  --use-graphs true                                # use graphs in src-dir"
+   echo "  --transform-dir <transform-dir>                  # directory to find fMLLR transforms"
+   echo "  --cmd (utils/run.pl|utils/queue.pl <queue opts>) # how to run jobs."
+   exit 1;
+fi
+
+data=$1
+lang=$2
+srcdir=$3
+dir=$4
+
+oov=`cat $lang/oov.int` || exit 1;
+silphonelist=`cat $lang/phones/silence.csl` || exit 1;
+splice_opts=`cat $srcdir/splice_opts 2>/dev/null` # frame-splicing options.
+sdata=$data/split$nj
+
+mkdir -p $dir/log
+cp $srcdir/splice_opts $dir 2>/dev/null # frame-splicing options.
+echo $nj > $dir/num_jobs
+[[ -d $sdata && $data/feats.scp -ot $sdata ]] || split_data.sh $data $nj || exit 1;
+
+cp $srcdir/{tree,final.mdl} $dir || exit 1;
+[ -f $srcdir/final.alimdl ] && cp $srcdir/final.alimdl $dir
+cp $srcdir/final.occs $dir;
+
+## Set up features.
+if [ -f $srcdir/final.mat ]; then feat_type=lda; else feat_type=bnf; fi
+echo "$0: feature type is $feat_type"
+
+# For the bnf case, this may not be the best manner
+case $feat_type in
+  bnf) feats="ark,s,cs:splice-feats --left-context=0 --right-context=0 scp:$sdata/JOB/feats.scp ark:- |";;
+  lda) feats="ark,s,cs:splice-feats $splice_opts scp:$sdata/JOB/feats.scp ark:- | transform-feats $srcdir/final.mat ark:- ark:- |"
+    cp $srcdir/final.mat $dir
+    ;;
+  *) echo "$0: invalid feature type $feat_type" && exit 1;
+esac
+##
+
+## Set up model and alignment model.
+mdl=$srcdir/final.mdl
+if [ -f $srcdir/final.alimdl ]; then
+  alimdl=$srcdir/final.alimdl
+else
+  alimdl=$srcdir/final.mdl
+fi
+[ ! -f $mdl ] && echo "$0: no such model $mdl" && exit 1;
+
+## Work out where we're getting the graphs from.
+if $use_graphs; then
+  [ "$nj" != "`cat $srcdir/num_jobs`" ] && \
+    echo "$0: you specified --use-graphs true, but #jobs mismatch." && exit 1;
+  [ ! -f $srcdir/fsts.1.gz ] && echo "No graphs in $srcdir" && exit 1;
+  graphdir=$srcdir
+  ln.pl $srcdir/fsts.*.gz $dir
+else
+  graphdir=$dir
+  if [ $stage -le 0 ]; then
+    echo "$0: compiling training graphs"
+    tra="ark:utils/sym2int.pl --map-oov $oov -f 2- $lang/words.txt $sdata/JOB/text|";   
+    $cmd JOB=1:$nj $dir/log/compile_graphs.JOB.log  \
+      compile-train-graphs $dir/tree $dir/final.mdl  $lang/L.fst "$tra" \
+        "ark:|gzip -c >$dir/fsts.JOB.gz" || exit 1;
+  fi
+fi
+
+## Work out where we're getting the Gaussian-selection info from
+if $use_gselect; then
+  [ "$nj" != "`cat $srcdir/num_jobs`" ] && \
+    echo "$0: you specified --use-gselect true, but #jobs mismatch." && exit 1;
+  [ ! -f $srcdir/gselect.1.gz ] && echo "No gselect info in $srcdir" && exit 1;
+  graphdir=$srcdir
+  gselect_opt="--gselect=ark:gunzip -c $srcdir/gselect.JOB.gz|"
+  ln.pl $srcdir/gselect.*.gz $dir
+else
+  graphdir=$dir
+  if [ $stage -le 1 ]; then
+    echo "$0: computing Gaussian-selection info"
+    # Note: doesn't matter whether we use $alimdl or $mdl, they will
+    # have the same gselect info.
+    $cmd JOB=1:$nj $dir/log/gselect.JOB.log \
+      sgmm2-gselect --full-gmm-nbest=$gselect $alimdl \
+      "$feats" "ark:|gzip -c >$dir/gselect.JOB.gz" || exit 1;
+  fi
+  gselect_opt="--gselect=ark:gunzip -c $dir/gselect.JOB.gz|"
+fi
+
+
+if [ $alimdl == $mdl ]; then 
+  # Speaker-independent decoding-- just one pass.  Not normal.
+  T=`sgmm2-info $mdl | grep 'speaker vector space' | awk '{print $NF}'` || exit 1;
+  [ "$T" -ne 0 ] && echo "No alignment model, yet speaker vector space nonempty" && exit 1;
+
+  if [ $stage -le 2 ]; then
+    echo "$0: aligning data in $data using model $mdl (no speaker-vectors)"
+    $cmd JOB=1:$nj $dir/log/align_pass1.JOB.log \
+      sgmm2-align-compiled $scale_opts --beam=$beam --retry-beam=$retry_beam $alimdl \
+      "ark:gunzip -c $graphdir/fsts.JOB.gz|" "$feats" "ark:|gzip -c >$dir/ali.JOB.gz" || exit 1;
+  fi
+  echo "$0: done aligning data."
+  exit 0;
+fi
+
+# Continue with system with speaker vectors.
+if [ $stage -le 2 ]; then
+  echo "$0: aligning data in $data using model $alimdl"
+  $cmd JOB=1:$nj $dir/log/align_pass1.JOB.log \
+    sgmm2-align-compiled $scale_opts "$gselect_opt" --beam=$beam --retry-beam=$retry_beam $alimdl \
+    "ark:gunzip -c $graphdir/fsts.JOB.gz|" "$feats" "ark:|gzip -c >$dir/pre_ali.JOB.gz" || exit 1;
+fi
+
+if [ $stage -le 3 ]; then
+  echo "$0: computing speaker vectors (1st pass)"
+  $cmd JOB=1:$nj $dir/log/spk_vecs1.JOB.log \
+    ali-to-post "ark:gunzip -c $dir/pre_ali.JOB.gz|" ark:- \| \
+    weight-silence-post 0.0 $silphonelist $alimdl ark:- ark:- \| \
+    sgmm2-post-to-gpost "$gselect_opt" $alimdl "$feats" ark:- ark:- \| \
+    sgmm2-est-spkvecs-gpost --spk2utt=ark:$sdata/JOB/spk2utt \
+     $mdl "$feats" ark,s,cs:- ark:$dir/pre_vecs.JOB || exit 1;
+fi
+
+if [ $stage -le 4 ]; then
+  echo "$0: computing speaker vectors (2nd pass)"
+  $cmd JOB=1:$nj $dir/log/spk_vecs2.JOB.log \
+    ali-to-post "ark:gunzip -c $dir/pre_ali.JOB.gz|" ark:- \| \
+    weight-silence-post 0.0 $silphonelist $alimdl ark:- ark:- \| \
+    sgmm2-est-spkvecs --spk2utt=ark:$sdata/JOB/spk2utt "$gselect_opt" \
+     --spk-vecs=ark:$dir/pre_vecs.JOB $mdl "$feats" ark,s,cs:- ark:$dir/vecs.JOB || exit 1;
+  rm $dir/pre_vecs.*
+fi
+
+if [ $stage -le 5 ]; then
+  echo "$0: doing final alignment."
+  $cmd JOB=1:$nj $dir/log/align_pass2.JOB.log \
+    sgmm2-align-compiled $scale_opts "$gselect_opt" --beam=$beam --retry-beam=$retry_beam \
+     --utt2spk=ark:$sdata/JOB/utt2spk --spk-vecs=ark:$dir/vecs.JOB \
+     $mdl "ark:gunzip -c $graphdir/fsts.JOB.gz|" "$feats" "ark:|gzip -c >$dir/ali.JOB.gz" || exit 1;
+fi
+
+rm $dir/pre_ali.*.gz
+
+echo "$0: done aligning data."
+
+utils/summarize_warnings.pl $dir/log
+
+exit 0;
---
+++ egs/babel/s5/steps_BNF/build_nnet_pfile.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+# Copyright 2013  Carnegie Mellon University (Author: Yajie Miao)
+# Apache 2.0
+
+# Create pfiles for deep neural network training
+# Assumes the training alignment is ready and features are (LDA+MLLT or delta+delta-delta)
+# + fMLLR
+# 
+
+# We splice 9 frames (+4, -4) of LDA+MLLT+fMLLR features, which have the dimension of 360 (if 
+# we use the default LDA dim of 40).
+# Then, an LDA is estimated and applied, projecting the spliced features down to the
+# dimension of [nnet_dim] (250 by default)
+
+# You can specify the neighbour size by --nnet-splice-opts and the final dimension by
+# --nnet-dim
+
+
+# Begin configuration section.  
+stage=1
+nj=4
+cmd=run.pl
+
+# Begin configuration.
+nnet_splice_opts="--left-context=4 --right-context=4" # frame-splicing options for nnet input
+nnet_dim=250 # the dimension after LDA, i.e., the dimension of the input for DNN
+pfile_unit_size=40 # the number of utterances of each small unit into which 
+#the whole (and huge) pfile is chopped 
+cv_ratio=0.05 # the ratio of CV data
+
+boost_silence=1.0 # factor by which to boost silence during alignment.
+randprune=4.0
+# End configuration options.
+
+echo "$0 $@"  # Print the command line for logging
+
+[ -f path.sh ] && . ./path.sh # source the path.
+. parse_options.sh || exit 1;
+
+if [ $# != 4 ]; then
+   echo "usage: steps/build_nnet_pfile.sh <data-dir> <lang-dir> <ali-dir> <exp-dir>"
+   echo "e.g.:  steps/build_nnet_pfile.sh data/train data/lang exp/tri4_ali exp/tri4_pfile"
+   echo "main options (for others, see top of script file)"
+   echo "  --config <config-file>                           # config containing options"
+   echo "  --nj <nj>                                        # number of parallel jobs"
+   echo "  --nnet-splice-opts                               # how frames are spliced for DNN input"
+   echo "  --nnet-dim                                        # the final [# of dim] after LDA"
+   echo "  --cmd (utils/run.pl|utils/queue.pl <queue opts>) # how to run jobs."
+   exit 1;
+fi
+
+data=$1
+lang=$2
+alidir=$3
+dir=$4
+
+oov=`cat $lang/oov.int` || exit 1;
+silphonelist=`cat $lang/phones/silence.csl` || exit 1;
+sdata=$data/split$nj
+
+mkdir -p $dir/log
+echo $nj > $dir/num_jobs
+echo $nnet_splice_opts > $dir/nnet_splice_opts
+splice_opts=`cat $alidir/splice_opts 2>/dev/null`
+[[ -d $sdata && $data/feats.scp -ot $sdata ]] || split_data.sh $data $nj || exit 1;
+
+## Setup features (prior to LDA)
+if [ -f $alidir/final.mat ]; then feat_type=lda; else feat_type=delta; fi
+echo "$0: feature type is $feat_type"
+
+case $feat_type in
+  delta) sifeats="ark,s,cs:apply-cmvn --norm-vars=false --utt2spk=ark:$sdata/JOB/utt2spk scp:$sdata/JOB/cmvn.scp scp:$sdata/JOB/feats.scp ark:- | add-deltas ark:- ark:- |";;
+  lda) sifeats="ark,s,cs:apply-cmvn --norm-vars=false --utt2spk=ark:$sdata/JOB/utt2spk scp:$sdata/JOB/cmvn.scp scp:$sdata/JOB/feats.scp ark:- | splice-feats $splice_opts ark:- ark:- | transform-feats $alidir/final.mat ark:- ark:- |"
+   ;;
+  *) echo "Invalid feature type $feat_type" && exit 1;
+esac
+if [ -f $alidir/trans.1 ]; then
+  echo "$0: using transforms from $alidir"
+  sifeats="$sifeats transform-feats --utt2spk=ark:$sdata/JOB/utt2spk ark,s,cs:$alidir/trans.JOB ark:- ark:- |"
+fi
+# Now the spliced features for LDA
+splicedfeats="$sifeats | splice-feats $nnet_splice_opts ark:- ark:- |"
+##
+
+if [ $stage -le 1 ]; then
+  echo "Accumulating LDA statistics."
+  $cmd JOB=1:$nj $dir/log/lda_acc.JOB.log \
+    ali-to-post "ark:gunzip -c $alidir/ali.JOB.gz|" ark:- \| \
+      weight-silence-post 0.0 $silphonelist $alidir/final.mdl ark:- ark:- \| \
+      acc-lda --rand-prune=$randprune $alidir/final.mdl "$splicedfeats" ark,s,cs:- \
+       $dir/lda.JOB.acc || exit 1;
+  est-lda --dim=$nnet_dim $dir/final.mat $dir/lda.*.acc \
+      2>$dir/log/lda_est.log || exit 1;
+  rm $dir/lda.*.acc
+fi
+
+# Now the feats input to nnet
+feats="$splicedfeats transform-feats $dir/final.mat ark:- ark:- |"
+
+if [ $stage -le 2 ]; then
+  echo "Generate nnet training pfiles"
+  $cmd JOB=1:$nj $dir/log/build_pfile.JOB.log \
+    build-pfile-from-ali $alidir/final.mdl "ark:gunzip -c $alidir/ali.JOB.gz|" \
+      "$feats" "|pfile_create -i - -o $dir/pfile.JOB -f $nnet_dim -l 1" || exit 1;
+  # concatenate the pfiles into one
+  for n in `seq 1 $nj`; do
+    all_pfiles="$all_pfiles $dir/pfile.$n"
+  done
+  pfile_concat $all_pfiles -o $dir/concat.pfile 2>$dir/log/pfile_cat.log || exit 1;
+  rm $dir/pfile.*
+fi
+
+if [ $stage -le 3 ]; then
+  echo "Split data into training and cross-validation"
+  mkdir -p concat
+  # Chop the whole pfile into small units
+  perl steps_BNF/pfile_burst -i $dir/concat.pfile -o $dir/concat -s $pfile_unit_size \
+     2>$dir/log/pfile_burst.log || exit 1;
+
+  # Split the units accoring to cv_ratio
+  perl steps_BNF/pfile_rconcat -o $dir/valid.pfile,${cv_ratio} -o $dir/train.pfile \ 
+     $dir/concat/*.pfile 2>$data/log/pfile_rconcat.log || exit 1;
+  rm -r concat
+  echo "## Info of the training pfile: ##"
+  pfile_info $dir/train.pfile
+  echo "## Info of the cross-validation pfile: ##"
+  pfile_info $dir/valid.pfile
+
+  # Compress the two (if everything is correct) pfiles 
+  gzip train.pfile valid.pfile
+fi
+
+echo "$0: done creating pfiles."
+
+exit 0;
