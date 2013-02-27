103a2618e296c1fc51f249c289dee80c08e8613c
Arnab Ghoshal <arnab13@gmail.com>
2013-02-27 12:26:35 +0000

    fixing make_denlats.sh to work with 'lang' directories with any name
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2093 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/wsj/s5/steps/make_denlats_nnet.sh
+++ egs/wsj/s5/steps/make_denlats_nnet.sh
@@ -58,21 +58,22 @@ mkdir -p $dir
 cp -r $lang $dir/
 
 # Compute grammar FST which corresponds to unigram decoding graph.
-
+new_lang="$dir/"$(basename "$lang")
+echo "Making unigram grammar FST in $new_lang"
 cat $data/text | utils/sym2int.pl --map-oov $oov -f 2- $lang/words.txt | \
   awk '{for(n=2;n<=NF;n++){ printf("%s ", $n); } printf("\n"); }' | \
-  utils/make_unigram_grammar.pl | fstcompile > $dir/lang/G.fst \
+  utils/make_unigram_grammar.pl | fstcompile > $new_lang/G.fst \
    || exit 1;
 
 # mkgraph.sh expects a whole directory "lang", so put everything in one directory...
 # it gets L_disambig.fst and G.fst (among other things) from $dir/lang, and
 # final.mdl from $srcdir; the output HCLG.fst goes in $dir/graph.
 
-
+echo "Compiling decoding graph in $dir/dengraph"
 if [ -s $dir/dengraph/HCLG.fst ]; then
    echo "Graph $dir/dengraph/HCLG.fst already exists: skipping graph creation."
 else
-  utils/mkgraph.sh $dir/lang $srcdir $dir/dengraph || exit 1;
+  utils/mkgraph.sh $new_lang $srcdir $dir/dengraph || exit 1;
 fi
 
 
