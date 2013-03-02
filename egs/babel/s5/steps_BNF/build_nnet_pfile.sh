89c42baa2c635f6a1a33e8b35fc0527113d47fce
Yajie Miao <yajiemiao@gmail.com>
2013-03-02 16:58:17 +0000

    correct a dir error
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2114 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/babel/s5/steps_BNF/build_nnet_pfile.sh
+++ egs/babel/s5/steps_BNF/build_nnet_pfile.sh
@@ -120,7 +120,7 @@ if [ $stage -le 3 ]; then
 
   # Split the units accoring to cv_ratio
   perl steps_BNF/pfile_rconcat.pl -o $dir/valid.pfile,${cv_ratio} -o $dir/train.pfile \ 
-     $dir/concat/*.pfile 2>$data/log/pfile_rconcat.log || exit 1;
+     $dir/concat/*.pfile 2>$dir/log/pfile_rconcat.log || exit 1;
   rm -r $dir/concat
   echo "## Info of the training pfile: ##"
   pfile_info $dir/train.pfile
