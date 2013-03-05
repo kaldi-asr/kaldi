008704992cf37da7208acc1e7b7057540913e73e
Dan Povey <dpovey@gmail.com>
2013-03-05 03:49:33 +0000

    Add missing file phone-align-lattice.h
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2139 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ src/lat/phone-align-lattice.h
@@ -0,0 +1,57 @@
+// lat/phone-align-lattice.h
+
+// Copyright 2009-2012  Microsoft Corporation  Johns Hopkins University (Author: Daniel Povey)
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef KALDI_FSTEXT_PHONE_ALIGN_LATTICE_H_
+#define KALDI_FSTEXT_PHONE_ALIGN_LATTICE_H_
+#include <fst/fstlib.h>
+#include <fst/fst-decl.h>
+
+#include "base/kaldi-common.h"
+#include "util/common-utils.h"
+#include "fstext/fstext-lib.h"
+#include "hmm/transition-model.h"
+#include "lat/kaldi-lattice.h"
+
+namespace kaldi {
+
+
+struct PhoneAlignLatticeOptions {
+  bool reorder;
+  bool replace_output_symbols;
+  PhoneAlignLatticeOptions(): reorder(true), replace_output_symbols(false) { }
+  void Register(ParseOptions *po) {
+    po->Register("reorder", &reorder, "True if lattice was created from HCLG with "
+                 "--reorder=true option.");
+    po->Register("replace-output-symbols", &replace_output_symbols, "If true, "
+                 "the output symbols (typically words) will be replaced with "
+                 "phones.");
+  }
+};
+
+/// Returns a lattice in which the arcs correspond exactly to phones.  (Note:
+/// it's possible in principle at the end of the lattice to have arcs with words
+/// on them but no transition-ids al all, although this is unlikely.)  returns
+/// true if everything was OK, false if some kind of error was detected
+/// (e.g. the "reorder" option was incorrectly specified.)
+bool PhoneAlignLattice(const CompactLattice &lat,
+                       const TransitionModel &tmodel,
+                       const PhoneAlignLatticeOptions &opts,
+                       CompactLattice *lat_out);
+
+
+} // end namespace kaldi
+#endif
