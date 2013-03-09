6115d7f7cfd1830fb1c5118107edfc6fa76bd750
Dan Povey <dpovey@gmail.com>
2013-03-09 20:18:58 +0000

    Fix path issue in build_nnet_pfile.sh
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2172 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/babel/s5/steps_BNF/build_nnet_pfile.sh
+++ egs/babel/s5/steps_BNF/build_nnet_pfile.sh
@@ -59,6 +59,23 @@ nj=`cat $alidir/num_jobs` || exit 1;
 silphonelist=`cat $lang/phones/silence.csl` || exit 1;
 sdata=$data/split$nj
 
+if ! which pfile_create >/dev/null; then # pfile_create not on our path.
+  [ -z "$KALDI_ROOT" ] && KALDI_ROOT=`pwd`/../../.. # normal case.
+  try_path=$KALDI_ROOT/tools/pfile_utils-v0_51/bin/
+  if [ -f $try_path/pfile_create ]; then
+    PPATH=$try_path
+  else
+    echo "You do not have pfile_create (part of pfile-utils) on your path,"
+    echo "and it is not accessible in the normal place e.g. $try_path/pfile_create"
+    echo "Try going to KALDI_ROOT/tools/ and running ./install_pfile_utils.sh"
+    exit 1
+  fi
+else
+  PPATH=$(dirname `which pfile_create`)
+fi
+export PATH=$PATH:$PPATH
+
+
 mkdir -p $dir/log
 echo $nj > $dir/num_jobs
 echo $nnet_splice_opts > $dir/nnet_splice_opts
@@ -102,13 +119,13 @@ if [ $stage -le 2 ]; then
   echo "Generate nnet training pfiles"
   $cmd JOB=1:$nj $dir/log/build_pfile.JOB.log \
     build-pfile-from-ali $alidir/final.mdl "ark:gunzip -c $alidir/ali.JOB.gz|" \
-      "$feats" "|pfile_create -i - -o $dir/pfile.JOB -f $nnet_dim -l 1" || exit 1;
+      "$feats" "|$PPATH/pfile_create -i - -o $dir/pfile.JOB -f $nnet_dim -l 1" || exit 1;
   # concatenate the pfiles into one
   all_pfiles=""
   for n in `seq 1 $nj`; do
     all_pfiles="$all_pfiles $dir/pfile.$n"
   done
-  pfile_concat $all_pfiles -o $dir/concat.pfile 2>$dir/log/pfile_cat.log || exit 1;
+  $PPATH/pfile_concat $all_pfiles -o $dir/concat.pfile 2>$dir/log/pfile_cat.log || exit 1;
   rm $dir/pfile.*
 fi
 
@@ -122,9 +139,9 @@ if [ $stage -le 3 ]; then
   perl steps_BNF/pfile_rconcat.pl -o $dir/valid.pfile,${cv_ratio} -o $dir/train.pfile $dir/concat/*.pfile 2> $dir/log/pfile_rconcat.log || exit 1;
   rm -r $dir/concat
   echo "## Info of the training pfile: ##"
-  pfile_info $dir/train.pfile
+  $PPATH/pfile_info $dir/train.pfile
   echo "## Info of the cross-validation pfile: ##"
-  pfile_info $dir/valid.pfile
+  $PPATH/pfile_info $dir/valid.pfile
 
   # Compress the two (if everything is correct) pfiles 
   gzip $dir/train.pfile $dir/valid.pfile
