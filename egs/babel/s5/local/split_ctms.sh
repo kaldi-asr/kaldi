ccad72acb296cb6a83eadb3c92bb449a74a87045
Jan Trmal <jtrmal@gmail.com>
2013-03-15 00:25:50 +0000

    Filtering and splitting the ctm according to data directories
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2218 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ egs/babel/s5/local/split_ctms.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+data=$1
+name=`basename $data`; shift
+q=$2; shift
+
+for i in $@ ; do
+    p=$q/`basename $i`
+    [ ! -f $i/reco2file_and_channel ] && "The file reco2file_and_channel not present in the $i directory!" && exit 1
+    for lmw in $q/score_* ; do
+        d=$p/`basename $lmw`
+        mkdir -p $d
+        [ -f $lmw/$name.char.ctm ] && \
+          utils/filter_scp.pl <(cut -f 1 -d ' ' $i/reco2file_and_channel) $lmw/$name.char.ctm > $d/`basename $i`.char.ctm
+        [ -f $lmw/$name.ctm ] && \
+          utils/filter_scp.pl <(cut -f 1 -d ' ' $i/reco2file_and_channel) $lmw/$name.ctm > $d/`basename $i`.ctm
+    done
+
+    if [ -f $i/stm ] && [ -f $i/glm ]; then
+        local/score_scm.sh --cmd "$decode_cmd" $i data/lang $p
+    else
+        echo "Not running scoring, file $i/stm does not exist"
+    fi
+
+done
+return 0
+
