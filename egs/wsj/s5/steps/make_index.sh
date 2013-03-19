9aa9d515061f9c320414f53283426ff8b00a5848
Guoguo Chen <chenguoguo06@gmail.com>
2013-03-19 18:33:30 +0000

    Added --max-option for kws-index-union
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2251 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/wsj/s5/steps/make_index.sh
+++ egs/wsj/s5/steps/make_index.sh
@@ -9,6 +9,7 @@ cmd=run.pl
 acwt=0.083333
 lmwt=1.0
 max_silence_frames=50
+max_states=1000000
 strict=true
 # End configuration section.
 
@@ -58,6 +59,6 @@ $cmd JOB=1:$nj $kwsdir/log/index.JOB.log \
  lattice-align-words $word_boundary $model "ark:gzip -cdf $decodedir/lat.JOB.gz|" ark:- \| \
    lattice-scale --acoustic-scale=$acwt --lm-scale=$lmwt ark:- ark:- \| \
    lattice-to-kws-index --strict=$strict ark:$utter_id ark:- ark:- \| \
-   kws-index-union --strict=$strict ark:- "ark:|gzip -c > $kwsdir/index.JOB.gz"
+   kws-index-union --strict=$strict --max-states=$max_states ark:- "ark:|gzip -c > $kwsdir/index.JOB.gz"
 
 exit 0;
