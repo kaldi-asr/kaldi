083c90ef7f67e93a10a108808493ac9fd6caef86
Dan Povey <dpovey@gmail.com>
2013-03-29 20:16:30 +0000

    Deleting s6 recipes.
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2289 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/swbd/s6/path.sh
+++
@@ -1,4 +0,0 @@
-export KALDI_ROOT=`pwd`/../../..
-export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$KALDI_ROOT/src/nnetbin:$KALDI_ROOT/src/nnet-cpubin:$PWD:$PATH
-#$KALDI_ROOT/tools/srilm/bin:$KALDI_ROOT/tools/srilm/bin/i686-m64:$KALDI_ROOT/tools/srilm/bin/i686:$PATH
-export LC_ALL=C
