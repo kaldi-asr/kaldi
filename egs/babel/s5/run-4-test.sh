d572e66f8b1a4558a3332a8588c90c17c9c6b903
Jan Trmal <jtrmal@gmail.com>
2013-04-06 21:53:43 +0000

    Fixed version, should work now...
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2318 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/babel/s5/run-4-test.sh
+++ egs/babel/s5/run-4-test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash 
 set -e
 set -o pipefail
+set -u
+
+type=dev2h
 
 . conf/common_vars.sh || exit 1;
 . ./lang.conf || exit 1;
-
 
 
 . utils/parse_options.sh     
@@ -29,9 +31,9 @@ else
 fi
 
 
-[ ! -f $test_data_cmudb_shadow ] && echo "The CMU db does not exist... " && exit 1
-[ ! -f $test_data_list ] && echo "The test data list does not exist... " && exit 1
-[ ! -d $test_data_dir ] && echo "The test data directory does not exist... " && exit 1
+[ ! -f $eval_data_cmudb ] && echo "The CMU db does not exist... " && exit 1
+#[ ! -f $eval_data_list ] && echo "The test data list does not exist... " && exit 1
+#[ ! -d $eval_data_dir ] && echo "The test data directory does not exist... " && exit 1
 
 [ -z $cer ] && cer=0
 
@@ -71,13 +73,18 @@ fi
 #
 #####################################################################
 if [ $type == dev10h ]; then
+  dev10h_data_cmudb=$eval_data_cmudb
+  dev10h_ecf_file=$ecf_file
+  dev10h_kwlist_file=$kwlist_file
+  dev10h_rttm_file=$rttm_file
+  decode_nj=$dev10h_nj
   if [ ! -f data/dev10h.uem/.done ]; then
-    local/cmu_uem2kaldi_dir.sh --filelist $dev10h_data_list $dev10h_data_cmudb_shadow  $dev10h_data_dir data/dev10h.uem
-    cp data/eval.pem/stm data/dev10h.uem
-    cp data/eval.pem/glm data/dev10h.uem
+    local/cmu_uem2kaldi_dir.sh --filelist $dev10h_data_list $dev10h_data_cmudb  $dev10h_data_dir data/dev10h.uem
+    cp data/dev10h/stm data/dev10h.uem
+    cp data/dev10h/glm data/dev10h.uem
     if [ $cer == 1 ] ; then
-      [ ! -f data/eval.pem/char.stm ] && echo "CER=1 and file eval.pem/char.stm not present" && exit 1
-      cp data/eval.pem/char.stm data/dev10h.uem
+      [ ! -f data/dev10h/char.stm ] && echo "CER=1 and file dev10h/char.stm not present" && exit 1
+      cp data/dev10h/char.stm data/dev10h.uem
     fi
     steps/make_plp.sh --cmd "$train_cmd" --nj $decode_nj data/dev10h.uem exp/make_plp/dev10h.uem plp
     steps/compute_cmvn_stats.sh data/dev10h.uem exp/make_plp/dev10h.uem plp
@@ -88,6 +95,29 @@ if [ $type == dev10h ]; then
   fi
 fi
 
+if [ $type == dev2h ]; then
+  dev2h_data_cmudb=$eval_data_cmudb
+  dev2h_ecf_file=$ecf_file
+  dev2h_kwlist_file=$kwlist_file
+  dev2h_rttm_file=$rttm_file
+  if [ ! -f data/dev2h.uem/.done ]; then
+    local/cmu_uem2kaldi_dir.sh --filelist $dev2h_data_list $dev2h_data_cmudb  $dev2h_data_dir data/dev2h.uem
+    cp data/dev2h/stm data/dev2h.uem
+    cp data/dev2h/glm data/dev2h.uem
+    if [ $cer == 1 ] ; then
+      [ ! -f data/dev2h/char.stm ] && echo "CER=1 and file eval.pem/char.stm not present" && exit 1
+      cp data/dev2h/char.stm data/dev2h.uem
+    fi
+    steps/make_plp.sh --cmd "$train_cmd" --nj $decode_nj data/dev2h.uem exp/make_plp/dev2h.uem plp
+    steps/compute_cmvn_stats.sh data/dev2h.uem exp/make_plp/dev2h.uem plp
+    utils/fix_data_dir.sh data/dev2h.uem
+
+    local/kws_setup.sh --case-insensitive $case_insensitive \
+      --subset-ecf $dev2h_data_list --rttm-file $dev2h_rttm_file \
+      $dev2h_ecf_file $dev2h_kwlist_file data/lang data/dev2h.uem
+    touch data/dev2h.uem/.done
+  fi
+  decode_nj=$decode_nj
 fi
 
 ####################################################################
@@ -99,7 +129,7 @@ fi
 
 
 if [ ! -f exp/tri5/decode_${type}.uem/.done ]; then
-  steps/decode_fmllr_extra.sh --skip-scoring "$skip_scoring" --nj 64 --cmd "$decode_cmd" \
+  steps/decode_fmllr_extra.sh --skip-scoring "$skip_scoring" --nj $decode_nj --cmd "$decode_cmd" \
     --num-threads 6 --parallel-opts "-pe smp 6 -l ram_free=0.5G" \
     exp/tri5/graph data/${type}.uem exp/tri5/decode_${type}.uem  | tee  exp/tri5/decode_${type}.uem.log
   touch exp/tri5/decode_${type}.uem/.done
@@ -132,14 +162,14 @@ fi
 ## SGMM2 decoding 
 ####################################################################
 if [ ! -f exp/sgmm5/decode_fmllr_${type}.uem/.done ]; then
-  steps/decode_sgmm2.sh --skip-scoring "$skip_scoring" --use-fmllr true --nj 64 \
+  steps/decode_sgmm2.sh --skip-scoring "$skip_scoring" --use-fmllr true --nj $decode_nj \
     --cmd "$decode_cmd" --transform-dir exp/tri5/decode_${type}.uem \
     --num-threads 6 --parallel-opts "-pe smp 6 -l ram_free=0.5G" \
     exp/sgmm5/graph data/${type}.uem exp/sgmm5/decode_fmllr_${type}.uem | tee exp/sgmm5/decode_fmllr_${type}.uem.log
 fi  
 
 if [ ! -f exp/sgmm5/decode_fmllr_${type}.uem/.kws.done ]; then
-  local/lattice_to_ctm.sh --cmd "$decode_cmd" data/shadow.uem data/lang exp/sgmm5/decode_fmllr_shadow.uem 
+  local/lattice_to_ctm.sh --cmd "$decode_cmd" data/$type.uem data/lang exp/sgmm5/decode_fmllr_$type.uem 
   if [ $type == shadow ]; then
     split_ctms exp/sgmm5/decode_fmllr_shadow.uem data/dev2h data/eval.uem
     local/shadow_set_kws_search.sh --cmd "$decode_cmd" data/shadow.uem data/lang \
