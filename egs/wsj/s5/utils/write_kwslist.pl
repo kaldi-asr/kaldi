88d9f7ae009c9ad50953492a06fdee90b82b9504
Guoguo Chen <chenguoguo06@gmail.com>
2013-04-09 05:23:28 +0000

    write_kwslist.pl updates
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2338 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/wsj/s5/utils/write_kwslist.pl
+++ egs/wsj/s5/utils/write_kwslist.pl
@@ -14,17 +14,22 @@ Usage: utils/write_kwslist.pl [options] <raw_result_in|-> <kwslist_out|->
 
 Allowed options:
   --beta                      : Beta value when computing ATWV              (float,   default = 999.9)
+  --digits                    : How many digits should the score use        (int,     default = "infinite")
+  --duptime                   : Tolerance for duplicates                    (float,   default = 0.5)
   --duration                  : Duration of all audio, you must set this    (float,   default = 999.9)
   --ecf-filename              : ECF file name                               (string,  default = "") 
   --flen                      : Frame length                                (float,   default = 0.01)
   --index-size                : Size of index                               (float,   default = 0)
+  --kwlist-filename           : Kwlist.xml file name                        (string,  default = "") 
   --language                  : Language type                               (string,  default = "cantonese")
   --map-utter                 : Map utterance for evaluation                (string,  default = "")
   --normalize                 : Normalize scores or not                     (boolean, default = false)
-  --Ntrue-scale               : Keyword independent scale factor for Ntrue  (float,   default = 1.0) 
+  --Ntrue-scale               : Keyword independent scale factor for Ntrue  (float,   default = 1.0)
+  --remove-dup                : Remove duplicates                           (boolean, default = false) 
   --segments                  : Segments file from Kaldi                    (string,  default = "")
   --system-id                 : System ID                                   (string,  default = "")
-  --digits                    : How many digits should the score use        (int,     default = "infinite")
+  --YES-cutoff                : Only keep "\$YES-cutoff" yeses for each kw   (int,     default = -1)
+  --verbose                   : Verbose level (higher --> more kws section) (integer, default 0)
 EOU
 
 my $segment = "";
@@ -39,7 +44,11 @@ my $normalize = "false";
 my $map_utter = "";
 my $Ntrue_scale = 1.0;
 my $digits = 0;
-
+my $kwlist_filename = "";
+my $verbose = 0;
+my $duptime = 0.5;
+my $remove_dup = "false";
+my $YES_cutoff = -1;
 GetOptions('segments=s'     => \$segment,
   'flen=f'         => \$flen,
   'beta=f'         => \$beta,
@@ -51,11 +60,19 @@ GetOptions('segments=s'     => \$segment,
   'normalize=s'    => \$normalize,
   'map-utter=s'    => \$map_utter,
   'Ntrue-scale=f'  => \$Ntrue_scale,
-  'digits=i'       => \$digits);
-
+  'digits=i'       => \$digits,
+  'kwlist-filename=s' => \$kwlist_filename,
+  'verbose=i'         => \$verbose,
+  'duptime=f'         => \$duptime,
+  'remove-dup=s'      => \$remove_dup,
+  'YES-cutoff=i'      => \$YES_cutoff);
 
 if ($normalize ne "true" && $normalize ne "false") {
   die "Bad value for option --normalize. \n";
+}
+
+if ($remove_dup ne "true" && $remove_dup ne "false") {
+  die "Bad value for option --remove-dup. \n";
 }
 
 if ($segment) {
@@ -83,13 +100,6 @@ if ($filein eq "-") {
   $source = "I";
 }
 
-# Open output fst list
-my $sourceout = "";
-if ($fileout ne "-") {
-  if (!open(O, ">$fileout")) {print "Fail to open output file: $fileout\n"; exit 1;}
-  $sourceout = "O";
-}
-
 # Get symbol table and start time
 my %tbeg = ();
 if ($segment) {
@@ -112,16 +122,68 @@ if ($map_utter) {
   }
 }
 
+# Function for printing Kwslist.xml
+sub PrintKwslist {
+  my ($info, $KWS) = @_;
+
+  my $kwslist = "";
+
+  # Start printing
+  $kwslist .= "<kwslist kwlist_filename=\"$info->[0]\" language=\"$info->[1]\" system_id=\"$info->[2]\">\n";
+  my $prev_kw = "";
+  foreach my $kwentry (@{$KWS}) {
+    if ($prev_kw ne $kwentry->[0]) {
+      if ($prev_kw ne "") {$kwslist .= "  </detected_kwlist>\n";}
+      $kwslist .= "  <detected_kwlist search_time=\"1\" kwid=\"$kwentry->[0]\" oov_count=\"0\">\n";
+      $prev_kw = $kwentry->[0];
+    }
+    $kwslist .= "    <kw file=\"$kwentry->[1]\" channel=\"$kwentry->[2]\" tbeg=\"$kwentry->[3]\" dur=\"$kwentry->[4]\" score=\"$kwentry->[5]\" decision=\"$kwentry->[6]\"";
+    if (defined($kwentry->[7])) {$kwslist .= " threshold=\"$kwentry->[7]\"";}
+    if (defined($kwentry->[8])) {$kwslist .= " raw_score=\"$kwentry->[8]\"";}
+    $kwslist .= "/>\n";
+  }
+  $kwslist .= "  </detected_kwlist>\n";
+  $kwslist .= "</kwslist>\n";
+
+  return $kwslist;
+}
+
+# Function for sorting
+sub KwslistOutputSort {
+  if ($a->[0] ne $b->[0]) {
+    if ($a->[0] =~ m/[0-9]+$/ && $b->[0] =~ m/[0-9]+$/) {
+      ($a->[0] =~ /([0-9]*)$/)[0] <=> ($b->[0] =~ /([0-9]*)$/)[0]
+    } else {
+      $a->[0] cmp $b->[0];
+    }
+  } elsif ($a->[5] ne $b->[5]) {
+    $b->[5] <=> $a->[5];
+  } else {
+    $a->[1] cmp $b->[1];
+  }
+}
+sub KwslistDupSort {
+  if ($a->[0] ne $b->[0]) {
+    $a->[0] cmp $b->[0];
+  } elsif ($a->[1] ne $b->[1]) {
+    $a->[1] cmp $b->[1];
+  } elsif ($a->[2] ne $b->[2]) {
+    $a->[2] cmp $b->[2];
+  } else {
+    $a->[3] <=> $b->[3];
+  }
+}
+
 # Processing
-my %results = ();
+my @KWS;
 while (<$source>) {
   chomp;
   my @col = split(" ", $_);
   @col == 5 || die "Bad number of columns in raw results\n";
-  my $keyword = shift @col;
+  my $kwid = shift @col;
   my $utter = $col[0];
-  my $start = $col[1]*$flen;
-  my $dur = $col[2]*$flen-$start;
+  my $start = sprintf("%.2f", $col[1]*$flen);
+  my $dur = sprintf("%.2f", $col[2]*$flen-$start);
   my $score = exp(-$col[3]);
 
   if ($segment) {
@@ -131,32 +193,22 @@ while (<$source>) {
     $utter = $utter_mapper{$utter};
   }
 
-  push(@{$results{$keyword}}, [$utter, $start, $dur, $score]);
+  push(@KWS, [$kwid, $utter, 1, $start, $dur, $score, ""]);
 }
 
-my $key;
-my $item;
 my %Ntrue = ();
-foreach $key (keys %results) {
-  foreach $item (@{$results{$key}}) {
-    if (!defined($Ntrue{$key})) {
-      $Ntrue{$key} = 0.0;
-    }
-    $Ntrue{$key} += @{$item}[3];
+foreach my $kwentry (@KWS) {
+  if (!defined($Ntrue{$kwentry->[0]})) {
+    $Ntrue{$kwentry->[0]} = 0.0;
   }
+  $Ntrue{$kwentry->[0]} += $kwentry->[5];
 }
 
 # Scale the Ntrue
-foreach $key (keys %Ntrue) {
-  $Ntrue{$key} = $Ntrue{$key} * $Ntrue_scale;
-}
-
-sub mysort {
-  if ($a =~ m/[0-9]+$/ and $b =~ m/[0-9]+$/) {
-    ($a =~ /([0-9]*)$/)[0] <=> ($b =~ /([0-9]*)$/)[0];
-  } else {
-    $a cmp $b;
-  }
+my %threshold;
+foreach my $key (keys %Ntrue) {
+  $Ntrue{$key} *= $Ntrue_scale;
+  $threshold{$key} = $Ntrue{$key}/($duration/$beta+($beta-1)/$beta*$Ntrue{$key});
 }
 
 my $format_string = "%g";
@@ -164,51 +216,85 @@ if ($digits gt 0 ) {
   $format_string = "%." . $digits ."f";
 }
 
-eval "print $sourceout \'<kwslist kwlist_filename=\"$ecf_filename\" language=\"$language\" system_id=\"$system_id\">\n\'";
-foreach $key (sort mysort (keys %results)) {
-  my $term_search_time = "1";
-  my $oov_term_count = "0";
-  eval "print $sourceout \'<detected_kwlist kwid=\"$key\" search_time=\"$term_search_time\" oov_count=\"$oov_term_count\">\n\'";
-  # Collect results
-  my %list = ();
-  my @list = ();
-  foreach $item (@{$results{$key}}) {
-    my $decision = "NO";
-    my $score = $Ntrue{$key}/($duration/$beta+($beta-1)/$beta*$Ntrue{$key}); 
-    if (@{$item}[3] > $score) {
-      # if (@{$item}[3] > $score && @{$item}[2] > 0.05 && @{$item}[2] < 2) {
-      $decision = "YES";
+my @info = ($kwlist_filename, $language, $system_id);
+foreach my $kwentry (@KWS) {
+  my $threshold = $threshold{$kwentry->[0]};
+  if ($kwentry->[5] > $threshold) {
+    $kwentry->[6] = "YES";
+  } else {
+    $kwentry->[6] = "NO";
+  }
+  if ($verbose > 0) {
+    push(@{$kwentry}, sprintf("%g", $threshold));
+  }
+  if ($normalize eq "true") {
+    if ($verbose > 0) {
+      push(@{$kwentry}, $kwentry->[5]);
     }
-    if ($normalize eq "true") {
-      # $score = (@{$item}[3]-$score+1)/2;             # Normalize here
-      my $numerator = (1-$score)*@{$item}[3];
-      my $denominator = (1-$score)*@{$item}[3]+$score*(1-@{$item}[3]);
-      if ($denominator != 0) {
-        $score = $numerator/$denominator;
+    my $numerator = (1-$threshold)*$kwentry->[5];
+    my $denominator = (1-$threshold)*$kwentry->[5]+(1-$kwentry->[5])*$threshold;
+    if ($denominator != 0) {
+      $kwentry->[5] = sprintf($format_string, $numerator/$denominator);
+    }
+  } else {
+    $kwentry->[5] = sprintf($format_string, $kwentry->[5]);
+  }
+}
+
+# Removing duplicates
+if ($remove_dup eq "true") {
+  my @tmp = sort KwslistDupSort @KWS;
+  @KWS = ();
+  push(@KWS, $tmp[0]);
+  for (my $i = 1; $i < scalar(@tmp); $i ++) {
+    my $prev = $KWS[-1];
+    my $curr = $tmp[$i];
+    if ((abs($prev->[3]-$curr->[3]) < $duptime ) &&
+        ($prev->[2] eq $curr->[2]) &&
+        ($prev->[1] eq $curr->[1]) &&
+        ($prev->[0] eq $curr->[0])) {
+      if (($prev->[6] eq "NO") && ($curr->[6] eq "YES")) {
+        $KWS[-1] = $curr;
       }
     } else {
-      $score = @{$item}[3];
-    }
-    @{$item}[1] = sprintf("%.2f", @{$item}[1]);
-    @{$item}[2] = sprintf("%.2f", @{$item}[2]);
-    
-    $score = sprintf($format_string, $score);
-    my $utter = @{$item}[0];
-    push (@list, "<kw file=\"$utter\" channel=\"1\" tbeg=\"@{$item}[1]\" dur=\"@{$item}[2]\" score=\"$score\" decision=\"$decision\"/>\n");
-    $list{$score} = 1;
-  }
-  # Now sort results by score
-  foreach(sort {$b <=> $a} keys %list) {
-    my $scorekey = $_;
-    foreach(grep(/score=\"$scorekey\"/, @list)) {
-      eval "print $sourceout \'$_\'";
+      push(@KWS, $curr);
     }
   }
-  eval "print $sourceout \'</detected_kwlist>\n\'";
 }
-eval "print $sourceout \'</kwslist>\n\'";
+
+# Output sorting
+my @tmp = sort KwslistOutputSort @KWS;
+
+# Process the YES-cutoff. Note that you don't need this for the normal cases where
+# hits and false alarms are balanced
+if ($YES_cutoff != -1) {
+  my $count = 1;
+  for (my $i = 1; $i < scalar(@tmp); $i ++) { 
+    if ($tmp[$i]->[0] ne $tmp[$i-1]->[0]) {
+      $count = 1;
+      next;
+    }
+    if (($count == $YES_cutoff) && ($tmp[$i]->[6] eq "YES")) {
+      $tmp[$i]->[6] = "NO";
+      $tmp[$i]->[5] = 0.5;
+      next;
+    }
+    if ($tmp[$i]->[6] eq "YES") {
+      $count ++;
+    }
+  }
+}
+
+# Printing
+my $kwslist = PrintKwslist(\@info, \@tmp);
 
 if ($segment) {close(SEG);}
 if ($map_utter) {close(UTT);}
 if ($filein  ne "-") {close(I);}
-if ($fileout ne "-") {close(O);}
+if ($fileout eq "-") {
+    print $kwslist;
+} else {
+  open(O, ">$fileout") || die "Fail to open output file: $fileout\n";
+  print O $kwslist;
+  close(O);
+}
