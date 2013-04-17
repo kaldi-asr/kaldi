952ad008b993e28b5a976087124246f70f623b5f
Karel Vesely <vesis84@gmail.com>
2013-04-17 20:55:24 +0000

    fixin the order of loading libraries
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2368 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/makefiles/linux_cuda.mk
+++ src/makefiles/linux_cuda.mk
@@ -4,5 +4,5 @@ CUDA_FLAGS = -g -Xcompiler -fPIC --verbose --machine 32 -DHAVE_CUDA
 
 CXXFLAGS += -DHAVE_CUDA -I$(CUDATKDIR)/include 
 LDFLAGS += -L$(CUDATKDIR)/lib -Wl,-rpath=$(CUDATKDIR)/lib
-LDFLAGS += -lcublas -lcudart
+LDLIBS += -lcublas -lcudart #LDLIBS : The libs are loaded later than static libs in implicit rule
 
--- src/makefiles/linux_x86_64_cuda.mk
+++ src/makefiles/linux_x86_64_cuda.mk
@@ -4,5 +4,5 @@ CUDA_FLAGS = -g -Xcompiler -fPIC --verbose --machine 64 -DHAVE_CUDA
 
 CXXFLAGS += -DHAVE_CUDA -I$(CUDATKDIR)/include 
 LDFLAGS += -L$(CUDATKDIR)/lib64 -Wl,-rpath,$(CUDATKDIR)/lib64
-LDFLAGS += -lcublas -lcudart
+LDLIBS += -lcublas -lcudart #LDLIBS : The libs are loaded later than static libs in implicit rule
 
