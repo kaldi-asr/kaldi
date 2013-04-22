cbef4b9000d01c011e03f13f68aa10c4826af49d
Jan Trmal <jtrmal@gmail.com>
2013-04-22 00:31:32 +0000

    BABEL: make the BNF test use the same decoding flags
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2379 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/babel/s5/run-5-test-bnf.sh
+++ egs/babel/s5/run-5-test-bnf.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 set -e
 set -o pipefail
-set -x
+set -u
+set -v
 
 cer=0
 type=shadow # "type" may be "dev10h", "dev2h", or "eval", or "shadow" which means eval + dev2h. 
@@ -11,43 +12,43 @@ sopt2="--min-lmwt 15 --max-lmwt 30"
 . conf/common_vars.sh || exit 1;
 . ./lang.conf || exit 1;
 
-. conf/parse_options.sh     
+. utils/parse_options.sh     
 
 if [ $# -ne 0 ]; then
   echo "Usage: $(basename $0) [options]"
 fi
 
+tmpdir=`pwd`
 
-
-[ ! -f data/${type}.uem ] && echo "No such directory data/${type}.uem" && exit 1;
+[ ! -d data/${type}.uem ] && echo "No such directory data/${type}.uem" && exit 1;
 [ ! -d exp/tri5/decode_${type}.uem ] && echo "No such directory exp/tri5/decode_${type}.uem" && exit 1;
 
 # Set decode_nj; typically 64.
 decode_nj=`cat exp/tri5/decode_${type}.uem/num_jobs` || exit 1;
 
 
-if [ ! -f data/${type}_bnf.uem/.done ]; then
-  [ ! -d data/${type}_bnf.uem ] && mkdir -p $tmpdir/data/${type}_bnf.uem && ln -s $tmpdir/data/${type}_bnf.uem data/${type}_bnf.uem
+if [ ! -f data/bnf_$type.uem/.done ]; then
+  [ ! -d data/bnf_$type.uem ] && mkdir -p $tmpdir/data/bnf_$type.uem && ln -s $tmpdir/data/bnf_$type.uem data/bnf_$type.uem
   steps_BNF/make_bnf_feat.sh --stage 0 --nj $decode_nj --cmd "$train_cmd" --transform_dir exp/tri5/decode_${type}.uem/ \
-    data/${type}.uem data/${type}_bnf.uem exp_BNF/bnf_dnn exp/tri5_ali exp_BNF/make_bnf_${type} || exit 1
-  touch data/${type}_bnf.uem/.done
+    data/${type}.uem data/bnf_$type.uem exp_BNF/bnf_dnn exp/tri5_ali exp_BNF/make_bnf_${type} || exit 1
+  touch data/bnf_$type.uem/.done
 fi
 
-if [ ! -f data/${type}_sat.uem/.done ]; then
-  [ ! -d data/${type}_sat.uem ] && mkdir -p $tmpdir/data/${type}_sat.uem && ln -s $tmpdir/data/${type}_sat.uem data/${type}_sat.uem
+if [ ! -f data/sat_$type.uem/.done ]; then
+  [ ! -d data/sat_$type.uem ] && mkdir -p $tmpdir/data/sat_$type.uem && ln -s $tmpdir/data/sat_$type.uem data/sat_$type.uem
   steps/make_fmllr_feats.sh --cmd "$train_cmd -tc 10" --nj $decode_nj --transform-dir exp/tri5/decode_${type}.uem  \
-    data/${type}_sat.uem data/${type}.uem exp/tri5 exp_BNF/make_fmllr_feats_${type}/log plp_processed || exit 1
-  touch data/${type}_sat.uem/.done
+    data/sat_$type.uem data/${type}.uem exp/tri5 exp_BNF/make_fmllr_feats_${type}/log plp_processed || exit 1
+  touch data/sat_$type.uem/.done
 fi
 
-if [ ! -f data/${type}_app.uem/.done ]; then
-  [ ! -d data/${type}_app.uem ] && mkdir -p $tmpdir/data/${type}_app.uem && ln -s $tmpdir/data/${type}_app.uem data/${type}_app.uem
+if [ ! -f data/app_$type.uem/.done ]; then
+  [ ! -d data/app_$type.uem ] && mkdir -p $tmpdir/data/app_$type.uem && ln -s $tmpdir/data/app_$type.uem data/app_$type.uem
   steps/append_feats.sh --cmd "$train_cmd" --nj 4 \
-    data/${type}_bnf.uem data/${type}_sat.uem data/${type}_app.uem exp_BNF/append_feats/log plp_processed/  || exit 1
+    data/bnf_$type.uem data/sat_$type.uem data/app_$type.uem exp_BNF/append_feats/log plp_processed/  || exit 1
   steps/compute_cmvn_stats.sh --fake \
-    data/${type}_app.uem exp/make_plp/${type}_app.uem plp_processed/ || exit 1
-  ln -s `pwd`/data/${type}.uem/kws data/${type}_app.uem/kws
-  touch data/${type}_app.uem/.done
+    data/app_$type.uem exp/make_plp/app_$type.uem plp_processed/ || exit 1
+  ln -s `pwd`/data/${type}.uem/kws data/app_$type.uem/kws
+  touch data/app_$type.uem/.done
 fi
 
 
@@ -56,14 +57,15 @@ decode_lda_mllt() {
   dir=$2
   decode=$3
   if [ ! -f $decode/.done ]; then
-    steps/decode.sh --nj $decode_nj --acwt 0.0333 --scoring-opts "$sopt" \
-      --cmd "$decode_cmd" --skip-scoring true \
+    steps/decode.sh --nj $decode_nj --acwt 0.0333 
+      --scoring-opts "$sopt" --skip-scoring true \
+      --cmd "$decode_cmd" "${decode_extra_opts[@]}"\
       $dir/graph $data $decode |tee $decode/decode.log || exit 1;
     touch $decode/.done
   fi
 }
 
-#decode_lda_mllt data/${type}_app.uem/ exp_BNF/tri5  exp_BNF/tri5/decode_${type}.uem
+#decode_lda_mllt data/app_$type.uem/ exp_BNF/tri5  exp_BNF/tri5/decode_${type}.uem
 
 
 decode_sat() { 
@@ -74,13 +76,13 @@ decode_sat() {
   mkdir -p $decode
   if [ ! -f $decode/.done ]; then
     steps/decode_fmllr_extra.sh --nj $decode_nj --acwt 0.0333  --skip-scoring true\
-      --cmd "queue.pl -l mem_free=2G,ram_free=0.5G" --num-threads 6 --parallel-opts "-pe smp 6" \
+      --cmd "$decode_cmd" "${decode_extra_opts[@]}"\
       $dir/graph $data $decode |tee $decode/decode.log || exit 1;
     touch $decode/.done
   fi
 }
 
-decode_sat data/${type}_app.uem exp_BNF/tri6  exp_BNF/tri6/decode_${type}.uem
+decode_sat data/app_$type.uem exp_BNF/tri6  exp_BNF/tri6/decode_${type}.uem
 
 
 echo ---------------------------------------------------------------------
@@ -96,15 +98,14 @@ decode_sgmm2() {
     steps/decode_sgmm2.sh --use-fmllr true --nj $decode_nj \
       --beam 15 --lat-beam 8 \
       --transform-dir $transforms \
-      --cmd "queue.pl -l mem_free=3G,ram_free=3G" \
-      --num-threads 6 --parallel-opts "-pe smp 6 -l mem_free=3G,ram_free=0.6G" \
       --acwt 0.05 --scoring-opts "$sopt2" --skip-scoring true \
+      --cmd "$decode_cmd" "${decode_extra_opts[@]}"\
       $dir/graph $data $decode | tee $decode/decode.log || exit 1;
     touch $decode/.done;
   fi
 }
 
-decode_sgmm2 data/${type}_app.uem   exp_BNF/sgmm7 exp_BNF/sgmm7/decode_fmllr_${type}.uem   exp_BNF/tri6/decode_${type}.uem
+decode_sgmm2 data/app_$type.uem   exp_BNF/sgmm7 exp_BNF/sgmm7/decode_fmllr_${type}.uem   exp_BNF/tri6/decode_${type}.uem
 
 
 for iter in 1 2 3 4; do
@@ -114,20 +115,20 @@ for iter in 1 2 3 4; do
     
     steps/decode_sgmm2_rescore.sh --scoring-opts "$sopt2 --cer $cer" --skip-scoring true\
         --cmd "$decode_cmd" --iter $iter --transform-dir exp_BNF/tri6/decode_${type}.uem \
-        data/lang data/${type}_app.uem/ exp_BNF/sgmm7/decode_fmllr_${type}.uem $decode || exit 1;
+        data/lang data/app_$type.uem/ exp_BNF/sgmm7/decode_fmllr_${type}.uem $decode || exit 1;
   
     touch $decode/.done 
   fi
 
-  decode=exp_BNF/sgmm7_mmi_b0.1/decode_fmllr_shadow.uem_it$iter
-  if [ ! -f $decode/.done ]; then
-    
-    steps/decode_sgmm2_rescore.sh --scoring-opts "$sopt2 --cer $cer" --skip-scoring true\
-        --cmd "$decode_cmd" --iter $iter --transform-dir exp_BNF/tri6/decode_shadow.uem \
-        data/lang data/shadow_app.uem/ exp_BNF/sgmm7/decode_fmllr_shadow.uem $decode || exit 1;
-  
-    touch $decode/.done 
-  fi
+  #decode=exp_BNF/sgmm7_mmi_b0.1/decode_fmllr_shadow.uem_it$iter
+  #if [ ! -f $decode/.done ]; then
+  #  
+  #  steps/decode_sgmm2_rescore.sh --scoring-opts "$sopt2 --cer $cer" --skip-scoring true\
+  #      --cmd "$decode_cmd" --iter $iter --transform-dir exp_BNF/tri6/decode_shadow.uem \
+  #      data/lang data/shadow_app.uem/ exp_BNF/sgmm7/decode_fmllr_shadow.uem $decode || exit 1;
+  # 
+  #  touch $decode/.done 
+  #fi
 
 done
 
@@ -155,7 +156,7 @@ for iter in 1 2 3 4; do
     if [ $type != "shadow" ]; then
       local/kws_search.sh $sopt2 --cmd "$decode_cmd" \
         --duptime $duptime --max-states 150000 \
-        data/lang data/${type}_app.uem $decode || exit 1
+        data/lang data/app_$type.uem $decode || exit 1
 
     else # shadow data.
       local/split_ctms.sh $sopt2 data/shadow_app.uem $decode data/dev data/test.uem || exit 1
