a9b52fbf39ce97410377ce7ef336c02450cc4fca
Bagher BabaAli <bagher.babaali@gmail.com>
2013-04-30 20:26:31 +0000

    fix to timit/s5/run.sh
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2424 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/timit/s5/run.sh
+++ egs/timit/s5/run.sh
@@ -94,53 +94,6 @@ steps/decode_fmllr.sh --nj 30 --cmd "$decode_cmd" \
  exp/tri3/graph_bg data/test exp/tri3/decode_bg_test || exit 1;
 
 echo ============================================================================
-echo "            fMMI + MMI Training & Decoding on top of tri3                 "
-echo ============================================================================
-
-# Train and test MMI, and boosted MMI, on tri3 (LDA+MLLT+SAT on all the data).
-# From tri3 system, align all train data.
-steps/align_fmllr.sh --nj 30 --cmd "$train_cmd" \
- data/train data/lang exp/tri3 exp/tri3_ali_train || exit 1;
-
-steps/make_denlats.sh --nj 30 --sub-split 30 --cmd "$train_cmd" \
- --transform-dir exp/tri3_ali_train data/train data/lang \
- exp/tri3 exp/tri3_denlats_train || exit 1;
-
-steps/train_mmi.sh --cmd "$train_cmd" --boost 0.1 \
- data/train data/lang exp/tri3_ali_train exp/tri3_denlats_train \
- exp/tri3_mmi_b0.1  || exit 1;
-
-steps/decode.sh --nj 30 --cmd "$decode_cmd" --transform-dir exp/tri3/decode_bg_test \
- exp/tri3/graph_bg data/test exp/tri3_mmi_b0.1/decode_bg_test
-
-
-#first, train UBM for fMMI experiments.
-steps/train_diag_ubm.sh --silence-weight 0.5 --nj 30 --cmd "$train_cmd" \
- 600 data/train data/lang exp/tri3_ali_train exp/dubm3
-
-# Next, fMMI+MMI.
-steps/train_mmi_fmmi.sh  --boost 0.1 --cmd "$train_cmd" \
- data/train data/lang exp/tri3_ali_train exp/dubm3 exp/tri3_denlats_train \
- exp/tri3_fmmi_a || exit 1;
-
-for iter in 1 2 3 4; do
- steps/decode_fmmi.sh --nj 30  --cmd "$decode_cmd" --iter $iter \
-   --transform-dir exp/tri3/decode_bg_test  exp/tri3/graph_bg data/test \
-  exp/tri3_fmmi_a/decode_bg_test_it$iter 
-done
-
-# fMMI + mmi with indirect differential.
-steps/train_mmi_fmmi_indirect.sh --boost 0.1 --cmd "$train_cmd" \
- data/train data/lang exp/tri3_ali_train exp/dubm3 exp/tri3_denlats_train \
- exp/tri3_fmmi_indirect || exit 1;
-
-for iter in 1 2 3 4; do
- steps/decode_fmmi.sh --nj 30  --cmd "$decode_cmd" --iter $iter \
-   --transform-dir exp/tri3/decode_bg_test  exp/tri3/graph_bg data/test \
-  exp/tri3_fmmi_indirect/decode_bg_test_it$iter 
-done
-
-echo ============================================================================
 echo "                        SGMM2 Training & Decoding                         "
 echo ============================================================================
 
@@ -183,25 +136,12 @@ for iter in 1 2 3 4; do
 done
 
 echo ============================================================================
-echo "                  Combining some of the Best Decodings                    "
-echo ============================================================================
-
-local/score_combine.sh data/test data/lang_test_bg \
- exp/tri3_fmmi_a/decode_bg_test_it1 exp/sgmm2_4_mmi_b0.1_z/decode_bg_test_it1 \
- exp/combine_tri3b_fmmi_a_sgmm2_4_mmi_b0.1_z/decode_bg_test_it1_1 || exit 1;
-
-# Checking MBR decode of baseline:
-cp -r -T exp/sgmm2_4_mmi_b0.1_z/decode_bg_test_it1{,.mbr}
-local/score_mbr.sh data/test data/lang_test_bg \
- exp/sgmm2_4_mmi_b0.1_z/decode_bg_test_it1.mbr || exit 1;
-
-echo ============================================================================
 echo "                    Getting Results [see RESULTS file]                    "
 echo ============================================================================
 
 for x in exp/*/decode*; do
   [ -d $x ] && grep WER $x/wer_* | utils/best_wer.sh
-done
+done 
 
 exit 0;
 
