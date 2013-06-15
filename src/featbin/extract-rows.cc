a2b9f7579fa6ff44c9e953d27c6d033d56181879
Korbinian Riedhammer <korbinianr@gmail.com>
2013-06-15 23:50:29 +0000

    corrected extract-rows to match code guidelines
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2515 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/featbin/extract-rows.cc
+++ src/featbin/extract-rows.cc
@@ -25,12 +25,15 @@ int main(int argc, char *argv[]) {
     using namespace std;
 
     const char *usage =
-      "Extract certain rows of feature matrices, e.g. to train classifiers to determine\n"
-      "the quality of a proposed KWS hit.  The program expects a segments file in the\n"
+      "Extract certain row ranges of matrices.  This is most useful to extract segments\n"
+      "segments from feature files, for example to tighten segmentations or to extract features\n"
+      "corresponding to certain alignments.  The program expects a segments file in the\n"
       "form of\n"
       "  segment-name utterance-id start end\n"
-      "where start/end can frame/row numbers or seconds.msec, if invoked with --frame-shift\n"
-      "  e.g. extract-rows segments ark:feats-in.ark ark:feats-out.ark\n";
+      "where the segment-name is chosen by the user, utterance-id indexes to input file,\n"
+      "and start/end denote the row range.  The range can be row numbers or, if invoked\n"
+      "with --frame-shift, a time in seconds.\n"
+      "  e.g. extract-rows --frame-shift=0.01 segments ark:feats-in.ark ark:feats-out.ark\n";
 
     ParseOptions po(usage);
 
@@ -46,13 +49,13 @@ int main(int argc, char *argv[]) {
       exit(1);
     }
 
-    string seg_rspecifier = po.GetArg(1);
-    string ft_rspecifier = po.GetArg(2);
-    string ft_wspecifier = po.GetArg(3);
+    string segment_rspecifier = po.GetArg(1);
+    string feat_rspecifier = po.GetArg(2);
+    string feat_wspecifier = po.GetArg(3);
 
-    Input ki(seg_rspecifier);
-    RandomAccessBaseFloatMatrixReader *rd = new RandomAccessBaseFloatMatrixReader(ft_rspecifier);
-    BaseFloatMatrixWriter writer(ft_wspecifier);
+    Input ki(segment_rspecifier);
+    RandomAccessBaseFloatMatrixReader reader(feat_rspecifier);
+    BaseFloatMatrixWriter writer(feat_wspecifier);
 
     int32 num_lines = 0, num_missing = 0;
 
@@ -82,11 +85,11 @@ int main(int argc, char *argv[]) {
         // ignored if start or end time cannot be converted to real.
         double t1, t2;
         if (!ConvertStringToReal(start_str, &t1)) {
-          KALDI_WARN << "Invalid line in segments file [bad start]: " << line;
+          KALDI_ERR << "Invalid line in segments file [bad start]: " << line;
           continue;
         }
         if (!ConvertStringToReal(end_str, &t2)) {
-          KALDI_WARN << "Invalid line in segments file [bad end]: " << line;
+          KALDI_ERR << "Invalid line in segments file [bad end]: " << line;
           continue;
         }
 
@@ -94,11 +97,11 @@ int main(int argc, char *argv[]) {
         end = (int) (t2 / frame_shift);
       } else {
         if (!ConvertStringToInteger(start_str, &start)) {
-          KALDI_WARN<< "Invalid line in segments file [bad start]: " << line;
+          KALDI_ERR << "Invalid line in segments file [bad start]: " << line;
           continue;
         }
         if (!ConvertStringToInteger(end_str, &end)) {
-          KALDI_WARN << "Invalid line in segments file [bad end]: " << line;
+          KALDI_ERR << "Invalid line in segments file [bad end]: " << line;
           continue;
         }
       }
@@ -108,12 +111,14 @@ int main(int argc, char *argv[]) {
         continue;
       }
 
-      if (rd->HasKey(utt)) {
-        Matrix<BaseFloat> feats = rd->Value(utt);
+      if (reader.HasKey(utt)) {
+        Matrix<BaseFloat> feats = reader.Value(utt);
+
         if (feats.NumRows() < end)
           end = feats.NumRows();
-        Matrix<BaseFloat> toWrite(feats.RowRange(start, (end-start)));
-        writer.Write(segment, toWrite);
+
+        Matrix<BaseFloat> to_write(feats.RowRange(start, (end-start)));
+        writer.Write(segment, to_write);
       } else {
         KALDI_WARN << "Missing requested utterance " << utt;
         num_missing += 1;
@@ -121,8 +126,10 @@ int main(int argc, char *argv[]) {
 
     }
 
-    KALDI_LOG << "processed " << num_lines << ", " << (num_lines - num_missing)
-        << "successfull, " << num_missing << " missing utterances";
+    KALDI_LOG << "processed " << num_lines << " segments, " << (num_lines - num_missing)
+        << " successful, " << num_missing << " had invalid utterances";
+
+    return 0;
   } catch(const std::exception &e) {
     std::cerr << e.what();
     return -1;
