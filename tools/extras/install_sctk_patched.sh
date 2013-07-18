37f9a8367873998cdfeb21712c724a64ff548beb
Ho Yin Chan <ricky.hoyin.chan@gmail.com>
2013-07-18 11:18:32 +0000

    add script for smooth sctk-2.4.0 installation under Cygwin
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2694 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ tools/extras/install_sctk_patched.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# A patch for sctk-2.4.0 smooth installation under Cygwin
+
+os=`uname -a | awk '{printf $NF}'`
+
+if [ "$os" == "Cygwin" ]
+then
+  cp src/rfilter1/makefile.in src/rfilter1/makefile.in.orig
+  sed 's/OPTIONS=-DNEED_STRCMP=1/OPTIONS=/g' src/rfilter1/makefile.in > tmpf
+  mv tmpf src/rfilter1/makefile.in   
+fi
