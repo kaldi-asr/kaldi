25d7864d0955d18e0c397fdc70816ad3b61b7777
Jan Trmal <jtrmal@gmail.com>
2013-07-19 20:40:33 +0000

    BABEL: remove checking the existence char.stm and glm as these files are not used anymore
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2704 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/babel/s5/local/split_ctms.sh
+++ egs/babel/s5/local/split_ctms.sh
@@ -1,4 +1,18 @@
 #!/bin/bash 
+# Copyright 2013  Johns Hopkins University (authors: Yenda Trmal)
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+# WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+# MERCHANTABLITY OR NON-INFRINGEMENT.
+# See the Apache 2 License for the specific language governing permissions and
+# limitations under the License.
 
 # begin configuration section.
 min_lmwt=7
@@ -29,19 +43,15 @@ for i in $@ ; do
     p=$q/`basename $i`
     [ ! -f $i/reco2file_and_channel ] && "The file reco2file_and_channel not present in the $i directory!" && exit 1
     for lmw in $q/score_* ; do
+        test -d $lmw || exit 1; #this is to protect us before creating directory "score_*" in cases no real score_[something] directory exists
         d=$p/`basename $lmw`
         mkdir -p $d
-        #echo " $lmw/$name.char.ctm "
-        if [ $cer -eq 1 ] ; then
-          [ !  -f $lmw/$name.char.ctm ] && echo "File $lmw/$name.char.ctm does not exist!" && exit 1
-          utils/filter_scp.pl <(cut -f 1 -d ' ' $i/reco2file_and_channel) $lmw/$name.char.ctm > $d/`basename $i`.char.ctm
-        fi
 
         [ ! -f $lmw/$name.ctm ] && echo "File $lmw/$name.ctm does not exist!" && exit 1
         utils/filter_scp.pl <(cut -f 1 -d ' ' $i/reco2file_and_channel) $lmw/$name.ctm > $d/`basename $i`.ctm
     done
 
-    if [ -f $i/stm ] && [ -f $i/glm ]; then
+    if [ -f $i/stm ] ; then
         local/score_stm.sh --min-lmwt $min_lmwt --max-lmwt $max_lmwt --cer $cer --cmd "$cmd" $i data/lang $p
     else
         echo "Not running scoring, file $i/stm does not exist"
