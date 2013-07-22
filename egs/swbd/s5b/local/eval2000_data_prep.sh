6ba50382f9476c695002ed14639136f0066cda55
Ho Yin Chan <ricky.hoyin.chan@gmail.com>
2013-07-22 05:36:25 +0000

    fix typo on comment
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2717 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/swbd/s5b/local/eval2000_data_prep.sh
+++ egs/swbd/s5b/local/eval2000_data_prep.sh
@@ -5,7 +5,7 @@
 
 # To be run from one directory above this script.
 
-# The input is two directory names (possibly the same) ontaining the 
+# The input is two directory names (possibly the same) containing the 
 # 2000 Hub5 english evaluation test set and transcripts, which are
 # respectively: LDC2002S09  LDC2002T43
 # e.g. see
