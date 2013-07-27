4320685f6ceba5303bb97e0345c0a0ced7182880
Jan Trmal <jtrmal@gmail.com>
2013-07-27 00:38:44 +0000

    BABEL: the DNN decoding is part of run-5-anydecode.sh and run-4-test.sh
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2732 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/babel/s5/run-2a-nnet.sh
+++ egs/babel/s5/run-2a-nnet.sh
@@ -29,18 +29,23 @@ if [ ! -f exp/tri6_nnet/.done ]; then
   touch exp/tri6_nnet/.done
 fi
 
-if [ ! -f exp/tri6_nnet/decode_dev2h/.done ]; then
-  [ ! -f exp/tri5/graph/.done ] && utils/mkgraph.sh data/lang exp/tri5 exp/tri5/graph && touch exp/tri5/graph/.done
-  steps/decode_nnet_cpu.sh --cmd "$decode_cmd" --nj $decode_nj \
-  "${decode_extra_opts[@]}" --transform-dir exp/tri5/decode_dev2h \
-    exp/tri5/graph data/dev2h exp/tri6_nnet/decode_dev2h 
-  touch exp/tri6_nnet/decode_dev2h/.done
-fi
-if [ ! -f exp/tri6_nnet/decode_dev2h/kws/.done ]; then
-  local/kws_search.sh --cmd "$decode_cmd" --duptime $duptime \
-      data/lang data/dev2h exp/tri6_nnet/decode_dev2h
-  touch exp/tri6_nnet/decode_dev2h/kws/.done 
-fi  
+#The following has been commented as the 5-anydecode.sh script takes care about
+#decoding and keyword search in more complete/systematic fashion
+
+#[ ! -f exp/tri5/graph/.done ] && utils/mkgraph.sh data/lang exp/tri5 exp/tri5/graph && touch exp/tri5/graph/.done
+
+#-if [ ! -f exp/tri6_nnet/decode_dev2h/.done ]; then
+#-  [ ! -f exp/tri5/graph/.done ] && utils/mkgraph.sh data/lang exp/tri5 exp/tri5/graph && touch exp/tri5/graph/.done
+#-  steps/decode_nnet_cpu.sh --cmd "$decode_cmd" --nj $decode_nj \
+#-  "${decode_extra_opts[@]}" --transform-dir exp/tri5/decode_dev2h \
+#-    exp/tri5/graph data/dev2h exp/tri6_nnet/decode_dev2h 
+#-  touch exp/tri6_nnet/decode_dev2h/.done
+#-fi
+#-if [ ! -f exp/tri6_nnet/decode_dev2h/.kws.done ]; then
+#-  local/kws_search.sh --cmd "$decode_cmd" --duptime $duptime \
+#-      data/lang data/dev2h exp/tri6_nnet/decode_dev2h
+#-  touch exp/tri6_nnet/decode_dev2h/.kws.done 
+#-fi  
 
 
 exit 0
