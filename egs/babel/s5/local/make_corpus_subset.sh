22c32b4f2544365be4d1be783ae681410e660458
Jan Trmal <jtrmal@gmail.com>
2013-08-08 18:31:49 +0000

    BABEL: allow for ignoring missing *txt file when explicitly asked This may be correct in some cases, when the *txt transcripts are not a part of the BABEL delivery (as long as the STM file or RTTM file is)
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@2769 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/babel/s5/local/make_corpus_subset.sh
+++ egs/babel/s5/local/make_corpus_subset.sh
@@ -3,6 +3,10 @@
 # Copyright 2012  Johns Hopkins University (Author: Yenda Trmal)
 # Apache 2.0.
 
+#Begin configuration
+ignore_missing_txt=false  #If the reference transcript txt is missing, \
+                          #shall we ignore it or treat it as a fatal error?
+#End configuration
 echo "$0 $@"  # Print the command line for logging
 
 help_message="$0: create subset of the input directory (specified as the first directory).
@@ -51,8 +55,11 @@ for file_basename in `cat $input_data_list`; do
     if [[ -e $abs_src_dir/transcription/$file_basename.txt ]] ; then
         ln -sf $abs_src_dir/transcription/$file_basename.txt $abs_tgt_dir/transcription || exit 1
     else
-        echo "File $abs_src_dir/audio/$file_basename.sph does not exist!"
-        exit 1
+        echo "File $abs_src_dir/audio/$file_basename.txt does not exist!"
+
+        if ! $ignore_missing_txt ; then
+          exit 1;
+        fi
     fi
 done
 
