d8f88fe8645e23462946e1b0cf662f294dc0a83f
Dan Povey <dpovey@gmail.com>
2013-08-10 19:49:57 +0000

    sandbox/dan2: Adding new version of nnet training scripts for RM.
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/sandbox/dan2@2778 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ egs/rm/s5/local/nnet2/run_4a.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+. cmd.sh
+
+
+# This examples runs on top of "raw-fMLLR" features.
+
+(  steps/nnet2/train_tanh.sh --splice-width 7 \
+     --egs-dir exp/tri4a1xa32_nnet/egs \
+     --softmax-learning-rate-factor 0.5 --cleanup false \
+     --initial-learning-rate 0.08 --final-learning-rate 0.008 \
+     --num-hidden-layers 2 \
+     --num-epochs-extra 10 --add-layers-period 1 \
+     --mix-up 4000 \
+     --cmd "$decode_cmd" \
+     --hidden-layer-dim 375 \
+      data/train data/lang exp/tri3c_ali exp/nnet4a  || exit 1
+
+   steps/nnet2/decode.sh --config conf/decode.config --cmd "$decode_cmd" --nj 20 --feat-type raw \
+     --transform-dir exp/tri3c/decode \
+     exp/tri3c/graph data/test exp/nnet4a/decode
+
+   steps/nnet2/decode.sh --config conf/decode.config --cmd "$decode_cmd" --nj 20 --feat-type raw \
+     --transform-dir exp/tri3c/decode_ug \
+     exp/tri3c/graph_ug data/test exp/nnet4a/decode_ug
+ ) 
+
---
+++ egs/rm/s5/local/nnet2/run_4b.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+
+# This trains only unadapted (just cepstral mean normalized) features,
+# and uses various combinations of VTLN warping factor and time-warping
+# factor to artificially expand the amount of data.
+
+. cmd.sh
+
+set -e
+
+if true; then 
+  featdir=`pwd`/mfcc/nnet4b; mkdir -p $featdir
+  mkdir -p conf/nnet4b
+  all_fbankdirs=""
+  all_mfccdirs=""
+  pairs="1.1-1.0 1.05-1.2 1.0-0.8 0.95-1.1 0.9-0.9"
+  for pair in $pairs; do
+    vtln_warp=`echo $pair | cut -d- -f1`
+    time_warp=`echo $pair | cut -d- -f2`
+    fs=`perl -e "print ($time_warp*10);"`
+    fbank_conf=conf/nnet4b/fbank_vtln${vtln_warp}_time${time_warp}.conf
+    ( echo "--num-mel-bins=40"; echo "--frame-shift=$fs"; echo "--vtln-warp=$vtln_warp" ) > $fbank_conf
+    echo "Making filterbank features for $pair"
+    fbank_data=data/nnet4b/train_fbank_vtln${vtln_warp}_time${time_warp}
+    all_fbankdirs="$all_fbankdirs $fbank_data"
+    utils/copy_data_dir.sh --spk-prefix ${pair}- --utt-prefix ${pair}- data/train $fbank_data
+    steps/make_fbank.sh --fbank-config $fbank_conf --nj 8 --cmd "run.pl" $fbank_data exp/nnet4b/make_mfcc/mfcc_$pair $featdir
+    steps/compute_cmvn_stats.sh $fbank_data exp/nnet4b/fbank_$pair $featdir
+
+    echo "Making MFCC features for $pair"      
+    mfcc_data=data/nnet4b/train_mfcc_vtln${vtln_warp}_time${time_warp}
+    mfcc_conf=conf/nnet4b/mfcc_vtln${vtln_warp}_time${time_warp}.conf
+    ( echo "--use-energy=false"; echo "--frame-shift=$fs" ; echo "--vtln-warp=$vtln_warp" ) > $mfcc_conf
+    utils/copy_data_dir.sh --spk-prefix ${pair}- --utt-prefix ${pair}- data/train $mfcc_data
+    steps/make_mfcc.sh --mfcc-config $mfcc_conf --nj 8 --cmd "run.pl" $mfcc_data exp/nnet4b/make_mfcc/mfcc_$pair $featdir
+    steps/compute_cmvn_stats.sh $mfcc_data exp/nnet4b/mfcc_$pair $featdir
+    all_mfccdirs="$all_mfccdirs $mfcc_data"
+  done
+
+  utils/combine_data.sh data/nnet4b/train_fbank_all $all_fbankdirs
+  utils/combine_data.sh data/nnet4b/train_mfcc_all $all_mfccdirs
+
+  steps/align_fmllr.sh --nj 8 --cmd "$train_cmd" \
+    data/nnet4b/train_mfcc_all data/lang exp/tri3b exp/tri3b_ali_nnet4b
+fi
+
+
+(  steps/nnet2/train_block.sh \
+     --bias-stddev 0.5 --splice-width 7 --egs-opts "--feat-type raw" \
+     --softmax-learning-rate-factor 0.5 --cleanup false \
+     --initial-learning-rate 0.04 --final-learning-rate 0.004 \
+     --num-epochs-extra 10 --add-layers-period 1 \
+     --mix-up 4000 \
+     --cmd "$decode_cmd" \
+     --hidden-layer-dim 450 \
+      data/nnet4b/train_fbank_all data/lang exp/tri3b_ali_nnet4b exp/nnet4b  || exit 1
+
+   steps/nnet2/decode.sh --config conf/decode.config --cmd "$decode_cmd" --nj 20 --feat-type raw \
+     exp/tri3b/graph data/test_fbank exp/nnet4b/decode
+
+   steps/nnet2/decode.sh --config conf/decode.config --cmd "$decode_cmd" --nj 20 --feat-type raw \
+     exp/tri3b/graph_ug data/test_fbank exp/nnet4b/decode_ug
+ ) 
