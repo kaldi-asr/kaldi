75c33ecdb8ebdcf01df6928c790821c00af74911
Yanmin Qian <yanminqian@users.sf.net>
2013-09-05 15:38:16 +0000

    sandbox/dan2:add cu-vector-speed-test.cc
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/sandbox/dan2@2918 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ src/cudamatrix/cu-vector-speed-test.cc
@@ -0,0 +1,95 @@
+// cudamatrix/cu-vector-speed-test.cc
+
+// Copyright 2013  Johns Hopkins University (author: Daniel Povey)
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+#include "base/kaldi-common.h"
+#include "util/common-utils.h"
+#include "cudamatrix/cu-matrix.h"
+#include "cudamatrix/cu-vector.h"
+#include "cudamatrix/cu-math.h"
+
+using namespace kaldi;
+
+
+namespace kaldi {
+
+template<class Real>
+std::string NameOf() {
+  return (sizeof(Real) == 8 ? "<double>" : "<float>");
+}
+
+template<class Real> void TestCuVectorSoftmax(int32 dim) {
+  BaseFloat time_in_secs = 0.05;
+  CuVector<Real> M(dim);
+  M.SetRandn();
+
+  Timer tim;
+  int32 iter = 0;
+  for (;tim.Elapsed() < time_in_secs; iter++) {
+    M.ApplySoftMax();
+  }
+
+  BaseFloat fdim = dim;
+  BaseFloat gflops = (fdim * fdim * iter) / (tim.Elapsed() * 1.0e+09);
+  KALDI_LOG << "For CuVector::Softmax" << NameOf<Real>() << ", for dim = "
+            << dim << ", speed was " << gflops << " gigaflops.";
+}
+
+
+
+
+
+
+template<class Real> void CudaVectorSpeedTest() {
+  std::vector<int32> sizes;
+  sizes.push_back(16);
+  sizes.push_back(128);
+  sizes.push_back(256);
+  sizes.push_back(1024);
+  int32 ns = sizes.size();
+  for (int32 s = 0; s < ns; s++)
+	  TestCuVectorSoftmax<Real>(sizes[s]);
+
+}
+
+
+} // namespace kaldi
+
+
+int main() {
+    //Select the GPU
+#if HAVE_CUDA == 1
+    CuDevice::Instantiate().SelectGpuId(-2); //-2 .. automatic selection
+#endif
+
+    kaldi::CudaVectorSpeedTest<float>();
+#if HAVE_CUDA == 1
+  if (CuDevice::Instantiate().DoublePrecisionSupported()) {
+    kaldi::CudaVectorSpeedTest<double>();
+  } else {
+    KALDI_WARN << "Double precision not supported";
+  }
+#else
+  kaldi::CudaMatrixSpeedTest<double>();
+#endif
+  std::cout << "Tests succeeded.\n";
+}
+
