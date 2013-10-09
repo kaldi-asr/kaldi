57efd31d185e596492c399c2110fb868abcc4c98
Dan Povey <dpovey@gmail.com>
2013-10-09 20:08:31 +0000

    sandbox/dan2: changes to Makefiles so that CUDA libraries are not needed for those programs that don't use CUDA.
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/sandbox/dan2@3065 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/makefiles/linux_x86_64_cuda.mk
+++ src/makefiles/linux_x86_64_cuda.mk
@@ -3,6 +3,6 @@ CUDA_INCLUDE= -I$(CUDATKDIR)/include
 CUDA_FLAGS = -g -Xcompiler -fPIC --verbose --machine 64 -DHAVE_CUDA
 
 CXXFLAGS += -DHAVE_CUDA -I$(CUDATKDIR)/include 
-LDFLAGS += -L$(CUDATKDIR)/lib64 -Wl,-rpath,$(CUDATKDIR)/lib64
-LDLIBS += -lcublas -lcudart #LDLIBS : The libs are loaded later than static libs in implicit rule
+CUDA_LDFLAGS += -L$(CUDATKDIR)/lib64 -Wl,-rpath,$(CUDATKDIR)/lib64
+CUDA_LDLIBS += -lcublas -lcudart #LDLIBS : The libs are loaded later than static libs in implicit rule
 
