726f51cce0384b0b938948c8bf781040f693abcb
Jan Trmal <jtrmal@gmail.com>
2013-11-06 16:23:46 +0000

    (sandbox/xsede_kaldi) New version of the lonestar.py, performs much more checks if everything is ok
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/sandbox/tacc_kaldi@3135 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/babel/s5/local/lonestar.py
+++ egs/babel/s5/local/lonestar.py
@@ -15,6 +15,12 @@ def make_path(path):
 		elif not os.path.isdir(path):
 			raise
 
+def tail(n, filename):
+    import subprocess
+    p=subprocess.Popen(['tail','-n',str(n),filename], stdout=subprocess.PIPE)
+    soutput,sinput=p.communicate()
+    soutput=soutput.split("\n")
+    return soutput
 
 def KaldiLauncher(lo, **kwargs):
 	jobid = JobId()
@@ -23,6 +29,7 @@ def KaldiLauncher(lo, **kwargs):
 	cores = lo.nof_threads;
 
 	ce=SSHExecutor(workdir=qdir, debug=debug, force_workdir=True, catch_output=True)
+	ce.outstring="out."
 	ce.execstring=lo.taskname + "."
 
 	hostpool=HostPool(hostlist=HostListByName(), commandexecutor=ce )
@@ -30,15 +37,15 @@ def KaldiLauncher(lo, **kwargs):
 	completion=lambda x:FileCompletion( taskid=x, stamproot="done.", stampdir=qdir)	
 
 	logfiles = list()
-	tasks = list()
+	commands = list()
 	for q in xrange(lo.jobstart, lo.jobend+1):
 		s = "bash " + lo.queue_scriptfile + " " + str(q) 
-		tasks.append(s)
+		commands.append(s)
 
 		logfile = lo.logfile.replace("${PY_LAUNCHER_ID}", str(q))
 		logfiles.append(logfile)
 
-	generator=ListCommandlineGenerator(list=tasks, cores=cores)
+	generator=ListCommandlineGenerator(list=commands, cores=cores)
 	tasks = TaskGenerator(generator, completion=completion, debug=debug )
 
  	job = LauncherJob( hostpool=hostpool, 
@@ -46,6 +53,73 @@ def KaldiLauncher(lo, **kwargs):
         debug=debug,**kwargs)
 
 	job.run()
+	#At this point all the .done files should exist and everything should be finalized.
+	num_failed=0;
+	for logfile in logfiles:
+		if not os.path.isfile(logfile):
+			sys.stderr.write("The file " + logfile + "does not exists.\n");
+			sys.exit(-1);
+	
+		lines=tail(10, logfile)
+		with_status=filter(lambda x:re.search(r'with status (\d+)', x), lines)
+		
+		if len(with_status) == 0:
+			sys.stderr.write("The last line(s) of the log-file " + logfile + " does not seem"
+					" to indicate return status as expected\n");
+			num_failed+=1;
+		elif len(with_status) > 1:
+			sys.stderr.write("The last line(s) of the log-file " + logfile + " does seem"
+					" to indicate multiple return statuses \n");
+			num_failed+=1;
+		else: 
+			status_re=re.search(r'with status (\d+)', with_status[0]);
+			status=status_re.group(1);
+			if status != '0':
+				num_failed += 1;
+	if num_failed != 0:
+		sys.stderr.write(sys.argv[0] + ": " + str(num_failed) + "/" + str(len(logfiles)) +  " failed \n");
+		sys.stderr.write(sys.argv[0] + ": See  " + lo.logfile.replace("${PY_LAUNCHER_ID}", "*" ) + " for details\n");
+		sys.exit(-1);
+
+	#Remove service files. Be careful not to remove something that might be needed in problem diagnostics	
+	for i in xrange(len(commands)):
+		out_file=os.path.join(qdir, ce.outstring+str(i))
+		if not os.path.isfile(out_file):
+			sys.stderr.write("ERROR: " + "The following file is missing:\n")
+			sys.stderr.write("ERROR: " + "\t" + out_file + "\n")
+			sys.stderr.write("ERROR: " + "That means something went wrong, but we don't know what. Try to figure out what and fix it\n");
+			sys.exit(-1);
+		elif os.stat(out_file).st_size != 0:
+			sys.stderr.write("ERROR: " + "The following file has non-zero size:\n")
+			sys.stderr.write("ERROR: " + "\t" + out_file + "\n")
+			sys.stderr.write("ERROR: " + "That means something went wrong, but we don't know what. Try to figure out what and fix it\n");
+			sys.exit(-1);
+		else:
+			exec_file=os.path.join(qdir, ce.execstring+str(i))
+			done_file=os.path.join(qdir, "done."+str(i))
+			if (not os.path.isfile(exec_file) ) or (not os.path.isfile(done_file)):
+				sys.stderr.write("ERROR: " + "One of the following files is missing:\n")
+				sys.stderr.write("ERROR: " + "\t" + exec_file + "\n")
+				sys.stderr.write("ERROR: " + "\t" + done_file + "\n")
+				sys.stderr.write("ERROR: " + "\t" + out_file + "\n")
+				sys.stderr.write("ERROR: " + "That means something went wrong, but we don't know what. Try to figure out what and fix it\n");
+				sys.exit(-1);
+			elif os.stat(done_file).st_size != 0:
+				sys.stderr.write("ERROR: " + "The following file has non-zero size:\n")
+				sys.stderr.write("ERROR: " + "\t" + done_file + "\n")
+				sys.stderr.write("ERROR: " + "That means something went wrong, but we don't know what. Try to figure out what and fix it\n");
+				sys.exit(-1);
+			else:
+				os.remove(exec_file)
+				os.remove(done_file)
+				os.remove(out_file)
+	try:
+		os.rmdir(qdir) 
+	except OSError:
+		sys.stderr.write("ERROR: " + "Failed to remove the pylauncher task dir " + qdir + "\n");
+		sys.stderr.write("ERROR: " + "Find out what is wrong and fix it\n")
+		sys.exit(-1);
+	
 	#print job.final_report()
 
 class LauncherOpts:
@@ -138,7 +212,7 @@ def setup_paths_and_vars(opts):
 
 	if opts.varname and (opts.varname not in opts.logfile ) and (opts.jobstart != opts.jobend):
 		print >>sys.stderr, "lonestar.py: you are trying to run a parallel job" \
-			"but you are putting the output into just ne log file (" + opts.logfile + ")";
+			"but you are putting the output into just one log file (" + opts.logfile + ")";
 		sys.exit(1)
 
 	if not os.path.isabs(opts.logfile):
@@ -213,7 +287,7 @@ def create_scriptfile(scriptname, opts):
 	f.write("( " + cmd + ") 2>>" + logfile + " >>" + logfile + " \n")
 	f.write("ret=$?\n")
 	f.write("time2=`date +\"%s\"`\n")
-	f.write("echo '#' Accounting $(($time2 - $time1)) threads=" + str(nof_threads) + " >> " + logfile + "\n")
+	f.write("echo '#' Accounting time=$(($time2 - $time1)) threads=" + str(nof_threads) + " >> " + logfile + "\n")
 
 	f.write("echo '#' Finished at `date` with status $ret >>" + logfile + "\n")
 	f.write("exit $ret \n")
