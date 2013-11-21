c432b8dda8ba570c454db0751e129e5f23e73ff9
Pegah Ghahremani <pegahgh@gmail.com>
2013-11-21 19:47:08 +0000

    sandbox/pitch: script fix for pitch extraction
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/sandbox/pitch@3189 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/babel/s5/local/make_pitch_kaldi.sh
+++ egs/babel/s5/local/make_pitch_kaldi.sh
@@ -93,10 +93,10 @@ else
   utils/split_scp.pl $scp $split_scps || exit 1;
  
   $cmd JOB=1:$nj $logdir/make_pitch.JOB.log \
-    process-kaldi-pitch-feats  --verbose=2 --config=$pitch_config scp:$logdir/wav.JOB.scp \
-      ark,scp:$pitchdir/pitch_$name.JOB.ark,$pitchdir/pitch_$name.JOB.scp \
+    compute-kaldi-pitch-feats --verbose=2 --config=$pitch_config ark:- ark:- \| \
+    process-kaldi-pitch-feats $postprocess_config_opt ark:- \
+      ark,scp:$pitchdir/raw_pitch_$name.JOB.ark,$pitchdir/raw_pitch_$name.JOB.scp \
       || exit 1;
-
 fi
 
 
