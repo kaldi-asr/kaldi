8f8a788f0753f097ca3bb7999a4a51e785f981e3
Dan Povey <dpovey@gmail.com>
2013-12-14 05:41:20 +0000

    trunk: changes to number of parameters in pnorm example script for WSJ; adding RESULTS.
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@3310 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/wsj/s5/local/nnet2/run_5d_gpu.sh
+++ egs/wsj/s5/local/nnet2/run_5d_gpu.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # This is pnorm neural net training on top of adapted 40-dimensional features.
-# 
 
 
 train_stage=-100
@@ -22,29 +21,30 @@ dir=exp/nnet5d_gpu
     ln -s $temp_dir/$dir/egs $dir/
   fi
 
-  steps/nnet2/train_pnorm.sh \
+  steps/nnet2/train_pnorm.sh --stage $train_stage \
    --num-jobs-nnet 4 --num-threads 1 --parallel-opts "$parallel_opts" \
+   --egs-dir exp/nnet5d_gpu/egs \
    --mix-up 8000 \
    --initial-learning-rate 0.02 --final-learning-rate 0.002 \
    --num-hidden-layers 4 \
-   --pnorm-input-dim 5000 --pnorm-output-dim 1000\
+   --pnorm-input-dim 2000 --pnorm-output-dim 400 \
    --cmd "$decode_cmd" \
    --p 2 \
     data/train_si284 data/lang exp/tri4b_ali_si284 $dir || exit 1
-  steps/nnet2/decode.sh --cmd "$decode_cmd" --nj 10 \
+
+  steps/decode_nnet_cpu.sh --cmd "$decode_cmd" --nj 10 \
     --transform-dir exp/tri4b/decode_tgpr_dev93 \
      exp/tri4b/graph_tgpr data/test_dev93 $dir/decode_tgpr_dev93
 
-  steps/nnet2/decode.sh --cmd "$decode_cmd" --nj 8 \
+  steps/decode_nnet_cpu.sh --cmd "$decode_cmd" --nj 8 \
     --transform-dir exp/tri4b/decode_tgpr_eval92 \
      exp/tri4b/graph_tgpr data/test_eval92 $dir/decode_tgpr_eval92
 
-  steps/nnet2/decode.sh --cmd "$decode_cmd" --nj 10 \
+  steps/decode_nnet_cpu.sh --cmd "$decode_cmd" --nj 10 \
     --transform-dir exp/tri4b/decode_bd_tgpr_dev93 \
      exp/tri4b/graph_bd_tgpr data/test_dev93 $dir/decode_bd_tgpr_dev93
 
-  steps/nnet2/decode.sh --cmd "$decode_cmd" --nj 8 \
+  steps/decode_nnet_cpu.sh --cmd "$decode_cmd" --nj 8 \
     --transform-dir exp/tri4b/decode_bd_tgpr_eval92 \
      exp/tri4b/graph_bd_tgpr data/test_eval92 $dir/decode_bd_tgpr_eval92
 )
-
