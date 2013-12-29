92aa431e053636d9d425d42982d07c9f0d617604
Nickolay V. Shmyrev <nshmyrev@gmail.com>
2013-12-29 13:46:19 +0000

    trunk: Updated yesno to s5 since s3 is removed from wsj
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@3373 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/yesno/s5/local/prepare_data.sh
+++ egs/yesno/s5/local/prepare_data.sh
@@ -6,13 +6,15 @@ scripts=`pwd`/scripts
 
 export PATH=$PATH:`pwd`/../../../tools/irstlm/bin
 
+echo "Preparing train and test data"
+
 train_base_name=train_yesno
 test_base_name=test_yesno
 waves_dir=$1
 
 cd data/local
 
-ls -1 $waves_dir > waves_all.list
+ls -1 ../../$waves_dir > waves_all.list
 ../../local/create_yesno_waves_test_train.pl waves_all.list waves.test waves.train
 
 ../../local/create_yesno_wav_scp.pl ${waves_dir} waves.test > ${test_base_name}_wav.scp
@@ -27,4 +29,12 @@ cp ../../input/task.arpabo lm_tg.arpa
 
 cd ../..
 
-echo "Data preparation succeeded"
+# This stage was copied from WSJ example
+for x in train_yesno test_yesno; do 
+  mkdir -p data/$x
+  cp data/local/${x}_wav.scp data/$x/wav.scp
+  cp data/local/$x.txt data/$x/text
+  cat data/$x/text | awk '{printf("%s global\n", $1);}' > data/$x/utt2spk
+  utils/utt2spk_to_spk2utt.pl <data/$x/utt2spk >data/$x/spk2utt
+done
+
---
+++ egs/yesno/s5/local/prepare_lm.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+. path.sh
+ 
+echo Preparing language models for test
+
+for lm_suffix in tg; do
+  test=data/lang_test_${lm_suffix}
+
+  rm -rf data/lang_test_${lm_suffix}
+  cp -r data/lang data/lang_test_${lm_suffix}
+
+  cat input/task.arpabo | arpa2fst - | fstprint | utils/eps2disambig.pl | utils/s2eps.pl | fstcompile --isymbols=$test/words.txt --osymbols=$test/words.txt --keep_isymbols=false --keep_osymbols=false | fstrmepsilon > $test/G.fst
+  #cat input/G.txt | utils/eps2disambig.pl | utils/s2eps.pl | fstcompile --isymbols=$test/words.txt --osymbols=$test/words.txt --keep_isymbols=false --keep_osymbols=false | fstrmepsilon > $test/G.fst
+  fstisstochastic $test/G.fst
+      
+ # The output is like:
+ # 9.14233e-05 -0.259833
+ # we do expect the first of these 2 numbers to be close to zero (the second is
+ # nonzero because the backoff weights make the states sum to >1).
+ # Because of the <s> fiasco for these particular LMs, the first number is not
+ # as close to zero as it could be.
+
+  # Everything below is only for diagnostic.
+  # Checking that G has no cycles with empty words on them (e.g. <s>, </s>);
+  # this might cause determinization failure of CLG.
+  # #0 is treated as an empty word.
+  mkdir -p tmpdir.g
+  awk '{if(NF==1){ printf("0 0 %s %s\n", $1,$1); }} END{print "0 0 #0 #0"; print "0";}' \
+    < data/local/dict/lexicon.txt  >tmpdir.g/select_empty.fst.txt
+  fstcompile --isymbols=$test/words.txt --osymbols=$test/words.txt tmpdir.g/select_empty.fst.txt | \
+   fstarcsort --sort_type=olabel | fstcompose - $test/G.fst > tmpdir.g/empty_words.fst
+  fstinfo tmpdir.g/empty_words.fst | grep cyclic | grep -w 'y' && 
+    echo "Language model has cycles with empty words" && exit 1
+  rm -r tmpdir.g
+done
+
+echo "Succeeded in formatting data."
