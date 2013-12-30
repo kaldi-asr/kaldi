5a9ff80f0c1d0c6c8f4eb2f4bb801e21e4071ef4
Dan Povey <dpovey@gmail.com>
2013-12-30 07:14:26 +0000

    trunk: Removing outdated option from outdated nnet2 training script.
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@3375 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/wsj/s5/steps/train_nnet_cpu.sh
+++ egs/wsj/s5/steps/train_nnet_cpu.sh
@@ -65,11 +65,6 @@ mix_up=0 # Number of components to mix up to (should be > #tree leaves, if
         # specified.)
 num_threads=16 # Number of threads to run in parallel; you need to
                # keep this in sync with parallel_opts.
-momentum_minibatches=0 # Note: if you set this to e.g. 100 it uses momentum (we
-    # formulate it slightly differently, as a time constant, e.g.  mu = 1 - 1/momentum_minibatches.
-    # This does not seem to be that useful in stabilizing the update-- possibly an interaction
-    # with the asychronous SGD.  Use an option like --nnet-config-opts "--max-change 50"
-    # which is more helpful.
 
 random_copy=false
 cleanup=true
@@ -446,7 +441,7 @@ while [ $x -lt $num_iters ]; do
        nnet-shuffle-egs --buffer-size=$shuffle_buffer_size --srand=$x \
          ark:$dir/egs/egs.JOB.$[$x%$iters_per_epoch].ark ark:- \| \
        nnet-train-parallel --num-threads=$num_threads --minibatch-size=$minibatch_size \
-        --momentum-minibatches=$momentum_minibatches --srand=$x "$mdl" ark:- $dir/$[$x+1].JOB.mdl \
+         --srand=$x "$mdl" ark:- $dir/$[$x+1].JOB.mdl \
        || exit 1;
 
     nnets_list=
