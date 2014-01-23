2ce46f180aeb32e09cbe1d5d214342980620fa66
Karel Vesely <vesis84@gmail.com>
2014-01-23 18:13:05 +0000

    trunk : un-doing last change to RM script, which was accidental
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@3432 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/rm/s5/cmd.sh
+++ egs/rm/s5/cmd.sh
@@ -5,12 +5,8 @@
 # (make sure your --num-jobs options are no more than
 # the number of cpus on your machine.
 
-decode_cmd="queue.pl -q all.q@blade* -l mem_free=2G,ram_free=2G"
-train_cmd="queue.pl -q all.q@blade* -l mem_free=2G,ram_free=2G"
-cuda_cmd="queue.pl -q long.q@dellgpu*,long.q@pcspeech-gpu -l gpu=1"
-
-#train_cmd="queue.pl -l arch=*64"
-#decode_cmd="queue.pl -l arch=*64"
+train_cmd="queue.pl -l arch=*64"
+decode_cmd="queue.pl -l arch=*64"
 #train_cmd="run.pl"
 # Do training locally.  Note: for jobs on smallish subsets,
 # it's way faster to run on a single machine with a handful of CPUs, as
