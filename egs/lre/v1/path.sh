d382fc9ba5dd800164bebfea06a0907d8261d7cd
David Snyder <david.ryan.snyder@gmail.com>
2014-02-18 01:05:05 +0000

    sandbox/language_id: Creating example directory in /egs/lre/v1 with common and shared (linked with wsj) scripts. Also adding local/make-sre08-data.sh which creates the training data for this example.
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/sandbox/language_id@3533 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ egs/lre/v1/path.sh
@@ -0,0 +1,3 @@
+export KALDI_ROOT=$(cd ../../..; pwd)
+export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$KALDI_ROOT/src/nnetbin:$KALDI_ROOT/src/nnet2bin:$KALDI_ROOT/src/ivectorbin:$KALDI_ROOT/tools/sph2pipe_v2.5:$PWD:$PATH
+export LC_ALL=C
