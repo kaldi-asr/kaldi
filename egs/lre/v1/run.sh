4ed9fc8c17c0fe0d67606eaeeb6f785218cda52d
David Snyder <david.ryan.snyder@gmail.com>
2014-02-23 17:39:58 +0000

    sandbox/language_id: In run.sh now combines and uses all sre08 training data
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/sandbox/language_id@3573 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/lre/v1/run.sh
+++ egs/lre/v1/run.sh
@@ -12,24 +12,26 @@ mfccdir=`pwd`/mfcc
 vaddir=`pwd`/mfcc
 
 local/make_sre_2008_train.pl local/language_abbreviations.txt /export/corpora5/LDC/LDC2011S05 data
-utils/combine_data.sh data/sre08_train_short2 data/sre08_train_short2_male data/sre08_train_short2_female
+utils/combine_data.sh data/sre08_train data/sre08_train_10sec_female data/sre08_train_10sec_male \
+    data/sre08_train_3conv_female data/sre08_train_3conv_male data/sre08_train_8conv_female \
+    data/sre08_train_8conv_male data/sre08_train_short2_male data/sre08_train_short2_female
 
 mfccdir=`pwd`/mfcc
 vaddir=`pwd`/mfcc
 
 set -e
-steps/make_mfcc.sh --mfcc-config conf/mfcc.conf --nj 40 --cmd "$train_cmd" data/sre08_train_short2 exp/make_mfcc $mfccdir
+steps/make_mfcc.sh --mfcc-config conf/mfcc.conf --nj 40 --cmd "$train_cmd" data/sre08_train exp/make_mfcc $mfccdir
 
-lid/compute_vad_decision.sh --nj 4 --cmd "$train_cmd" data/sre08_train_short2 exp/make_vad $vaddir
+lid/compute_vad_decision.sh --nj 4 --cmd "$train_cmd" data/sre08_train exp/make_vad $vaddir
 
 
-lid/train_diag_ubm.sh --nj 30 --cmd "$train_cmd" data/sre08_train_short2 2048 exp/diag_ubm_2048
-lid/train_full_ubm.sh --nj 30 --cmd "$train_cmd" data/sre08_train_short2 exp/diag_ubm_2048 exp/full_ubm_2048
+lid/train_diag_ubm.sh --nj 30 --cmd "$train_cmd" data/sre08_train 2048 exp/diag_ubm_2048
+lid/train_full_ubm.sh --nj 30 --cmd "$train_cmd" data/sre08_train exp/diag_ubm_2048 exp/full_ubm_2048
 
 
 lid/train_ivector_extractor.sh --cmd "$train_cmd -l mem_free=2G,ram_free=2G" \
-  --num-iters 5 exp/full_ubm_2048/final.ubm data/sre08_train_short2 \
+  --num-iters 5 exp/full_ubm_2048/final.ubm data/sre08_train \
   exp/extractor_2048
 
 lid/extract_ivectors.sh --cmd "$train_cmd -l mem_free=3G,ram_free=3G" --nj 50 \
-   exp/extractor_2048 data/sre08_train_short2 exp/ivectors_sre08_train_short2
+   exp/extractor_2048 data/sre08_train exp/ivectors_sre08_train
