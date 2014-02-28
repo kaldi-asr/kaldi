bb9a9e5a6a190e808059ee8ade01822882933aa7
David Snyder <david.ryan.snyder@gmail.com>
2014-02-28 00:17:27 +0000

    sandbox/language_id: Adding LogisticRegression class which uses L-BFGS internally. Logistic regression is the standard classifier for language-id.
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/sandbox/language_id@3618 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ src/ivector/logistic-regression.cc
@@ -0,0 +1,163 @@
+// ivector/logistic-regression.cc
+
+// Copyright 2014  David Snyder
+
+// See ../../COPYING for clarification regarding multiple authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include "ivector/logistic-regression.h"
+
+namespace kaldi {
+
+void LogisticRegression::Train(const Matrix<double> &xs, 
+                               const std::vector<int32> &ys,
+                               Matrix<double> *weights, int32 max_steps) {
+
+  int32 xs_num_rows = xs.NumRows(), xs_num_cols = xs.NumCols(),
+                     w_num_rows = weights->NumRows(), 
+                     w_num_cols = weights->NumCols(),
+                     num_ys = ys.size();
+  KALDI_ASSERT(xs_num_cols == w_num_cols);
+  KALDI_ASSERT(xs_num_rows == num_ys);
+  
+  // Adding on extra column for each x to handle the prior.
+  Matrix<double> xs_with_prior(xs_num_rows, xs_num_cols + 1);
+  SubMatrix<double> sub_xs(xs_with_prior, 0, xs_num_rows, 0, xs_num_cols);
+  sub_xs.CopyFromMat(xs);
+  Matrix<double> xw(xs_num_rows, w_num_rows);
+  weights_.Resize(w_num_rows, w_num_cols + 1);
+
+  // Adding on extra column for each x to handle the prior.
+  for (int32 i = 0; i < xs_num_rows; i++) {
+    xs_with_prior(i, xs_num_cols) = 1.0;
+  }
+
+  LbfgsOptions lbfgs_opts;
+  lbfgs_opts.minimize = false;
+
+  weights_.SetZero();
+
+  // Get initial w vector
+  Vector<double> init_w(weights_.NumRows() * weights_.NumCols());
+  init_w.CopyRowsFromMat(weights_);
+  OptimizeLbfgs<double> lbfgs(init_w, lbfgs_opts);
+
+  for (int32 step = 0; step < max_steps; step++) {
+    DoStep(xs_with_prior, &xw, ys, &lbfgs);
+  }
+
+  Vector<double> best_w(lbfgs.GetValue());
+  weights_.CopyRowsFromVec(best_w);
+  SubMatrix<double> trained_weights(weights_, 0, w_num_rows, 
+                                    0, xs_num_cols);
+  weights->SetZero();
+  weights->CopyFromMat(trained_weights);
+}
+
+void LogisticRegression::GetPosteriors(const Matrix<double> &xs,
+                   Matrix<double> *posteriors) {
+  int32 xs_num_rows = xs.NumRows(),
+        xs_num_cols = xs.NumCols();
+  
+  KALDI_ASSERT(xs_num_rows == posteriors->NumRows());
+
+  Matrix<double> xs_with_prior(xs_num_rows, xs_num_cols + 1);
+  SubMatrix<double> sub_xs(xs_with_prior, 0, xs_num_rows, 0, xs_num_cols);
+  sub_xs.CopyFromMat(xs);
+  // Adding on extra column for each x to handle the prior.
+  for (int32 i = 0; i < xs_num_rows; i++) {
+    xs_with_prior(i, xs_num_cols) = 1.0;
+  }
+  posteriors->AddMatMat(1.0, xs_with_prior, kNoTrans, weights_, 
+                        kTrans, 0.0);
+  for (int32 i = 0; i < posteriors->NumRows(); i++) {
+    posteriors->Row(i).ApplySoftMax();
+    posteriors->Row(i).ApplyLog();
+  }
+}
+
+double LogisticRegression::DoStep(const Matrix<double> &xs,
+    Matrix<double> *xw,
+    const std::vector<int32> &ys, OptimizeLbfgs<double> *lbfgs) {
+  Matrix<double> gradient(weights_.NumRows(), weights_.NumCols());
+  // Vector form of the above matrix
+  Vector<double> grad_vec(weights_.NumRows() * weights_.NumCols());
+    
+  // Calculate XW.T. The rows correspond to the x
+  // training examples and the columns to the class labels.
+  xw->AddMatMat(1.0, xs, kNoTrans, weights_, kTrans, 0.0);
+
+  // Calculate both the gradient and the objective function.
+  double objf = GetObjfAndGrad(xs, ys, *xw, &gradient);
+
+  // Convert gradient (a matrix) into a vector of size
+  // gradient.NumCols * gradient.NumRows.
+  grad_vec.CopyRowsFromMat(gradient);
+
+  // Compute next step in L-BFGS.
+  lbfgs->DoStep(objf, grad_vec);
+  
+  // Update weights
+  Vector<double> new_w(lbfgs->GetProposedValue());
+  weights_.CopyRowsFromVec(new_w);
+  return objf;
+}
+
+double LogisticRegression::GetObjfAndGrad(const Matrix<double> &xs,
+  const std::vector<int32> &ys, 
+  const Matrix<double> &xw,
+  Matrix<double> *grad) {
+  double objf = 0.0;
+  // For each training example class
+  for (int32 i = 0; i < ys.size(); i++) {
+    Vector<double> row(xw.NumCols());
+    row.CopyFromVec(xw.Row(i));
+    row.ApplySoftMax();
+    objf += std::log(std::max(row(ys[i]), 1.0e-20));
+    SubVector<double> x = xs.Row(i);
+    // Iterate over the class labels
+    for (int32 k = 0; k < weights_.NumRows(); k++) {
+      // p(y = k | x_i)
+      double p = row(k);
+      if (k == ys[i]) {
+        grad->Row(k).AddVec(1.0 - p, x);
+      } else {
+        grad->Row(k).AddVec(-1.0 * p, x);
+      }
+    }
+  }
+  grad->Scale(1.0/ys.size());
+  return objf/ys.size();
+}
+
+void LogisticRegression::SetWeights(const Matrix<double> &weights) {
+  weights_.Resize(weights.NumRows(), weights.NumCols());
+  weights_.CopyFromMat(weights);
+}
+
+void LogisticRegression::Write(std::ostream &os, bool binary) const {
+  WriteToken(os, binary, "<LogisticRegression>");
+  weights_.Write(os, binary);
+  WriteToken(os, binary, "</LogisticRegression>");
+}
+
+void LogisticRegression::Read(std::istream &is, bool binary) {
+  ExpectToken(is, binary, "<LogisticRegression>");
+  weights_.Read(is, binary);
+  ExpectToken(is, binary, "</LogisticRegression>");
+}
+
+}
---
+++ src/ivector/logistic-regression.h
@@ -0,0 +1,67 @@
+// ivector/logistic-regression.h
+
+// Copyright 2014  David Snyder
+
+// See ../../COPYING for clarification regarding multiple authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+
+#include "base/kaldi-common.h"
+#include "util/common-utils.h"
+#include "matrix/matrix-lib.h"
+//#include "thread/kaldi-task-sequence.h"
+#include <numeric>
+
+
+namespace kaldi {
+
+class LogisticRegression {
+  public:
+    // xs and ys are the training data. Each row of xs is a vector
+    // corresponding to the class label in the same row of ys. 
+    // weights contains the trained parameters.
+    void Train(const Matrix<double> &xs, const std::vector<int32> &ys,
+               Matrix<double> *weights, int32 max_steps);
+ 
+    // Calculates the posterior of the class label of input xs.
+    // The rows of posteriors corresponds to the rows of xs: the 
+    // individual data points to be evaluated. The columns of 
+    // posteriors are the integer class labels.
+    void GetPosteriors(const Matrix<double> &xs, Matrix<double> *posteriors);
+ 
+    // Performs a step in the L-BFGS. This is mostly used internally
+    // By Train() and for testing.   
+    double DoStep(const Matrix<double> &xs, 
+    Matrix<double> *xw, const std::vector<int32> &ys, 
+                OptimizeLbfgs<double> *lbfgs);
+
+    // Returns the objective function given the training data, xs, ys.
+    // The gradient is also calculated, and returned in grad.
+    double GetObjfAndGrad(const Matrix<double> &xs, 
+                         const std::vector<int32> &ys, 
+                         const Matrix<double> &xw, 
+                         Matrix<double> *grad);
+
+    void Write(std::ostream &os, bool binary) const;
+    void Read(std::istream &is, bool binary);
+    
+    // Sets the weights. This is generalyl used for testing.
+    void SetWeights(const Matrix<double> &weights);
+  private:
+    // Each row of weights_ corresponds to the class labels
+    // and each column, a feature in the input vectors. 
+    Matrix<double> weights_;
+};
+
+}
