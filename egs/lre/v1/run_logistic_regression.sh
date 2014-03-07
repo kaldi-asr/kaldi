ac7dee24b03a767d1cbceae93994656ffbe14032
David Snyder <david.ryan.snyder@gmail.com>
2014-03-07 23:14:18 +0000

    sandbox/language_id: Updating run_logistic_regression.sh to use lre07 in test. Also adding a script to remove the dialect portion of the languages in an utt2lang file; this is necessary currently, since the training and testing data often have non-overlapping dialects. Also adding a script balance_priors_to_test.pl which rebalances the priors in the logistic regression model.
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/sandbox/language_id@3733 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/lre/v1/run_logistic_regression.sh
+++ egs/lre/v1/run_logistic_regression.sh
@@ -12,46 +12,62 @@ mfccdir=`pwd`/mfcc
 vaddir=`pwd`/mfcc
 config=conf/logistic-regression.conf
 
-awk '{print $2}' data/train/utt2lang  | sort -u | \
+awk '{print $2}' <(utils/remove_dialect.pl data/train/utt2lang) | sort -u | \
   awk '{print $1, NR-1}' >  exp/ivectors_train/languages.txt
 
 
 log=exp/ivectors_train/log/logistic_regression.log
 
 model=exp/ivectors_train/logistic_regression
+model_rebalanced=exp/ivectors_train/logistic_regression_rebalanced
 train_ivectors=exp/ivectors_train/ivector.scp
-classes="ark:utils/sym2int.pl -f 2 exp/ivectors_train/languages.txt data/train/utt2lang|"
+classes="ark:utils/remove_dialect.pl data/train/utt2lang \
+         | utils/sym2int.pl -f 2 exp/ivectors_train/languages.txt - |"
 
+# An alternative prior.
+#utils/sym2int.pl -f 2 exp/ivectors_train/languages.txt \
+#  <(utils/remove_dialect.pl data/train/utt2lang) | \
+#  awk '{print $2}' | sort -n | uniq -c | \
+#  awk 'BEGIN{printf(" [ ");} {printf("%s ", 1.0/$1); } END{print(" ]"); }' \
+#   >exp/ivectors_train/inv_priors.vec
 
-utils/sym2int.pl -f 2 exp/ivectors_train/languages.txt data/train/utt2lang | \
-  awk '{print $2}' | sort -n | uniq -c | \
-  awk 'BEGIN{printf(" [ ");} {printf("%s ", 1.0/$1); } END{print(" ]"); }' \
-   >exp/ivectors_train/inv_priors.vec
-
-
-
-. path.sh
+# Create priors to rebalance the model. The following script rebalances
+# the languages as count(lang_test) / (count(lang_test) + count(lang_train)).
+utils/balance_priors_to_test.pl \
+    <(utils/remove_dialect.pl data/train/utt2lang) \
+    <(utils/remove_dialect.pl data/lre07/utt2lang) \
+    exp/ivectors_train/languages.txt \
+    exp/ivectors_train/priors.vec
 
 logistic-regression-train --config=$config scp:$train_ivectors \
                           "$classes" $model 2>$log 
 
-#( logistic-regression-train --config=$config scp:$train_ivectors \
-#                          "$classes" - | \
-# logistic-regression-copy --scale-priors=exp/ivectors_train/inv_priors.vec - $model ) 2>$log
+( logistic-regression-train --config=$config scp:$train_ivectors \
+                          "$classes" - | \
+ logistic-regression-copy --scale-priors=exp/ivectors_train/priors.vec - \
+ $model_rebalanced ) 2>$log
 
-trials="utils/sym2int.pl -f 2 exp/ivectors_train/languages.txt data/train/utt2lang|"
-scores="|utils/int2sym.pl -f 2 exp/ivectors_train/languages.txt  >exp/ivectors_train/train_scores"
+trials="utils/remove_dialect.pl data/train/utt2lang \
+        | utils/sym2int.pl -f 2 exp/ivectors_train/languages.txt -|"
+scores="|utils/int2sym.pl -f 2 exp/ivectors_train/languages.txt  \
+        >exp/ivectors_train/train_scores"
 
-logistic-regression-eval $model scp:$train_ivectors ark,t:exp/ivectors_train/posteriors
+logistic-regression-eval $model scp:$train_ivectors \
+  ark,t:exp/ivectors_train/posteriors
+
 logistic-regression-eval $model "ark:$trials" scp:$train_ivectors "$scores"
 
 logistic-regression-eval $model scp:$train_ivectors ark,t:- | \
-  awk '{max=$3; argmax=3; for(f=3;f<NF;f++) { if ($f>max) { max=$f; argmax=f; }}  print $1, (argmax - 3); }' | \
-  utils/int2sym.pl -f 2 exp/ivectors_train/languages.txt >exp/ivectors_train/output
+  awk '{max=$3; argmax=3; for(f=3;f<NF;f++) { if ($f>max) 
+                          { max=$f; argmax=f; }}  
+                          print $1, (argmax - 3); }' | \
+  utils/int2sym.pl -f 2 exp/ivectors_train/languages.txt \
+    >exp/ivectors_train/output
 
 # note: we treat the language as a sentence; it happens that the WER/SER
 # corresponds to the recognition error rate.
-compute-wer --text ark:data/train/utt2lang ark:exp/ivectors_train/output
+compute-wer --text ark:<(utils/remove_dialect.pl data/train/utt2lang) \
+  ark:exp/ivectors_train/output
 
 # It perfectly classifies the training data:
 #%WER 0.00 [ 0 / 10173, 0 ins, 0 del, 0 sub ]
@@ -59,18 +75,20 @@ compute-wer --text ark:data/train/utt2lang ark:exp/ivectors_train/output
 #Scored 10173 sentences, 0 not present in hyp.
 
 
-logistic-regression-eval $model scp:exp/ivectors_test/ivector.scp ark,t:- | \
-  awk '{max=$3; argmax=3; for(f=3;f<NF;f++) { if ($f>max) { max=$f; argmax=f; }}  print $1, (argmax - 3); }' | \
-  utils/int2sym.pl -f 2 exp/ivectors_train/languages.txt >exp/ivectors_test/output
+logistic-regression-eval $model_rebalanced \
+  scp:exp/lre07/ivector.scp ark,t:- | \
+  awk '{max=$3; argmax=3; for(f=3;f<NF;f++) { if ($f>max) 
+                          { max=$f; argmax=f; }}  
+                          print $1, (argmax - 3); }' | \
+  utils/int2sym.pl -f 2 exp/ivectors_train/languages.txt >exp/lre07/output
 
 
 # someone needs to extend this to run on the dev data.
 
-compute-wer --text ark:data/test/utt2lang ark:exp/ivectors_test/output
-
-#compute-wer --text ark:data/test/utt2lang ark:exp/ivectors_test/output
-#compute-wer --text ark:data/test/utt2lang ark:exp/ivectors_test/output 
-#%WER 2.97 [ 119 / 4000, 0 ins, 0 del, 119 sub ]
-#%SER 2.97 [ 119 / 4000 ]
-#Scored 4000 sentences, 0 not present in hyp.
+compute-wer --text ark:<(utils/remove_dialect.pl data/lre07/utt2lang)\
+  ark:exp/lre07/output
+# compute-wer --text ark:/dev/fd/63 ark:exp/lre07/output 
+# %WER 52.58 [ 3958 / 7527, 0 ins, 0 del, 3958 sub ]
+# %SER 52.58 [ 3958 / 7527 ]
+# Scored 7527 sentences, 0 not present in hyp.
 
