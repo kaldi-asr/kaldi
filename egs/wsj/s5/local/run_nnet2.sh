7200f2a529e54d9d14897c8aca7c1c1a09168fb8
Xiaohui Zhang <samuelzhang1104@gmail.com>
2014-03-10 21:34:10 +0000

    added discriminative training recipes for wsj-s5-nnet2 recipe
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@3758 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/wsj/s5/local/run_nnet2.sh
+++ egs/wsj/s5/local/run_nnet2.sh
@@ -9,7 +9,9 @@ use_gpu=true
 if $use_gpu; then
   local/nnet2/run_5b_gpu.sh # various VTLN combinations,  Mel-filterbank features, si284 train (multiplied by 5).
   local/nnet2/run_5c_gpu.sh # this is on top of fMLLR features.
+  local/nnet2/run_6c_gpu.sh # this is discriminative training of tanh neural nets on top of run_5c_gpu.sh
   local/nnet2/run_5d_gpu.sh # this is p-norm training on top of fMLLR features.
+  local/nnet2/run_6d_gpu.sh # this is discriminative training of p-norm neural nets on top of run_5d_gpu.sh
 else
   local/nnet2/run_5b.sh # various VTLN combinations, Mel-filterbank features,  si284 train (multiplied by 5).
   local/nnet2/run_5c.sh # this is on top of fMLLR features.
