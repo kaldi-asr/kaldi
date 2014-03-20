8ba72bca8711107baed7649733a478fa84497068
Dan Povey <dpovey@gmail.com>
2014-03-20 21:17:00 +0000

    trunk: fix to cuda-compiled.cc to let it compile when CUDA not enabled.
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@3792 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/nnet2bin/cuda-compiled.cc
+++ src/nnet2bin/cuda-compiled.cc
@@ -17,6 +17,7 @@
 // See the Apache 2 License for the specific language governing permissions and
 // limitations under the License.
 
+#include "base/kaldi-common.h"
 #include "cudamatrix/cu-device.h"
 
 int main(int argc, char *argv[]) {
