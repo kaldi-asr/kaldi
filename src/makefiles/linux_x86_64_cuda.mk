f67874f274b60c795c53b122f605bf4c96bca89d
Dan Povey <dpovey@gmail.com>
2014-06-25 22:48:37 +0000

    trunk (thanks to Steven Eliuk): changes for better compatibility [specify pattern to mktemp; changes to allow compilation with CUDA on Darwin.]
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@4083 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/makefiles/linux_x86_64_cuda.mk
+++ src/makefiles/linux_x86_64_cuda.mk
@@ -3,6 +3,12 @@ CUDA_INCLUDE= -I$(CUDATKDIR)/include
 CUDA_FLAGS = -g -Xcompiler -fPIC --verbose --machine 64 -DHAVE_CUDA
 
 CXXFLAGS += -DHAVE_CUDA -I$(CUDATKDIR)/include 
+UNAME := $(shell uname)
+#aware of fact in cuda60 there is no lib64, just lib.
+ifeq ($(UNAME), Darwin)
+CUDA_LDFLAGS += -L$(CUDATKDIR)/lib -Wl,-rpath,$(CUDATKDIR)/lib
+else
 CUDA_LDFLAGS += -L$(CUDATKDIR)/lib64 -Wl,-rpath,$(CUDATKDIR)/lib64
+endif
 CUDA_LDLIBS += -lcublas -lcudart #LDLIBS : The libs are loaded later than static libs in implicit rule
 
