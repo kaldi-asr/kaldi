e459d6d1797d11215dc6e42df9b326d4e445e130
Dan Povey <dpovey@gmail.com>
2014-08-05 17:18:28 +0000

    trunk: adding Fisher-english example scripts for data-cleanup method (doesn't seem to help on Fisher, but may be useful elsewhere.)
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@4255 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/fisher_english/s5/run.sh
+++ egs/fisher_english/s5/run.sh
@@ -156,12 +156,35 @@ steps/train_sat.sh  --cmd "$train_cmd" \
     exp/tri5a/graph data/dev exp/tri5a/decode_dev
 )&
 
-#
-# steps/cleanup/find_bad_utts.sh --nj 200 --cmd "$train_cmd" data/train data/lang \
-#   exp/tri5a exp/tri5a_cleanup
+
+# The step below won't run by default; it demonstrates a data-cleaning method.
+# It doesn't seem to help in this setup; maybe the data was clean enough already.
+false && (
+ steps/cleanup/find_bad_utts.sh --nj 200 --cmd "$train_cmd" data/train data/lang \
+   exp/tri5a exp/tri5a_cleanup
+
+ # with threshold of 0.05 we keep 1.1 million out of 1.6 million utterances, and
+ # around 8.7 million out of 18.1 million words
+ # with threshold of 0.1 we keep 1.3 out of 1.6 million utterances, and around
+ # 13.2 million out of 18.1 million words.
+ thresh=0.1
+ cat exp/tri5a_cleanup/all_info.txt | awk -v threshold=$thresh '{ errs=$2;ref=$3; if (errs <= threshold*ref) { print $1; } }' > uttlist
+ utils/subset_data_dir.sh --utt-list uttlist data/train data/train.thresh$thresh
+
+ steps/align_fmllr.sh --nj 30 --cmd "$train_cmd" \
+   data/train.thresh$thresh data/lang exp/tri4a exp/tri4a_ali_$thresh
+
+ steps/train_sat.sh  --cmd "$train_cmd" \
+   10000 300000 data/train data/lang exp/tri4a_ali_$thresh  exp/tri5a_$thresh || exit 1;
+
+ (
+   utils/mkgraph.sh data/lang_test exp/tri5a_$thresh exp/tri5a_$thresh/graph
+   steps/decode_fmllr.sh --nj 25 --cmd "$decode_cmd" --config conf/decode.config \
+     exp/tri5a_$thresh/graph data/dev exp/tri5a_$thresh/decode_dev
+ )&
+)
 
 
 # local/run_for_spkid.sh
 
-# we don't have to results for the step below yet.
 # local/run_nnet2.sh
