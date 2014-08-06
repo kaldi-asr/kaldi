05a3f2b9fb8d09297528b0b7745c7fa679345df6
Gaurav Kumar <gaurav.bison@gmail.com>
2014-08-06 14:26:57 +0000

    Added new scripts for processing arabic script, converting lattices and extracting n-best lists
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@4265 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/callhome_egyptian/s5/local/lattice_main.sh
+++ egs/callhome_egyptian/s5/local/lattice_main.sh
@@ -3,17 +3,17 @@
 outDir=exp/lat
 mkdir -p $outDir
 
-stage=1
+stage=2
 
 if [ $stage -lt 1 ]; then
 
   # First convert all lattices into the pruned, minimized version
   decodeDir=exp/tri5a/decode_dev
-  acousticScale=0.8333
+  acousticScale=0.08333
   local/latconvert.sh $outDir $decodeDir $acousticScale
 
   decodeDir=exp/tri5a/decode_test
-  acousticScale=0.8333
+  acousticScale=0.08333
   local/latconvert.sh $outDir $decodeDir $acousticScale
 
 fi
@@ -29,4 +29,21 @@ if [ $stage -lt 2 ]; then
   textFile=data/test/text
   symTable=exp/tri5a/graph/words.txt
   local/get_oracle.sh $latticeDir $symTable $textFile
+fi
+
+if [ $stage -lt 3 ]; then
+  # Create a script lexicon if it does not exist
+  if [ ! -f data/local/dict/lexicon_script.txt ]; then
+    local/callhome_prepare_script_dict.py /export/corpora/LDC/LDC99L22/ \
+      exp/tri5a/graph/words.txt data/local/dict/lexicon_script.txt
+  fi
+
+  # Now get the n-best files from the lattices
+  decodeDir=exp/tri5a/decode_dev
+  acousticScale=0.08333
+  local/get_nbest.sh $outDir $decodeDir $acousticScale dev
+
+  decodeDir=exp/tri5a/decode_test
+  acousticScale=0.08333
+  local/get_nbest.sh $outDir $decodeDir $acousticScale test
 fi
