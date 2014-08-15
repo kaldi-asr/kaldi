2568ec23c1d69715bb8e80319ec0570d3bbdea3e
Dan Povey <dpovey@gmail.com>
2014-08-15 22:44:32 +0000

    sandbox/online: adding some new files from trunk that were skipped in the last merge.
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/sandbox/online@4319 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ egs/wsj/s5/local/run_rnnlm-hs_tri3b.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+. cmd.sh
+ # This step interpolates a small RNNLM (with weight 0.15) with the 4-gram LM.
+steps/rnnlmrescore.sh --rnnlm_ver rnnlm-hs-0.1b \
+  --N 100 --cmd "$decode_cmd" --inv-acwt 17 \
+  0.15 data/lang_test_bd_fg data/local/rnnlm-hs.h30.voc10k data/test_eval92 \
+  exp/tri3b/decode_bd_tgpr_eval92_fg exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs30_0.15  \
+  || exit 1;
+
+steps/rnnlmrescore.sh --rnnlm_ver rnnlm-hs-0.1b \
+  --N 100 --cmd "$decode_cmd" --inv-acwt 17 \
+  0.3 data/lang_test_bd_fg data/local/rnnlm-hs.h100.voc20k data/test_eval92 \
+  exp/tri3b/decode_bd_tgpr_eval92_fg exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs100_0.3 \
+  || exit 1;
+
+steps/rnnlmrescore.sh --rnnlm_ver rnnlm-hs-0.1b \
+  --N 100 --cmd "$decode_cmd" --inv-acwt 17 \
+  0.3 data/lang_test_bd_fg data/local/rnnlm-hs.h300.voc30k data/test_eval92 \
+  exp/tri3b/decode_bd_tgpr_eval92_fg exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs300_0.3 \
+  || exit 1;
+
+steps/rnnlmrescore.sh --rnnlm_ver rnnlm-hs-0.1b \
+  --N 100 --cmd "$decode_cmd" --inv-acwt 17 \
+  0.3 data/lang_test_bd_fg data/local/rnnlm-hs.h400.voc40k data/test_eval92 \
+  exp/tri3b/decode_bd_tgpr_eval92_fg exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs400_0.3 \
+  || exit 1;
+
+steps/rnnlmrescore.sh --rnnlm_ver rnnlm-hs-0.1b \
+  --N 1000 --cmd "$decode_cmd" --inv-acwt 17 \
+  0.3 data/lang_test_bd_fg data/local/rnnlm-hs.h400.voc40k data/test_eval92 \
+  exp/tri3b/decode_bd_tgpr_eval92_fg exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs400_0.3_N1000 
+
+steps/rnnlmrescore.sh --rnnlm_ver rnnlm-hs-0.1b \
+  --N 1000 --cmd "$decode_cmd" --inv-acwt 17 \
+  0.3 data/lang_test_bd_fg data/local/rnnlm-hs.h400.voc40k data/test_eval92 \
+  exp/tri3b/decode_bd_tgpr_eval92_fg exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs400_0.3_N1000 \
+  || exit 1;
+
+dir=exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs400_0.4_N1000
+rm -rf $dir
+cp -r exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs400_0.3_N1000 $dir
+steps/rnnlmrescore.sh --rnnlm_ver rnnlm-hs-0.1b \
+  --stage 7 --N 1000 --cmd "$decode_cmd" --inv-acwt 17 \
+  0.4 data/lang_test_bd_fg data/local/rnnlm-hs.h400.voc40k data/test_eval92 \
+  exp/tri3b/decode_bd_tgpr_eval92_fg $dir
+
+dir=exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs400_0.4
+rm -rf $dir
+cp -r exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs400_0.3 $dir
+steps/rnnlmrescore.sh --rnnlm_ver rnnlm-hs-0.1b \
+  --stage 7 --N 100 --cmd "$decode_cmd" --inv-acwt 17 \
+  0.4 data/lang_test_bd_fg data/local/rnnlm-hs.h400.voc40k data/test_eval92 \
+  exp/tri3b/decode_bd_tgpr_eval92_fg $dir
+
+dir=exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs400_0.15
+rm -rf $dir
+cp -r exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs400_0.3 $dir
+steps/rnnlmrescore.sh --rnnlm_ver rnnlm-hs-0.1b \
+  --stage 7 --N 100 --cmd "$decode_cmd" --inv-acwt 17 \
+  0.15 data/lang_test_bd_fg data/local/rnnlm-hs.h400.voc40k data/test_eval92 \
+  exp/tri3b/decode_bd_tgpr_eval92_fg $dir
+
+steps/rnnlmrescore.sh --rnnlm_ver rnnlm-hs-0.1b \
+  --N 10 --cmd "$decode_cmd" --inv-acwt 17 \
+  0.3 data/lang_test_bd_fg data/local/rnnlm-hs.h400.voc40k data/test_eval92 \
+  exp/tri3b/decode_bd_tgpr_eval92_fg exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs400_0.3_N10 \
+  || exit 1;
+
+dir=exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs400_0.4_N1000
+rm -rf $dir
+cp -r exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs400_0.3_N1000 $dir
+steps/rnnlmrescore.sh --rnnlm_ver rnnlm-hs-0.1b \
+  --stage 7 --N 1000 --cmd "$decode_cmd" --inv-acwt 17 \
+  0.4 data/lang_test_bd_fg data/local/rnnlm-hs.h400.voc40k data/test_eval92 \
+  exp/tri3b/decode_bd_tgpr_eval92_fg $dir
+
+dir=exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs400_0.15_N1000
+rm -rf $dir
+cp -r exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs400_0.3_N1000 $dir
+steps/rnnlmrescore.sh --rnnlm_ver rnnlm-hs-0.1b \
+  --stage 7 --N 1000 --cmd "$decode_cmd" --inv-acwt 17 \
+  0.15 data/lang_test_bd_fg data/local/rnnlm-hs.h400.voc40k data/test_eval92 \
+  exp/tri3b/decode_bd_tgpr_eval92_fg $dir
+
+dir=exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs400_0.5_N1000
+rm -rf $dir
+cp -r exp/tri3b/decode_bd_tgpr_eval92_fg_rnnlm-hs400_0.3_N1000 $dir
+steps/rnnlmrescore.sh --rnnlm_ver rnnlm-hs-0.1b \
+  --stage 7 --N 1000 --cmd "$decode_cmd" --inv-acwt 17 \
+  0.5 data/lang_test_bd_fg data/local/rnnlm-hs.h400.voc40k data/test_eval92 \
+  exp/tri3b/decode_bd_tgpr_eval92_fg $dir
