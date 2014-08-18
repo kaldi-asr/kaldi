9bd10e674abbcb83f28e48206f9fce6ac6326ae4
Karel Vesely <vesis84@gmail.com>
2014-08-18 16:55:50 +0000

    trunk,nnet,scripts : - steps/nnet/train_scheduler.sh : add option --keep-lr-iters, to delay learning-rate halving when requested - utils/nnet/make_nnet_proto.py : add max-norm support - timit/s5/local/score*.sh : faster scoring - tedlium/s5/local/run_dnn.sh : bugfix, for sMBR step decode once
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@4323 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/tedlium/s5/local/run_dnn.sh
+++ egs/tedlium/s5/local/run_dnn.sh
@@ -131,15 +131,15 @@ if [ $stage -le 6 ]; then
   steps/nnet/train_mpe.sh --cmd "$cuda_cmd" --num-iters 4 --acwt $acwt --do-smbr true \
     $data_fmllr/train data/lang $srcdir ${srcdir}_ali ${srcdir}_denlats $dir || exit 1
   # Decode (reuse HCLG graph)
-  for ITER in 1 2 3 4; do
+  for ITER in 4; do
     steps/nnet/decode.sh --nj $decode_nj --cmd "$decode_cmd" --config conf/decode_dnn.config \
       --num-threads 3 --parallel-opts "-pe smp 4" \
       --nnet $dir/${ITER}.nnet --acwt $acwt \
-      $gmmdir/graph $data_fmllr/dev $dir/decode_dev || exit 1;
+      $gmmdir/graph $data_fmllr/dev $dir/decode_dev_it$ITER || exit 1;
     steps/nnet/decode.sh --nj $decode_nj --cmd "$decode_cmd" --config conf/decode_dnn.config \
       --num-threads 3 --parallel-opts "-pe smp 4" \
       --nnet $dir/${ITER}.nnet --acwt $acwt \
-      $gmmdir/graph $data_fmllr/test $dir/decode_test || exit 1;
+      $gmmdir/graph $data_fmllr/test $dir/decode_test_it$ITER || exit 1;
   done 
 fi
 
