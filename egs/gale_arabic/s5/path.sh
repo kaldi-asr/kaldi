463f643f98e5475e462ada66bd19f924f15132da
Ahmed Ali <ahmaksod1@users.sf.net>
2014-08-26 19:23:44 +0000

    add nn path
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@4343 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/gale_arabic/s5/path.sh
+++ egs/gale_arabic/s5/path.sh
@@ -1,3 +1,3 @@
 export KALDI_ROOT=$(pwd)/../../..
-export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$PWD:$PATH
+export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$KALDI_ROOT/src/nnetbin:$KALDI_ROOT/src/nnet2bin/:$KALDI_ROOT/src/kwsbin:$PWD:$PATH
 export LC_ALL=C
