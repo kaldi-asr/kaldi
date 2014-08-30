2b276deb2f857f81f8599ca3bc1a4a83e509ded5
Dan Povey <dpovey@gmail.com>
2014-08-30 19:45:33 +0000

    update to eval2000 data-prep to die if source broken.
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@4356 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/swbd/s5b/local/eval2000_data_prep.sh
+++ egs/swbd/s5b/local/eval2000_data_prep.sh
@@ -115,3 +115,7 @@ echo Data preparation and formatting completed for Eval 2000
 echo "(but not MFCC extraction)"
 
 utils/fix_data_dir.sh $dest
+
+if [ $(wc -l < $dest/wav.scp) -ne 80 ]; then
+  echo "$0: error: expected 80 lines in wav.scp, got $(wc -l < $dest/wav.scp)"
+fi
