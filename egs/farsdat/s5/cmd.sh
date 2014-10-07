fe4f2bacb7fbfae4a06e9b3175a17b0d11693e75
Bagher BabaAli <bagher.babaali@gmail.com>
2014-10-07 18:45:31 +0000

    Adding farsdat recipe
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@4514 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ egs/farsdat/s5/cmd.sh
@@ -0,0 +1,25 @@
+# "queue.pl" uses qsub.  The options to it are
+# options to qsub.  If you have GridEngine installed,
+# change this to a queue you have access to.
+# Otherwise, use "run.pl", which will run jobs locally
+# (make sure your --num-jobs options are no more than
+# the number of cpus on your machine.
+
+#a) JHU cluster options
+export train_cmd="queue.pl -l arch=*64"
+export decode_cmd="queue.pl -l arch=*64,mem_free=2G,ram_free=2G"
+export mkgraph_cmd="queue.pl -l arch=*64,ram_free=4G,mem_free=4G"
+export cuda_cmd="run.pl"
+
+
+#b) BUT cluster options
+#export train_cmd="queue.pl -q all.q@blade[01][0126789][123456789] -l ram_free=2500M,mem_free=2500M,matylda5=0.5"
+#export decode_cmd="queue.pl -q all.q@blade[01][0126789][123456789] -l ram_free=3000M,mem_free=3000M,matylda5=0.1"
+#export mkgraph_cmd="queue.pl -q all.q@blade[01][0126789][123456789] -l ram_free=4G,mem_free=4G,matylda5=3"
+#export cuda_cmd="queue.pl -q long.q@pcspeech-gpu,long.q@dellgpu*,long.q@pco203-0[0124] -l gpu=1" 
+
+#c) run locally...
+#export train_cmd=run.pl
+#export decode_cmd=run.pl
+#export cuda_cmd=run.pl
+#export mkgraph_cmd=run.pl
---
+++ egs/farsdat/s5/local/farsdat_prepare_dict.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Copyright 2013   (Authors: Daniel Povey, Bagher BabaAli)
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+# WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+# MERCHANTABLITY OR NON-INFRINGEMENT.
+# See the Apache 2 License for the specific language governing permissions and
+# limitations under the License.
+
+# Call this script from one level above, e.g. from the s3/ directory.  It puts
+# its output in data/local/.
+
+# The parts of the output of this that will be needed are
+# [in data/local/dict/ ]
+# lexicon.txt
+# extra_questions.txt
+# nonsilence_phones.txt
+# optional_silence.txt
+# silence_phones.txt
+
+# run this from ../
+srcdir=data/train
+dir=data/local/dict
+lmdir=data/local/nist_lm
+tmpdir=data/local/lm_tmp
+
+mkdir -p $dir $lmdir $tmpdir
+
+[ -f path.sh ] && . ./path.sh
+
+#(1) Dictionary preparation:
+
+# Make phones symbol-table (adding in silence and verbal and non-verbal noises at this point).
+
+# silence phones, one per line.
+echo sil > $dir/silence_phones.txt
+echo sil > $dir/optional_silence.txt
+
+# nonsilence phones; on each line is a list of phones that correspond
+# really to the same base phone.
+
+# Create the lexicon, which is just an identity mapping
+cut -d' ' -f2- $srcdir/text | tr ' ' '\n' | sort -u | sed '/^$/d' > $dir/phones.txt
+paste $dir/phones.txt $dir/phones.txt > $dir/lexicon.txt || exit 1;
+grep -v -F -f $dir/silence_phones.txt $dir/phones.txt > $dir/nonsilence_phones.txt 
+
+# A few extra questions that will be added to those obtained by automatically clustering
+# the "real" phones.  These ask about stress; there's also one for silence.
+cat $dir/silence_phones.txt| awk '{printf("%s ", $1);} END{printf "\n";}' > $dir/extra_questions.txt || exit 1;
+cat $dir/nonsilence_phones.txt | perl -e 'while(<>){ foreach $p (split(" ", $_)) {
+  $p =~ m:^([^\d]+)(\d*)$: || die "Bad phone $_"; $q{$2} .= "$p "; } } foreach $l (values %q) {print "$l\n";}' \
+ >> $dir/extra_questions.txt || exit 1;
+
+# (2) Create the phone bigram LM
+  [ -z "$IRSTLM" ] && \
+    echo "LM building won't work without setting the IRSTLM env variable" && exit 1;
+  ! which build-lm.sh 2>/dev/null  && \
+    echo "IRSTLM does not seem to be installed (build-lm.sh not on your path): " && \
+    echo "go to <kaldi-root>/tools and try 'make irstlm_tgt'" && exit 1;
+
+  cut -d' ' -f2- $srcdir/text | sed -e 's:^:<s> :' -e 's:$: </s>:' \
+    > $srcdir/lm_train
+  build-lm.sh -i $srcdir/lm_train -n 2 -o $tmpdir/lm_phone_bg.ilm.gz
+
+  compile-lm $tmpdir/lm_phone_bg.ilm.gz -t=yes /dev/stdout | \
+  grep -v unk | gzip -c > $lmdir/lm_phone_bg.arpa.gz 
+
+
+echo "Dictionary & language model preparation succeeded"
