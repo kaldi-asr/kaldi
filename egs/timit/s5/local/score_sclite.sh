bdd752b4bf95079851bac53a22b987d1487a8899
Karel Vesely <vesis84@gmail.com>
2014-11-20 16:05:12 +0000

    WARNING, TIMIT BASELINES CHANGE BY THIS COMMIT! The change is in how the PER is computed. Previously there was optional silence with no output symbol, and forgiving of silence deletions. This was however slight cheating (Traditionally in TIMIT, the silence is scored).
    The change consists of:
    - removing the optional silence from he lexicon (see call of utils/prepare_lang.sh in run.sh)
    - removing the forgiving of 'sil' deletions from scoring (see change in local/score.sh)
    - updating the RESULTS
    
    From now on the PER scores will involve no cheating and will be comparable to experiments in the literature.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@4639 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/timit/s5/local/score_sclite.sh
+++ egs/timit/s5/local/score_sclite.sh
@@ -46,8 +46,7 @@ done
 mkdir -p $dir/scoring/log
 
 # Map reference to 39 phone classes, the silence is optional (.):
-local/timit_norm_trans.pl -i $data/stm -m $phonemap -from 48 -to 39 | \
- sed 's: sil: (sil):g' > $dir/scoring/stm_39phn
+local/timit_norm_trans.pl -i $data/stm -m $phonemap -from 48 -to 39 >$dir/scoring/stm_39phn
 cp $data/glm $dir/scoring/glm_39phn
 
 if [ $stage -le 0 ]; then
