39ae325c9b17ed65ebd66420e3d6da567e2e81d5
Minhua Wu <mwupangpang722@gmail.com>
2015-01-20 04:58:53 +0000

    trunk: Adding Swahili example scripts; thanks to Elodie Gauthier and Laurent Besacier
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@4801 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ egs/swahili/s5/local/prepare_dict.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir -p data/lang data/local/dict
+
+cat asr_swahili/lang/lexicon.txt | sed '1,4d' > data/local/dict/lexicon_words.txt
+
+cp asr_swahili/lang/lexicon.txt data/local/dict/.
+
+cp asr_swahili/lang/nonsilence_phones.txt data/local/dict/.
+
+cp asr_swahili/lang/optional_silence.txt data/local/dict/.
+
+touch data/local/dict/extra_questions.txt
+
+echo "SIL" > data/local/dict/silence_phones.txt
+echo "SPN" >> data/local/dict/silence_phones.txt
+echo "LAU" >>  data/local/dict/silence_phones.txt
+echo "MUS" >>  data/local/dict/silence_phones.txt
+
+echo "<UNK>" > data/lang/oov.txt
+
+echo "Dictionary preparation succeeded"
