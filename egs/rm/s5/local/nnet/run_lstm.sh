18e3a73ff6a702a91fbf5e2f71c461a06d3c5ab2
Minhua Wu <mwupangpang722@gmail.com>
2015-02-11 01:23:27 +0000

    trunk: tune lstm recipe parameters
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@4872 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/rm/s5/local/nnet/run_lstm.sh
+++ egs/rm/s5/local/nnet/run_lstm.sh
@@ -43,9 +43,9 @@ if [ $stage -le 1 ]; then
 
   # Train
   $cuda_cmd $dir/log/train_nnet.log \
-    steps/nnet/train.sh --network-type lstm --learn-rate 0.00001 \
+    steps/nnet/train.sh --network-type lstm --learn-rate 0.0001 \
       --cmvn-opts "--norm-means=true --norm-vars=true" --feat-type plain --splice 0 \
-      --train-opts "--momentum 0.9 --halving-factor 0.8" \
+      --train-opts "--momentum 0.7 --halving-factor 0.8" \
       --train-tool "nnet-train-lstm-streams --num-stream=4 --targets-delay=5" \
     ${train}_tr90 ${train}_cv10 data/lang $ali $ali $dir || exit 1;
 
