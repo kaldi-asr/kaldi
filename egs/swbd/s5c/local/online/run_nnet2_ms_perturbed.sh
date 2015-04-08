4cf7f7028b9f1ec8410693ec231f13b004572841
Tom Ko <tomkocse@users.sf.net>
2015-04-08 09:03:16 +0000

    trunk: refactor of run_nnet2_ms_perturbed.sh
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@4993 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/swbd/s5c/local/online/run_nnet2_ms_perturbed.sh
+++ egs/swbd/s5c/local/online/run_nnet2_ms_perturbed.sh
@@ -8,13 +8,14 @@
 
 . ./cmd.sh
 set -e 
-stage=6
+stage=1
 train_stage=-10
 use_gpu=true
 # splice_indexes="layer0/-4:-3:-2:-1:0:1:2:3:4 layer2/-5:-3:3"
 splice_indexes="layer0/-2:-1:0:1:2 layer1/-1:2 layer2/-3:3 layer3/-7:2"
 common_egs_dir=
-dir=exp/nnet2_online/nnet_ms_b
+nnet_dir=nnet2_online_sp
+dir=exp/${nnet_dir}/nnet_ms_a
 has_fisher=true
 
 . ./path.sh
@@ -44,22 +45,27 @@ fi
 # Run the common stages of training, including training the iVector extractor
  local/online/run_nnet2_common.sh --stage $stage || exit 1;
 
+
 if [ $stage -le 6 ]; then
   #Although the nnet will be trained by high resolution data, we still have to perturbe the normal data to get the alignment
   # _sp stands for speed-perturbed
-  utils/perturb_data_dir_speed.sh 0.9 data/train_nodup data/temp1
-  utils/perturb_data_dir_speed.sh 1.0 data/train_nodup data/temp2
-  utils/perturb_data_dir_speed.sh 1.1 data/train_nodup data/temp3
-  utils/combine_data.sh --extra-files utt2uniq data/train_nodup_sp data/temp1 data/temp2 data/temp3
-  rm -r data/temp1 data/temp2 data/temp3
 
-  mfccdir=mfcc_perturbed
-  for x in train_nodup_sp; do
+  for datadir in train_nodup; do
+    utils/perturb_data_dir_speed.sh 0.9 data/${datadir} data/temp1
+    utils/perturb_data_dir_speed.sh 1.1 data/${datadir} data/temp2
+    utils/combine_data.sh --extra-files utt2uniq data/${datadir}_tmp data/temp1 data/temp2
+    rm -r data/temp1 data/temp2
+
+    mfccdir=mfcc_perturbed
     steps/make_mfcc.sh --cmd "$train_cmd" --nj 50 \
-      data/$x exp/make_mfcc/$x $mfccdir || exit 1;
-    steps/compute_cmvn_stats.sh data/$x exp/make_mfcc/$x $mfccdir || exit 1;
+      data/${datadir}_tmp exp/make_mfcc/${datadir}_tmp $mfccdir || exit 1;
+    steps/compute_cmvn_stats.sh data/${datadir}_tmp exp/make_mfcc/${datadir}_tmp $mfccdir || exit 1;
+    utils/fix_data_dir.sh data/${datadir}_tmp
+
+    utils/copy_data_dir.sh --spk-prefix sp1.0- --utt-prefix sp1.0- data/${datadir} data/temp0
+    utils/combine_data.sh data/${datadir}_sp data/${datadir}_tmp data/temp0
+    rm -r data/temp0 data/${datadir}_tmp
   done
-  utils/fix_data_dir.sh data/train_nodup_sp
 fi
 
 if [ $stage -le 7 ]; then
@@ -69,12 +75,8 @@ if [ $stage -le 7 ]; then
 fi
 
 if [ $stage -le 8 ]; then
-  #Now perturb the high resolution daa
-  utils/perturb_data_dir_speed.sh 0.9 data/train_nodup data/temp1
-  utils/perturb_data_dir_speed.sh 1.0 data/train_nodup data/temp2
-  utils/perturb_data_dir_speed.sh 1.1 data/train_nodup data/temp3
-  utils/combine_data.sh --extra-files utt2uniq data/train_hires_nodup_sp data/temp1 data/temp2 data/temp3
-  rm -r data/temp1 data/temp2 data/temp3
+  #Now perturb the high resolution data
+  utils/copy_data_dir.sh data/train_nodup_sp data/train_hires_nodup_sp
 
   # do volume perturbation of the data
   data_dir=data/train_hires_nodup_sp
@@ -99,6 +101,13 @@ for line in sys.stdin.readlines():
 fi
 
 if [ $stage -le 9 ]; then
+  # train a new extractor which have seen the perturbed data
+  # use already-built UBM.
+  steps/online/nnet2/train_ivector_extractor.sh --cmd "$train_cmd" --nj 50 \
+    data/train_hires_nodup_sp exp/nnet2_online/diag_ubm exp/${nnet_dir}/extractor || exit 1;
+fi
+
+if [ $stage -le 10 ]; then
   # We extract iVectors on all the train_nodup data, which will be what we
   # train the system on.
 
@@ -107,15 +116,15 @@ if [ $stage -le 9 ]; then
   steps/online/nnet2/copy_data_dir.sh --utts-per-spk-max 2 data/train_hires_nodup_sp data/train_hires_nodup_sp_max2
 
   steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd" --nj 30 \
-    data/train_hires_nodup_sp_max2 exp/nnet2_online/extractor exp/nnet2_online/ivectors_train_hires_nodup_sp2 || exit 1;
+    data/train_hires_nodup_sp_max2 exp/${nnet_dir}/extractor exp/${nnet_dir}/ivectors_train_hires_nodup_sp2 || exit 1;
 fi
 
-if [ $stage -le 10 ]; then
+if [ $stage -le 11 ]; then
   steps/nnet2/train_multisplice_accel2.sh --stage $train_stage \
     --num-epochs 2 --num-jobs-initial 3 --num-jobs-final 16 \
     --num-hidden-layers 4 --splice-indexes "$splice_indexes" \
     --feat-type raw \
-    --online-ivector-dir exp/nnet2_online/ivectors_train_hires_nodup_sp2 \
+    --online-ivector-dir exp/${nnet_dir}/ivectors_train_hires_nodup_sp2 \
     --cmvn-opts "--norm-means=false --norm-vars=false" \
     --num-threads "$num_threads" \
     --minibatch-size "$minibatch_size" \
@@ -131,23 +140,22 @@ if [ $stage -le 10 ]; then
     data/train_hires_nodup_sp data/lang exp/tri4_ali_nodup_sp $dir  || exit 1;
 fi
 
-if [ $stage -le 11 ]; then
+if [ $stage -le 12 ]; then
   for data in eval2000_hires train_hires_dev; do
     steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd" --nj 20 \
-      data/${data} exp/nnet2_online/extractor exp/nnet2_online/ivectors_${data} || exit 1;
+      data/${data} exp/${nnet_dir}/extractor exp/${nnet_dir}/ivectors_${data} || exit 1;
   done
 fi
 
-if [ $stage -le 12 ]; then
+if [ $stage -le 13 ]; then
   # this does offline decoding that should give the same results as the real
   # online decoding (the one with --per-utt true)
   graph_dir=exp/tri4/graph_sw1_tg
   # use already-built graphs.
- # for data in eval2000_hires train_hires_dev; do
-  for data in eval2000_hires ; do
+  for data in eval2000_hires train_hires_dev; do
     steps/nnet2/decode.sh --nj 30 --cmd "$decode_cmd" \
       --config conf/decode.config \
-      --online-ivector-dir exp/nnet2_online/ivectors_${data} \
+      --online-ivector-dir exp/${nnet_dir}/ivectors_${data} \
       $graph_dir data/${data} $dir/decode_${data}_sw1_tg || exit 1;
     if $has_fisher; then
       steps/lmrescore_const_arpa.sh --cmd "$decode_cmd" \
@@ -157,14 +165,14 @@ if [ $stage -le 12 ]; then
   done
 fi
 
-if [ $stage -le 13 ]; then
+if [ $stage -le 14 ]; then
   # If this setup used PLP features, we'd have to give the option --feature-type plp
   # to the script below.
   steps/online/nnet2/prepare_online_decoding.sh --mfcc-config conf/mfcc_hires.conf \
-      data/lang exp/nnet2_online/extractor "$dir" ${dir}_online || exit 1;
+      data/lang exp/${nnet_dir}/extractor "$dir" ${dir}_online || exit 1;
 fi
 
-if [ $stage -le 14 ]; then
+if [ $stage -le 15 ]; then
   # do the actual online decoding with iVectors, carrying info forward from 
   # previous utterances of the same speaker.
   graph_dir=exp/tri4/graph_sw1_tg
@@ -181,7 +189,7 @@ if [ $stage -le 14 ]; then
   done
 fi
 
-if [ $stage -le 15 ]; then
+if [ $stage -le 16 ]; then
   # this version of the decoding treats each utterance separately
   # without carrying forward speaker information.
   graph_dir=exp/tri4/graph_sw1_tg
