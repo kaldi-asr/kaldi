22de253706979d8727b8640aa12b78b971378d07
Ahmed Ali <ahmaksod1@users.sf.net>
2015-05-06 12:22:02 +0000

    update results and run.sh
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@5050 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/gale_arabic/s5/run.sh
+++ egs/gale_arabic/s5/run.sh
@@ -6,45 +6,31 @@
 . ./path.sh
 . ./cmd.sh ## You'll want to change cmd.sh to something that will work on your system.
            ## This relates to the queue.
-nJobs=60
+nJobs=120
 nDecodeJobs=40
 
 #NB: You can add whatever number of copora you like. The supported extensions 
 #NB: (formats) are wav and flac. Flac will be converted using sox and in contrast
 #NB: with the old approach, the conversion will be on-the-fly and one-time-only
 #NB: during the parametrization.
-audio=(
-  /alt/data/speech/LDC/LDC2013S02/gale_p2_arb_bc_speech_p1_d1
-  /alt/data/speech/LDC/LDC2013S02/gale_p2_arb_bc_speech_p1_d2
-  /alt/data/speech/LDC/LDC2013S02/gale_p2_arb_bc_speech_p1_d3
-  /alt/data/speech/LDC/LDC2013S02/gale_p2_arb_bc_speech_p1_d4
-  /alt/data/speech/LDC/LDC2013S07/gale_p2_arb_bc_spch_p2_d1
-  /alt/data/speech/LDC/LDC2013S07/gale_p2_arb_bc_spch_p2_d2
-)
 
 #NB: Text corpora scpecification. We support either tgz files, which are unpacked
 #NB: or just plain (already unpacked) directories. The list of transcript is then
 #NB: obtained using find command
-text=(
-  /alt/data/speech/LDC/LDC2013T17.tgz
-  /alt/data/speech/LDC/LDC2013T04.tgz
-)
 
 #This is CLSP configuration. We add the 2014 GALE data. We got around 2 % 
-#improvement just by including it. The gain might be larger if someone would tweak
+#improvement just by including it. The gain might be large if someone would tweak
 # the number of leaves and states and so on.
-if false; then
 audio=(
-  /export/corpora5/LDC/LDC2013S02 
-  /export/corpora5/LDC/LDC2013S07 
-  /export/corpora5/LDC/LDC2014S07 
+  /data/sls/scratch/amali/data/GALE/LDC2013S02
+  /data/sls/scratch/amali/data/GALE/LDC2013S07 
+  /data/sls/scratch/amali/data/GALE/LDC2014S07 
 )
 text=(
-  /export/corpora5/LDC/LDC2013T17 
-  /export/corpora5/LDC/LDC2013T04 
-  /export/corpora5/LDC/LDC2014T17
+  /data/sls/scratch/amali/data/GALE/LDC2013T17.tgz 
+  /data/sls/scratch/amali/data/GALE/LDC2013T04.tgz 
+  /data/sls/scratch/amali/data/GALE/LDC2014T17.tgz
 )
-fi
 
 galeData=GALE
 #prepare the data
@@ -53,7 +39,7 @@ galeData=GALE
 
 # You can run the script from here automatically, but it is recommended to run the data preparation,
 # and features extraction manually and and only once.
-# By copying and pasting into the shell.
+# By copying and pasting into your shell.
 
 #copy the audio files to local folder wav and convet flac files to wav
 local/gale_data_prep_audio.sh  "${audio[@]}" $galeData 
@@ -74,8 +60,8 @@ utils/prepare_lang.sh data/local/dict "<UNK>" data/local/lang data/lang
 # LM training
 local/gale_train_lms.sh
 
-# G compilation, check LG composition
 local/gale_format_data.sh 
+# G compilation, check LG composition
 
 # Now make MFCC features.
 # mfccdir should be some place with a largish disk where you
@@ -83,7 +69,6 @@ local/gale_format_data.sh
 mfccdir=mfcc
 
 for x in train test ; do
-  utils/fix_data_dir.sh data/$x # some files fail to get mfcc for many reasons
   steps/make_mfcc.sh --cmd "$train_cmd" --nj $nJobs \
     data/$x exp/make_mfcc/$x $mfccdir
   utils/fix_data_dir.sh data/$x # some files fail to get mfcc for many reasons
@@ -101,6 +86,7 @@ utils/subset_data_dir.sh data/train 10000 data/train.10K || exit 1;
 steps/train_mono.sh --nj 40 --cmd "$train_cmd" \
   data/train.10K data/lang exp/mono || exit 1;
 
+
 # Get alignments from monophone system.
 steps/align_si.sh --nj $nJobs --cmd "$train_cmd" \
   data/train data/lang exp/mono exp/mono_ali || exit 1;
@@ -110,10 +96,10 @@ steps/train_deltas.sh --cmd "$train_cmd" \
   2500 30000 data/train data/lang exp/mono_ali exp/tri1 || exit 1;
 
 # First triphone decoding
-time utils/mkgraph.sh data/lang_test exp/tri1 exp/tri1/graph || exit 1;
-time steps/decode.sh  --nj $nDecodeJobs --cmd "$decode_cmd" \
+utils/mkgraph.sh data/lang_test exp/tri1 exp/tri1/graph
+steps/decode.sh  --nj $nDecodeJobs --cmd "$decode_cmd" \
   exp/tri1/graph data/test exp/tri1/decode
-
+  
 steps/align_si.sh --nj $nJobs --cmd "$train_cmd" \
   data/train data/lang exp/tri1 exp/tri1_ali || exit 1;
 
@@ -122,98 +108,43 @@ steps/train_deltas.sh --cmd "$train_cmd" \
   3000 40000 data/train data/lang exp/tri1_ali exp/tri2a || exit 1;
 
 # tri2a decoding
-time utils/mkgraph.sh data/lang_test exp/tri2a exp/tri2a/graph || exit 1;
-time steps/decode.sh --nj $nDecodeJobs --cmd "$decode_cmd" \
+utils/mkgraph.sh data/lang_test exp/tri2a exp/tri2a/graph
+steps/decode.sh --nj $nDecodeJobs --cmd "$decode_cmd" \
   exp/tri2a/graph data/test exp/tri2a/decode
 
 # train and decode tri2b [LDA+MLLT]
 steps/train_lda_mllt.sh --cmd "$train_cmd" 4000 50000 \
   data/train data/lang exp/tri1_ali exp/tri2b || exit 1;
-time utils/mkgraph.sh data/lang_test exp/tri2b exp/tri2b/graph || exit 1;
-time steps/decode.sh --nj $nDecodeJobs --cmd "$decode_cmd" exp/tri2b/graph data/test exp/tri2b/decode
+
+utils/mkgraph.sh data/lang_test exp/tri2b exp/tri2b/graph
+steps/decode.sh --nj $nDecodeJobs --cmd "$decode_cmd" \
+  exp/tri2b/graph data/test exp/tri2b/decode
 
 # Align all data with LDA+MLLT system (tri2b)
 steps/align_si.sh --nj $nJobs --cmd "$train_cmd" \
   --use-graphs true data/train data/lang exp/tri2b exp/tri2b_ali  || exit 1;
 
-#  Do MMI on top of LDA+MLLT.
-steps/make_denlats.sh --nj $nJobs --cmd "$train_cmd" \
- data/train data/lang exp/tri2b exp/tri2b_denlats || exit 1;
-steps/train_mmi.sh --cmd "$train_cmd" data/train data/lang exp/tri2b_ali \
- exp/tri2b_denlats exp/tri2b_mmi || exit 1;
-
-steps/decode.sh  --iter 4 --nj $nJobs --cmd "$decode_cmd"  exp/tri2b/graph \
- data/test exp/tri2b_mmi/decode_it4
-steps/decode.sh  --iter 3 --nj $nJobs --cmd "$decode_cmd" exp/tri2b/graph \
- data/test exp/tri2b_mmi/decode_it3 # Do the same with boosting.
-
-steps/train_mmi.sh --cmd "$train_cmd" --boost 0.05 data/train data/lang exp/tri2b_ali \
-exp/tri2b_denlats exp/tri2b_mmi_b0.05 || exit 1;
-
-steps/decode.sh  --iter 4 --nj $nJobs --cmd "$decode_cmd" exp/tri2b/graph \
- data/test exp/tri2b_mmi_b0.05/decode_it4 || exit 1;
-steps/decode.sh  --iter 3 --nj $nJobs --cmd "$decode_cmd" exp/tri2b/graph \
- data/test exp/tri2b_mmi_b0.05/decode_it3 || exit 1;
-
-# Do MPE.
-steps/train_mpe.sh --cmd "$train_cmd" data/train data/lang exp/tri2b_ali exp/tri2b_denlats exp/tri2b_mpe || exit 1;
-
-steps/decode.sh  --iter 4 --nj $nDecodeJobs --cmd "$decode_cmd" exp/tri2b/graph \
- data/test exp/tri2b_mpe/decode_it4 || exit 1;
-
-steps/decode.sh  --iter 3 --nj $nDecodeJobs --cmd "$decode_cmd" exp/tri2b/graph \
- data/test exp/tri2b_mpe/decode_it3 || exit 1;
-
 
 # From 2b system, train 3b which is LDA + MLLT + SAT.
 steps/train_sat.sh --cmd "$train_cmd" \
   5000 100000 data/train data/lang exp/tri2b_ali exp/tri3b || exit 1;
-utils/mkgraph.sh data/lang_test exp/tri3b exp/tri3b/graph|| exit 1;
-steps/decode_fmllr.sh --nj $nDecodeJobs --cmd "$decode_cmd" \
-  exp/tri3b/graph data/test exp/tri3b/decode || exit 1;
+
+utils/mkgraph.sh data/lang_test exp/tri3b exp/tri3b/graph
+steps/decode_fmllr.sh --nj $nDecodeJobs --cmd \
+  "$decode_cmd" exp/tri3b/graph data/test exp/tri3b/decode
 
 # From 3b system, align all data.
 steps/align_fmllr.sh --nj $nJobs --cmd "$train_cmd" \
   data/train data/lang exp/tri3b exp/tri3b_ali || exit 1;
   
 
-## SGMM (subspace gaussian mixture model), excluding the "speaker-dependent weights"
-steps/train_ubm.sh --cmd "$train_cmd" 700 \
- data/train data/lang exp/tri3b_ali exp/ubm5a || exit 1;
- 
-steps/train_sgmm2.sh --cmd "$train_cmd" 5000 20000 data/train data/lang exp/tri3b_ali \
-  exp/ubm5a/final.ubm exp/sgmm_5a || exit 1;
+# train mmi mpe 
+local/run_mmi_mpe.sh &  # we keep it for completion but not getting the best results
 
-utils/mkgraph.sh data/lang_test exp/sgmm_5a exp/sgmm_5a/graph || exit 1;
+# train sgmm 
+local/run_sgmm.sh &    # we keep it for completion but not getting the best results
 
-steps/decode_sgmm2.sh --nj $nDecodeJobs --cmd "$decode_cmd" --config conf/decode.config \
-  --transform-dir exp/tri3b/decode exp/sgmm_5a/graph data/test exp/sgmm_5a/decode
-
-steps/align_sgmm2.sh --nj $nJobs --cmd "$train_cmd" --transform-dir exp/tri3b_ali \
-  --use-graphs true --use-gselect true data/train data/lang exp/sgmm_5a exp/sgmm_5a_ali || exit 1;
-
-## boosted MMI on SGMM
-steps/make_denlats_sgmm2.sh --nj $nJobs --sub-split 30 --beam 9.0 --lattice-beam 6 \
-  --cmd "$decode_cmd" --transform-dir \
-  exp/tri3b_ali data/train data/lang exp/sgmm_5a_ali exp/sgmm_5a_denlats || exit 1;
-  
-steps/train_mmi_sgmm2.sh --cmd "$train_cmd" --num-iters 8 --transform-dir exp/tri3b_ali --boost 0.1 \
-  data/train data/lang exp/sgmm_5a exp/sgmm_5a_denlats exp/sgmm_5a_mmi_b0.1
- 
-#decode GMM MMI
-utils/mkgraph.sh data/lang_test exp/sgmm_5a_mmi_b0.1 exp/sgmm_5a_mmi_b0.1/graph || exit 1;
-
-steps/decode_sgmm2.sh --nj $nDecodeJobs --cmd "$decode_cmd" --config conf/decode.config \
-  --transform-dir exp/tri3b/decode exp/sgmm_5a_mmi_b0.1/graph data/test exp/sgmm_5a_mmi_b0.1/decode
-  
-for n in 1 2 3 4; do
-  steps/decode_sgmm2_rescore.sh --cmd "$decode_cmd" --iter $n --transform-dir exp/tri3b/decode data/lang_test \
-    data/test exp/sgmm_5a_mmi_b0.1/decode exp/sgmm_5a_mmi_b0.1/decode$n
-  
-  steps/decode_sgmm_rescore.sh --cmd "$decode_cmd" --iter $n --transform-dir exp/tri3b/decode data/lang_test \
-    data/test exp/sgmm_5a/decode exp/sgmm_5a_mmi_onlyRescoreb0.1/decode$n
-done
-
+wait 
 local/nnet/run_dnn.sh
 
 time=$(date +"%Y-%m-%d-%H-%M-%S")
