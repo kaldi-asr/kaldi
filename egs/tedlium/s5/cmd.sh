a8539b577db6aaea97125c266a33c5cd64c3d8b9
Jan Trmal <jtrmal@gmail.com>
2015-05-28 21:02:15 +0000

    (trunk/egs) Adding improved slurm wrapper (being tested on  MARCC) and improvements of the TEDLIUM recipe to use scheduler-independent switches
    git-svn-id: https://svn.code.sf.net/p/kaldi/code/trunk@5096 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/tedlium/s5/cmd.sh
+++ egs/tedlium/s5/cmd.sh
@@ -15,13 +15,18 @@ export train_cmd="queue.pl -l arch=*64*"
 export decode_cmd="queue.pl -l arch=*64* -l ram_free=4G,mem_free=4G"
 export cuda_cmd="queue.pl -l arch=*64*,gpu=1 -q g.q"
 
-# BUT cluster:
-host=$(hostname)
+host=$(hostname -f)
 if [ ${host#*.} == "fit.vutbr.cz" ]; then
+  # BUT cluster:
   queue="all.q@@blade,all.q@@speech"
   gpu_queue="long.q@supergpu*,long.q@dellgpu*,long.q@pcspeech-gpu,long.q@pcgpu*"
   storage="matylda5"
   export train_cmd="queue.pl -q $queue -l ram_free=1500M,mem_free=1500M,${storage}=1"
   export decode_cmd="queue.pl -q $queue -l ram_free=2500M,mem_free=2500M,${storage}=0.5"
   export cuda_cmd="queue.pl -q $gpu_queue -l gpu=1" 
+elif [ ${host#*.} == "cm.cluster" ]; then
+  # MARCC bluecrab cluster:
+  export train_cmd="slurm.pl"
+  export decode_cmd="slurm.pl --mem 4G"
+  export cuda_cmd="slurm.pl" 
 fi
--- egs/tedlium/s5/run.sh
+++ egs/tedlium/s5/run.sh
@@ -58,22 +58,22 @@ fi
 
 # Train
 if [ $stage -le 3 ]; then
-  steps/train_mono.sh --nj 20 --cmd "$train_cmd" \
-    data/train_10kshort_nodup data/lang_nosp exp/mono0a || exit 1
+  #steps/train_mono.sh --nj 20 --cmd "$train_cmd" \
+  #  data/train_10kshort_nodup data/lang_nosp exp/mono0a || exit 1
 
-  steps/align_si.sh --nj $nj --cmd "$train_cmd" \
-    data/train data/lang_nosp exp/mono0a exp/mono0a_ali || exit 1
+  #steps/align_si.sh --nj $nj --cmd "$train_cmd" \
+  #  data/train data/lang_nosp exp/mono0a exp/mono0a_ali || exit 1
 
-  steps/train_deltas.sh --cmd "$train_cmd" \
-    2500 30000 data/train data/lang_nosp exp/mono0a_ali exp/tri1 || exit 1
+  #steps/train_deltas.sh --cmd "$train_cmd" \
+  #  2500 30000 data/train data/lang_nosp exp/mono0a_ali exp/tri1 || exit 1
 
-  utils/mkgraph.sh data/lang_nosp_test exp/tri1 exp/tri1/graph_nosp || exit 1
+  #utils/mkgraph.sh data/lang_nosp_test exp/tri1 exp/tri1/graph_nosp || exit 1
 
   steps/decode.sh --nj $decode_nj --cmd "$decode_cmd" \
-    --num-threads 4 --parallel-opts "-pe smp 4" \
+    --num-threads 4 --parallel-opts "--num-threads 4" \
     exp/tri1/graph_nosp data/dev exp/tri1/decode_nosp_dev || exit 1
   steps/decode.sh --nj $decode_nj --cmd "$decode_cmd" \
-    --num-threads 4 --parallel-opts "-pe smp 4" \
+    --num-threads 4 --parallel-opts "--num-threads 4" \
     exp/tri1/graph_nosp data/test exp/tri1/decode_nosp_test || exit 1
 fi
 
@@ -87,10 +87,10 @@ if [ $stage -le 4 ]; then
   utils/mkgraph.sh data/lang_nosp_test exp/tri2 exp/tri2/graph_nosp || exit 1
 
   steps/decode.sh --nj $decode_nj --cmd "$decode_cmd" \
-    --num-threads 4 --parallel-opts "-pe smp 4" \
+    --num-threads 4 --parallel-opts "--num-threads 4" \
     exp/tri2/graph_nosp data/dev exp/tri2/decode_nosp_dev || exit 1
   steps/decode.sh --nj $decode_nj --cmd "$decode_cmd" \
-    --num-threads 4 --parallel-opts "-pe smp 4" \
+    --num-threads 4 --parallel-opts "--num-threads 4" \
     exp/tri2/graph_nosp data/test exp/tri2/decode_nosp_test || exit 1
 fi
 
@@ -110,10 +110,10 @@ if [ $stage -le 5 ]; then
   utils/mkgraph.sh data/lang_test exp/tri2 exp/tri2/graph || exit 1
 
   steps/decode.sh --nj $decode_nj --cmd "$decode_cmd" \
-    --num-threads 4 --parallel-opts "-pe smp 4" \
+    --num-threads 4 --parallel-opts "--num-threads 4" \
     exp/tri2/graph data/dev exp/tri2/decode_dev || exit 1
   steps/decode.sh --nj $decode_nj --cmd "$decode_cmd" \
-    --num-threads 4 --parallel-opts "-pe smp 4" \
+    --num-threads 4 --parallel-opts "--num-threads 4" \
     exp/tri2/graph data/test exp/tri2/decode_test || exit 1
 fi
 
@@ -127,10 +127,10 @@ if [ $stage -le 6 ]; then
   utils/mkgraph.sh data/lang_test exp/tri3 exp/tri3/graph || exit 1
 
   steps/decode_fmllr.sh --nj $decode_nj --cmd "$decode_cmd" \
-    --num-threads 4 --parallel-opts "-pe smp 4" \
+    --num-threads 4 --parallel-opts "--num-threads 4" \
     exp/tri3/graph data/dev exp/tri3/decode_dev || exit 1
   steps/decode_fmllr.sh --nj $decode_nj --cmd "$decode_cmd" \
-    --num-threads 4 --parallel-opts "-pe smp 4" \
+    --num-threads 4 --parallel-opts "--num-threads 4" \
     exp/tri3/graph data/test exp/tri3/decode_test || exit 1
 fi
 
@@ -147,10 +147,10 @@ if [ $stage -le 7 ]; then
 
   for iter in 4; do
   steps/decode.sh --transform-dir exp/tri3/decode_dev --nj $decode_nj --cmd "$decode_cmd" --iter $iter \
-    --num-threads 4 --parallel-opts "-pe smp 4" \
+    --num-threads 4 --parallel-opts "--num-threads 4" \
     exp/tri3/graph data/dev exp/tri3_mmi_b0.1/decode_dev_it$iter || exit 1
   steps/decode.sh --transform-dir exp/tri3/decode_test --nj $decode_nj --cmd "$decode_cmd" --iter $iter \
-    --num-threads 4 --parallel-opts "-pe smp 4" \
+    --num-threads 4 --parallel-opts "--num-threads 4" \
     exp/tri3/graph data/test exp/tri3_mmi_b0.1/decode_test_it$iter || exit 1
   done
 fi
