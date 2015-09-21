6b73ca2452231a7cacacf16ca6729e1d642ec852
Vijayaditya Peddinti <vijayaditya.p@gmail.com>
2015-09-21 02:26:28 -0400

    changed default shrink value in lstm recipe.

--- egs/ami/s5/local/nnet3/run_lstm.sh
+++ egs/ami/s5/local/nnet3/run_lstm.sh
@@ -30,10 +30,12 @@ common_egs_dir=
 ng_per_element_scale_options=
 ng_affine_options=
 num_epochs=5
+
 # training options
 initial_effective_lrate=0.0003
 final_effective_lrate=0.00003
-shrink=0.0
+momentum=0.0
+shrink=1.0
 num_chunk_per_minibatch=100
 num_bptt_steps=20
 samples_per_iter=20000
@@ -47,8 +49,8 @@ echo "$0 $@"  # Print the command line for logging
 . ./utils/parse_options.sh
 
 if ! cuda-compiled; then
-  cat <<EOF && exit 1 
-This script is intended to be used with GPUs but you have not compiled Kaldi with CUDA 
+  cat <<EOF && exit 1
+This script is intended to be used with GPUs but you have not compiled Kaldi with CUDA
 If you want to use GPUs (and have them), go to src/, and configure and make on a machine
 where "nvcc" is installed.
 EOF
@@ -97,6 +99,7 @@ if [ $stage -le 8 ]; then
     --online-ivector-dir exp/$mic/nnet3/ivectors_${train_set}_hires \
     --cmvn-opts "--norm-means=false --norm-vars=false" \
     --initial-effective-lrate $initial_effective_lrate --final-effective-lrate $final_effective_lrate \
+    --momentum $momentum \
     --shrink $shrink \
     --cmd "$decode_cmd" \
     --num-lstm-layers $num_lstm_layers \
