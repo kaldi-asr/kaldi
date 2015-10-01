96148e39c05a351262398d11a27dd2e13c178102
Vimal Manohar <vimal.manohar91@gmail.com>
2015-10-01 14:30:16 -0400

    snr: Added programs matrix-scale and matrix-sum-cols useful for SNR prediction

---
+++ src/bin/matrix-scale.cc
@@ -0,0 +1,64 @@
+// bin/matrix-scale.cc
+
+// Copyright 2009-2011  Microsoft Corporation
+//                2014  Johns Hopkins University (author: Daniel Povey)
+//                2015  Vimal Manohar
+
+// See ../../COPYING for clarification regarding multiple authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+
+#include "base/kaldi-common.h"
+#include "util/common-utils.h"
+#include "matrix/kaldi-matrix.h"
+
+
+int main(int argc, char *argv[]) {
+  try {
+    using namespace kaldi;
+
+    const char *usage =
+        "Scale a set of matrices in a Table\n"
+        "Usage: matrix-scale [options] <in-rspecifier> <out-wspecifier>\n";
+
+    ParseOptions po(usage);
+    BaseFloat scale = 1.0;
+
+    po.Register("scale", &scale, "Scaling factor for matrices");
+    po.Read(argc, argv);
+
+    if (po.NumArgs() != 2) {
+      po.PrintUsage();
+      exit(1);
+    }
+
+    std::string rspecifier = po.GetArg(1);
+    std::string wspecifier = po.GetArg(2);
+
+    BaseFloatMatrixWriter mat_writer(wspecifier);
+
+    SequentialBaseFloatMatrixReader mat_reader(rspecifier);
+    for (; !mat_reader.Done(); mat_reader.Next()) {
+      Matrix<BaseFloat> mat(mat_reader.Value());
+      mat.Scale(scale);
+      mat_writer.Write(mat_reader.Key(), mat);
+    }
+    return 0;
+  } catch(const std::exception &e) {
+    std::cerr << e.what();
+    return -1;
+  }
+}
+
+
