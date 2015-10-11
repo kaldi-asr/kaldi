b59e7f03eccbe2c71e3c7361232401586e163c50
Vijayaditya Peddinti <vijayaditya.p@gmail.com>
2015-10-11 19:48:56 -0400

    Refined the options steps/nnet3/lstm/train.sh and added LSTM 8 epoch training results for swbd

--- egs/swbd/s5c/local/nnet3/run_lstm.sh
+++ egs/swbd/s5c/local/nnet3/run_lstm.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+# Copyright 2015  Johns Hopkins University (Author: Daniel Povey).
+#           2015  Vijayaditya Peddinti
+#           2015  Xingyu Na
+#           2015  Pegah Ghahrmani
+# Apache 2.0.
+
+
 # this is a basic lstm script
+# LSTM script runs for more epochs than the TDNN script
+# and each epoch takes twice the time
 
 # At this script level we don't support not running on GPU, as it would be painfully slow.
-# If you want to run without GPU you'd have to call lstm/train.sh with --gpu false,
-# --num-threads 16 and --minibatch-size 128.
-set -e
+# If you want to run without GPU you'd have to call lstm/train.sh with --gpu false
 
 stage=0
 train_stage=-10
+has_fisher=true
 use_sat_alignments=true
 affix=
+speed_perturb=true
+common_egs_dir=
+
+# LSTM options
 splice_indexes="-2,-1,0,1,2 0 0"
 label_delay=5
 num_lstm_layers=3
@@ -19,36 +31,40 @@ hidden_dim=1024
 recurrent_projection_dim=256
 non_recurrent_projection_dim=256
 chunk_width=20
-chunk_left_context=20
-clipping_threshold=5.0
+chunk_left_context=40
+clipping_threshold=30.0
 norm_based_clipping=true
-common_egs_dir=
-has_fisher=true
 
-# natural gradient options
-ng_per_element_scale_options=
-ng_affine_options=
-num_epochs=10
+num_epochs=8
+
 # training options
 initial_effective_lrate=0.0003
 final_effective_lrate=0.00003
-num_jobs_initial=1
-num_jobs_final=12
-shrink=0.99
-momentum=0.9
+num_jobs_initial=3
+num_jobs_final=15
+momentum=0.5
+adaptive_shrink=true
+shrink=0.98
 num_chunk_per_minibatch=100
-num_bptt_steps=20
+num_bptt_steps=
 samples_per_iter=20000
 remove_egs=true
+
+#decode options
+extra_left_context=
+frames_per_chunk=
+
 # End configuration section.
+
+echo "$0 $@"  # Print the command line for logging
 
 . cmd.sh
 . ./path.sh
 . ./utils/parse_options.sh
 
 if ! cuda-compiled; then
-  cat <<EOF && exit 1 
-This script is intended to be used with GPUs but you have not compiled Kaldi with CUDA 
+  cat <<EOF && exit 1
+This script is intended to be used with GPUs but you have not compiled Kaldi with CUDA
 If you want to use GPUs (and have them), go to src/, and configure and make on a machine
 where "nvcc" is installed.
 EOF
@@ -57,19 +73,24 @@ fi
 use_delay=false
 if [ $label_delay -gt 0 ]; then use_delay=true; fi
 
-dir=exp/nnet3/lstm${affix:+_$affix}${use_delay:+_ld$label_delay}
-if [ "$use_sat_alignments" == "true" ] ; then
-  gmm_dir=exp/tri4
-else
-  gmm_dir=exp/tri3
+suffix=
+if [ "$speed_perturb" == "true" ]; then
+  suffix=_sp
 fi
+dir=exp/nnet3/lstm
+dir=$dir${affix:+_$affix}${use_delay:+_ld$label_delay}
+dir=${dir}$suffix
+train_set=train_nodup$suffix
+ali_dir=exp/tri4_ali_nodup$suffix
 
-ali_dir=${gmm_dir}_ali_nodup
+local/nnet3/run_ivector_common.sh --stage $stage \
+  --speed-perturb $speed_perturb \
+  --generate-alignments false || exit 1;
 
-if [ $stage -le 7 ]; then
+if [ $stage -le 9 ]; then
   if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d $dir/egs/storage ]; then
     utils/create_split_dir.pl \
-     /export/b0{1,2,3,4}/$USER/kaldi-data/egs/swbd-$(date +'%m_%d_%H_%M')/s5c/$dir/egs/storage $dir/egs/storage
+     /export/b0{3,4,5,6}/$USER/kaldi-data/egs/swbd-$(date +'%m_%d_%H_%M')/s5/$dir/egs/storage $dir/egs/storage
   fi
 
   steps/nnet3/lstm/train.sh --stage $train_stage \
@@ -79,9 +100,12 @@ if [ $stage -le 7 ]; then
     --samples-per-iter $samples_per_iter \
     --splice-indexes "$splice_indexes" \
     --feat-type raw \
+    --online-ivector-dir exp/nnet3/ivectors_${train_set} \
     --cmvn-opts "--norm-means=false --norm-vars=false" \
     --initial-effective-lrate $initial_effective_lrate --final-effective-lrate $final_effective_lrate \
-    --shrink $shrink --momentum $momentum \
+    --momentum $momentum \
+    --adaptive-shrink "$adaptive_shrink" \
+    --shrink $shrink \
     --cmd "$decode_cmd" \
     --num-lstm-layers $num_lstm_layers \
     --cell-dim $cell_dim \
@@ -91,32 +115,36 @@ if [ $stage -le 7 ]; then
     --non-recurrent-projection-dim $non_recurrent_projection_dim \
     --chunk-width $chunk_width \
     --chunk-left-context $chunk_left_context \
-    --num-bptt-steps $num_bptt_steps \
+    --num-bptt-steps "$num_bptt_steps" \
     --norm-based-clipping $norm_based_clipping \
-    --ng-per-element-scale-options "$ng_per_element_scale_options" \
-    --ng-affine-options "$ng_affine_options" \
     --egs-dir "$common_egs_dir" \
     --remove-egs $remove_egs \
-    data/train_nodup data/lang $ali_dir $dir  || exit 1;
+    data/${train_set}_hires data/lang $ali_dir $dir  || exit 1;
 fi
 
-if [ $stage -le 8 ]; then
-  # this version of the decoding treats each utterance separately
-  # without carrying forward speaker information.
-  num_jobs=`cat data/eval2000/utt2spk|cut -d' ' -f2|sort -u|wc -l`
-  steps/nnet3/lstm/decode.sh --nj $num_jobs --cmd "$decode_cmd" \
-    --extra-left-context $chunk_left_context --frames-per-chunk $chunk_width \
-    $gmm_dir/graph_sw1_tg data/eval2000 \
-    $dir/decode_eval2000_sw1_tg || exit 1;
-
-  if $has_fisher; then
-    steps/lmrescore_const_arpa.sh --cmd "$decode_cmd" \
-      data/lang_sw1_{tg,fsh_fg} data/eval2000 \
-      $dir/decode_eval2000_sw1_{tg,fsh_fg} || exit 1;
+graph_dir=exp/tri4/graph_sw1_tg
+if [ $stage -le 10 ]; then
+  if [ -z $extra_left_context ]; then
+    extra_left_context=$chunk_left_context
   fi
-
+  if [ -z $frames_per_chunk ]; then
+    frames_per_chunk=$chunk_width
+  fi
+  for decode_set in train_dev eval2000; do
+      (
+      num_jobs=`cat data/${decode_set}_hires/utt2spk|cut -d' ' -f2|sort -u|wc -l`
+      steps/nnet3/lstm/decode.sh --nj 250 --cmd "$decode_cmd" \
+          --extra-left-context $extra_left_context  \
+          --frames-per-chunk "$frames_per_chunk" \
+          --online-ivector-dir exp/nnet3/ivectors_${decode_set} \
+         $graph_dir data/${decode_set}_hires $dir/decode_${decode_set}_sw1_tg || exit 1;
+      if $has_fisher; then
+          steps/lmrescore_const_arpa.sh --cmd "$decode_cmd" \
+            data/lang_sw1_{tg,fsh_fg} data/${decode_set}_hires \
+            $dir/decode_${decode_set}_sw1_{tg,fsh_fg} || exit 1;
+      fi
+      ) &
+  done
 fi
 wait;
-
 exit 0;
-
