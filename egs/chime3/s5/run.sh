c3399f99962b3946b7a519163afee15b31dffe1e
Shinji Watanabe <watanabe@merl.com>
2015-10-16 08:51:06 -0400

    chime3-egs: 1) Add Beamformit script in addition to the use of official enhanced speech 2) Modify/clean up scirpts    - using "stage" and so on. (following Karel's style)    - adding "cmd.sh execution"

--- egs/chime3/s5/run.sh
+++ egs/chime3/s5/run.sh
@@ -10,25 +10,53 @@
 . ./path.sh
 . ./cmd.sh 
 
+# Config:
+stage=0 # resume training with --stage=N
+
+. utils/parse_options.sh || exit 1;
+
 # You can execute run_init.sh only "once"
 # This creates LMs, basic task files, basic models, 
 # baseline results without speech enhancement techniques, and so on.
 # Please set a main root directory of the CHiME3 data
 # If you use kaldi scripts distributed in the CHiME3 data, 
-chime3_data=`pwd`/../.. 
+# chime3_data=`pwd`/../.. 
 # Otherwise, please specify it, e.g., 
-# chime3_data=/local_data/watanabe/work/201410CHiME3/CHiME3
-local/run_init.sh $chime3_data
+chime3_data=/remote/kiki/data2/watanabe/work/201410CHiME3/CHiME3
+if [ $stage -le 0 ]; then
+  local/run_init.sh $chime3_data
+fi
+
+# Using Beamformit
+# This results in better performance than the CHiME3 official beamforming
+# See Hori et al, "The MERL/SRI system for the 3rd CHiME challenge using beamforming, 
+# robust feature extraction, and advanced speech recognition,“ ASRU’15
+# note that beamformed wav files are generated in the following directory
+enhancement_method=beamformit_5ch
+enhancement_data=`pwd`/$enhancement_method
+if [ $stage -le 1 ]; then
+  ! hash BeamformIt && echo "Missing BeamformIt, run 'cd ../../../tools/; make beamformit;'" && exit 1
+  local/chime3_beamform.sh --cmd "$train_cmd" --nj 8 $chime3_data/data/audio/16kHz/isolated $enhancement_data
+fi
 
 # GMM based ASR experiment
 # Please set a directory of your speech enhancement method.
 # run_gmm.sh can be done every time when you change a speech enhancement technique.
 # The directory structure and audio files must follow the attached baseline enhancement directory
-enhancement_method=enhanced
-enhancement_data=$chime3_data/data/audio/16kHz/enhanced
-local/run_gmm.sh $enhancement_method $enhancement_data
+# if you want tou use the CHiME3 official enhanced data, please comment out the following
+# enhancement_method=enhanced
+# enhancement_data=$chime3_data/data/audio/16kHz/enhanced
+if [ $stage -le 2 ]; then
+  local/run_gmm.sh $enhancement_method $enhancement_data
+fi
+
+exit
 
 # DNN based ASR experiment
 # Since it takes time to evaluate DNN, we make the GMM and DNN scripts separately.
 # You may execute it after you would have promising results using GMM-based ASR experiments
-local/run_dnn.sh $enhancement_method $enhancement_data
+if [ $stage -le 3 ]; then
+  local/run_dnn.sh $enhancement_method $enhancement_data
+fi
+
+echo "Done."
