f37566db3139aff4f15d2077eb49eb864acebeea
Vimal Manohar <vimal.manohar91@gmail.com>
2015-10-29 19:39:05 -0400

    snr: Dnn SAD with quantized feat

--- egs/wsj_noisy/s5/local/snr/run_train_sad.sh
+++ egs/wsj_noisy/s5/local/snr/run_train_sad.sh
@@ -29,6 +29,8 @@ nj=40
 method=LogisticRegression
 splice_opts="--left-context=50 --right-context=50"
 max_param_change=1
+quantization_bins="-7.5:-2.5:2.5:7.5:12.5:17.5"
+feat_type=
 
 . cmd.sh
 . ./path.sh
@@ -112,7 +114,35 @@ EOF
   fi 
 fi
 
-if [ $stage -le 2 ]; then
+feats_opts="--feat-type raw"
+#if [ "$feat_type" == "sparse" ]; then
+#  if [ $stage -le 2 ]; then
+#    split_data.sh $datadir $nj
+#    mkdir -p $dir/quantized_data
+#
+#    $train_cmd JOB=1:$nj $dir/log/quantize_feats.JOB.log \
+#      quantize-feats scp:$datadir/split$nj/JOB/feats.scp $quantization_bins \
+#      ark,scp:$dir/quantized_data/feats.JOB.ark,$dir/quantized_data/feats.JOB.scp || exit 1
+#
+#    num_bins=`echo $quantization_bins | awk -F ':' '{print NF + 1}'` || exit 1
+#    feat_dim=`feat-to-dim scp:$datadir/feats.scp - 2>/dev/null` || exit 1
+#    sparse_input_dim=$[num_bins * feat_dim]
+#
+#    for n in `seq $nj`; do 
+#      cat $dir/quantized_data/feats.$n.scp
+#    done > $dir/quantized_data/feats.scp
+#
+#    echo $sparse_input_dim > $dir/quantized_data/sparse_input_dim
+#  fi
+#
+#  sparse_input_dim=`cat $dir/quantized_data/sparse_input_dim`
+#  feats_scp=$dir/quantized_data/feats.scp
+#  feats_opts="--feat-type $feat_type --feats-scp $feats_scp --sparse-input-dim $sparse_input_dim"
+#fi
+
+feats_opts="--feat-type $feat_type --quantization-bin-boundaries $quantization_bins"
+
+if [ $stage -le 3 ]; then
   case $method in
     "Gmm") 
       diarization/train_vad_gmm_supervised.sh \
@@ -136,7 +166,7 @@ if [ $stage -le 2 ]; then
       steps/nnet3/train_tdnn_raw.sh --stage $train_stage \
         --num-epochs $num_epochs --num-jobs-initial 1 --num-jobs-final 4 \
         --splice-indexes "$splice_indexes" --no-hidden-layers true --minibatch-size 512 \
-        --feat-type raw --egs-dir "$egs_dir" \
+        --egs-dir "$egs_dir" $feats_opts \
         --cmvn-opts "--norm-means=false --norm-vars=false" \
         --max-param-change $max_param_change \
         --initial-effective-lrate $initial_effective_lrate --final-effective-lrate $final_effective_lrate \
@@ -151,10 +181,10 @@ if [ $stage -le 2 ]; then
           /export/b0{3,4,5,6}/$USER/kaldi-data/egs/wsj_noisy-$(date +'%m_%d_%H_%M')/s5/$dir/egs/storage $dir/egs/storage
       fi
 
-      steps/nnet3/train_tdnn_raw.sh --stage $train_stage \
+      bash -x steps/nnet3/train_tdnn_raw.sh --stage $train_stage \
         --num-epochs $num_epochs --num-jobs-initial 2 --num-jobs-final 14 \
         --splice-indexes "$splice_indexes" \
-        --feat-type raw --egs-dir "$egs_dir" \
+        --egs-dir "$egs_dir" $feats_opts \
         --cmvn-opts "--norm-means=false --norm-vars=false" \
         --max-change-per-sample $max_change_per_sample \
         --initial-effective-lrate $initial_effective_lrate --final-effective-lrate $final_effective_lrate \
