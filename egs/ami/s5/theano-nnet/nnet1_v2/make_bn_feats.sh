43ce0f88ee6057bab9f9630f89f03a64d28e907f
Mallidi Sri Harish <qmallidi@svatava.fit.vutbr.cz>
2015-11-03 23:17:44 +0100

    small bug 	modified:   make_bn_feats.sh

--- egs/ami/s5/theano-nnet/nnet1_v2/make_bn_feats.sh
+++ egs/ami/s5/theano-nnet/nnet1_v2/make_bn_feats.sh
@@ -75,7 +75,8 @@ if [ $htk_save == false ]; then
     $nnet_fwdpass_tool $nnet_fwdpass_opts \
       --feat-preprocess=$bnfeadir/feat_preprocess.pkl \
       --utt2spk-file=$srcdata/utt2spk --cmvn-scp=$srcdata/cmvn.scp \
-      $bnfeadir $sdata/JOB/ | copy-feats ark,t:- ark,scp:$bnfeadir/raw_bnfea_${name}.JOB.ark,$bnfeadir/raw_bnfea_${name}.JOB.scp || exit 1;
+      $bnfeadir $sdata/JOB/ \| \
+      copy-feats ark,t:- ark,scp:$bnfeadir/raw_bnfea_${name}.JOB.ark,$bnfeadir/raw_bnfea_${name}.JOB.scp || exit 1;
   # concatenate the .scp files
   for ((n=1; n<=nj; n++)); do
     cat $bnfeadir/raw_bnfea_$name.$n.scp >> $data/feats.scp
