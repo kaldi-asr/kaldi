9dc32fc05295846b2628196a0b96a9dbf72c1d79
Daniel Povey <dpovey@gmail.com>
2015-12-10 23:55:50 -0500

    changes to make concat-feats less verbose when used with combine_short_segments.sh.

--- src/featbin/concat-feats.cc
+++ src/featbin/concat-feats.cc
@@ -26,7 +26,7 @@
 namespace kaldi {
 
 /*
-   This function concatenates several sets of feature vectors 
+   This function concatenates several sets of feature vectors
    to form a longer set. The length of the output will be equal
    to the sum of lengths of the inputs but the dimension will be
    the same to the inputs.
@@ -58,26 +58,26 @@ int main(int argc, char *argv[]) {
   try {
     using namespace kaldi;
     using namespace std;
-    
+
     const char *usage =
         "Concatenate feature files (assuming they have the same dimensions)\n"
         "Usage: concat-feats <in-rxfilename1> <in-rxfilename2> [<in-rxfilename3> ...] <out-wxfilename>\n"
         " e.g. concat-feats mfcc/foo.ark:12343 mfcc/foo.ark:56789 -\n"
         "See also: copy-feats, append-vector-to-feats, paste-feats\n";
-    
+
     ParseOptions po(usage);
 
     bool binary = true;
     po.Register("binary", &binary, "If true, output files in binary "
                 "(only relevant for single-file operation, i.e. no tables)");
-    
+
     po.Read(argc, argv);
-    
+
     if (po.NumArgs() < 3) {
       po.PrintUsage();
       exit(1);
     }
-    
+
     std::vector<Matrix<BaseFloat> > feats(po.NumArgs() - 1);
     for (int32 i = 1; i < po.NumArgs(); i++)
       ReadKaldiObject(po.GetArg(i), &(feats[i-1]));
@@ -85,9 +85,10 @@ int main(int argc, char *argv[]) {
     ConcatFeats(feats, &output);
     std::string output_wxfilename = po.GetArg(po.NumArgs());
     WriteKaldiObject(output, output_wxfilename, binary);
-    KALDI_LOG << "Wrote concatenated features to " << output_wxfilename;
-    return 0;
 
+    // This will tend to produce too much output if we have a logging mesage.
+    // KALDI_LOG << "Wrote concatenated features to " << output_wxfilename;
+    return 0;
   } catch(const std::exception &e) {
     std::cerr << e.what();
     return -1;
