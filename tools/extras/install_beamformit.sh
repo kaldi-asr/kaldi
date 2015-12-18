b20b8aa1b667210586fe77d49f128677a68b6954
vesis84 <vesis84@gmail.com>
2015-12-18 01:07:38 +0100

    beamformit: updating the build script,
    - fixing the case when 'libsnd' installed to local dirs

--- tools/extras/install_beamformit.sh
+++ tools/extras/install_beamformit.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-#Installs beamformit from the location https://github.com/xanguera/BeamformIt
+# Installs beamformit from the location https://github.com/xanguera/BeamformIt
 
 # libsndfile needed by beamformit,
-  wget http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.25.tar.gz
-  tar xvf libsndfile-1.0.25.tar.gz 
-  ( 
-  cd libsndfile-1.0.25;
-  ./configure --prefix=$PWD/libsndfile-1.0.25/
-  make
-  make install
-  );
+wget http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.25.tar.gz
+tar xvf libsndfile-1.0.25.tar.gz 
+( 
+cd libsndfile-1.0.25;
+./configure --prefix=$PWD
+make
+make install
+);
 
-  git clone https://github.com/xanguera/BeamformIt
-  (
-  cd BeamformIt
-  cmake . 
-  make
-  );
+# building beamformit,
+git clone https://github.com/xanguera/BeamformIt
+(
+cd BeamformIt
+git apply ../extras/beamformit.patch
+cmake -DLIBSND_INSTALL_DIR=$PWD/../libsndfile-1.0.25 .
+make
+);
