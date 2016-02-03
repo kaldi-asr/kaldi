8e2f1c809e4510772444ae6cd48cffdc9e4e7dc6
sw005320 <sw005320@gmail.com>
2016-02-02 20:16:49 -0500

    changed mode and fixed bugs.

--- egs/reverb/s5/local/get_results.sh
+++ egs/reverb/s5/local/get_results.sh
@@ -5,14 +5,14 @@
 
 # LDA-STC  fMLLR  MCT    DT     LM     MBR
 # No       No     No     No     BG     No
-./local/calc_wer.sh
+local/calc_wer.sh
 # No       No     Yes    No     BG     No
-./local/calc_wer.sh --am tri2a_mc
+local/calc_wer.sh --am tri2a_mc
 # No       Yes    Yes    No     BG     No
-./local/calc_wer.sh --am tri2a_mc --decode basis_fmllr
+local/calc_wer.sh --am tri2a_mc --decode basis_fmllr
 # Yes      Yes    Yes    No     TG     No
-./local/calc_wer.sh --am tri2b_mc --lm tg_5k --decode basis_fmllr
+local/calc_wer.sh --am tri2b_mc --lm tg_5k --decode basis_fmllr
 # Yes      Yes    Yes    Yes    TG     No
-./local/calc_wer.sh --am tri2b_mc_mmi_b0.1 --lm tg_5k --decode basis_fmllr
+local/calc_wer.sh --am tri2b_mc_mmi_b0.1 --lm tg_5k --decode basis_fmllr
 # Yes      Yes    Yes    Yes    TG     Yes
-./local/calc_wer.sh --am tri2b_mc_mmi_b0.1 --lm tg_5k --decode mbr_basis_fmllr
+local/calc_wer.sh --am tri2b_mc_mmi_b0.1 --lm tg_5k --decode mbr_basis_fmllr
