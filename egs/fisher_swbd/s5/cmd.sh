58252ae90170e580d2a86ea26f8569a702c16b6c
Ke Li <keli1@t01.clsp.jhu.edu>
2016-02-06 15:45:36 -0500

    modified cmd.sh again

--- egs/fisher_swbd/s5/cmd.sh
+++ egs/fisher_swbd/s5/cmd.sh
@@ -5,20 +5,18 @@
 # (make sure your --num-jobs options are no more than
 # the number of cpus on your machine.
 
-# Default opts,
-export train_cmd="queue.pl -l arch=*64* --mem 4G"
-export decode_cmd="queue.pl -l arch=*64* --mem 4G"
-export cuda_cmd=run.pl # Run on local machine,
-export mkgraph_cmd="queue.pl -l arch=*64* --mem 4G"
+# you can change cmd.sh depending on what type of queue you are using.
+# If you have no queueing system and want to run on a local machine, you
+# can change all instances 'queue.pl' to run.pl (but be careful and run 
+# commands one by one: most recipes will exhaust the memory on your
+# machine). queue.pl works with GridEngine (qsub). slurm.pl works
+# with slurm. Different queues are configured differently, with different
+# queue names and different ways of specifying things like memory;
+# to account for these differences you can create and edit the file
+# conf/queue.conf to match your queue's configuration. Search for
+# conf/queue.conf in http://kaldi-asr.org/doc/queue.html for more information,
+# or search for the string 'default_config' in utils/queue.pl or utils/slurm.pl.
 
-# BUT options,
-if [ "$(hostname -d)" == "fit.vutbr.cz" ]; then
-  # BUT cluster:
-  queue="all.q@@blade,all.q@@speech"
-  gpu_queue="long.q@supergpu*,long.q@dellgpu*,long.q@pcspeech-gpu,long.q@pcgpu*"
-  storage="matylda5"
-  export train_cmd="queue.pl -q $queue -l ram_free=1.5G,mem_free=1.5G,${storage}=0.25"
-  export decode_cmd="queue.pl -q $queue -l ram_free=2.5G,mem_free=2.5G,${storage}=0.1"
-  export cuda_cmd="queue.pl -q $gpu_queue -l gpu=1"
-fi 
-
+export train_cmd="queue.pl --mem 4G"
+export decode_cmd="queue.pl --mem 4G"
+export mkgraph_cmd="queue.pl --mem 8G"
