d093ec0f02d822aa8c9dc1d215a4fa67f9c03db3
Nicolás Serrano Martínez-Santos <nicolass@speechmatics.com>
2016-02-15 15:49:07 +0000

    Kaldified source code and added a system comparison script

--- src/bin/compute-wer-confidence.cc
+++ src/bin/compute-wer-confidence.cc
@@ -29,15 +29,14 @@
 
 namespace kaldi {
 
-void GetEditionsSingleHyp( const std::string &hyp_rspecifier, 
-        const std::string &ref_rspecifier,
-        const std::string &mode,
-        std::vector<std::pair<int32, int32> > & edit_word_per_hyp) {
+void GetEditsSingleHyp( const std::string &hyp_rspecifier, 
+      const std::string &ref_rspecifier,
+      const std::string &mode,
+      std::vector<std::pair<int32, int32> > & edit_word_per_hyp) {
     
     // Both text and integers are loaded as vector of strings,
     SequentialTokenVectorReader ref_reader(ref_rspecifier);
     RandomAccessTokenVectorReader hyp_reader(hyp_rspecifier);
-
     int32 num_words = 0, word_errs = 0, num_ins = 0, num_del = 0, num_sub = 0;
     
     // Main loop, store WER stats per hyp,
@@ -55,24 +54,24 @@ void GetEditionsSingleHyp( const std::string &hyp_rspecifier,
         hyp_sent = hyp_reader.Value(key);
       }
       num_words = ref_sent.size();
-      word_errs = LevenshteinEditDistance(ref_sent, hyp_sent, &num_ins, &num_del, &num_sub);
+      word_errs = LevenshteinEditDistance(ref_sent, hyp_sent, 
+                                            &num_ins, &num_del, &num_sub);
       edit_word_per_hyp.push_back(std::pair<int32, int32>(word_errs, num_words));
     }
-
 }
 
-void GetEditionsDualHyp(const std::string &hyp_rspecifier, 
-        const std::string &hyp_rspecifier2, 
-        const std::string &ref_rspecifier,
-        std::vector<std::pair<int32, int32> > & edit_word_per_hyp,
-        std::vector<std::pair<int32, int32> > & edit_word_per_hyp2) {
+void GetEditsDualHyp(const std::string &hyp_rspecifier, 
+      const std::string &hyp_rspecifier2, 
+      const std::string &ref_rspecifier,
+      std::vector<std::pair<int32, int32> > & edit_word_per_hyp,
+      std::vector<std::pair<int32, int32> > & edit_word_per_hyp2) {
     
     // Both text and integers are loaded as vector of strings,
     SequentialTokenVectorReader ref_reader(ref_rspecifier);
     RandomAccessTokenVectorReader hyp_reader(hyp_rspecifier);
     RandomAccessTokenVectorReader hyp_reader2(hyp_rspecifier2);
-
-    int32 num_words = 0, word_errs = 0, num_ins = 0, num_del = 0, num_sub = 0;
+    int32 num_words = 0, word_errs = 0, 
+            num_ins = 0, num_del = 0, num_sub = 0;
     
     // Main loop, store WER stats per hyp,
     for (; !ref_reader.Done(); ref_reader.Next()) {
@@ -88,72 +87,62 @@ void GetEditionsDualHyp(const std::string &hyp_rspecifier,
       }
       num_words = ref_sent.size();
 
-      word_errs = LevenshteinEditDistance(ref_sent, hyp_sent, &num_ins, &num_del, &num_sub);
+      word_errs = LevenshteinEditDistance(ref_sent, hyp_sent, 
+                                            &num_ins, &num_del, &num_sub);
       edit_word_per_hyp.push_back(std::pair<int32, int32>(word_errs, num_words));
 
-      word_errs = LevenshteinEditDistance(ref_sent, hyp_sent2, &num_ins, &num_del, &num_sub);
+      word_errs = LevenshteinEditDistance(ref_sent, hyp_sent2, 
+                                            &num_ins, &num_del, &num_sub);
       edit_word_per_hyp2.push_back(std::pair<int32, int32>(word_errs, num_words));
     }
 
 }
 
-void GetBootstrapWERInterval(const std::vector<std::pair<int32, int32> > & edit_word_per_hyp,
-        int32 replications, 
-        BaseFloat *mean, BaseFloat *interval) {
+void GetBootstrapWERInterval(
+      const std::vector<std::pair<int32, int32> > & edit_word_per_hyp,
+      int32 replications, 
+      BaseFloat *mean, BaseFloat *interval) {
     BaseFloat wer_accum = 0.0, wer_mult_accum = 0.0;
 
     for (int32 i = 0; i <= replications; ++i) {
-
-        int32 num_words = 0, word_errs = 0;
-
-        for (int32 j = 0; j <= edit_word_per_hyp.size(); ++j) {
-            int32 random_pos = kaldi::RandInt(0, edit_word_per_hyp.size());
-
-            word_errs += edit_word_per_hyp[random_pos].first;
-            num_words += edit_word_per_hyp[random_pos].second;
+      int32 num_words = 0, word_errs = 0;
+      for (int32 j = 0; j <= edit_word_per_hyp.size(); ++j) {
+        int32 random_pos = kaldi::RandInt(0, edit_word_per_hyp.size());
+        word_errs += edit_word_per_hyp[random_pos].first;
+        num_words += edit_word_per_hyp[random_pos].second;
         }
 
-        BaseFloat wer_rep = static_cast<BaseFloat>(word_errs) / num_words;
-        wer_accum += wer_rep;
-        wer_mult_accum += wer_rep*wer_rep;
-
+      BaseFloat wer_rep = static_cast<BaseFloat>(word_errs) / num_words;
+      wer_accum += wer_rep;
+      wer_mult_accum += wer_rep*wer_rep;
     }
 
     // Compute mean WER and std WER
     *mean = wer_accum / (replications-1);
-    *interval = 1.96*sqrt(wer_mult_accum / (replications-1) - (*mean)*(*mean));
-
+    *interval = 1.96*sqrt(wer_mult_accum/(replications-1)-(*mean)*(*mean));
 }
 
 void GetBootstrapWERTwoSystemComparison(
-        const std::vector<std::pair<int32, int32> > & edit_word_per_hyp,
-        const std::vector<std::pair<int32, int32> > & edit_word_per_hyp2,
-        int32 replications, BaseFloat *p_improv) {
-
+      const std::vector<std::pair<int32, int32> > & edit_word_per_hyp,
+      const std::vector<std::pair<int32, int32> > & edit_word_per_hyp2,
+      int32 replications, BaseFloat *p_improv) {
     int32 improv_accum = 0.0;
 
     for (int32 i = 0; i <= replications; ++i) {
-
-        int32 word_errs = 0;
-
-        for (int32 j = 0; j <= edit_word_per_hyp.size(); ++j) {
-            int32 random_pos = kaldi::RandInt(0, edit_word_per_hyp.size());
-
-            word_errs += edit_word_per_hyp[random_pos].first - 
-                            edit_word_per_hyp2[random_pos].first;
+      int32 word_errs = 0;
+      for (int32 j = 0; j <= edit_word_per_hyp.size(); ++j) {
+        int32 random_pos = kaldi::RandInt(0, edit_word_per_hyp.size());
+        word_errs += edit_word_per_hyp[random_pos].first - 
+                        edit_word_per_hyp2[random_pos].first;
         }
-
-        if(word_errs > 0)
-            ++improv_accum;
-
+      if(word_errs > 0)
+        ++improv_accum;
     }
-
     // Compute mean WER and std WER
     *p_improv = static_cast<BaseFloat>(improv_accum) / (replications-1);
-
 }
 
-}
+} //namespace kaldi
 
 int main(int argc, char *argv[]) {
   using namespace kaldi;
@@ -161,17 +150,18 @@ int main(int argc, char *argv[]) {
 
   try {
     const char *usage =
-        "Compute a bootstrapping of WER to extract the 95\% confidence interval\n"
-        "Take a reference and a transcription file, in integer or text format,\n"
-        "and outputs overall WER statistics to standard output along with its confidence interval\n"
-        "using the bootstrap methos of Bisani and Ney\n"
-        "If a second transcription file corresponding to the same reference is provided, a\n"
-        "bootstrap comparison of the two transcription is performed to estimate the probability\n"
-        "of improvement.\n"
-        "\n"
-        "Usage: compute-wer-confidence [options] <ref-rspecifier> <hyp-rspecifier> [<hyp2-rspecifier>]\n"
-        "E.g.: compute-wer-confidence --replications=10000 --mode=present ark:data/train/text ark:hyp_text\n"
-        "See also: compute-wer\n";
+      "Compute a bootstrapping of WER to extract the 95\% confidence interval.\n"
+      "Take a reference and a transcription file, in integer or text format,\n"
+      "and outputs overall WER statistics to standard output along with its\n"
+      "confidence interval using the bootstrap methos of Bisani and Ney.\n"
+      "If a second transcription file corresponding to the same reference is\n" 
+      "provided, a bootstrap comparison of the two transcription is performed\n"
+      "to estimate the probability of improvement.\n"
+      "\n"
+      "Usage: compute-wer-confidence [options] <ref-rspecifier> <hyp-rspecifier> [<hyp2-rspecifier>]\n"
+      "E.g.: compute-wer-confidence --mode=present ark:data/train/text ark:hyp_text\n"
+      "or compute-wer-confidence ark:data/train/text ark:hyp_text ark:hyp_text2\n"
+      "See also: compute-wer\n";
 
     ParseOptions po(usage);
 
@@ -183,11 +173,12 @@ int main(int argc, char *argv[]) {
                 "  \"strict\" means die if all in ref not also in hyp");
 
     int32 replications = 10000;
-    po.Register("replications", &replications, "Number of replications to compute the intervals");
+    po.Register("replications", &replications, 
+            "Number of replications to compute the intervals");
 
     po.Read(argc, argv);
 
-    if (po.NumArgs() < 2 && po.NumArgs() > 3) {
+    if (po.NumArgs() < 2 || po.NumArgs() > 3) {
       po.PrintUsage();
       exit(1);
     }
@@ -197,41 +188,52 @@ int main(int argc, char *argv[]) {
     std::string hyp2_rspecifier = (po.NumArgs() == 3?po.GetArg(3):"");
 
     if (mode != "strict" && mode != "present" && mode != "all") {
-      KALDI_ERR << "--mode option invalid: expected \"present\"|\"all\"|\"strict\", got "
-                << mode;
+      KALDI_ERR << 
+          "--mode option invalid: expected \"present\"|\"all\"|\"strict\", got "
+          << mode;
+    }
+
+    if (mode != "strict" && !hyp2_rspecifier.empty()){
+      KALDI_WARN << 
+          "--mode is always assumed strict if a second transcription is provided\n";
     }
 
     //Get editions per each utterance
     std::vector<std::pair<int32, int32> > edit_word_per_hyp, edit_word_per_hyp2;
     if(hyp2_rspecifier.empty())
-        GetEditionsSingleHyp(hyp_rspecifier, ref_rspecifier, mode, edit_word_per_hyp);
+      GetEditsSingleHyp(hyp_rspecifier, ref_rspecifier, mode, edit_word_per_hyp);
     else
-        GetEditionsDualHyp(hyp_rspecifier, hyp2_rspecifier, ref_rspecifier, 
-                edit_word_per_hyp, edit_word_per_hyp2);
+      GetEditsDualHyp(hyp_rspecifier, hyp2_rspecifier, ref_rspecifier, 
+              edit_word_per_hyp, edit_word_per_hyp2);
 
-    //Extract WER for a number of replications of the same size as the hypothesis extracted
-    BaseFloat mean_wer = 0.0, interval = 0.0, mean_wer2 = 0.0, interval2 = 0.0, p_improv = 0.0;
+    //Extract WER for a number of replications of the same size 
+    //as the hypothesis extracted
+    BaseFloat mean_wer = 0.0, interval = 0.0, 
+              mean_wer2 = 0.0, interval2 = 0.0, 
+              p_improv = 0.0;
 
-    GetBootstrapWERInterval(edit_word_per_hyp, replications, &mean_wer, &interval);
+    GetBootstrapWERInterval(edit_word_per_hyp, replications, 
+            &mean_wer, &interval);
 
     if(!hyp2_rspecifier.empty()) {
-        GetBootstrapWERInterval(edit_word_per_hyp2, replications, &mean_wer2, &interval2);
+      GetBootstrapWERInterval(edit_word_per_hyp2, replications, 
+              &mean_wer2, &interval2);
 
-        GetBootstrapWERTwoSystemComparison(edit_word_per_hyp, edit_word_per_hyp2,
-                replications, &p_improv);
+      GetBootstrapWERTwoSystemComparison(edit_word_per_hyp, edit_word_per_hyp2,
+             replications, &p_improv);
     }
 
     // Print the output,
     std::cout.precision(2);
     std::cerr.precision(2);
-    std::cout << "Set1: %WER " << std::fixed << 100*mean_wer << " 95\% Conf Interval [ " << 
-              100*mean_wer-100*interval << ", " << 100*mean_wer+100*interval
-              << " ]" << '\n'; 
+    std::cout << "Set1: %WER " << std::fixed << 100*mean_wer << 
+              " 95\% Conf Interval [ " << 100*mean_wer-100*interval << 
+              ", " << 100*mean_wer+100*interval << " ]" << '\n'; 
 
     if(!hyp2_rspecifier.empty()) {
-        std::cout << "Set2: %WER " << std::fixed << 100*mean_wer2 << " 95\% Conf Interval [ " << 
-                  100*mean_wer2-100*interval2 << ", " << 100*mean_wer2+100*interval2
-                  << " ]" << '\n';
+        std::cout << "Set2: %WER " << std::fixed << 100*mean_wer2 << 
+            " 95\% Conf Interval [ " << 100*mean_wer2-100*interval2 << 
+            ", " << 100*mean_wer2+100*interval2 << " ]" << '\n';
 
         std::cout << "Probability of Set2 improving Set1: " << std::fixed << 
             100*p_improv << '\n';
