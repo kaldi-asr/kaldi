075e471fdb57b169b10fd301fef458656ef57624
Vijayaditya Peddinti <vijayaditya.p@gmail.com>
2016-02-22 06:07:16 -0500

    steps/nnet3/report: Added support for comparing multiple experiments

--- egs/wsj/s5/steps/nnet3/report/generate_plots.py
+++ egs/wsj/s5/steps/nnet3/report/generate_plots.py
@@ -38,15 +38,151 @@ logger.info('Generating plots')
 
 
 def GetArgs():
-    parser = argparse.ArgumentParser(description="Parses the training logs and generates a variety of plots.")
+    parser = argparse.ArgumentParser(description="""
+Parses the training logs and generates a variety of plots.
+example : steps/nnet3/report/generate_plots.py --comparison-dir exp/nnet3/tdnn1 --comparison-dir exp/nnet3/tdnn2 exp/nnet3/tdnn exp/nnet3/tdnn/report
+""")
+    parser.add_argument("--comparison-dir", type=str, action='append', help="other experiment directories for comparison. These will only be used for plots, not tables")
+    parser.add_argument("--start-iter", type=int, help="Iteration from which plotting will start", default = 1)
     parser.add_argument("exp_dir", help="experiment directory, e.g. exp/nnet3/tdnn")
     parser.add_argument("output_dir", help="experiment directory, e.g. exp/nnet3/tdnn/report")
 
     args = parser.parse_args()
-
+    if args.comparison_dir is not None and len(args.comparison_dir) > 6:
+        raise Exception("max 6 --comparison-dir options can be specified. If you want to compare with more comparison_dir, you would have to carefully tune the plot_colors variable which specified colors used for plotting.")
+    assert(args.start_iter >= 1)
     return args
 
-def GeneratePlots(exp_dir, output_dir):
+plot_colors = ['red', 'blue', 'green', 'black', 'magenta', 'yellow', 'cyan' ]
+
+def GenerateAccuracyPlots(exp_dir, output_dir, plot, key = 'accuracy', file_basename = 'accuracy', comparison_dir = None, start_iter = 1):
+    assert(start_iter >= 1)
+
+    if plot:
+        plt.figure()
+        plots = []
+
+    comparison_dir = [] if comparison_dir is None else comparison_dir
+    dirs = [exp_dir] + comparison_dir
+    index = 0
+    for dir in dirs:
+        [accuracy_report, accuracy_times, accuracy_data] = nlp.GenerateAccuracyReport(dir, key)
+        if index == 0:
+            # this is the main experiment directory
+            acc_file = open("{0}/{1}.log".format(output_dir, file_basename), "w")
+            acc_file.write(accuracy_report)
+            acc_file.close()
+
+        if plot:
+            color_val = plot_colors[index]
+            data = np.array(accuracy_data)
+            data = data[data[:,0]>=start_iter, :]
+            plot_handle, = plt.plot(data[:, 0], data[:, 1], color = color_val, linestyle = "--", label = "train {0}".format(dir))
+            plots.append(plot_handle)
+            plot_handle, = plt.plot(data[:, 0], data[:, 2], color = color_val, label = "valid {0}".format(dir))
+            plots.append(plot_handle)
+        index += 1
+    if plot:
+        plt.xlabel('Iteration')
+        plt.ylabel(key)
+        lgd = plt.legend(handles=plots, loc='lower center', bbox_to_anchor=(0.5, -0.2 + len(dirs) * -0.1 ), ncol=1, borderaxespad=0.)
+        plt.grid(True)
+        plt.savefig('{0}/{1}.pdf'.format(output_dir, file_basename), bbox_extra_artists=(lgd,), bbox_inches='tight')
+
+def GenerateNonlinStatsPlots(exp_dir, output_dir, plot, comparison_dir = None, start_iter = 1):
+    assert(start_iter >= 1)
+
+    comparison_dir = [] if comparison_dir is None else comparison_dir
+    dirs = [exp_dir] + comparison_dir
+    index = 0
+    stats_per_dir = {}
+
+    for dir in dirs:
+        stats_per_component_per_iter = nlp.ParseProgressLogsForNonlinearityStats(dir)
+        stats_per_dir[dir] = stats_per_component_per_iter
+
+    # convert the nonlin stats into tables
+    stat_tables_per_component_per_dir = {}
+    for dir in dirs:
+        stats_per_component_per_iter = stats_per_dir[dir]
+        component_names = stats_per_component_per_iter.keys()
+        stat_tables_per_component = {}
+        for component_name in component_names:
+            comp_data = stats_per_component_per_iter[component_name]
+            comp_type = comp_data['type']
+            comp_stats = comp_data['stats']
+            iters = comp_stats.keys()
+            iters.sort()
+            iter_stats = []
+            for iter in iters:
+                iter_stats.append([iter] + comp_stats[iter])
+            stat_tables_per_component[component_name] = iter_stats
+        stat_tables_per_component_per_dir[dir] = stat_tables_per_component
+
+    main_stat_tables = stat_tables_per_component_per_dir[exp_dir]
+    for component_name in main_stat_tables.keys():
+        # this is the main experiment directory
+        file = open("{dir}/nonlinstats_{comp_name}.log".format(dir = output_dir, comp_name = component_name), "w")
+        file.write("Iteration\tValueMean\tValueStddev\tDerivMean\tDerivStddev\n")
+        iter_stat_report = ""
+        iter_stats = main_stat_tables[component_name]
+        for row in iter_stats:
+            iter_stat_report += "\t".join(map(lambda x: str(x), row)) + "\n"
+        file.write(iter_stat_report)
+        file.close()
+
+    if plot:
+        main_component_names = main_stat_tables.keys()
+        main_component_names.sort()
+
+        plot_component_names = set(main_component_names)
+        for dir in dirs:
+            component_names = set(stats_per_dir[dir].keys())
+            plot_component_names = plot_component_names.intersection(component_names)
+        plot_component_names = list(plot_component_names)
+        plot_component_names.sort()
+        if plot_component_names != main_component_names:
+            logger.warning("The components in all the neural networks in the given experiment dirs are not the same, so comparison plots are provided only for common component names. Make sure that these are comparable experiments before analyzing these plots.")
+
+        fig = plt.figure()
+        for component_name in main_component_names:
+            fig.clf()
+            index = 0
+            plots = []
+            for dir in dirs:
+                color_val = plot_colors[index]
+                index += 1
+                try:
+                    iter_stats = stat_tables_per_component_per_dir[dir][component_name]
+                except KeyError:
+                    # this component is not available in this network so lets not just plot it
+                    continue
+
+                data = np.array(iter_stats)
+                data = data[data[:,0] >=start_iter, :]
+                ax = plt.subplot(211)
+                mp, = ax.plot(data[:,0], data[:,1], color=color_val, label="Mean {0}".format(dir))
+                msph, = ax.plot(data[:,0], data[:,1] + data[:,2], color=color_val, linestyle='--', label = "Mean+-Stddev {0}".format(dir))
+                mspl, = ax.plot(data[:,0], data[:,1] - data[:,2], color=color_val, linestyle='--')
+                plots.append(mp)
+                plots.append(msph)
+                ax.set_ylabel('Mean-{0}'.format(comp_type))
+                ax.grid(True)
+
+                ax = plt.subplot(212)
+                mp, = ax.plot(data[:,0], data[:,3], color=color_val)
+                msph, = ax.plot(data[:,0], data[:,3] + data[:,4], color=color_val, linestyle='--')
+                mspl, = ax.plot(data[:,0], data[:,3] - data[:,4], color=color_val, linestyle='--')
+                ax.set_xlabel('Iteration')
+                ax.set_ylabel('Derivative-{0}'.format(comp_type))
+                ax.grid(True)
+
+            lgd = plt.legend(handles=plots, loc='lower center', bbox_to_anchor=(0.5, -0.5 + len(dirs) * -0.2 ), ncol=1, borderaxespad=0.)
+            plt.grid(True)
+            fig.suptitle("Mean and Standard deviation of the value and derivative {comp_name}".format(comp_name = component_name), fontsize = 25)
+            fig.savefig('{dir}/nonlinstats_{comp_name}.pdf'.format(dir = output_dir, comp_name = component_name), bbox_extra_artists=(lgd,), bbox_inches='tight')
+
+def GeneratePlots(exp_dir, output_dir, comparison_dir = None, start_iter = 1):
     try:
         os.makedirs(output_dir)
     except OSError as e:
@@ -56,74 +192,12 @@ def GeneratePlots(exp_dir, output_dir):
             raise e
 
     logger.info("Generating accuracy plots")
-    [accuracy_report, accuracy_times, accuracy_data] = nlp.GenerateAccuracyReport(exp_dir, "accuracy")
-    acc_file = open("{0}/accuracy.log".format(output_dir), "w")
-    acc_file.write(accuracy_report)
-    acc_file.close()
-    if plot:
-        data = np.array(accuracy_data)
-        plt.clf()
-        plt.plot(data[:, 0], data[:, 1], 'r', data[:, 0], data[:, 2], 'b')
-        plt.xlabel('Iteration')
-        plt.ylabel('Accuracy')
-        plt.grid(True)
-        plt.savefig('{0}/accuracy.pdf'.format(output_dir))
-
+    GenerateAccuracyPlots(exp_dir, output_dir, plot, key = 'accuracy', file_basename = 'accuracy', comparison_dir = comparison_dir, start_iter = start_iter)
     logger.info("Generating log-likelihood plots")
-    [loglike_report, loglike_times, loglike_data] = nlp.GenerateAccuracyReport(exp_dir, "log-likelihood")
-    ll_file = open("{0}/loglikelihood.log".format(output_dir), "w")
-    ll_file.write(loglike_report)
-    if plot:
-        data = np.array(loglike_data)
-        plt.clf()
-        plt.plot(data[:, 0], data[:, 1], 'r', data[:, 0], data[:, 2], 'b')
-        plt.xlabel('Iteration')
-        plt.ylabel('Log-likelihood')
-        plt.grid(True)
-        plt.savefig('{0}/loglikelihood.pdf'.format(output_dir))
+    GenerateAccuracyPlots(exp_dir, output_dir, plot, key = 'log-likelihood', file_basename = 'loglikelihood', comparison_dir = comparison_dir, start_iter = start_iter)
 
     logger.info("Generating non-linearity stats plots")
-    stats_per_component_per_iter = nlp.ParseProgressLogsForNonlinearityStats(exp_dir)
-    component_names = stats_per_component_per_iter.keys()
-    for component_name in component_names:
-        comp_data = stats_per_component_per_iter[component_name]
-        comp_type = comp_data['type']
-        comp_stats = comp_data['stats']
-        #stats are stored as [value_mean, value_stddev, deriv_mean, deriv_stddev]
-        file = open("{dir}/nonlinstats_{comp_name}.log".format(dir = output_dir, comp_name = component_name), "w")
-        file.write("Iteration\tValueMean\tValueStddev\tDerivMean\tDerivStddev\n")
-        iters = comp_stats.keys()
-        iters.sort()
-        iter_stats = []
-        iter_stat_report = ""
-        for iter in iters:
-            iter_stats.append([iter] + comp_stats[iter])
-            iter_stat_report += "\t".join(map(lambda x: str(x), iter_stats[-1])) + "\n"
-        file.close()
-
-        if plot:
-            data = np.array(iter_stats)
-            plt.clf()
-            ax = plt.subplot(211)
-            mp, = ax.plot(data[:,0], data[:,1], color='blue', label="Mean")
-            msph, = ax.plot(data[:,0], data[:,1] + data[:,2], color='red', label = "Mean+Stddev")
-            mspl, = ax.plot(data[:,0], data[:,1] - data[:,2], color='red', label = "Mean-Stddev")
-            lgd = plt.legend(handles=[mp, msph, mspl])
-            plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='upper center',
-                               ncol=3, mode="expand", borderaxespad=0.)
-
-            ax.set_ylabel('Mean-{0}'.format(comp_type))
-            ax.grid(True)
-
-            ax = plt.subplot(212)
-            mp, = ax.plot(data[:,0], data[:,3], color='blue', label="Mean")
-            msph, = ax.plot(data[:,0], data[:,3] + data[:,4], color='red', label = "Mean+Stddev")
-            mspl, = ax.plot(data[:,0], data[:,3] - data[:,4], color='red', label = "Mean-Stddev")
-            ax.set_xlabel('Iteration')
-            ax.set_ylabel('Derivative-{0}'.format(comp_type))
-            ax.grid(True)
-            plt.savefig('{dir}/nonlinstats_{comp_name}.pdf'.format(dir = output_dir, comp_name = component_name), bbox_extra_artists=(lgd,), bbox_inches='tight')
-
+    GenerateNonlinStatsPlots(exp_dir, output_dir, plot, comparison_dir = comparison_dir, start_iter = start_iter)
 
     logger.info("Generating parameter difference files")
     # Parameter changes
@@ -138,7 +212,7 @@ def GeneratePlots(exp_dir, output_dir):
 
 def Main():
     args = GetArgs()
-    GeneratePlots(args.exp_dir, args.output_dir)
+    GeneratePlots(args.exp_dir, args.output_dir, args.comparison_dir, args.start_iter)
 
 if __name__ == "__main__":
     Main()
