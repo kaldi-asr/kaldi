ba0f363e8bca460ca329f6840761dada5cadd610
Tom Ko <tomkocse@gmail.com>
2016-03-08 22:45:33 -0500

    Adding CNN option to TDNN recipe

--- egs/swbd/s5c/local/nnet3/run_tdnn.sh
+++ egs/swbd/s5c/local/nnet3/run_tdnn.sh
@@ -15,6 +15,8 @@ affix=
 train_stage=-10
 has_fisher=true
 speed_perturb=true
+use_cnn=true
+cmvn_opts="--norm-means=false --norm-vars=false"
 common_egs_dir=
 reporting_email=
 remove_egs=true
@@ -36,6 +38,35 @@ suffix=
 if [ "$speed_perturb" == "true" ]; then
   suffix=_sp
 fi
+
+cnn_opts=()
+if [ "$use_cnn" == "true" ]; then
+# When the cnn option is on, the LDA is replaced by
+# an CNN layer at the front of the network
+# and the recipe uses fbank features as the CNN input
+# As the dimension of the CNN output is usually large, we place
+# a linear layer at the output of CNN for dimension reduction
+# The ivectors are processed through a fully connected affine layer,
+# then concatenated with the CNN bottleneck output and
+# passed to the deeper part of the network.
+# Due to the data compression issue, it is better to convert MFCC
+# to FBANK in the network instead of directly using FBANK features
+# from the storage. This script uses MFCC features as its input
+# and were converted to FBANK features with an inverse of DCT matrix
+# at the first layer of the network.
+
+  cnn_layer="--filt-x-dim=3 --filt-y-dim=8 --filt-x-step=1 --filt-y-step=1 --num-filters=256 --pool-x-size=1 --pool-y-size=3 --pool-z-size=1 --pool-x-step=1 --pool-y-step=3 --pool-z-step=1"
+  cnn_opts+=(--cnn.layer "$cnn_layer")
+  cnn_bottleneck_dim=256  # remove this option if you don't want to add the bottleneck affine
+  cepstral_lifter=  # have to fill this in if you are not using the default lifter value in the production of MFCC
+                    # Here we assume your are using the default lifter value (22.0)
+  [ ! -z "$cnn.bottleneck_dim" ] && cnn_opts+=(--cnn.bottleneck-dim $cnn_bottleneck_dim)
+  [ ! -z "$cepstral_lifter" ] && cnn_opts+=(--cnn.cepstral-lifter $cepstral_lifter)
+  affix=cnn
+  # We found that using CNN with mean-normalized features obtains better results
+  cmvn_opts="--norm-means=true --norm-vars=false"
+fi
+
 dir=exp/nnet3/tdnn
 dir=$dir${affix:+_$affix}
 dir=${dir}$suffix
@@ -50,6 +81,7 @@ if [ $stage -le 9 ]; then
 
   # create the config files for nnet initialization
   python steps/nnet3/tdnn/make_configs.py  \
+    "${cnn_opts[@]}" \
     --feat-dir data/${train_set}_hires \
     --ivector-dir exp/nnet3/ivectors_${train_set} \
     --ali-dir $ali_dir \
@@ -58,7 +90,6 @@ if [ $stage -le 9 ]; then
     --use-presoftmax-prior-scale true \
    $dir/configs || exit 1;
 fi
-
 
 
 if [ $stage -le 10 ]; then
@@ -70,7 +101,7 @@ if [ $stage -le 10 ]; then
   steps/nnet3/train_dnn.py --stage=$train_stage \
     --cmd="$decode_cmd" \
     --feat.online-ivector-dir exp/nnet3/ivectors_${train_set} \
-    --feat.cmvn-opts="--norm-means=false --norm-vars=false" \
+    --feat.cmvn-opts="$cmvn_opts" \
     --trainer.num-epochs 2 \
     --trainer.optimization.num-jobs-initial 3 \
     --trainer.optimization.num-jobs-final 16 \
