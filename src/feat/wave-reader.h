0e57a26a436106f45e84977edddf76d014316452
Daniel Povey <dpovey@gmail.com>
2016-03-21 19:16:39 -0400

    chain branch: copying some changes from xvector branch to make getting wave headers more efficient

--- src/feat/wave-reader.h
+++ src/feat/wave-reader.h
@@ -37,6 +37,11 @@
 //  each WAVE chunk has header sub-chunk 'fmt_'
 //  and one or more data sub-chunks 'data'
 //
+//  [Note from Dan: to say that the wave format was ever "specified" anywhere is
+//   not quite right.  The guy who invented the wave format attempted to create
+//   a formal specification but it did not completely make sense.  And there
+//   doesn't seem to be a consensus on what makes a valid wave file,
+//   particularly where the accuracy of header information is concerned.]
 */
 
 
@@ -59,6 +64,8 @@ const BaseFloat kWaveSampleMax = 32768.0;
 /// This class's purpose is to read in Wave files.
 class WaveData {
  public:
+  enum ReadDataType { kReadData, kLeaveDataUndefined };
+
   WaveData(BaseFloat samp_freq, const MatrixBase<BaseFloat> &data)
       : data_(data), samp_freq_(samp_freq) {}
 
@@ -67,7 +74,7 @@ class WaveData {
   /// Read() will throw on error.  It's valid to call Read() more than once--
   /// in this case it will destroy what was there before.
   /// "is" should be opened in binary mode.
-  void Read(std::istream &is);
+  void Read(std::istream &is, ReadDataType read_data = kReadData);
 
   /// Write() will throw on error.   os should be opened in binary mode.
   void Write(std::ostream &os) const;
@@ -145,6 +152,49 @@ class WaveHolder {
     // We don't look for the binary-mode header here [always binary]
     try {
       t_.Read(is);  // throws exception on failure.
+      return true;
+    } catch (const std::exception &e) {
+      KALDI_WARN << "Exception caught in WaveHolder object (reading).";
+      if (!IsKaldiError(e.what())) { std::cerr << e.what(); }
+      return false;  // write failure.
+    }
+  }
+
+ private:
+  T t_;
+};
+
+
+// This is like WaveHolder but when you just want the metadata-
+// it leaves the actual data undefined, it doesn't read it.
+class WaveInfoHolder {
+ public:
+  typedef WaveData T;
+
+  static bool Write(std::ostream &os, bool binary, const T &t) {
+    KALDI_ERR << "This holder type does not support writing.";
+    return true;
+  }
+
+  void Copy(const T &t) { t_.CopyFrom(t); }
+
+  static bool IsReadInBinary() { return true; }
+
+  void Clear() { t_.Clear(); }
+
+  const T &Value() { return t_; }
+
+  WaveInfoHolder &operator = (const WaveInfoHolder &other) {
+    t_.CopyFrom(other.t_);
+    return *this;
+  }
+  WaveInfoHolder(const WaveInfoHolder &other): t_(other.t_) {}
+
+  WaveInfoHolder() {}
+
+  bool Read(std::istream &is) {
+    try {
+      t_.Read(is, WaveData::kLeaveDataUndefined);  // throws exception on failure.
       return true;
     } catch (const std::exception &e) {
       KALDI_WARN << "Exception caught in WaveHolder object (reading).";
