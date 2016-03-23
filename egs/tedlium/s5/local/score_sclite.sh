ff68bc178acbaba89e90fa3f695c3d031ab31b2d
Daniel Galvez <dt.galvez@gmail.com>
2016-03-22 22:51:37 -0400

    tedlium: Scoring fix to work with changes in 66192b
    Add --iter option.

--- egs/tedlium/s5/local/score_sclite.sh
+++ egs/tedlium/s5/local/score_sclite.sh
@@ -13,6 +13,7 @@ beam=7  # speed-up, but may affect MBR confidences.
 word_ins_penalty=0.0,0.5,1.0
 min_lmwt=10
 max_lmwt=20
+iter=final
 #end configuration section.
 
 [ -f ./path.sh ] && . ./path.sh
@@ -32,7 +33,7 @@ data=$1
 lang=$2 # Note: may be graph directory not lang directory, but has the necessary stuff copied.
 dir=$3
 
-model=$dir/../final.mdl # assume model one level up from decoding dir.
+model=$dir/../$iter.mdl # assume model one level up from decoding dir.
 
 hubscr=$KALDI_ROOT/tools/sctk/bin/hubscr.pl 
 [ ! -f $hubscr ] && echo "Cannot find scoring program at $hubscr" && exit 1;
