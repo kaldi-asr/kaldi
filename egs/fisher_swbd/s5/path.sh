12539e86cd74abf185caf559ac484d84d2119e8d
Jan Trmal <yendovy.konference@seznam.cz>
2016-03-30 09:57:03 -0400

    moving the src/path.sh into tools/config/common_path.sh

--- egs/fisher_swbd/s5/path.sh
+++ egs/fisher_swbd/s5/path.sh
@@ -1,6 +1,6 @@
 export KALDI_ROOT=`pwd`/../../../
 export PWD=`pwd`
 export PATH=$KALDI_ROOT/src/ivectorbin:$PWD/stanford-utils:$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$KALDI_ROOT/tools/kaldi_lm:$KALDI_ROOT/tools/srilm/bin:$KALDI_ROOT/tools/srilm/bin/i686-m64:$PATH
-[ ! -f $KALDI_ROOT/src/path.sh ] && echo >&2 "The standard file $KALDI_ROOT/src/path.sh is not present -> Exit!" && exit 1
-. $KALDI_ROOT/src/path.sh
+[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
+. $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
