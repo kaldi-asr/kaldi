395d8d1845cdbcf7f512f0e06c55066cb2cec14c
Takaaki Hori <thori@merl.com>
2016-04-29 08:20:19 -0400

    Simplified the script 'local/chime4_train_lms.sh'

--- egs/chime4/s5_1ch/local/chime4_train_lms.sh
+++ egs/chime4/s5_1ch/local/chime4_train_lms.sh
@@ -48,14 +48,7 @@ mkdir -p $dir
 srclm=$srcdir/lm_tgpr_5k.arpa.gz
 if [ -f $srclm ]; then
   echo "Getting vocabulary from a baseline language model";
-  gunzip -c $srclm | awk 'BEGIN{unig=0}{
-    if(unig==0){
-      if($1=="\\1-grams:"){unig=1}}
-    else {
-      if ($1 != "") {
-        if ($1=="\\2-grams:" || $1=="\\end\\") {exit}
-        else {print $2}}
-    }}' > $dir/vocab_5k.txt
+  ngram -lm $srclm -unk -map-unk '<UNK>' -write-vocab $dir/vocab_5k.txt
 else
   echo "Language model $srclm does not exist" && exit 1;
 fi
@@ -93,7 +86,7 @@ fi
 lm_suffix=${order}gkn_5k
 if [ -f $dir/lm_${lm_suffix}.arpa.gz ]; then
   echo "A $order-gram language model aready exists and is not constructed again"
-  echo "To reconstruct, remove $dir/$dir/lm_${lm_suffix}.arpa.gz first"
+  echo "To reconstruct, remove $dir/lm_${lm_suffix}.arpa.gz first"
 else
   echo "Training a $order-gram language model"
   ngram-count -text $dir/train.gz -order $order \
@@ -108,56 +101,17 @@ ngram -order $order -ppl $dir/valid.gz -lm $dir/lm_${lm_suffix}.arpa.gz
 # file data/local/local_lm/valid.txt: 3280 sentences, 54239 words, 3 OOVs
 # 0 zeroprobs, logprob= -96775.5 ppl= 48.1486 ppl1= 60.8611
 
-# Next, create the corresponding FST and lang_test_* directory.
-echo "Preparing language models for test"
-tmpdir=data/local/lm_tmp
-lexicon=data/local/lang_tmp/lexiconp.txt
-mkdir -p $tmpdir
-
+# convert arpa LM to G.fst
+echo "Converting the $order-gram language model to G.fst"
 test=data/lang_test_${lm_suffix}
 mkdir -p $test
-for f in phones.txt words.txt phones.txt L.fst L_disambig.fst \
-   phones; do
-  cp -r data/lang/$f $test
-done
+cp -r data/lang/* $test || exit 1;
+
 gunzip -c $dir/lm_${lm_suffix}.arpa.gz | \
- utils/find_arpa_oovs.pl $test/words.txt  > $tmpdir/oovs_${lm_suffix}.txt
+    arpa2fst --disambig-symbol=#0 \
+             --read-symbol-table=$test/words.txt - $test/G.fst
 
-# grep -v '<s> <s>' because the LM seems to have some strange and useless
-# stuff in it with multiple <s>'s in the history.  Encountered some other similar
-# things in a LM from Geoff.  Removing all "illegal" combinations of <s> and </s>,
-# which are supposed to occur only at being/end of utt.  These can cause
-# determinization failures of CLG [ends up being epsilon cycles].
-gunzip -c $dir/lm_${lm_suffix}.arpa.gz | \
-  grep -v '<s> <s>' | \
-  grep -v '</s> <s>' | \
-  grep -v '</s> </s>' | \
-  arpa2fst - | fstprint | \
-  utils/remove_oovs.pl $tmpdir/oovs_${lm_suffix}.txt | \
-  utils/eps2disambig.pl | utils/s2eps.pl | fstcompile --isymbols=$test/words.txt \
-    --osymbols=$test/words.txt  --keep_isymbols=false --keep_osymbols=false | \
-   fstrmepsilon | fstarcsort --sort_type=ilabel > $test/G.fst
-fstisstochastic $test/G.fst
-# The output is like:
-# 9.14233e-05 -0.259833
-# we do expect the first of these 2 numbers to be close to zero (the second is
-# nonzero because the backoff weights make the states sum to >1).
-# Because of the <s> fiasco for these particular LMs, the first number is not
-# as close to zero as it could be.
+utils/validate_lang.pl --skip-determinization-check $test || exit 1;
 
-# Everything below is only for diagnostic.
-# Checking that G has no cycles with empty words on them (e.g. <s>, </s>);
-# this might cause determinization failure of CLG.
-# #0 is treated as an empty word.
-mkdir -p $tmpdir/g
-awk '{if(NF==1){ printf("0 0 %s %s\n", $1,$1); }} END{print "0 0 #0 #0"; print "0";}' \
-    < "$lexicon"  >$tmpdir/g/select_empty.fst.txt
-fstcompile --isymbols=$test/words.txt --osymbols=$test/words.txt $tmpdir/g/select_empty.fst.txt | \
- fstarcsort --sort_type=olabel | fstcompose - $test/G.fst > $tmpdir/g/empty_words.fst
-fstinfo $tmpdir/g/empty_words.fst | grep cyclic | grep -w 'y' &&
-  echo "Language model has cycles with empty words" && exit 1
-rm -r $tmpdir/g
-
-echo "Succeeded in preparing a large ${order}-gram LM"
-rm -r $tmpdir
+echo "Succeeded in $order-gram LM training and conversion to G.fst"
 
