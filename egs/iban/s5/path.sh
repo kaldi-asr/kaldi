983ba552fa33fcd6a5a04dc6477d6388787dcab3
Yenda Trmal <jtrmal@gmail.com>
2016-05-03 00:29:35 +0000

    adding the right path.sh

--- egs/iban/s5/path.sh
+++ egs/iban/s5/path.sh
@@ -1,10 +1,6 @@
-#!/bin/bash
-
 export KALDI_ROOT=`pwd`/../../..
-. $KALDI_ROOT/tools/config/common_path.sh
-
+[ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
-
-. $KALDI_ROOT/tools/env.sh
+[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
+. $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
-
