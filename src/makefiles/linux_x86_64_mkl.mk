2f6809c57ade724b2b10f8594d976b30a703de3c
Allen Guo <guoguo12@gmail.com>
2016-05-15 06:34:14 -0700

    Clean up unneeded flags

--- src/makefiles/linux_x86_64_mkl.mk
+++ src/makefiles/linux_x86_64_mkl.mk
@@ -21,7 +21,7 @@ MKLLIB ?= $(MKLROOT)/lib/em64t
 
 DOUBLE_PRECISION = 0
 CXXFLAGS = -m64 -msse -msse2 -pthread -Wall -I.. \
-      -DKALDI_DOUBLEPRECISION=$(DOUBLE_PRECISION) -DHAVE_POSIX_MEMALIGN \
+      -DKALDI_DOUBLEPRECISION=$(DOUBLE_PRECISION) \
       -Wno-sign-compare -Wno-unused-local-typedefs -Winit-self \
       -DHAVE_EXECINFO_H=1 -rdynamic -DHAVE_CXXABI_H \
       -DHAVE_MKL -I$(MKLROOT)/include \
