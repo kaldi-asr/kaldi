fc5b00054aab98f6208d41bdcbb200cfaaf31fed
Daniel Povey <dpovey@gmail.com>
2016-05-24 21:40:43 -0400

    Add an extra check to mkgraph.sh to verify that the FST it produced is nonempty.

--- egs/wsj/s5/utils/mkgraph.sh
+++ egs/wsj/s5/utils/mkgraph.sh
@@ -127,6 +127,13 @@ if [[ ! -s $dir/HCLG.fst || $dir/HCLG.fst -ot $dir/HCLGa.fst ]]; then
   fi
 fi
 
+# note: the empty FST has 66 bytes.  this check is for whether the final FST
+# is the empty file or is the empty FST.
+if ! [ $(head -c 67 $dir/HCLG.fst | wc -c) -eq 67 ]; then
+  echo "$0: it looks like the result in $dir/HCLG.fst is empty"
+  exit 1
+fi
+
 # keep a copy of the lexicon and a list of silence phones with HCLG...
 # this means we can decode without reference to the $lang directory.
 
