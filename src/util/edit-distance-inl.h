af41ceb791e4f89c38af727c295a5164294bd7b6
Jakub Łopuszański <qbolec@gmail.com>
2016-06-05 21:43:42 +0100

    an algorithm that favors substitutions and is much faster

--- src/util/edit-distance-inl.h
+++ src/util/edit-distance-inl.h
@@ -65,146 +65,111 @@ int32 LevenshteinEditDistance(const std::vector<T> &a,
   }
   return e.back();
 }
-
 //
-struct Particle {
+struct error_stats {
   int32 ins_num;
   int32 del_num;
   int32 sub_num;
   int32 total_cost;  // minimum total cost to the current alignment.
-  int32 a_i;
-  int32 b_i;
-  void Delete() {
-    a_i++;
-    del_num++;
-    total_cost++;
-  }
-  void Insert() {
-    b_i++;
-    ins_num++;
-    total_cost++;
-  }
-  void Substitue() {
-    a_i++;
-    b_i++;
-    sub_num++;
-    total_cost++;
-  }
-  void Match() {
-    a_i++;
-    b_i++;
+  inline bool operator < (const error_stats&other) const {
+    return total_cost < other.total_cost ||
+      (total_cost == other.total_cost && other.sub_num < sub_num);
   }
 };
-
-void ConsiderParticle(const Particle p,
-                      const size_t A,
-                      const size_t B,
-                      std::vector<Particle> &future_diagonals,
-                      std::vector<int> &diagonals_indexes) {
-  if (p.a_i <= A && p.b_i <= B && 0 <= p.a_i && 0 <= p.b_i) {
-    const int nbr_diagonal_index = p.a_i - p.b_i + B;
-    Particle &nbr = future_diagonals[nbr_diagonal_index];
-    if (nbr.a_i == -1) {
-      // An uninitialized particle.
-      // We'll initialize it later.
-      // For now, just track its index.
-      diagonals_indexes.push_back(nbr_diagonal_index);
-    } else {
-      // A particle at this diagonal is already found,
-      // we expect it to be from the future generation.
-      assert(nbr.total_cost == p.total_cost);
-    }
-    if (nbr.a_i < p.a_i || (nbr.a_i == p.a_i && nbr.sub_num < p.sub_num)) {
-      nbr = p;
-    }
-  }
-}
-
 // Note that both hyp and ref should not contain noise word in
 // the following implementation.
-// The current algorithm is symmetric, so `hyp` and `ref` are named `a` and `b`
 
 template<class T>
-int32 LevenshteinEditDistance(const std::vector<T> &a,
-                              const std::vector<T> &b,
+int32 LevenshteinEditDistance(const std::vector<T> &ref,
+                              const std::vector<T> &hyp,
                               int32 *ins, int32 *del, int32 *sub) {
-  size_t A = a.size(), B = b.size();
-  // There are A+B+1 diagonals, and at most that many Particles.
-  // We denote uninitialized Particle as located in imaginary (a_i,b_i)=(-1,-1).
-  std::vector<Particle> diagonals(A+B+1), future_diagonals;
-  for (int d = 0;d < A+B+1;++d) {
-    Particle &p = diagonals[d];
-    p.b_i = p.a_i = -1;
-  }
-  future_diagonals = diagonals;
-  // We start with a single Particle in the upper left corner: (0,0).
-  // Mapping between position (a_i,b_i) and diagonal index is index=a_i-b_i+B.
-  Particle &start = diagonals[0-0+B];
-  start.a_i = start.b_i = 0;
-  start.total_cost = 0;
-  start.del_num = 0;
-  start.ins_num = 0;
-  start.sub_num = 0;
-  // We maintain the list of indexs of active particles, which never shrinks,
-  // and initially contains the single `start` particle.
-  std::vector<int> diagonals_indexes;
-  diagonals_indexes.push_back(0-0+B);
-  while (true) {
-    // We progress all Particles along their diagonals as far as possible
-    // without incurring any cost.
-    for (size_t d = diagonals_indexes.size();d--;) {
-      Particle &p = diagonals[diagonals_indexes[d]];
-      while (p.a_i < A && p.b_i < B && a[p.a_i] == b[p.b_i]) {
-        p.Match();
-      }
-      if (p.a_i == A && p.b_i == B) {
-        *ins = p.ins_num;
-        *del = p.del_num;
-        *sub = p.sub_num;
-        return p.total_cost;
-      }
+  // temp sequence to remember error type and stats.
+  std::vector<error_stats> e(ref.size()+1);
+  std::vector<error_stats> cur_e(ref.size()+1);
+  const ssize_t length_difference = (ssize_t)ref.size()-(ssize_t)hyp.size();
+  const int32 HUGE_COST = 1<<30;
+  // max_d is our current estimate of the upper bound for the true value of
+  // total_cost. We use this to limit the number of the diagonals of the matrix
+  // that we compute. We know that the total_cost has to at least account for
+  // the difference in lengths of the two strings. We then double it each time
+  // we find it too small.
+  for (ssize_t max_d = labs(length_difference); ; max_d*=2) {
+    // initialize the first hypothesis aligned to the reference at each
+    // position:[hyp_index =0][ref_index]
+    for (size_t i =0; i < e.size(); i ++) {
+      e[i].ins_num = 0;
+      e[i].sub_num = 0;
+      e[i].del_num = i;
+      e[i].total_cost = i;
     }
-    // None of particles can go any further without paying.
-    // Moving one cell in any direction will cost 1 unit.
-    // There are three interesting directions which we will consider:
-    // Case 1) along the diagonal (a_i+1,b_i+1), which increases sub_num
-    // Case 2) to the next diagonal (a_i+1,b_i), which increases del_num
-    // Case 3) to the previous diagonal (a_i,b_i-1), which increases ins_num
-    // Therefore, each old particle in diagonals[] can spawn at most three
-    // new particles in future_particles[].
-    // We merge these propositions, and maintain only the best particle
-    // for each diagonal.
-    // Case 1)
-    for (size_t d = diagonals_indexes.size();d--;) {
-      const int diagonal_index = diagonals_indexes[d];
-      Particle &future = future_diagonals[diagonal_index];
-      future = diagonals[diagonal_index];
-      if (future.a_i < A && future.b_i < B) {
-        future.Substitue();
-      } else {
-        // The particle hit the wall.
-        // We could remove this particle from the list,
-        // but I prefer to simply make its cost infavorable.
-        future.total_cost++;
+    // for other alignments
+    bool has_chance = true;
+    // These two variables control the active range of cur_e. The idea is that
+    // cur_e[first_ok-1 or last_ok+1] plus the cost to get to the goal diagonal
+    // is too big.
+    ssize_t first_ok = 0;
+    ssize_t last_ok = (max_d+length_difference)/2;
+    ssize_t hyp_index;
+    for (hyp_index = 1; hyp_index <= (ssize_t)hyp.size(); hyp_index ++) {
+      cur_e[0] = e[0];
+      cur_e[0].ins_num++;
+      cur_e[0].total_cost++;
+      // we need a guardian value in the array for the computation of del_err
+      if (0 < first_ok) {
+        cur_e[first_ok-1].total_cost = HUGE_COST;
       }
+      const ssize_t start = std::max<ssize_t>(1, first_ok);
+      const ssize_t stop = std::min<ssize_t>(ref.size(), last_ok);
+      if (stop < start) {
+        has_chance = false;
+        break;
+      }
+      for (ssize_t ref_index = start; ref_index <= stop; ref_index++) {
+        if (hyp[hyp_index-1] == ref[ref_index-1]) {
+          cur_e[ref_index] = e[ref_index-1];
+        } else {
+          const error_stats &ins_err = e[ref_index];
+          const error_stats &del_err = cur_e[ref_index-1];
+          const error_stats &sub_err = e[ref_index-1];
+
+          if (sub_err < ins_err && sub_err < del_err) {
+            cur_e[ref_index] = sub_err;
+            cur_e[ref_index].sub_num++;  // substitution error is increased.
+          } else if (del_err < ins_err) {
+            cur_e[ref_index] = del_err;
+            cur_e[ref_index].del_num++;    // deletion number is increased.
+          } else {
+            cur_e[ref_index] = ins_err;
+            cur_e[ref_index].ins_num++;    // insertion number is increased.
+          }
+          cur_e[ref_index].total_cost++;
+        }
+      }
+      const ssize_t goal = hyp_index + length_difference;
+      while (first_ok <= last_ok &&
+        max_d < cur_e[first_ok].total_cost+labs(first_ok - goal)) {
+        ++first_ok;
+      }
+      while (first_ok <= last_ok &&
+        max_d < cur_e[last_ok].total_cost+labs(last_ok - goal)) {
+        --last_ok;
+      }
+      if (last_ok < ref.size()) {
+        // we need to set a guardian value
+        cur_e[last_ok+1].total_cost = HUGE_COST;
+        // we need (and can) increase the active range
+        last_ok++;
+      }
+      swap(e, cur_e);  // alternate for the next recursion.
     }
-    for (size_t d = diagonals_indexes.size();d--;) {
-      const int diagonal_index = diagonals_indexes[d];
-      const Particle p = diagonals[diagonal_index];
-      // Case 2)
-      Particle nbr = p;
-      nbr.Delete();
-      ConsiderParticle(nbr, A, B, future_diagonals, diagonals_indexes);
-      // Case 3)
-      nbr = p;
-      nbr.Insert();
-      ConsiderParticle(nbr, A, B, future_diagonals, diagonals_indexes);
+    size_t ref_index = e.size()-1;
+    if (has_chance && e[ref_index].total_cost <= max_d) {
+      *ins = e[ref_index].ins_num, *del =
+        e[ref_index].del_num, *sub = e[ref_index].sub_num;
+      return e[ref_index].total_cost;
     }
-    swap(diagonals, future_diagonals);
   }
 }
-
-
 
 template<class T>
 int32 LevenshteinAlignment(const std::vector<T> &a,
