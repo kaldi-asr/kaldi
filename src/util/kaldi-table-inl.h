9f06614603ad2cef02cafee8f6de8970d7898d36
Daniel Povey <dpovey@gmail.com>
2016-07-27 21:23:25 -0400

    Make it so that matrix row ranges are supported when reading single objects as opposed to just when reading tables.

--- src/util/kaldi-table-inl.h
+++ src/util/kaldi-table-inl.h
@@ -41,18 +41,6 @@ namespace kaldi {
 /// \addtogroup table_impl_types
 /// @{
 
-// In SequentialTableReaderScriptImpl and RandomAccessTableReaderScriptImpl, for
-// cases where the scp contained 'range specifiers' (things in square brackets
-// identifying parts of objects like matrices), use this function to separate
-// the input string 'line' (e.g "1.ark:100[1:2,2:10]") into the data_rxfilename
-// (e.g. "1.ark:100") and the optional range specifier which will be everything
-// inside the square brackets.  It returns true if everything seems OK, and
-// false if for example the string contained more than one '['.  This function
-// should only be called if 'line' ends in '[', otherwise it is an error.
-bool ExtractRangeSpecifier(const std::string &line,
-                           std::string *data_rxfilename,
-                           std::string *range);
-
 template<class Holder> class SequentialTableReaderImplBase {
  public:
   typedef typename Holder::T T;
