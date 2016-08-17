2f2e8c5e258cb4a4f4f3695a5d68a00b4b2c2e99
Vijayaditya Peddinti <vijayaditya.p@gmail.com>
2016-08-16 21:47:38 -0400

    Added blstm+chain recipe

---
+++ egs/ami/s5b/local/chain/run_chain_common.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+
+# This script is called from local/chain/run_{tdnn,blstm}.sh (and may eventually
+# be called by more scripts).  It contains the common stages for chain model training
+# of the script.  See those scripts for examples of usage.
+
+stage=11
+nj=30
+cmd=run.pl
+
+. ./cmd.sh
+. ./path.sh
+. ./utils/parse_options.sh
+
+# input_directories
+train_data_dir=$1
+gmm_dir=$2
+
+# output directories
+ali_dir=$3
+lat_dir=$4
+tree_dir=$5
+
+for f in $gmm_dir/final.mdl $train_data_dir/feats.scp ; do
+  [ ! -f $f ] && echo "$0: expected file $f to exist" && exit 1
+done
+
+
+if [ $stage -le 11 ]; then
+  if [ -f $ali_dir/ali.1.gz ]; then
+    echo "$0: alignments in $ali_dir appear to already exist.  Please either remove them "
+    echo " ... or use a later --stage option."
+    exit 1
+  fi
+  echo "$0: aligning perturbed, short-segment-combined data"
+  steps/align_fmllr.sh --nj $nj --cmd "$cmd" \
+     ${train_data_dir} data/lang $gmm_dir $ali_dir
+fi
+
+[ ! -f $ali_dir/ali.1.gz ] && echo  "$0: expected $ali_dir/ali.1.gz to exist" && exit 1
+
+if [ $stage -le 12 ]; then
+  echo "$0: creating lang directory with one state per phone."
+  # Create a version of the lang/ directory that has one state per phone in the
+  # topo file. [note, it really has two states.. the first one is only repeated
+  # once, the second one has zero or more repeats.]
+  if [ -d data/lang_chain ]; then
+    if [ data/lang_chain/L.fst -nt data/lang/L.fst ]; then
+      echo "$0: data/lang_chain already exists, not overwriting it; continuing"
+    else
+      echo "$0: data/lang_chain already exists and seems to be older than data/lang..."
+      echo " ... not sure what to do.  Exiting."
+      exit 1;
+    fi
+  else
+    cp -r data/lang data/lang_chain
+    silphonelist=$(cat data/lang_chain/phones/silence.csl) || exit 1;
+    nonsilphonelist=$(cat data/lang_chain/phones/nonsilence.csl) || exit 1;
+    # Use our special topology... note that later on may have to tune this
+    # topology.
+    steps/nnet3/chain/gen_topo.py $nonsilphonelist $silphonelist >data/lang_chain/topo
+  fi
+fi
+
+if [ $stage -le 13 ]; then
+  # Get the alignments as lattices (gives the chain training more freedom).
+  # use the same num-jobs as the alignments
+  steps/align_fmllr_lats.sh --nj 100 --cmd "$cmd" ${train_data_dir} \
+    data/lang $gmm_dir $lat_dir
+  rm $lat_dir/fsts.*.gz # save space
+fi
+
+if [ $stage -le 14 ]; then
+  # Build a tree using our new topology.  We know we have alignments for the
+  # speed-perturbed data (local/nnet3/run_ivector_common.sh made them), so use
+  # those.
+  if [ -f $tree_dir/final.mdl ]; then
+    echo "$0: $tree_dir/final.mdl already exists, refusing to overwrite it."
+    exit 1;
+  fi
+  steps/nnet3/chain/build_tree.sh --frame-subsampling-factor 3 \
+      --context-opts "--context-width=2 --central-position=1" \
+      --leftmost-questions-truncate -1 \
+      --cmd "$cmd" 4200 ${train_data_dir} data/lang_chain $ali_dir $tree_dir
+fi
