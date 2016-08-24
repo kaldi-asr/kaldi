c1fd37771f57b6c9d05dd75126fdec92ce7bdf49
Yiming Wang <freewym@gmail.com>
2016-08-24 00:08:27 -0400

    change option name self-repair-scale to self-repair-scale-nonlinearity for all tdnn recipes (#1002)

--- egs/swbd/s5c/local/chain/run_tdnn_6t.sh
+++ egs/swbd/s5c/local/chain/run_tdnn_6t.sh
@@ -34,7 +34,7 @@
 # this is to help disentangle whether the improvement really comes from the
 # higher-resolution features, or from the improved data shifting.
 
-# _6r is as _6q, but adding --self-repair-scale 0.00001 --xent-separate-forward-affine=true
+# _6r is as _6q, but adding --self-repair-scale-nonlinearity 0.00001 --xent-separate-forward-affine=true
 
 # _6q is as _5n (which is a double-frame-rate system), but putting back
 # the iVectors and otherwise changing the configuration as in 5j -> 6g,
@@ -461,7 +461,7 @@ if [ $stage -le 14 ]; then
     --xent-regularize 0.1 \
     --leaky-hmm-coefficient 0.1 \
     --l2-regularize 0.00005 \
-    --jesus-opts "--jesus-forward-input-dim 500  --jesus-forward-output-dim 1800 --jesus-hidden-dim 7500 --jesus-stddev-scale 0.2 --final-layer-learning-rate-factor 0.25 --self-repair-scale 0.00001 --xent-separate-forward-affine=true" \
+    --jesus-opts "--jesus-forward-input-dim 500  --jesus-forward-output-dim 1800 --jesus-hidden-dim 7500 --jesus-stddev-scale 0.2 --final-layer-learning-rate-factor 0.25 --self-repair-scale-nonlinearity 0.00001 --xent-separate-forward-affine=true" \
     --splice-indexes "-1,1 -2,0,2 -4,-2,0,2 -6,0,6 -6,0,6 -12,-6,0" \
     --apply-deriv-weights false \
     --frames-per-iter 3000000 \
