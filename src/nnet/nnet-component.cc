cb6d01e5c6e18e9799079b30c7b94886110dcff5
Harish Mallidi <mallidi.harish@gmail.com>
2016-08-25 09:20:36 -0400

    Added ReLU activation, from https://github.com/naxingyu/kaldi-nn/blob/master/src/nnet/nnet-activation.h
    	modified:   nnet-activation.h
    	modified:   nnet-component.cc
    	modified:   nnet-component.h

--- src/nnet/nnet-component.cc
+++ src/nnet/nnet-component.cc
@@ -65,6 +65,7 @@ const struct Component::key_value Component::kMarkerMap[] = {
   { Component::kHiddenSoftmax, "<HiddenSoftmax>" },
   { Component::kBlockSoftmax, "<BlockSoftmax>" },
   { Component::kSigmoid, "<Sigmoid>" },
+  { Component::kReLU, "<ReLU>" },
   { Component::kTanh, "<Tanh>" },
   { Component::kDropout, "<Dropout>" },
   { Component::kLengthNormComponent, "<LengthNormComponent>" },
@@ -148,6 +149,9 @@ Component* Component::NewComponentOfType(ComponentType comp_type,
       break;
     case Component::kSigmoid :
       ans = new Sigmoid(input_dim, output_dim);
+      break;
+    case Component::kReLU :
+      ans = new ReLU(input_dim, output_dim);
       break;
     case Component::kTanh :
       ans = new Tanh(input_dim, output_dim);
