e031cdecf96d3ba557c59f7a8682ad120eb4cb52
vesis84 <vesis84@gmail.com>
2016-09-14 11:37:24 +0200

    cudamatrix: replacing the random number generator,

--- src/makefiles/cuda_32bit.mk
+++ src/makefiles/cuda_32bit.mk
@@ -8,4 +8,4 @@ CUDA_FLAGS = -g -Xcompiler -fPIC --verbose --machine 32 -DHAVE_CUDA \
              -DKALDI_DOUBLEPRECISION=$(DOUBLE_PRECISION)
 CXXFLAGS += -DHAVE_CUDA -I$(CUDATKDIR)/include
 LDFLAGS += -L$(CUDATKDIR)/lib -Wl,-rpath=$(CUDATKDIR)/lib
-LDLIBS += -lcublas -lcudart #LDLIBS : The libs are loaded later than static libs in implicit rule
+LDLIBS += -lcublas -lcudart -lcurand #LDLIBS : The libs are loaded later than static libs in implicit rule
