901d6965c7f7f21a8a44e64acdd37bc73d0b6965
Vijayaditya Peddinti <vijayaditya.p@gmail.com>
2016-10-01 15:37:18 -0400

    minor bugfix nnet3 python training scripts. Does not affect results

--- egs/wsj/s5/steps/nnet3/train_rnn.py
+++ egs/wsj/s5/steps/nnet3/train_rnn.py
@@ -219,7 +219,7 @@ def GetArgs():
                         help="""If true, remove egs after experiment""")
     parser.add_argument("--cleanup.preserve-model-interval", dest = "preserve_model_interval",
                         type=int, default=100,
-                        help="Determines iterations for which models will be preserved during cleanup. If iter % preserve_model_interval == 0 model will be preserved.")
+                        help="Determines iterations for which models will be preserved during cleanup. If mod(iter,preserve_model_interval) == 0 model will be preserved.")
 
     parser.add_argument("--reporting.email", dest = "email",
                         type=str, default=None, action = NullstrToNoneAction,
