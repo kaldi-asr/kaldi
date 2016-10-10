7efa27f61eca81cb67148fa9bee6bfc310c90285
Dan Povey <dpovey@gmail.com>
2016-10-10 18:49:20 -0400

    Further refactoring to nnet3 compilation to make it easier to implement online computation.

--- src/nnet3/nnet-utils.cc
+++ src/nnet3/nnet-utils.cc
@@ -68,8 +68,8 @@ void EvaluateComputationRequest(
     const ComputationRequest &request,
     std::vector<std::vector<bool> > *is_computable) {
   ComputationGraph graph;
-  ComputationGraphBuilder builder(nnet, request, &graph);
-  builder.Compute();
+  ComputationGraphBuilder builder(nnet, &graph);
+  builder.Compute(request);
   builder.GetComputableInfo(is_computable);
   if (GetVerboseLevel() >= 4) {
     std::ostringstream graph_pretty;
