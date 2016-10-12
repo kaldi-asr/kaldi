ea961d39e74dce0b2f5925bd82e382d51e931591
Axel Horndasch <axel.horndasch@fau.de>
2016-10-12 14:13:29 +0200

    "--extra-word-disambig-syms" for prepare_lang.sh
    The new command line option '--extra-word-disambig-syms disambig_syms.txt'
    for prepare_lang.sh adds the possibility to add extra word-level
    disambiguation symbols to the list of automatic disambiguation symbols.
    
    The extra disambiguation symbols can be used during the modification of
    G.fst graphs, e.g. when embedding word classes (a word-class disambiguation
    symbol can for example be transduced to <eps> when entering or leaving a
    word-class sub-language model).

---
+++ egs/wsj/s5/utils/validate_disambig_sym_file.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/env perl
+#!/usr/bin/perl -w
+# Copyright 2016 FAU Erlangen (Author: Axel Horndasch)
+# Apache 2.0.
+#
+# Concept: Dan Povey
+
+use strict;
+use warnings;
+use Getopt::Long;
+
+my $Usage = <<EOU;
+Usage:  validate_disambig_sym_file.pl [options] disambig_syms.txt
+
+This scripts checks if the entries of a file containing disambiguation symbols
+(word or phone level) are all valid. To be valid the symbols
+- must start with the hash mark '#',
+- must not contain any whitespace,
+- must not be equal to '#-1' (disallowed because it is used internally in some)
+  FST stuff.
+
+In case the option '--extra-disambig-syms' is used with 'true', the symbols must
+also be non-numeric (to avoid overlap with the automatically generated symbols).
+
+Allowed options:
+  --extra-disambig-syms  : Indicate extra symbols  (boolean,  default = "false")
+
+EOU
+
+# command line options
+my $extra_disambig_syms = "false";
+
+# Get the optional command line options
+GetOptions(
+    "extra-disambig-syms=s" => \$extra_disambig_syms,
+    ) or die ($Usage);
+
+if (@ARGV != 1) {
+  die($Usage);
+}
+
+my $disambig_sym_file = shift @ARGV;
+
+print "$0: Checking $disambig_sym_file ...\n";
+if (-z $disambig_sym_file) {
+  print "$0: The file $disambig_sym_file is empty or does not exist, exiting ...\n"; exit 1;
+}
+
+if (not open(SYMS, "<$disambig_sym_file")) {
+  print "$0: Could not open file $disambig_sym_file, exiting ...\n"; exit 1;
+}
+
+while (<SYMS>) {
+  chomp;
+  my $symbol = $_;
+
+  if ($symbol =~ /^#(.*)$/) {
+    my $sympart = $1;
+    if ($sympart eq "") {
+      print "$0: Only \"$symbol\" is not allowed as a disambiguation symbol, exiting ...\n"; exit 1;
+    }
+    if ($sympart =~/\s+/) {
+      print "$0: The disambiguation symbol \"$symbol\" contains whitespace, exiting ...\n"; exit 1;
+    }
+    if ($sympart eq "-1") {
+      print "$0: The disambiguation symbol \"$symbol\" is not allowed, exiting ...\n"; exit 1;
+    }
+    if ($extra_disambig_syms eq "true" &&
+	$sympart =~/^[0-9]+$/) {
+      print "$0: Since \"$symbol\" is supposed to be an extra disambiguation symbol, it must not be numeric, exiting ...\n"; exit 1;
+    }
+  } else {
+    print "$0: The disambiguation symbol \"$symbol\" does not start with a '#', exiting ...\n"; exit 1;
+  }
+}
+
+print "--> SUCCESS [validating disambiguation symbol file $disambig_sym_file]\n";
+exit 0;
+
