be2aac81abdc7ea689b520c018ba12f683df58b1
Gaofeng Cheng <770579626@qq.com>
2016-10-13 12:46:42 -0500

    revise the chain lstm scripts in a new branch #1100 (#1101)
    add self-repair-scale-nonlinearity for the lstm scripts under
    chain/tuning/

--- egs/swbd/s5c/local/chain/tuning/run_lstm_d.sh
+++ egs/swbd/s5c/local/chain/tuning/run_lstm_d.sh
@@ -29,7 +29,7 @@ chunk_width=150
 chunk_left_context=40
 chunk_right_context=0
 xent_regularize=0.025
-
+self_repair_scale=0.00001
 # decode options
 extra_left_context=
 extra_right_context=
@@ -108,7 +108,7 @@ if [ $stage -le 11 ]; then
       --leftmost-questions-truncate $leftmost_questions_truncate \
       --cmd "$train_cmd" 9000 data/$train_set $lang $ali_dir $treedir
 fi
-
+repair_opts=${self_repair_scale:+" --self-repair-scale-nonlinearity $self_repair_scale "}
 if [ $stage -le 12 ]; then
   echo "$0: creating neural net configs";
 
@@ -120,6 +120,7 @@ if [ $stage -le 12 ]; then
   [ ! -z "$lstm_delay" ] && config_extra_opts+=(--lstm-delay "$lstm_delay")
 
   steps/nnet3/lstm/make_configs.py  "${config_extra_opts[@]}" \
+    $repair_opts \
     --feat-dir data/${train_set}_hires \
     --ivector-dir exp/nnet3/ivectors_${train_set} \
     --tree-dir $treedir \
@@ -132,7 +133,6 @@ if [ $stage -le 12 ]; then
     --recurrent-projection-dim $recurrent_projection_dim \
     --non-recurrent-projection-dim $non_recurrent_projection_dim \
     --label-delay $label_delay \
-    --self-repair-scale 0.00001 \
    $dir/configs || exit 1;
 
 fi
