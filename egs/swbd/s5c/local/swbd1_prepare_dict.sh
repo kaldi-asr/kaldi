3cbf806381fdf013c62f388cc29855a12c3eff63
Albert Vernon <avernon@iqventures.com>
2016-10-26 16:44:38 -0400

    Suppress lexiconp.txt warning using /dev/null

--- egs/swbd/s5c/local/swbd1_prepare_dict.sh
+++ egs/swbd/s5c/local/swbd1_prepare_dict.sh
@@ -89,6 +89,6 @@ cat $dir/acronyms_raw.map | sort -u > $dir/acronyms.map
 pushd $dir >&/dev/null
 ln -sf lexicon5.txt lexicon.txt # This is the final lexicon.
 popd >&/dev/null
-rm -f $dir/lexiconp.txt
+rm $dir/lexiconp.txt 2>/dev/null
 echo Prepared input dictionary and phone-sets for Switchboard phase 1.
 
