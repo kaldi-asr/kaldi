9ed83d6233a1c2bcf6582cac4716e353fc9fbf7c
Hainan Xu <hainan.xv@gmail.com>
2016-10-31 18:18:51 -0400

    divide egs into archives for rnnlm training

--- egs/ami/s5/local/rnnlm/run-rnnlm.sh
+++ egs/ami/s5/local/rnnlm/run-rnnlm.sh
@@ -3,6 +3,8 @@
 train_text=data/sdm1/train/text
 dev_text=data/sdm1/dev/text
 
+cmd=run.pl
+
 num_words_in=10000
 num_words_out=10000
 hidden_dim=200
@@ -13,16 +15,20 @@ eos="</s>"
 oos="<oos>"
 
 max_param_change=20
-num_iters=20
+num_iters=40
 
 shuffle_buffer_size=5000 # This "buffer_size" variable controls randomization of the samples
 minibatch_size=64
 
+num_train_frames_combine=10000 # # train frames for the above.                  
+num_frames_diagnostic=2000 # number of frames for "compute_prob" jobs  
+num_archives=9
+
 initial_learning_rate=0.008
 final_learning_rate=0.0004
-learning_rate_decline_factor=1.2
+learning_rate_decline_factor=1.05
 
-type=rnn
+type=rnn-newformat-40
 
 . cmd.sh
 . path.sh
@@ -31,12 +37,12 @@ type=rnn
 outdir=data/sdm1/rnnlm-sigmoid-$initial_learning_rate-$final_learning_rate-$learning_rate_decline_factor-$minibatch_size-$type
 srcdir=data/local/dict
 
-
 set -e
 
 mkdir -p $outdir
 
 if [ $stage -le -4 ]; then
+  echo Data Preparation
   cat $srcdir/lexicon.txt | awk '{print $1}' | grep -v -w '!SIL' > $outdir/wordlist.all
 
 #  cat $train_text | cut -d" " -f2- > $outdir/train.txt.0
@@ -72,10 +78,40 @@ num_words_in=`wc -l $outdir/wordlist.in | awk '{print $1}'`
 num_words_out=`wc -l $outdir/wordlist.out | awk '{print $1}'`
 
 if [ $stage -le -3 ]; then
-  rnnlm-get-egs $outdir/train.txt $outdir/wordlist.in $outdir/wordlist.out ark,t:$outdir/egs
+  echo Get Examples
+  $cmd $outdir/log/get-egs.train.txt \
+    rnnlm-get-egs $outdir/train.txt $outdir/wordlist.in $outdir/wordlist.out ark,t:"| tr '\n' ' ' | sed 's=line=\nline=g' | grep . | shuf > $outdir/train.egs" &
+  $cmd $outdir/log/get-egs.dev.txt \
+    rnnlm-get-egs $outdir/dev.txt $outdir/wordlist.in $outdir/wordlist.out ark,t:"| tr '\n' ' ' | sed 's=line=\nline=g' | grep . > $outdir/dev.egs"
+
+  wait
+
+  [ -d $outdir/egs ] && rm $outdir/egs -r
+  mkdir -p $outdir/egs
+
+  echo Do split
+  split --number=l/$num_archives --numeric-suffixes=1 $outdir/train.egs $outdir/egs/train.
+
+  # delete leading zero's
+  for i in `seq 1 $num_archives`; do
+    j=$i
+    while [ ! -f $outdir/egs/train.$i ]; do
+      i=0$i
+    done
+    [ "$i" != "$j" ] && mv $outdir/egs/train.$i $outdir/egs/train.$j 
+  done
+
+  $cmd $outdir/log/create_train_subset_combine.log \
+     nnet3-subset-egs --n=$num_train_frames_combine ark:$outdir/train.egs \
+     ark,t:$outdir/train.subset.egs &                           
+  $cmd $outdir/log/create_train_subset_diagnostic.log \
+     nnet3-subset-egs --n=$num_frames_diagnostic ark:$outdir/dev.egs \
+     ark,t:$outdir/dev.subset.egs
+  wait
 fi
 
 if [ $stage -le -2 ]; then
+  echo Create nnet configs
   cat > $outdir/config <<EOF
   input-node name=input dim=$num_words_in
   component name=first_affine type=NaturalGradientAffineComponent input-dim=$[$num_words_in+$hidden_dim] output-dim=$hidden_dim  
@@ -98,7 +134,6 @@ if [ $stage -le 0 ]; then
   nnet3-init --binary=false $outdir/config $outdir/0.mdl
 fi
 
-
 cat data/local/dict/lexicon.txt | awk '{print $1}' > $outdir/wordlist.all.1
 cat $outdir/wordlist.in $outdir/wordlist.out | awk '{print $1}' > $outdir/wordlist.all.2
 cat $outdir/wordlist.all.[12] | sort -u > $outdir/wordlist.all
@@ -115,12 +150,17 @@ if [ $stage -le $num_iters ]; then
 #  fi
   learning_rate=$initial_learning_rate
 
+  this_archive=0
   for n in `seq $start $num_iters`; do
-    echo for iter $n, learning rate is $learning_rate
+    this_archive=$[$this_archive+1]
+
+    [ $this_archive -gt $num_archives ] && this_archive=1
+
+    echo for iter $n, learning rate is $learning_rate, training on archive $this_archive
     [ $n -ge $stage ] && (
         $cuda_cmd $outdir/log/train.rnnlm.$n.log nnet3-train \
         --max-param-change=$max_param_change "nnet3-copy --learning-rate=$learning_rate $outdir/$[$n-1].mdl -|" \
-        "ark:nnet3-shuffle-egs --buffer-size=$shuffle_buffer_size --srand=$n ark:$outdir/egs ark:- | nnet3-merge-egs --minibatch-size=$minibatch_size ark:- ark:- |" $outdir/$n.mdl
+        "ark:nnet3-shuffle-egs --buffer-size=$shuffle_buffer_size --srand=$n ark:$outdir/egs/train.$this_archive ark:- | nnet3-merge-egs --minibatch-size=$minibatch_size ark:- ark:- |" $outdir/$n.mdl
     )
 
     learning_rate=`echo $learning_rate | awk -v d=$learning_rate_decline_factor '{printf("%f", $1/d)}'`
@@ -129,15 +169,14 @@ if [ $stage -le $num_iters ]; then
     fi
 
     [ $n -ge $stage ] && (
-      nw=`wc -l $outdir/wordlist.all | awk '{print $1 - 3}'` # <s>, </s>, <oos>
-      nw=`wc -l $outdir/wordlist.all | awk '{print $1}'` # <s>, </s>, <oos>
 
-      echo $decode_cmd $outdir/dev.ppl.$n.log rnnlm-eval --num-words=$nw $outdir/$n.mdl $outdir/wordlist.in $outdir/wordlist.out $outdir/dev.txt $outdir/dev-probs-iter-$n.txt
-      $decode_cmd $outdir/dev.ppl.$n.log rnnlm-eval --num-words=$nw $outdir/$n.mdl $outdir/wordlist.in $outdir/wordlist.out $outdir/dev.txt $outdir/dev-probs-iter-$n.txt
-      nw=`cat $outdir/dev.txt | awk '{a+=NF-1}END{print a}' `
-      to_cost=`cat $outdir/dev-probs-iter-$n.txt | awk '{a+=$1}END{print -a}'`
-      ppl=`echo $to_cost $nw | awk '{print exp($1/$2)}'`
-      echo DEV PPL on model $n.mdl is $ppl | tee $outdir/log/dev.ppl.$n.txt
+      $decode_cmd $outdir/log/compute_prob_train.rnnlm.$n.log \
+        nnet3-compute-prob $outdir/$n.mdl ark:$outdir/train.subset.egs &
+      $decode_cmd $outdir/log/compute_prob_valid.rnnlm.$n.log \
+        nnet3-compute-prob $outdir/$n.mdl ark:$outdir/dev.subset.egs 
+
+      ppl=`grep Overall $outdir/log/compute_prob_valid.rnnlm.$n.log | grep like | awk '{print exp(-$8)}'`
+      echo DEV PPL on model $n.mdl is $ppl
     ) &
   done
   cp $outdir/$num_iters.mdl $outdir/rnnlm
