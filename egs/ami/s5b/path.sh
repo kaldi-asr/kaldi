f141eb0f86f2e25ec64f0b643735cc3bb649b324
Vimal Manohar <vimal.manohar91@gmail.com>
2016-11-22 12:42:44 -0500

    asr_diarization: Removing sctk and other additions to AMI path.sh

--- egs/ami/s5b/path.sh
+++ egs/ami/s5b/path.sh
@@ -9,7 +9,4 @@ LMBIN=$KALDI_ROOT/tools/irstlm/bin
 SRILM=$KALDI_ROOT/tools/srilm/bin/i686-m64
 BEAMFORMIT=$KALDI_ROOT/tools/BeamformIt
 
-export PATH=$PATH:$LMBIN:$BEAMFORMIT:$SRILM
-export PATH=$PATH:$KALDI_ROOT/tools/sph2pipe_v2.5
-export PATH=$PATH:/home/vmanoha1/kaldi-waveform/src/segmenterbin
-export PATH=$PATH:$KALDI_ROOT/tools/sctk/bin
+export PATH=$LMBIN:$BEAMFORMIT:$SRILM:$PATH
