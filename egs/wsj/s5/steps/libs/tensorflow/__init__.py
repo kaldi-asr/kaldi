c0e4ab236027fc91b75a1d54b39cce4e6da5c781
Gaofeng Cheng <770579626@qq.com>
2016-12-07 15:59:15 +0800

    unfinished 1

--- egs/wsj/s5/steps/libs/tensorflow/__init__.py
+++ egs/wsj/s5/steps/libs/tensorflow/__init__.py
@@ -3,3 +3,6 @@
 
 # This module has the python functions which facilitate the use of tensorflow toolkit under kaldi
 # It has two sub-modules
+# kaldi_tensorflow : including necessary interface module to run tensorflow under kaldi 
+# neuralNetworks :   including tensorflow neural network trining module, tensorflow neural network could be
+#                    expanded under this directory
