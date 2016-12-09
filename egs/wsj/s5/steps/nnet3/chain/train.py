4ece08946d76662db5467cc49d8101ef2b72eb86
Vimal Manohar <vimal.manohar91@gmail.com>
2016-12-09 15:06:05 -0500

    dropout_schedule: Fixing logging

--- egs/wsj/s5/steps/nnet3/chain/train.py
+++ egs/wsj/s5/steps/nnet3/chain/train.py
@@ -22,11 +22,11 @@ import libs.nnet3.train.chain_objf.acoustic_model as chain_lib
 import libs.nnet3.report.log_parse as nnet3_log_parse
 
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('libs')
 logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()
 handler.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s [%(filename)s:%(lineno)s - "
+formatter = logging.Formatter("%(asctime)s [%(pathname)s:%(lineno)s - "
                               "%(funcName)s - %(levelname)s ] %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
--- egs/wsj/s5/steps/nnet3/train_dnn.py
+++ egs/wsj/s5/steps/nnet3/train_dnn.py
@@ -26,7 +26,7 @@ logger = logging.getLogger('libs')
 logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()
 handler.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s [%(filename)s:%(lineno)s - "
+formatter = logging.Formatter("%(asctime)s [%(pathname)s:%(lineno)s - "
                               "%(funcName)s - %(levelname)s ] %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
