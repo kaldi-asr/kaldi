057c6629b913e758f41f91b15c7bb816ec746d31
Xingyu Na <asr.naxingyu@gmail.com>
2016-12-12 20:21:59 -0500

    rename gale_mandarin jobs-variables

--- egs/gale_mandarin/s5/run.sh
+++ egs/gale_mandarin/s5/run.sh
@@ -6,8 +6,8 @@
 . ./path.sh
 . ./cmd.sh
 
-nJobs=64
-nDecodeJobs=128
+num_jobs=64
+num_jobs_decode=128
 
 AUDIO=(
   /export/corpora/LDC/LDC2013S08/
@@ -61,7 +61,7 @@ fi
 
 for x in train dev ; do
   utils/fix_data_dir.sh data/$x
-  steps/make_mfcc_pitch.sh --cmd "$train_cmd" --nj $nJobs \
+  steps/make_mfcc_pitch.sh --cmd "$train_cmd" --nj $num_jobs \
     data/$x exp/make_mfcc/$x $mfccdir
   utils/fix_data_dir.sh data/$x # some files fail to get mfcc for many reasons
   steps/compute_cmvn_stats.sh data/$x exp/make_mfcc/$x $mfccdir
@@ -78,7 +78,7 @@ steps/train_mono.sh --nj 40 --cmd "$train_cmd" \
   data/train.10k data/lang exp/mono || exit 1;
 
 # Get alignments from monophone system.
-steps/align_si.sh --nj $nJobs --cmd "$train_cmd" \
+steps/align_si.sh --nj $num_jobs --cmd "$train_cmd" \
   data/train.50k data/lang exp/mono exp/mono_ali.50k || exit 1;
 
 # train tri1 [first triphone pass]
@@ -87,10 +87,10 @@ steps/train_deltas.sh --cmd "$train_cmd" \
 
 # First triphone decoding
 utils/mkgraph.sh data/lang_test exp/tri1 exp/tri1/graph || exit 1;
-steps/decode.sh  --nj $nDecodeJobs --cmd "$decode_cmd" \
+steps/decode.sh  --nj $num_jobs_decode --cmd "$decode_cmd" \
   exp/tri1/graph data/dev exp/tri1/decode &
 
-steps/align_si.sh --nj $nJobs --cmd "$train_cmd" \
+steps/align_si.sh --nj $num_jobs --cmd "$train_cmd" \
   data/train data/lang exp/tri1 exp/tri1_ali || exit 1;
 
 # Train tri2a, which is deltas+delta+deltas
@@ -99,60 +99,60 @@ steps/train_deltas.sh --cmd "$train_cmd" \
 
 # tri2a decoding
 utils/mkgraph.sh data/lang_test exp/tri2a exp/tri2a/graph || exit 1;
-steps/decode.sh --nj $nDecodeJobs --cmd "$decode_cmd" \
+steps/decode.sh --nj $num_jobs_decode --cmd "$decode_cmd" \
   exp/tri2a/graph data/dev exp/tri2a/decode &
 
-steps/align_si.sh --nj $nJobs --cmd "$train_cmd" \
+steps/align_si.sh --nj $num_jobs --cmd "$train_cmd" \
   data/train data/lang exp/tri2a exp/tri2a_ali || exit 1;
 
 # train and decode tri2b [LDA+MLLT]
 steps/train_lda_mllt.sh --cmd "$train_cmd" 4000 50000 \
   data/train data/lang exp/tri2a_ali exp/tri2b || exit 1;
 utils/mkgraph.sh data/lang_test exp/tri2b exp/tri2b/graph || exit 1;
-steps/decode.sh --nj $nDecodeJobs --cmd "$decode_cmd" exp/tri2b/graph data/dev exp/tri2b/decode &
+steps/decode.sh --nj $num_jobs_decode --cmd "$decode_cmd" exp/tri2b/graph data/dev exp/tri2b/decode &
 
 # Align all data with LDA+MLLT system (tri2b)
-steps/align_si.sh --nj $nJobs --cmd "$train_cmd" \
+steps/align_si.sh --nj $num_jobs --cmd "$train_cmd" \
   --use-graphs true data/train data/lang exp/tri2b exp/tri2b_ali  || exit 1;
 
 #  Do MMI on top of LDA+MLLT.
-steps/make_denlats.sh --nj $nJobs --cmd "$train_cmd" \
+steps/make_denlats.sh --nj $num_jobs --cmd "$train_cmd" \
  data/train data/lang exp/tri2b exp/tri2b_denlats || exit 1;
 
 steps/train_mmi.sh data/train data/lang exp/tri2b_ali \
  exp/tri2b_denlats exp/tri2b_mmi
 
-steps/decode.sh  --iter 4 --nj $nJobs --cmd "$decode_cmd"  exp/tri2b/graph \
+steps/decode.sh  --iter 4 --nj $num_jobs --cmd "$decode_cmd"  exp/tri2b/graph \
  data/dev exp/tri2b_mmi/decode_it4 &
-steps/decode.sh  --iter 3 --nj $nJobs --cmd "$decode_cmd" exp/tri2b/graph \
+steps/decode.sh  --iter 3 --nj $num_jobs --cmd "$decode_cmd" exp/tri2b/graph \
  data/dev exp/tri2b_mmi/decode_it3 & # Do the same with boosting.
 
 steps/train_mmi.sh --boost 0.1 data/train data/lang exp/tri2b_ali \
 exp/tri2b_denlats exp/tri2b_mmi_b0.1
 
-steps/decode.sh  --iter 4 --nj $nJobs --cmd "$decode_cmd" exp/tri2b/graph \
+steps/decode.sh  --iter 4 --nj $num_jobs --cmd "$decode_cmd" exp/tri2b/graph \
  data/dev exp/tri2b_mmi_b0.1/decode_it4 &
-steps/decode.sh  --iter 3 --nj $nJobs --cmd "$decode_cmd" exp/tri2b/graph \
+steps/decode.sh  --iter 3 --nj $num_jobs --cmd "$decode_cmd" exp/tri2b/graph \
  data/dev exp/tri2b_mmi_b0.1/decode_it3 &
 
 # Do MPE.
 steps/train_mpe.sh data/train data/lang exp/tri2b_ali exp/tri2b_denlats exp/tri2b_mpe || exit 1;
 
-steps/decode.sh  --iter 4 --nj $nDecodeJobs --cmd "$decode_cmd" exp/tri2b/graph \
+steps/decode.sh  --iter 4 --nj $num_jobs_decode --cmd "$decode_cmd" exp/tri2b/graph \
  data/dev exp/tri2b_mpe/decode_it4 &
 
-steps/decode.sh  --iter 3 --nj $nDecodeJobs --cmd "$decode_cmd" exp/tri2b/graph \
+steps/decode.sh  --iter 3 --nj $num_jobs_decode --cmd "$decode_cmd" exp/tri2b/graph \
  data/dev exp/tri2b_mpe/decode_it3 &
 
 # From 2b system, train 3b which is LDA + MLLT + SAT.
 steps/train_sat.sh --cmd "$train_cmd" \
   5000 100000 data/train data/lang exp/tri2b_ali exp/tri3b || exit 1;
 utils/mkgraph.sh data/lang_test exp/tri3b exp/tri3b/graph|| exit 1;
-steps/decode_fmllr.sh --nj $nDecodeJobs --cmd "$decode_cmd" \
+steps/decode_fmllr.sh --nj $num_jobs_decode --cmd "$decode_cmd" \
   exp/tri3b/graph data/dev exp/tri3b/decode &
 
 # From 3b system, align all data.
-steps/align_fmllr.sh --nj $nJobs --cmd "$train_cmd" \
+steps/align_fmllr.sh --nj $num_jobs --cmd "$train_cmd" \
   data/train data/lang exp/tri3b exp/tri3b_ali || exit 1;
 
 ## SGMM (subspace gaussian mixture model), excluding the "speaker-dependent weights"
@@ -163,14 +163,14 @@ steps/train_sgmm2.sh --cmd "$train_cmd" 5000 20000 data/train data/lang exp/tri3
   exp/ubm5a/final.ubm exp/sgmm_5a || exit 1;
 
 utils/mkgraph.sh data/lang_test exp/sgmm_5a exp/sgmm_5a/graph || exit 1;
-steps/decode_sgmm2.sh --nj $nDecodeJobs --cmd "$decode_cmd" --config conf/decode.config \
+steps/decode_sgmm2.sh --nj $num_jobs_decode --cmd "$decode_cmd" --config conf/decode.config \
   --transform-dir exp/tri3b/decode exp/sgmm_5a/graph data/dev exp/sgmm_5a/decode &
 
-steps/align_sgmm2.sh --nj $nJobs --cmd "$train_cmd" --transform-dir exp/tri3b_ali \
+steps/align_sgmm2.sh --nj $num_jobs --cmd "$train_cmd" --transform-dir exp/tri3b_ali \
   --use-graphs true --use-gselect true data/train data/lang exp/sgmm_5a exp/sgmm_5a_ali || exit 1;
 
 ## boosted MMI on SGMM
-steps/make_denlats_sgmm2.sh --nj $nJobs --sub-split $nJobs --beam 9.0 --lattice-beam 6 \
+steps/make_denlats_sgmm2.sh --nj $num_jobs --sub-split $num_jobs --beam 9.0 --lattice-beam 6 \
   --cmd "$decode_cmd" --num-threads 4 --transform-dir exp/tri3b_ali \
   data/train data/lang exp/sgmm_5a_ali exp/sgmm_5a_denlats || exit 1;
 
@@ -180,7 +180,7 @@ steps/train_mmi_sgmm2.sh --cmd "$train_cmd" --num-iters 8 --transform-dir exp/tr
 #decode GMM MMI
 utils/mkgraph.sh data/lang_test exp/sgmm_5a_mmi_b0.1 exp/sgmm_5a_mmi_b0.1/graph || exit 1;
 
-steps/decode_sgmm2.sh --nj $nDecodeJobs --cmd "$decode_cmd" --config conf/decode.config \
+steps/decode_sgmm2.sh --nj $num_jobs_decode --cmd "$decode_cmd" --config conf/decode.config \
   --transform-dir exp/tri3b/decode exp/sgmm_5a_mmi_b0.1/graph data/dev exp/sgmm_5a_mmi_b0.1/decode
 
 for n in 1 2 3 4; do
