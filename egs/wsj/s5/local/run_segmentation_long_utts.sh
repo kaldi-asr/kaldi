e34c6c11a2fd066b572e9e68a78a04010a155f48
Vimal Manohar <vimal.manohar91@gmail.com>
2016-12-16 13:55:00 -0500

    long_utts: Adding new results

--- egs/wsj/s5/local/run_segmentation_long_utts.sh
+++ egs/wsj/s5/local/run_segmentation_long_utts.sh
@@ -21,24 +21,26 @@ set -e -o pipefail
 # %WER 12.41 [ 1022 / 8234, 216 ins, 96 del, 710 sub ] exp/tri4c/decode_tgpr_dev93/wer_17_0.0
 
 ## Training directly on segmented data directory train_si284_reseg
-# %WER 7.55 [ 426 / 5643, 97 ins, 25 del, 304 sub ] exp/tri4c_reseg/decode_nosp_tgpr_eval92/wer_16_1.0
-# %WER 7.87 [ 444 / 5643, 113 ins, 25 del, 306 sub ] exp/tri3c_reseg/decode_nosp_tgpr_eval92/wer_14_1.0
+# %WER 7.69 [ 434 / 5643, 105 ins, 27 del, 302 sub ] exp/tri3c_reseg_d/decode_nosp_tgpr_eval92/wer_15_0.5
+# %WER 7.78 [ 439 / 5643, 105 ins, 20 del, 314 sub ] exp/tri4c_reseg_d/decode_nosp_tgpr_eval92/wer_15_0.5
 
-# %WER 11.94 [ 983 / 8234, 170 ins, 123 del, 690 sub ] exp/tri4c_reseg/decode_nosp_tgpr_dev93/wer_17_1.0
-# %WER 12.19 [ 1004 / 8234, 208 ins, 112 del, 684 sub ] exp/tri3c_reseg/decode_nosp_tgpr_dev93/wer_17_0.5
+# %WER 12.04 [ 991 / 8234, 187 ins, 119 del, 685 sub ] exp/tri4c_reseg_d/decode_nosp_tgpr_dev93/wer_16_1.0
+# %WER 12.29 [ 1012 / 8234, 224 ins, 105 del, 683 sub ] exp/tri3c_reseg_d/decode_nosp_tgpr_dev93/wer_14_0.5
 
 ## Using additional stage of cleanup.
-## Cleanup does not help here. The transcripts are already pretty good.
-# %WER 7.71 [ 435 / 5643, 110 ins, 26 del, 299 sub ] exp/tri4d_cleaned_a/decode_nosp_tgpr_eval92/wer_13_1.0
-# %WER 7.46 [ 421 / 5643, 110 ins, 18 del, 293 sub ] exp/tri4d_cleaned_c/decode_nosp_tgpr_eval92/wer_15_0.5
-# %WER 7.90 [ 446 / 5643, 106 ins, 32 del, 308 sub ] exp/tri4d_cleaned_b/decode_nosp_tgpr_eval92/wer_16_1.0
+# %WER 7.69 [ 434 / 5643, 105 ins, 23 del, 306 sub ] exp/tri4d_d_cleaned_a/decode_nosp_tgpr_eval92/wer_16_0.5
+# %WER 7.97 [ 450 / 5643, 113 ins, 20 del, 317 sub ] exp/tri4d_d_cleaned_c/decode_nosp_tgpr_eval92/wer_13_1.0
+# %WER 8.03 [ 453 / 5643, 111 ins, 31 del, 311 sub ] exp/tri4d_d_cleaned_b/decode_nosp_tgpr_eval92/wer_16_1.0
 
-# %WER 12.16 [ 1001 / 8234, 208 ins, 105 del, 688 sub ] exp/tri4d_cleaned_a/decode_nosp_tgpr_dev93/wer_15_0.5
-# %WER 12.24 [ 1008 / 8234, 213 ins, 100 del, 695 sub ] exp/tri4d_cleaned_c/decode_nosp_tgpr_dev93/wer_15_0.5
-# %WER 12.35 [ 1017 / 8234, 219 ins, 112 del, 686 sub ] exp/tri4d_cleaned_b/decode_nosp_tgpr_dev93/wer_17_0.5
+# %WER 11.84 [ 975 / 8234, 201 ins, 96 del, 678 sub ] exp/tri4d_d_cleaned_c/decode_nosp_tgpr_dev93/wer_14_0.5
+# %WER 11.96 [ 985 / 8234, 212 ins, 89 del, 684 sub ] exp/tri4d_d_cleaned_a/decode_nosp_tgpr_dev93/wer_16_0.0
+# %WER 12.47 [ 1027 / 8234, 228 ins, 105 del, 694 sub ] exp/tri4d_d_cleaned_b/decode_nosp_tgpr_dev93/wer_16_0.5
+
 
 . ./cmd.sh
 . ./path.sh
+
+affix=_d
 
 ###############################################################################
 ## Simulate unsegmented data directory.
@@ -56,39 +58,41 @@ steps/compute_cmvn_stats.sh data/train_si284_long \
 # Use a model trained on train_si84 (tri2b)
 ###############################################################################
 steps/cleanup/segment_long_utterances.sh --cmd "$train_cmd" \
+  --stage $segment_stage \
+  --config conf/segment_long_utts.conf \
   --max-segment-duration 30 --overlap-duration 5 \
-  --num-neighbors-to-search 0 --nj 80 \
-  exp/tri2b data/lang_nosp data/train_si284_long data/train_si284_reseg \
-  exp/segment_long_utts_train_si284
+  --num-neighbors-to-search 1 --nj 80 \
+  exp/tri2b data/lang_nosp data/train_si284_long data/train_si284_reseg${affix} \
+  exp/segment_long_utts${affix}_train_si284
 
-steps/compute_cmvn_stats.sh data/train_si284_reseg \
-  exp/make_mfcc/train_si284_reseg mfcc
-utils/fix_data_dir.sh data/train_si284_reseg
+steps/compute_cmvn_stats.sh data/train_si284_reseg${affix} \
+  exp/make_mfcc/train_si284_reseg${affix} mfcc
+utils/fix_data_dir.sh data/train_si284_reseg${affix}
 
 ###############################################################################
 # Train new model on segmented data directory starting from the same model
 # used for segmentation. (tri2b)
 ###############################################################################
 
-# Align tri2b system with reseg data
+# Align tri2b system with reseg${affix} data
 steps/align_si.sh  --nj 40 --cmd "$train_cmd" \
-  data/train_si284_reseg \
-  data/lang_nosp exp/tri2b exp/tri2b_ali_si284_reseg  || exit 1;
+  data/train_si284_reseg${affix} \
+  data/lang_nosp exp/tri2b exp/tri2b_ali_si284_reseg${affix}  || exit 1;
 
 # Train SAT system on reseg data
 steps/train_sat.sh --cmd "$train_cmd" 4200 40000 \
-  data/train_si284_reseg data/lang_nosp \
-  exp/tri2b_ali_si284_reseg exp/tri3c_reseg
+  data/train_si284_reseg${affix} data/lang_nosp \
+  exp/tri2b_ali_si284_reseg${affix} exp/tri3c_reseg${affix}
 
 (
 utils/mkgraph.sh data/lang_nosp_test_tgpr \
-  exp/tri3c_reseg exp/tri3c_reseg/graph_nosp_tgpr || exit 1;
+  exp/tri3c_reseg${affix} exp/tri3c_reseg${affix}/graph_nosp_tgpr || exit 1;
 steps/decode_fmllr.sh --nj 10 --cmd "$decode_cmd" \
-  exp/tri3c_reseg/graph_nosp_tgpr data/test_dev93 \
-  exp/tri3c_reseg/decode_nosp_tgpr_dev93 || exit 1;
+  exp/tri3c_reseg${affix}/graph_nosp_tgpr data/test_dev93 \
+  exp/tri3c_reseg${affix}/decode_nosp_tgpr_dev93 || exit 1;
 steps/decode_fmllr.sh --nj 8 --cmd "$decode_cmd" \
-  exp/tri3c_reseg/graph_nosp_tgpr data/test_eval92 \
-  exp/tri3c_reseg/decode_nosp_tgpr_eval92 || exit 1;
+  exp/tri3c_reseg${affix}/graph_nosp_tgpr data/test_eval92 \
+  exp/tri3c_reseg${affix}/decode_nosp_tgpr_eval92 || exit 1;
 ) &
 
 ###############################################################################
@@ -98,23 +102,23 @@ steps/decode_fmllr.sh --nj 8 --cmd "$decode_cmd" \
 
 # Align tri3b system with reseg data
 steps/align_fmllr.sh --nj 40 --cmd "$train_cmd" \
-  data/train_si284_reseg data/lang_nosp exp/tri3b \
-  exp/tri3b_ali_si284_reseg
+  data/train_si284_reseg${affix} data/lang_nosp exp/tri3b \
+  exp/tri3b_ali_si284_reseg${affix}
 
 # Train SAT system on reseg data
 steps/train_sat.sh --cmd "$train_cmd" 4200 40000 \
-  data/train_si284_reseg data/lang_nosp \
-  exp/tri3b_ali_si284_reseg exp/tri4c_reseg
+  data/train_si284_reseg${affix} data/lang_nosp \
+  exp/tri3b_ali_si284_reseg${affix} exp/tri4c_reseg${affix}
 
 (
  utils/mkgraph.sh data/lang_nosp_test_tgpr \
-   exp/tri4c_reseg exp/tri4c_reseg/graph_nosp_tgpr || exit 1;
+   exp/tri4c_reseg${affix} exp/tri4c_reseg${affix}/graph_nosp_tgpr || exit 1;
  steps/decode_fmllr.sh --nj 10 --cmd "$decode_cmd" \
-   exp/tri4c_reseg/graph_nosp_tgpr data/test_dev93 \
-   exp/tri4c_reseg/decode_nosp_tgpr_dev93 || exit 1;
+   exp/tri4c_reseg${affix}/graph_nosp_tgpr data/test_dev93 \
+   exp/tri4c_reseg${affix}/decode_nosp_tgpr_dev93 || exit 1;
  steps/decode_fmllr.sh --nj 8 --cmd "$decode_cmd" \
-   exp/tri4c_reseg/graph_nosp_tgpr data/test_eval92 \
-   exp/tri4c_reseg/decode_nosp_tgpr_eval92 || exit 1;
+   exp/tri4c_reseg${affix}/graph_nosp_tgpr data/test_eval92 \
+   exp/tri4c_reseg${affix}/decode_nosp_tgpr_eval92 || exit 1;
 ) &
 
 ###############################################################################
@@ -123,8 +127,8 @@ steps/train_sat.sh --cmd "$train_cmd" 4200 40000 \
 
 steps/cleanup/clean_and_segment_data.sh --cmd "$train_cmd" \
   --nj 80 \
-  data/train_si284_reseg data/lang_nosp \
-  exp/tri3b_ali_si284_reseg exp/tri3b_work_si284_reseg data/train_si284_reseg_cleaned_a
+  data/train_si284_reseg${affix} data/lang_nosp \
+  exp/tri3b_ali_si284_reseg${affix} exp/tri3b_work_si284_reseg${affix} data/train_si284_reseg${affix}_cleaned_a
 
 ###############################################################################
 # Train new model on the cleaned_a data directory
@@ -132,23 +136,23 @@ steps/cleanup/clean_and_segment_data.sh --cmd "$train_cmd" \
 
 # Align tri3b system with cleaned data
 steps/align_fmllr.sh --nj 40 --cmd "$train_cmd" \
-  data/train_si284_reseg_cleaned_a data/lang_nosp exp/tri3b \
-  exp/tri3b_ali_si284_reseg_cleaned_a
+  data/train_si284_reseg${affix}_cleaned_a data/lang_nosp exp/tri3b \
+  exp/tri3b_ali_si284_reseg${affix}_cleaned_a
 
 # Train SAT system on cleaned data
 steps/train_sat.sh --cmd "$train_cmd" 4200 40000 \
-  data/train_si284_reseg_cleaned_a data/lang_nosp \
-  exp/tri3b_ali_si284_reseg_cleaned_a exp/tri4d_cleaned_a
+  data/train_si284_reseg${affix}_cleaned_a data/lang_nosp \
+  exp/tri3b_ali_si284_reseg${affix}_cleaned_a exp/tri4d${affix}_cleaned_a
 
 (
  utils/mkgraph.sh data/lang_nosp_test_tgpr \
-   exp/tri4d_cleaned_a exp/tri4d_cleaned_a/graph_nosp_tgpr || exit 1;
+   exp/tri4d${affix}_cleaned_a exp/tri4d${affix}_cleaned_a/graph_nosp_tgpr || exit 1;
  steps/decode_fmllr.sh --nj 10 --cmd "$decode_cmd" \
-   exp/tri4d_cleaned_a/graph_nosp_tgpr data/test_dev93 \
-   exp/tri4d_cleaned_a/decode_nosp_tgpr_dev93 || exit 1;
+   exp/tri4d${affix}_cleaned_a/graph_nosp_tgpr data/test_dev93 \
+   exp/tri4d${affix}_cleaned_a/decode_nosp_tgpr_dev93 || exit 1;
  steps/decode_fmllr.sh --nj 8 --cmd "$decode_cmd" \
-   exp/tri4d_cleaned_a/graph_nosp_tgpr data/test_eval92 \
-   exp/tri4d_cleaned_a/decode_nosp_tgpr_eval92 || exit 1;
+   exp/tri4d${affix}_cleaned_a/graph_nosp_tgpr data/test_eval92 \
+   exp/tri4d${affix}_cleaned_a/decode_nosp_tgpr_eval92 || exit 1;
 ) &
 
 ###############################################################################
@@ -158,9 +162,9 @@ steps/train_sat.sh --cmd "$train_cmd" 4200 40000 \
 
 steps/cleanup/clean_and_segment_data.sh --cmd "$train_cmd" \
   --nj 80 \
-  data/train_si284_reseg data/lang_nosp \
-  exp/tri3c_reseg exp/tri3c_reseg_work_si284_reseg \
-  data/train_si284_reseg_cleaned_b
+  data/train_si284_reseg${affix} data/lang_nosp \
+  exp/tri3c_reseg${affix} exp/tri3c_reseg${affix}_work_si284_reseg${affix} \
+  data/train_si284_reseg${affix}_cleaned_b
 
 ###############################################################################
 # Train new model on the cleaned_b data directory
@@ -168,23 +172,23 @@ steps/cleanup/clean_and_segment_data.sh --cmd "$train_cmd" \
 
 # Align tri3c_reseg system with cleaned data
 steps/align_fmllr.sh --nj 40 --cmd "$train_cmd" \
-  data/train_si284_reseg_cleaned_b data/lang_nosp exp/tri3c_reseg \
-  exp/tri3c_reseg_ali_si284_reseg_cleaned_b
+  data/train_si284_reseg${affix}_cleaned_b data/lang_nosp exp/tri3c_reseg${affix} \
+  exp/tri3c_reseg${affix}_ali_si284_reseg${affix}_cleaned_b
 
 # Train SAT system on cleaned data
 steps/train_sat.sh --cmd "$train_cmd" 4200 40000 \
-  data/train_si284_reseg_cleaned_b data/lang_nosp \
-  exp/tri3c_reseg_ali_si284_reseg_cleaned_b exp/tri4d_cleaned_b
+  data/train_si284_reseg${affix}_cleaned_b data/lang_nosp \
+  exp/tri3c_reseg${affix}_ali_si284_reseg${affix}_cleaned_b exp/tri4d${affix}_cleaned_b
 
 (
  utils/mkgraph.sh data/lang_nosp_test_tgpr \
-   exp/tri4d_cleaned_b exp/tri4d_cleaned_b/graph_nosp_tgpr || exit 1;
+   exp/tri4d${affix}_cleaned_b exp/tri4d${affix}_cleaned_b/graph_nosp_tgpr || exit 1;
  steps/decode_fmllr.sh --nj 10 --cmd "$decode_cmd" \
-   exp/tri4d_cleaned_b/graph_nosp_tgpr data/test_dev93 \
-   exp/tri4d_cleaned_b/decode_nosp_tgpr_dev93 || exit 1;
+   exp/tri4d${affix}_cleaned_b/graph_nosp_tgpr data/test_dev93 \
+   exp/tri4d${affix}_cleaned_b/decode_nosp_tgpr_dev93 || exit 1;
  steps/decode_fmllr.sh --nj 8 --cmd "$decode_cmd" \
-   exp/tri4d_cleaned_b/graph_nosp_tgpr data/test_eval92 \
-   exp/tri4d_cleaned_b/decode_nosp_tgpr_eval92 || exit 1;
+   exp/tri4d${affix}_cleaned_b/graph_nosp_tgpr data/test_eval92 \
+   exp/tri4d${affix}_cleaned_b/decode_nosp_tgpr_eval92 || exit 1;
 ) &
 
 ###############################################################################
@@ -194,9 +198,9 @@ steps/train_sat.sh --cmd "$train_cmd" 4200 40000 \
 
 steps/cleanup/clean_and_segment_data.sh --cmd "$train_cmd" \
   --nj 80 \
-  data/train_si284_reseg data/lang_nosp \
-  exp/tri4c_reseg exp/tri4c_reseg_work_si284_reseg \
-  data/train_si284_reseg_cleaned_c
+  data/train_si284_reseg${affix} data/lang_nosp \
+  exp/tri4c_reseg${affix} exp/tri4c_reseg${affix}_work_si284_reseg${affix} \
+  data/train_si284_reseg${affix}_cleaned_c
 
 ###############################################################################
 # Train new model on the cleaned_c data directory
@@ -204,21 +208,21 @@ steps/cleanup/clean_and_segment_data.sh --cmd "$train_cmd" \
 
 # Align tri4c_reseg system with cleaned data
 steps/align_fmllr.sh --nj 40 --cmd "$train_cmd" \
-  data/train_si284_reseg_cleaned_c data/lang_nosp exp/tri4c_reseg \
-  exp/tri4c_reseg_ali_si284_reseg_cleaned_c
+  data/train_si284_reseg${affix}_cleaned_c data/lang_nosp exp/tri4c_reseg${affix} \
+  exp/tri4c_reseg${affix}_ali_si284_reseg${affix}_cleaned_c
 
 # Train SAT system on cleaned data
 steps/train_sat.sh --cmd "$train_cmd" 4200 40000 \
-  data/train_si284_reseg_cleaned_c data/lang_nosp \
-  exp/tri4c_reseg_ali_si284_reseg_cleaned_c exp/tri4d_cleaned_c
+  data/train_si284_reseg${affix}_cleaned_c data/lang_nosp \
+  exp/tri4c_reseg${affix}_ali_si284_reseg${affix}_cleaned_c exp/tri4d${affix}_cleaned_c
 
 (
  utils/mkgraph.sh data/lang_nosp_test_tgpr \
-   exp/tri4d_cleaned_c exp/tri4d_cleaned_c/graph_nosp_tgpr || exit 1;
+   exp/tri4d${affix}_cleaned_c exp/tri4d${affix}_cleaned_c/graph_nosp_tgpr || exit 1;
  steps/decode_fmllr.sh --nj 10 --cmd "$decode_cmd" \
-   exp/tri4d_cleaned_c/graph_nosp_tgpr data/test_dev93 \
-   exp/tri4d_cleaned_c/decode_nosp_tgpr_dev93 || exit 1;
+   exp/tri4d${affix}_cleaned_c/graph_nosp_tgpr data/test_dev93 \
+   exp/tri4d${affix}_cleaned_c/decode_nosp_tgpr_dev93 || exit 1;
  steps/decode_fmllr.sh --nj 8 --cmd "$decode_cmd" \
-   exp/tri4d_cleaned_c/graph_nosp_tgpr data/test_eval92 \
-   exp/tri4d_cleaned_c/decode_nosp_tgpr_eval92 || exit 1;
+   exp/tri4d${affix}_cleaned_c/graph_nosp_tgpr data/test_eval92 \
+   exp/tri4d${affix}_cleaned_c/decode_nosp_tgpr_eval92 || exit 1;
 ) &
