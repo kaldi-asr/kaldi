7708fdd92631202477f5d3a17e03a856bde251cf
Xingyu Na <asr.naxingyu@gmail.com>
2016-12-30 04:54:07 -0500

    update print() invoke in python script

--- egs/wsj/s5/utils/data/limit_feature_dim.sh
+++ egs/wsj/s5/utils/data/limit_feature_dim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+77;20003;0c
 # Copyright 2016  Alibaba Robotics Corp. (author: Xingyu Na)
 # Apache 2.0
 
@@ -14,7 +14,7 @@ if [ $# != 3 ]; then
   echo "The script creates a new data directory by selecting a specified"
   echo "dimension range of the features in the source directory."
   echo "e.g.:"
-  echo " $0 0:40 data/train_hires_pitch data/train_hires"
+  echo " $0 0:39 data/train_hires_pitch data/train_hires"
   exit 1;
 fi
 
