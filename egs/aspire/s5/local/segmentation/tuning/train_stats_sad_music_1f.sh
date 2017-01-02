a638ccad32b7baaedddb5c984e61ac938db0d136
Vimal Manohar <vimal.manohar91@gmail.com>
2017-01-02 18:37:34 -0500

    asr_diarization: more tuning scripts for music detection

--- egs/aspire/s5/local/segmentation/tuning/train_stats_sad_music_1f.sh
+++ egs/aspire/s5/local/segmentation/tuning/train_stats_sad_music_1f.sh
@@ -2,9 +2,10 @@
 
 # This is a script to train a time-delay neural network for speech activity detection (SAD) and
 # music-id using statistic pooling component for long-context information.
+# This script is same as 1e, but removes the stats component.
 
 set -o pipefail
-set -e 
+set -e
 set -u
 
 . cmd.sh
@@ -21,13 +22,15 @@ egs_opts=   # Directly passed to get_egs_multiple_targets.py
 # TDNN options
 relu_dim=256
 chunk_width=20  # We use chunk training for training TDNN
-extra_left_context=100  # Maximum left context in egs apart from TDNN's left context 
-extra_right_context=20  # Maximum right context in egs apart from TDNN's right context 
+num_chunk_per_minibatch=64
+
+extra_left_context=79 # Maximum left context in egs apart from TDNN's left context
+extra_right_context=11  # Maximum right context in egs apart from TDNN's right context
 
 # We randomly select an extra {left,right} context for each job between
 # min_extra_*_context and extra_*_context so that the network can get used
 # to different contexts used to compute statistics.
-min_extra_left_context=20   
+min_extra_left_context=20
 min_extra_right_context=0
 
 # training options
@@ -45,12 +48,13 @@ num_utts_subset_valid=50    # "utts" is actually recording. So this is prettly s
 num_utts_subset_train=50
 
 # target options
-train_data_dir=data/train_azteec_whole_sp_corrupted_hires
+train_data_dir=data/train_aztec_small_unsad_whole_all_corrupted_sp_hires_bp
 
-speech_feat_scp=
-music_labels_scp=
+speech_feat_scp=data/train_aztec_unsad_whole_corrupted_sp_hires_bp_2400/speech_feat.scp
+deriv_weights_scp=data/train_aztec_unsad_whole_corrupted_sp_hires_bp_2400/deriv_weights.scp
+music_labels_scp=data/train_aztec_small_unsad_whole_music_corrupted_sp_hires_bp/music_labels.scp
 
-deriv_weights_scp=
+extra_egs_copy_cmd="nnet3-copy-egs --keep-outputs=output-speech,output-music ark:- ark:- |"
 
 egs_dir=
 nj=40
@@ -71,16 +75,16 @@ fi
 dir=$dir${affix:+_$affix}
 
 if ! cuda-compiled; then
-  cat <<EOF && exit 1 
-This script is intended to be used with GPUs but you have not compiled Kaldi with CUDA 
+  cat <<EOF && exit 1
+This script is intended to be used with GPUs but you have not compiled Kaldi with CUDA
 If you want to use GPUs (and have them), go to src/, and configure and make on a machine
 where "nvcc" is installed.
 EOF
 fi
-  
+
 mkdir -p $dir
 
-if [ $stage -le 3 ]; then
+if [ $stage -le 1 ]; then
   echo "$0: creating neural net configs using the xconfig parser";
 
   mkdir -p $dir/configs
@@ -88,59 +92,96 @@ if [ $stage -le 3 ]; then
   input dim=`feat-to-dim scp:$train_data_dir/feats.scp -` name=input
   output name=output-temp input=Append(-3,-2,-1,0,1,2,3)
 
-  relu-renorm-layer name=tdnn1 input=Append(input@-3, input@-2, input@-1, input, input@1, input@2, input@3) dim=256
-  stats-layer name=tdnn2_stats config=mean+count(-99:3:9:99)
-  relu-renorm-layer name=tdnn2 input=Append(tdnn1@-9, tdnn1@-3, tdnn1, tdnn1@3, tdnn2_stats) dim=256
-  stats-layer name=tdnn3_stats config=mean+count(-108:9:27:108)
-  relu-renorm-layer name=tdnn3 input=Append(tdnn2@-27, tdnn2@-9, tdnn2, tdnn2@9, tdnn3_stats) dim=256
-  relu-renorm-layer name=tdnn4 dim=256
+  relu-renorm-layer name=tdnn1 input=Append(input@-3, input@-2, input@-1, input, input@1, input@2, input@3) dim=256 add-log-stddev=true
+  relu-renorm-layer name=tdnn2 input=Append(tdnn1@-9, tdnn1@-3, tdnn1, tdnn1@3) dim=256 add-log-stddev=true
+  relu-renorm-layer name=tdnn3 input=Append(tdnn2@-27, tdnn2@-9, tdnn2, tdnn2@9) dim=256 add-log-stddev=true
 
-  output-layer name=output-speech include-log-softmax=true dim=2 input=tdnn4 
-  output-layer name=output-music include-log-softmax=true dim=2 input=tdnn4
+  output-layer name=output-speech include-log-softmax=true dim=2 input=tdnn3
+  output-layer name=output-music include-log-softmax=true dim=2 input=tdnn3
 EOF
   steps/nnet3/xconfig_to_configs.py --xconfig-file $dir/configs/network.xconfig \
     --config-dir $dir/configs/ \
     --nnet-edits="rename-node old-name=output-speech new-name=output"
-  
+
   cat <<EOF >> $dir/configs/vars
 add_lda=false
 EOF
 fi
 
+samples_per_iter=`perl -e "print int(400000 / $chunk_width)"`
+speech_data_dir=$dir/`basename $train_data_dir`_speech
+music_data_dir=$dir/`basename $train_data_dir`_music
+
 if [ -z "$egs_dir" ]; then
-  egs_dir=$dir/egs
-  if [ $stage -le 4 ]; then
-    if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d $dir/egs/storage ]; then
+  egs_dir=$dir/egs_multi
+  if [ $stage -le 2 ]; then
+    if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d $dir/egs_speech/storage ]; then
       utils/create_split_dir.pl \
-        /export/b{03,04,05,06}/$USER/kaldi-data/egs/aspire-$(date +'%m_%d_%H_%M')/s5/$dir/egs/storage $dir/egs/storage
+        /export/b{03,04,05,06}/$USER/kaldi-data/egs/aspire-$(date +'%m_%d_%H_%M')/s5/$dir/egs_speech/storage $dir/egs_speech/storage
     fi
+
 
     . $dir/configs/vars
 
+    utils/subset_data_dir.sh --utt-list $speech_feat_scp ${train_data_dir} $dir/`basename ${train_data_dir}`_speech
+
     steps/nnet3/get_egs_multiple_targets.py --cmd="$decode_cmd" \
       $egs_opts \
-      --feat.dir="$train_data_dir" \
+      --feat.dir="$speech_data_dir" \
       --feat.cmvn-opts="--norm-means=false --norm-vars=false" \
       --frames-per-eg=$chunk_width \
       --left-context=$[model_left_context + extra_left_context] \
       --right-context=$[model_right_context + extra_right_context] \
       --num-utts-subset-train=$num_utts_subset_train \
       --num-utts-subset-valid=$num_utts_subset_valid \
-      --samples-per-iter=20000 \
+      --samples-per-iter=$samples_per_iter \
       --stage=$get_egs_stage \
       --targets-parameters="--output-name=output-speech --target-type=sparse --dim=2 --targets-scp=$speech_feat_scp --deriv-weights-scp=$deriv_weights_scp --scp2ark-cmd=\"extract-column --column-index=0 scp:- ark,t:- | steps/segmentation/quantize_vector.pl | ali-to-post ark,t:- ark:- |\" --compress=true" \
+      --generate-egs-scp=true \
+      --dir=$dir/egs_speech
+  fi
+
+  if [ $stage -le 3 ]; then
+    if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d $dir/egs_music/storage ]; then
+      utils/create_split_dir.pl \
+        /export/b{03,04,05,06}/$USER/kaldi-data/egs/aspire-$(date +'%m_%d_%H_%M')/s5/$dir/egs_music/storage $dir/egs_music/storage
+    fi
+
+    . $dir/configs/vars
+    utils/subset_data_dir.sh --utt-list $music_labels_scp ${train_data_dir} $dir/`basename ${train_data_dir}`_music
+
+    steps/nnet3/get_egs_multiple_targets.py --cmd="$decode_cmd" \
+      $egs_opts \
+      --feat.dir="$music_data_dir" \
+      --feat.cmvn-opts="--norm-means=false --norm-vars=false" \
+      --frames-per-eg=$chunk_width \
+      --left-context=$[model_left_context + extra_left_context] \
+      --right-context=$[model_right_context + extra_right_context] \
+      --num-utts-subset-train=$num_utts_subset_train \
+      --num-utts-subset-valid=$num_utts_subset_valid \
+      --samples-per-iter=$samples_per_iter \
+      --stage=$get_egs_stage \
       --targets-parameters="--output-name=output-music --target-type=sparse --dim=2 --targets-scp=$music_labels_scp --scp2ark-cmd=\"ali-to-post scp:- ark:- |\" --compress=true" \
-      --dir=$dir/egs
+      --generate-egs-scp=true \
+      --dir=$dir/egs_music
+  fi
+
+  if [ $stage -le 4 ]; then
+    steps/nnet3/multilingual/get_egs.sh \
+      --minibatch-size $[chunk_width * num_chunk_per_minibatch] \
+      --samples-per-iter $samples_per_iter \
+      2 $dir/egs_speech $dir/egs_music $dir/egs_multi
   fi
 fi
 
 if [ $stage -le 5 ]; then
   steps/nnet3/train_raw_rnn.py --stage=$train_stage \
     --feat.cmvn-opts="--norm-means=false --norm-vars=false" \
-    --egs.chunk-width=20 \
+    --egs.chunk-width=$chunk_width \
     --egs.dir="$egs_dir" --egs.stage=$get_egs_stage \
     --egs.chunk-left-context=$extra_left_context \
     --egs.chunk-right-context=$extra_right_context \
+    --egs.use-multitask-egs=true --egs.rename-multitask-outputs=false \
     ${extra_egs_copy_cmd:+--egs.extra-copy-cmd="$extra_egs_copy_cmd"} \
     --trainer.min-chunk-left-context=$min_extra_left_context \
     --trainer.min-chunk-right-context=$min_extra_right_context \
@@ -151,7 +192,7 @@ if [ $stage -le 5 ]; then
     --trainer.optimization.initial-effective-lrate=$initial_effective_lrate \
     --trainer.optimization.final-effective-lrate=$final_effective_lrate \
     --trainer.optimization.shrink-value=1.0 \
-    --trainer.rnn.num-chunk-per-minibatch=64 \
+    --trainer.rnn.num-chunk-per-minibatch=$num_chunk_per_minibatch \
     --trainer.deriv-truncate-margin=8 \
     --trainer.max-param-change=$max_param_change \
     --cmd="$decode_cmd" --nj 40 \
@@ -175,7 +216,7 @@ if [ $stage -le 6 ]; then
     matrix-sum-rows ark:- ark:- \| \
     vector-sum ark:- $dir/post_output-speech.vec.JOB
   eval vector-sum $dir/post_output-speech.vec.{`seq -s, 100`} $dir/post_output-speech.vec
-  
+
   $train_cmd JOB=1:100 $dir/log/compute_post_output-music.JOB.log \
     ali-to-post "scp:utils/split_scp.pl -j 100 \$[JOB-1] $music_labels_scp |" ark:- \| \
     post-to-feats --post-dim=2 ark:- ark:- \| \
@@ -183,3 +224,4 @@ if [ $stage -le 6 ]; then
     vector-sum ark:- $dir/post_output-music.vec.JOB
   eval vector-sum $dir/post_output-music.vec.{`seq -s, 100`} $dir/post_output-music.vec
 fi
+
--- egs/aspire/s5/local/segmentation/tuning/train_stats_sad_music_1g.sh
+++ egs/aspire/s5/local/segmentation/tuning/train_stats_sad_music_1g.sh
@@ -2,7 +2,7 @@
 
 # This is a script to train a time-delay neural network for speech activity detection (SAD) and
 # music-id using statistic pooling component for long-context information.
-# This script is same as 1d, but add add-log-stddev to norm layers.
+# This script is same as 1e, but removes the stats component in the 3rd layer.
 
 set -o pipefail
 set -e
@@ -30,8 +30,8 @@ extra_right_context=11  # Maximum right context in egs apart from TDNN's right c
 # We randomly select an extra {left,right} context for each job between
 # min_extra_*_context and extra_*_context so that the network can get used
 # to different contexts used to compute statistics.
-min_extra_left_context=79
-min_extra_right_context=11
+min_extra_left_context=20
+min_extra_right_context=0
 
 # training options
 num_epochs=2
@@ -44,15 +44,8 @@ max_param_change=0.2  # Small max-param change for small network
 extra_egs_copy_cmd=   # Used if you want to do some weird stuff to egs
                       # such as removing one of the targets
 
-num_utts_subset_valid=50    # "utts" is actually recording. So this is prettly small.
-num_utts_subset_train=50
-
-# target options
-train_data_dir=data/train_aztec_small_unsad_whole_all_corrupted_sp_hires_bp
-
-speech_feat_scp=data/train_aztec_unsad_whole_corrupted_sp_hires_bp_2400/speech_feat.scp
-deriv_weights_scp=data/train_aztec_unsad_whole_corrupted_sp_hires_bp_2400/deriv_weights.scp
-music_labels_scp=data/train_aztec_small_unsad_whole_music_corrupted_sp_hires_bp/music_labels.scp
+sad_data_dir=data/train_aztec_unsad_whole_corrupted_sp_hires_bp_2400
+music_data_dir=data/train_aztec_unsad_whole_music_corrupted_sp_hires_bp
 
 extra_egs_copy_cmd="nnet3-copy-egs --keep-outputs=output-speech,output-music ark:- ark:- |"
 
@@ -67,6 +60,10 @@ affix=a
 . cmd.sh
 . ./path.sh
 . ./utils/parse_options.sh
+
+num_utts=`cat $sad_data_dir/utt2spk $music_data_dir/utt2spk | wc -l`
+num_utts_subset_valid=`perl -e '$n=int($ARGV[0] * 0.005); print ($n > 4000 ? 4000 : $n)' $num_utts`
+num_utts_subset_train=`perl -e '$n=int($ARGV[0] * 0.005); print ($n > 4000 ? 4000 : $n)' $num_utts`
 
 if [ -z "$dir" ]; then
   dir=exp/nnet3_stats_sad_music/nnet_tdnn
@@ -84,21 +81,50 @@ fi
 
 mkdir -p $dir
 
+if [ $stage -le 0 ]; then
+  utils/split_data.sh $music_data_dir 100
+  utils/split_data.sh $sad_data_dir 100
+
+  $train_cmd JOB=1:100 $dir/log/compute_post_output-speech.JOB.log \
+    extract-column "scp:utils/filter_scp.pl $sad_data_dir/split100/JOB/utt2spk $sad_data_dir/speech_feat.scp |" ark,t:- \| \
+    steps/segmentation/quantize_vector.pl \| \
+    ali-to-post ark,t:- ark:- \| \
+    weight-post ark:- scp:$sad_data_dir/deriv_weights.scp ark:- \| \
+    post-to-feats --post-dim=2 ark:- ark:- \| \
+    matrix-sum-rows ark:- ark:- \| \
+    vector-sum ark:- $dir/post_output-speech.vec.JOB
+  eval vector-sum $dir/post_output-speech.vec.{`seq -s, 100`} $dir/post_output-speech.vec
+  rm $dir/post_output-speech.vec.*
+
+  $train_cmd JOB=1:100 $dir/log/compute_post_output-music.JOB.log \
+    ali-to-post "scp:utils/filter_scp.pl $music_data_dir/split100/JOB/utt2spk $music_data_dir/music_labels.scp |" ark:- \| \
+    post-to-feats --post-dim=2 ark:- ark:- \| \
+    matrix-sum-rows ark:- ark:- \| \
+    vector-sum ark:- $dir/post_output-music.vec.JOB
+  eval vector-sum $dir/post_output-music.vec.{`seq -s, 100`} $dir/post_output-music.vec
+  rm $dir/post_output-music.vec.*
+fi
+
+num_frames_sad=`copy-vector --binary=false $dir/post_output-speech.vec - | awk '{print $2+$3}'`
+num_frames_music=`copy-vector --binary=false $dir/post_output-music.vec - | awk '{print $2+$3}'`
+
 if [ $stage -le 1 ]; then
   echo "$0: creating neural net configs using the xconfig parser";
 
+  num_snr_bins=`feat-to-dim scp:$sad_data_dir/irm_targets.scp -`
+
   mkdir -p $dir/configs
   cat <<EOF > $dir/configs/network.xconfig
-  input dim=`feat-to-dim scp:$train_data_dir/feats.scp -` name=input
+  input dim=`feat-to-dim scp:$sad_data_dir/feats.scp -` name=input
   output name=output-temp input=Append(-3,-2,-1,0,1,2,3)
 
   relu-renorm-layer name=tdnn1 input=Append(input@-3, input@-2, input@-1, input, input@1, input@2, input@3) dim=256 add-log-stddev=true
   stats-layer name=tdnn2_stats config=mean+count(-99:3:9:99)
   relu-renorm-layer name=tdnn2 input=Append(tdnn1@-9, tdnn1@-3, tdnn1, tdnn1@3, tdnn2_stats) dim=256 add-log-stddev=true
-  stats-layer name=tdnn3_stats config=mean+count(-108:9:27:108)
-  relu-renorm-layer name=tdnn3 input=Append(tdnn2@-27, tdnn2@-9, tdnn2, tdnn2@9, tdnn3_stats) dim=256 add-log-stddev=true
+  relu-renorm-layer name=tdnn3 input=Append(tdnn2@-27, tdnn2@-9, tdnn2, tdnn2@9) dim=256 add-log-stddev=true
 
-  output-layer name=output-speech include-log-softmax=true dim=2 input=tdnn3
+  output-layer name=output-snr include-log-softmax=false dim=$num_snr_bins objective-type=quadratic objective-scale=`perl -e "print (($num_frames_music / $num_frames_sad) ** 0.25) / $num_snr_bins"` input=tdnn3
+  output-layer name=output-speech include-log-softmax=true dim=2 input=tdnn3 objective-scale=`perl -e "print (($num_frames_music / $num_frames_sad) ** 0.25)"`
   output-layer name=output-music include-log-softmax=true dim=2 input=tdnn3
 EOF
   steps/nnet3/xconfig_to_configs.py --xconfig-file $dir/configs/network.xconfig \
@@ -111,8 +137,6 @@ EOF
 fi
 
 samples_per_iter=`perl -e "print int(400000 / $chunk_width)"`
-speech_data_dir=$dir/`basename $train_data_dir`_speech
-music_data_dir=$dir/`basename $train_data_dir`_music
 
 if [ -z "$egs_dir" ]; then
   egs_dir=$dir/egs_multi
@@ -122,14 +146,11 @@ if [ -z "$egs_dir" ]; then
         /export/b{03,04,05,06}/$USER/kaldi-data/egs/aspire-$(date +'%m_%d_%H_%M')/s5/$dir/egs_speech/storage $dir/egs_speech/storage
     fi
 
-
     . $dir/configs/vars
-
-    utils/subset_data_dir.sh --utt-list $speech_feat_scp ${train_data_dir} $dir/`basename ${train_data_dir}`_speech
 
     steps/nnet3/get_egs_multiple_targets.py --cmd="$decode_cmd" \
       $egs_opts \
-      --feat.dir="$speech_data_dir" \
+      --feat.dir="$sad_data_dir" \
       --feat.cmvn-opts="--norm-means=false --norm-vars=false" \
       --frames-per-eg=$chunk_width \
       --left-context=$[model_left_context + extra_left_context] \
@@ -138,7 +159,8 @@ if [ -z "$egs_dir" ]; then
       --num-utts-subset-valid=$num_utts_subset_valid \
       --samples-per-iter=$samples_per_iter \
       --stage=$get_egs_stage \
-      --targets-parameters="--output-name=output-speech --target-type=sparse --dim=2 --targets-scp=$speech_feat_scp --deriv-weights-scp=$deriv_weights_scp --scp2ark-cmd=\"extract-column --column-index=0 scp:- ark,t:- | steps/segmentation/quantize_vector.pl | ali-to-post ark,t:- ark:- |\" --compress=true" \
+      --targets-parameters="--output-name=output-snr --target-type=dense --targets-scp=$sad_data_dir/irm_targets.scp --deriv-weights-scp=$sad_data_dir/deriv_weights_manual_seg.scp" \
+      --targets-parameters="--output-name=output-speech --target-type=sparse --dim=2 --targets-scp=$sad_data_dir/speech_feat.scp --deriv-weights-scp=$sad_data_dir/deriv_weights.scp --scp2ark-cmd=\"extract-column --column-index=0 scp:- ark,t:- | steps/segmentation/quantize_vector.pl | ali-to-post ark,t:- ark:- |\" --compress=true" \
       --generate-egs-scp=true \
       --dir=$dir/egs_speech
   fi
@@ -150,7 +172,6 @@ if [ -z "$egs_dir" ]; then
     fi
 
     . $dir/configs/vars
-    utils/subset_data_dir.sh --utt-list $music_labels_scp ${train_data_dir} $dir/`basename ${train_data_dir}`_music
 
     steps/nnet3/get_egs_multiple_targets.py --cmd="$decode_cmd" \
       $egs_opts \
@@ -169,8 +190,11 @@ if [ -z "$egs_dir" ]; then
   fi
 
   if [ $stage -le 4 ]; then
+    # num_chunk_per_minibatch is multiplied by 4 to allow a buffer to use
+    # the same egs with a different num_chunk_per_minibatch
     steps/nnet3/multilingual/get_egs.sh \
-      --minibatch-size $[chunk_width * num_chunk_per_minibatch] \
+      --cmd "$train_cmd" \
+      --minibatch-size $[num_chunk_per_minibatch * 4] \
       --samples-per-iter $samples_per_iter \
       2 $dir/egs_speech $dir/egs_music $dir/egs_multi
   fi
@@ -203,27 +227,8 @@ if [ $stage -le 5 ]; then
     --cleanup.preserve-model-interval=10 \
     --use-gpu=true \
     --use-dense-targets=false \
-    --feat-dir=$train_data_dir \
-    --targets-scp="$speech_feat_scp" \
+    --feat-dir=$sad_data_dir \
+    --targets-scp="$sad_data_dir/speech_feat.scp" \
     --dir=$dir || exit 1
-fi
-
-if [ $stage -le 6 ]; then
-  $train_cmd JOB=1:100 $dir/log/compute_post_output-speech.JOB.log \
-    extract-column "scp:utils/split_scp.pl -j 100 \$[JOB-1] $speech_feat_scp |" ark,t:- \| \
-    steps/segmentation/quantize_vector.pl \| \
-    ali-to-post ark,t:- ark:- \| \
-    weight-post ark:- scp:$deriv_weights_scp ark:- \| \
-    post-to-feats --post-dim=2 ark:- ark:- \| \
-    matrix-sum-rows ark:- ark:- \| \
-    vector-sum ark:- $dir/post_output-speech.vec.JOB
-  eval vector-sum $dir/post_output-speech.vec.{`seq -s, 100`} $dir/post_output-speech.vec
-
-  $train_cmd JOB=1:100 $dir/log/compute_post_output-music.JOB.log \
-    ali-to-post "scp:utils/split_scp.pl -j 100 \$[JOB-1] $music_labels_scp |" ark:- \| \
-    post-to-feats --post-dim=2 ark:- ark:- \| \
-    matrix-sum-rows ark:- ark:- \| \
-    vector-sum ark:- $dir/post_output-music.vec.JOB
-  eval vector-sum $dir/post_output-music.vec.{`seq -s, 100`} $dir/post_output-music.vec
 fi
 
