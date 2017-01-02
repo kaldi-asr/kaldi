cefc844d041165c55b3f36060ef4467ad3596b9e
Xingyu Na <asr.naxingyu@gmail.com>
2017-01-03 04:59:07 +0800

    [egs][scripts]  Add pitch recipe for nnet2/3 online setup (hkust) (#1280)

--- egs/hkust/s5/path.sh
+++ egs/hkust/s5/path.sh
@@ -1,5 +1,5 @@
 export KALDI_ROOT=`pwd`/../../..
-#export KALDI_ROOT=/home/dpovey/kaldi-trunk-test
+[ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
