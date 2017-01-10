136120ad834a5bcc794a13689f8dee024f3c1ba9
Dogan Can <dogancanbaz@gmail.com>
2017-01-10 11:53:10 -0800

    Testing Travis CI with different build settings 6.

--- tools/extras/travis_script.sh
+++ tools/extras/travis_script.sh
@@ -46,7 +46,7 @@ then
 fi
 
 # Prepare environment variables
-CF="\"$CFLAGS -pipe -g $(addsw -I $INCDIRS)\""
+CF="\"$CFLAGS -g $(addsw -I $INCDIRS)\""
 LDF="\"$LDFLAGS $(addsw -L $LIBDIRS)\""
 CCC="$(mtoken CXX "$CXX")"
 
