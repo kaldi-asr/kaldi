b29eed538882bf24aa615c5337b4065dc5dc920f
Daniel Povey <dpovey@gmail.com>
2017-01-15 16:59:06 -0500

    Fix minor bugs

--- egs/wsj/s5/steps/nnet3/get_degs.sh
+++ egs/wsj/s5/steps/nnet3/get_degs.sh
@@ -310,7 +310,7 @@ fi
 
 # set the command to determinize lattices, if specified.
 if $determinize_before_split; then
-  lattice_determinize_cmd="lattice-determinize-non-compact --acoustic-scale=$acwt --max-mem=$max_mem --minimize=true --prune --beam=$lattice_beam ark:- ark:-"
+  lattice_determinize_cmd="lattice-determinize-non-compact --acoustic-scale=$acwt --max-mem=$max_mem --minimize=true --prune=true --beam=$lattice_beam ark:- ark:-"
 else
   lattice_determinize_cmd="cat"
 fi
