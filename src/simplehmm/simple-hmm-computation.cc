c23060eb527957ab6c9e6a6064e9de3c28e0b657
Vimal Manohar <vimal.manohar91@gmail.com>
2017-01-25 14:59:34 -0500

    asr_diarization: Remove some accidentally added files

--- src/simplehmm/simple-hmm-computation.cc
+++
@@ -1,5 +0,0 @@
-SimpleHmmComputation::SimpleHmmComputation(
-    const SimpleHmm &model, 
-    const std::vector<int32> &num_pdfs, 
-    VectorFst<StdArc> *decode_fst, 
-    const Matrix<BaseFloat> &log_likes)
