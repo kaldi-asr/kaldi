60062fdb0c01a4dd5247c8f85c8e1bb17b1ed842
Vimal Manohar <vimal.manohar91@gmail.com>
2017-05-01 14:30:07 -0400

    long_utts: Minor bug fix

--- egs/wsj/s5/steps/cleanup/segment_long_utterances.sh
+++ egs/wsj/s5/steps/cleanup/segment_long_utterances.sh
@@ -299,7 +299,7 @@ if [ $stage -le 9 ]; then
       --query-tfidf=$dir/query_docs/split$nj/query_tf_idf.JOB.ark.txt \
       --source-text2tfidf-file=$dir/docs/source2tf_idf.scp \
       --source-text-id2doc-ids=$dir/docs/text2doc \
-      --query-id2source-text-id=$data_uniform_seg/new2orig_utt \
+      --query-id2source-text-id=$dir/new2orig_utt \
       --num-neighbors-to-search=$num_neighbors_to_search \
       --neighbor-tfidf-threshold=$neighbor_tfidf_threshold \
       --relevant-docs=$dir/query_docs/split$nj/relevant_docs.JOB.txt
