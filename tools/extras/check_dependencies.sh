0f5e16ed37afb1e960e8245d5153a1499e62050d
Daniel Povey <dpovey@gmail.com>
2017-05-25 12:51:12 -0400

    [build] Check python version is 2.7*, not just 2.*.

--- tools/extras/check_dependencies.sh
+++ tools/extras/check_dependencies.sh
@@ -84,7 +84,7 @@ fi
 
 if which python >&/dev/null ; then
   version=`python 2>&1 --version | awk '{print $2}' `
-  if [[ $version != "2."* ]] ; then
+  if [[ $version != "2.7"* ]] ; then
     if which python2.7 >&/dev/null  || which python2 >&/dev/null ; then
       echo "$0: python 2.7 is not the default python. You should either make it"
       echo "$0: default or create an bash alias for kaldi scripts to run correctly"
