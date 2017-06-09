f29ac11ea82fcd2161dd1b78e091dbf6a27048d3
Chun-Chieh Chang <cchunch1@jhu.edu>
2017-06-09 17:24:15 -0400

    The changes to steps were not needed. Reverted to using the steps symlink

--- egs/imagenet/v1/steps/libs/nnet3/xconfig/layers.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2016    Johns Hopkins University (Dan Povey)
-#           2016    Vijayaditya Peddinti
-#           2016    Yiming Wang
-# Apache 2.0.
-
-from basic_layers import *
-from convolution import *
-from lstm import *
