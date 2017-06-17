566576dc2c3f33caea768cdc7b682f49997e3915
aarora8 <aarora8@jhu.edu>
2017-06-17 18:50:20 -0400

    modification for runnning experiments on cnn and tdnn architecture with appending ivectors

--- egs/cifar/v1/path.sh
+++ egs/cifar/v1/path.sh
@@ -3,5 +3,4 @@ export KALDI_ROOT=`pwd`/../../..
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
-export LD_LIBRARY_PATH=/home/dpovey/libs:$LD_LIBRARY_PATH
 export LC_ALL=C
