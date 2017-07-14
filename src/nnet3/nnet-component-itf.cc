8e34cb0aa3661dc01219d5ca6a20a7f73f8e5817
Daniel Povey <dpovey@gmail.com>
2017-07-13 23:07:04 -0400

    [src] Add GruNonlinearityComponent; moving around some sources in nnet3 to avoid very large files.

--- src/nnet3/nnet-component-itf.cc
+++ src/nnet3/nnet-component-itf.cc
@@ -23,6 +23,7 @@
 #include <iomanip>
 #include "nnet3/nnet-component-itf.h"
 #include "nnet3/nnet-simple-component.h"
+#include "nnet3/nnet-special-component.h"
 #include "nnet3/nnet-general-component.h"
 #include "nnet3/nnet-convolutional-component.h"
 #include "nnet3/nnet-parse.h"
@@ -161,6 +162,8 @@ Component* Component::NewComponentOfType(const std::string &component_type) {
     ans = new TimeHeightConvolutionComponent();
   } else if (component_type == "SumBlockComponent") {
     ans = new SumBlockComponent();
+  } else if (component_type == "GruNonlinearityComponent") {
+    ans = new GruNonlinearityComponent();
   }
   if (ans != NULL) {
     KALDI_ASSERT(component_type == ans->Type());
