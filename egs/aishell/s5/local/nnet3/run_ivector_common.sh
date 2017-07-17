819cbea0bda190d0c4eabe326301232d6f7d3f89
Xingyu Na <asr.naxingyu@gmail.com>
2017-07-16 22:00:17 -0400

    update aishell

--- egs/aishell/s5/local/nnet3/run_ivector_common.sh
+++ egs/aishell/s5/local/nnet3/run_ivector_common.sh
@@ -56,7 +56,7 @@ if [ $stage -le 3 ]; then
   echo "$0: creating high-resolution MFCC features"
   mfccdir=mfcc_perturbed_hires
   if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d $mfccdir/storage ]; then
-    utils/create_split_dir.pl /export/b1{5,6,7,8}/$USER/kaldi-data/egs/aishell-$(date +'%m_%d_%H_%M')/s5/$mfccdir/storage $mfccdir/storage
+    utils/create_split_dir.pl /export/b0{5,6,7,8}/$USER/kaldi-data/egs/aishell-$(date +'%m_%d_%H_%M')/s5/$mfccdir/storage $mfccdir/storage
   fi
 
   for datadir in ${train_set}_sp ${test_sets}; do
