1549c8b66093fe345a8470f8064a8271b6e23c0d
aarora8 <aarora8@jhu.edu>
2017-07-28 18:09:24 -0400

    adding code for preparing character based language model

--- egs/iam_en/s5/local/prepare_data.sh
+++ egs/iam_en/s5/local/prepare_data.sh
@@ -74,14 +74,28 @@ fi
 
 mkdir -p data/{train,val_1,val_2,test}
 if [ $stage -le 0 ]; then
-  local/process_data.py $dl_dir data/train --dataset trainset || exit 1
-  local/process_data.py $dl_dir data/val_1 --dataset validationset1 || exit 1
-  local/process_data.py $dl_dir data/val_2 --dataset validationset2 || exit 1
-  local/process_data.py $dl_dir data/test --dataset testset || exit 1
+  local/process_data.py $dl_dir data/train --dataset trainset --model_type word || exit 1
+  local/process_data.py $dl_dir data/val_1 --dataset validationset1 --model_type word || exit 1
+  local/process_data.py $dl_dir data/val_2 --dataset validationset2 --model_type word || exit 1
+  local/process_data.py $dl_dir data/test --dataset testset --model_type word || exit 1
 
   utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
   utils/utt2spk_to_spk2utt.pl data/val_1/utt2spk > data/val_1/spk2utt
   utils/utt2spk_to_spk2utt.pl data/val_2/utt2spk > data/val_2/spk2utt
   utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
+
 fi
 
+mkdir -p char_data/{train,val_1,val_2,test}
+if [ $stage -le 1 ]; then
+  local/process_data.py $dl_dir char_data/train --dataset trainset --model_type character || exit 1
+  local/process_data.py $dl_dir char_data/val_1 --dataset validationset1 --model_type character || exit 1
+  local/process_data.py $dl_dir char_data/val_2 --dataset validationset2 --model_type character || exit 1
+  local/process_data.py $dl_dir char_data/test --dataset testset --model_type character || exit 1
+
+  utils/utt2spk_to_spk2utt.pl char_data/train/utt2spk > char_data/train/spk2utt
+  utils/utt2spk_to_spk2utt.pl char_data/val_1/utt2spk > char_data/val_1/spk2utt
+  utils/utt2spk_to_spk2utt.pl char_data/val_2/utt2spk > char_data/val_2/spk2utt
+  utils/utt2spk_to_spk2utt.pl char_data/test/utt2spk > char_data/test/spk2utt
+
+fi
--- egs/iam_en/s5/run.sh
+++ egs/iam_en/s5/run.sh
@@ -13,32 +13,37 @@ if [ $stage -le 0 ]; then
 fi
 
 mkdir -p data/{train,val_1,val_2,test}/data
+mkdir -p char_data/{train,val_1,val_2,test}/data
 if [ $stage -le 1 ]; then
   for f in train val_1 val_2 test; do
     local/make_feature_vect.py data/$f --scale-size 40 | \
       copy-feats --compress=true --compression-method=7 \
       ark:- ark,scp:data/$f/data/images.ark,data/$f/feats.scp || exit 1
   done
-
   steps/compute_cmvn_stats.sh data/train || exit 1;
 
-#  mkdir -p data/train/log
-#  image/split_ocr_dir.sh data/train/ $nj
-#  $cmd JOB=1:$nj data/train/log/make_feature_vect.JOB.log \
-#    local/make_feature_vect.py data/train/split${nj}/JOB/ --scale-size 40 \| \
-#    copy-feats --compress=true --compression-method=7 \
-#    ark:- ark,scp:data/train/split${nj}/JOB/data/images.ark,data/train/split${nj}/JOB/feats.scp \
-#    || exit 1
+
+  for f in train val_1 val_2 test; do
+    local/make_feature_vect.py char_data/$f --scale-size 40 | \
+      copy-feats --compress=true --compression-method=7 \
+      ark:- ark,scp:char_data/$f/data/images.ark,char_data/$f/feats.scp || exit 1
+  done
+  steps/compute_cmvn_stats.sh char_data/train || exit 1;
 fi
 
 if [ $stage -le 2 ]; then
   local/prepare_dict.sh data/train/ data/dict
   utils/prepare_lang.sh --position-dependent-phones false data/dict "<sil>" data/lang/temp data/lang
+
+  local/prepare_char_dict.sh char_data/train/ char_data/dict
+  utils/prepare_lang.sh --position-dependent-phones false --sil-prob 0.0 char_data/dict "<sil>" char_data/lang/temp char_data/lang
 fi
 
-#if [ $stage -le 3 ]; then
-#  ## Starting basic training on features
-#  ## passing value for variance floor
-#  steps/train_mono.sh --nj $nj \
-#    --variance_floor_val $variance_floor_val data/train data/lang exp/mono
-#fi
+if [ $stage -le 3 ]; then
+  ## Starting basic training on features
+  steps/train_mono.sh --nj $nj \
+    --variance_floor_val $variance_floor_val data/train data/lang exp/mono
+
+  steps/train_mono.sh --nj $nj \
+    --variance_floor_val $variance_floor_val char_data/train char_data/lang char_exp/mono
+fi
