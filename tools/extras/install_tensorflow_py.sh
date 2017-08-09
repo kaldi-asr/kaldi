cfcc4d15e1031395d6a0e968be047519d59d1d74
Hainan Xu <hainan.xv@gmail.com>
2017-08-09 17:20:17 -0400

    add RESULTS file for TF rnnlm rescoring

--- tools/extras/install_tensorflow_py.sh
+++ tools/extras/install_tensorflow_py.sh
@@ -2,7 +2,7 @@
 
 export HOME=$PWD/tensorflow_build/
 
-has_gpu=false
+has_gpu=true
 
 tf_source=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.2.0-cp27-none-linux_x86_64.whl
 
