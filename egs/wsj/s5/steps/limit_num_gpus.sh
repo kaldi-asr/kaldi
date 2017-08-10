4e9042bd694f0ec9e4314385796a03a890da44cd
Hainan Xu <hainan.xv@gmail.com>
2017-08-10 16:14:55 -0400

    fix typos

--- egs/wsj/s5/steps/limit_num_gpus.sh
+++ egs/wsj/s5/steps/limit_num_gpus.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# this script acts like a wrapper of a bash command that uses GPUs.
+# This script functions as a wrapper of a bash command that uses GPUs.
 #
-# it sets the CUDA_VISIBLE_DEVICES variable so that it limit the number of GPUs
-# used for programs. It is neccesary for the grid we run a program like
-# TensorFlow which automatically grabs all resources available on the system
+# It sets the CUDA_VISIBLE_DEVICES variable so that it limits the number of GPUs
+# used for programs. It is neccesary for running a job on the grid if the job
+# would automatically grabs all resources available on the system, e.g. a
+# TensorFlow program.
 
 num_gpus=1
 
@@ -16,11 +17,11 @@ num_total_gpus=`nvidia-smi -L | wc -l | awk '{print $1}'`
 num_gpus_assigned=0
 
 for i in `seq 0 $[$num_total_gpus-1]`; do
-# going over all GPus and check if idle and add to the list
+# going over all GPUs and check if it is idle, and add to the list if yes
   idle=`nvidia-smi -i $i | grep "No running processes found" | wc -l | awk '{print $1}'`
   [ $idle -eq 1 ] && CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}$i, && num_gpus_assigned=$[$num_gpus_assigned+1]
 
-# once we have enough GPUs break out of the loop
+# once we have enough GPUs, break out of the loop
   [ $num_gpus_assigned -eq $num_gpus ] && break
 done
 
