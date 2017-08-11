69397b208f6c54e8e601e70dd04eb4d9c58752b4
Hainan Xu <hainan.xv@gmail.com>
2017-08-11 18:42:31 -0400

    create utils/parallel and move appropriate scripts there

--- egs/ami/s5/local/tfrnnlm/run_lstm.sh
+++ egs/ami/s5/local/tfrnnlm/run_lstm.sh
@@ -26,7 +26,7 @@ fi
 mkdir -p $dir
 if [ $stage -le 2 ]; then
 # the following script uses TensorFlow. You could use tools/extras/install_tensorflow_py.sh to install it
-  $cuda_cmd $dir/train_rnnlm.log steps/limit_num_gpus.sh \
+  $cuda_cmd $dir/train_rnnlm.log utils/parallel/limit_num_gpus.sh \
     python steps/tfrnnlm/lstm.py --data-path=$dir --save-path=$dir/rnnlm --vocab-path=$dir/wordlist.rnn.final
 fi
 
