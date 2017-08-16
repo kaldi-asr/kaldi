056cfb5c7ee857d9e206798e5f92f6eb3ffa0538
tzyll <tangzy@cslt.riit.tsinghua.edu.cn>
2017-08-16 11:28:37 +0800

    ivector lid script is a little messy, clean it up

--- egs/ap17_olr/s5/run_ivector_lid.sh
+++ egs/ap17_olr/s5/run_ivector_lid.sh
@@ -17,28 +17,30 @@
 . ./path.sh
 
 # Number of components
-cnum=2048
-civ=400
-clda=9
+cnum=2048 # num of Gaussions
+civ=400   # dim of i-vector
+clda=6    # dim for i-vector with LDA
+
 testdir=dev_all  # you may change it to dev_1s, dev_3s or test
-
-svmkernel=linear
-
 exp=exp/ivector
+mkdir -p $exp
+
+stage=1
 
 set -e
 
 # When doing i-vector LID, the two files ('spk2utt' and 'utt2spk') in data/{train,dev_1s,dev_3s,dev_all},
 # should be replaced with those in local/olr/ivector/lan2utt, 
 # after then, each 'spk' actually indicates one language of the ten.
+# Those files in data/{train,dev_1s,dev_3s,dev_all}/lan2utt are outdated. 
 for subpath in {train,dev_1s,dev_3s,dev_all}/utt2spk; do
   if ! cmp data/$subpath local/olr/ivector/lan2utt/$subpath &> /dev/null; then
     echo "data/$subpath should be replaced with local/olr/ivector/lan2utt/$subpath and so is spk2utt" && exit 1;
   fi
 done
 
-if (( 1 == 1 ));then
-  # Feature extraction
+if [ $stage -le 1 ];then
+  # Feature extraction and VAD
   mfccdir=`pwd`/_mfcc
   vaddir=`pwd`/_vad
   
@@ -49,103 +51,109 @@ if (( 1 == 1 ));then
   sid/compute_vad_decision.sh --nj 4 --cmd "$cpu_cmd" data/$testdir $exp/_log_vad $vaddir
 fi
 
-if (( 2 == 2 ));then
+if [ $stage -le 2 ];then
   # Get smaller subsets of training data for faster training.
   utils/subset_data_dir.sh data/train 18000 data/train_18k
   utils/subset_data_dir.sh data/train 36000 data/train_36k
+
   # UBM and T-matrix training
   sid/train_diag_ubm.sh --nj 6 --cmd "$cpu_cmd" data/train_18k ${cnum} $exp/diag_ubm_${cnum} 
   sid/train_full_ubm.sh --nj 6 --cmd "$cpu_cmd" data/train_36k $exp/diag_ubm_${cnum} $exp/full_ubm_${cnum} 
-fi
 
-if (( 3 == 3 ));then
   sid/train_ivector_extractor.sh --nj 10 --cmd "$cpu_cmd -l mem_free=2G" \
     --num-iters 6 --ivector_dim $civ $exp/full_ubm_${cnum}/final.ubm data/train \
     $exp/extractor_${cnum}_${civ}
-  
+
+  # Extract i-vector
   sid/extract_ivectors.sh --cmd "$cpu_cmd -l mem_free=2G," --nj 20 \
     $exp/extractor_${cnum}_${civ} data/train $exp/ivectors_train_${cnum}_${civ}
-  
   sid/extract_ivectors.sh --cmd "$cpu_cmd -l mem_free=2G," --nj 20 \
     $exp/extractor_${cnum}_${civ} data/$testdir $exp/ivectors_${testdir}_${cnum}_${civ}
 fi
 
-### Demonstrate simple cosine-distance scoring:
-if (( 31 == 31 ));then
+# Demonstrate simple cosine-distance scoring
+if [ $stage -le 3 ];then
   trials=local/olr/ivector/trials.trl
+  mkdir -p $exp/score/total
   cat $trials | awk '{print $1, $2}' | \
    ivector-compute-dot-products - \
     scp:$exp/ivectors_train_${cnum}_${civ}/spk_ivector.scp \
     scp:$exp/ivectors_${testdir}_${cnum}_${civ}/ivector.scp \
-     exp/ivecotr/score/total/foo_cosine
+    $exp/score/total/foo_cosine
   
   echo i-vector
   echo
   printf '% 16s' 'EER% is:'
-  eer=$(awk '{print $3}' exp/ivecotr/score/total/foo_cosine | paste - $trials | awk '{print $1, $4}' | compute-eer - 2>/dev/null)
+  eer=$(awk '{print $3}' $exp/score/total/foo_cosine | paste - $trials | awk '{print $1, $4}' | compute-eer - 2>/dev/null)
   printf '% 5.2f' $eer
   echo
   
-  python local/olr/ivector/Compute_Cavg.py  exp/ivecotr/score/total/foo_cosine data/${testdir}/utt2spk
+  python local/olr/ivector/Compute_Cavg.py  $exp/score/total/foo_cosine data/${testdir}/utt2spk
 fi
 
-
-### Demonstrate what happens if we reduce the dimension with LDA
-if (( 4 == 4 ));then
+# Demonstrate what happens if we reduce the dimension with LDA
+if [ $stage -le 4 ];then
   ivector-compute-lda --dim=$clda  --total-covariance-factor=0.1 \
-    "ark:ivector-normalize-length scp:$exp/ivectors_train_${cnum}_${civ}/ivector.scp  ark:- |" ark:data/train/utt2spk \
-      $exp/ivectors_train_${cnum}_${civ}/transform_${clda}.mat
+    "ark:ivector-normalize-length scp:$exp/ivectors_train_${cnum}_${civ}/ivector.scp  ark:- |" \
+    ark:data/train/utt2spk \
+    $exp/ivectors_train_${cnum}_${civ}/transform_${clda}.mat
   
-  ivector-transform $exp/ivectors_train_${cnum}_${civ}/transform_${clda}.mat scp:$exp/ivectors_train_${cnum}_${civ}/ivector.scp ark:- | \
-  ivector-normalize-length ark:- ark:${exp}/ivectors_train_${cnum}_${civ}/lda_${clda}.ark
+  ivector-transform $exp/ivectors_train_${cnum}_${civ}/transform_${clda}.mat \
+    scp:$exp/ivectors_train_${cnum}_${civ}/ivector.scp ark:- | \
+    ivector-normalize-length ark:- ark:${exp}/ivectors_train_${cnum}_${civ}/lda_${clda}.ark
   
-  ivector-transform $exp/ivectors_train_${cnum}_${civ}/transform_${clda}.mat scp:$exp/ivectors_${testdir}_${cnum}_${civ}/ivector.scp ark:- | \
-  ivector-normalize-length ark:- ark:${exp}/ivectors_${testdir}_${cnum}_${civ}/ldatest_${clda}.ark
+  ivector-transform $exp/ivectors_train_${cnum}_${civ}/transform_${clda}.mat \
+    scp:$exp/ivectors_${testdir}_${cnum}_${civ}/ivector.scp ark:- | \
+    ivector-normalize-length ark:- ark:${exp}/ivectors_${testdir}_${cnum}_${civ}/lda_${clda}.ark
 fi
 
-if (( 51 == 51 ));then
+# Demonstrate cosine-distance scoring for i-vector with LDA
+if [ $stage -le 5 ];then
   dir=${exp}/ivectors_train_${cnum}_${civ}
   
-  ivector-mean ark:data/train/spk2utt ark:${exp}/ivectors_train_${cnum}_${civ}/lda_${clda}.ark ark:- ark,t:$dir/num_utts.ark | \
-  ivector-normalize-length ark:- ark,scp:$dir/lda_ivector.ark,$dir/lda_ivector.scp
+  ivector-mean ark:data/train/spk2utt \
+    ark:$dir/lda_${clda}.ark ark:- ark,t:$dir/num_utts.ark | \
+    ivector-normalize-length ark:- ark,scp:$dir/lda_ivector.ark,$dir/lda_ivector.scp
   
   trials=local/olr/ivector/trials.trl
   cat $trials | awk '{print $1, $2}' | \
    ivector-compute-dot-products - \
     scp:$exp/ivectors_train_${cnum}_${civ}/lda_ivector.scp \
-    ark:$exp/ivectors_${testdir}_${cnum}_${civ}/ldatest_${clda}.ark \
-     exp/ivecotr/score/total/foo_lda
+    ark:$exp/ivectors_${testdir}_${cnum}_${civ}/lda_${clda}.ark \
+    $exp/score/total/foo_lda
   
-  echo L-vector
+  echo 'L-vector (i-vector with LDA)'
   echo 
   printf '% 16s' 'EER% is:'
-  eer=$(awk '{print $3}' exp/ivecotr/score/total/foo_lda | paste - $trials | awk '{print $1, $4}' | compute-eer - 2>/dev/null)
+  eer=$(awk '{print $3}' $exp/score/total/foo_lda | paste - $trials | awk '{print $1, $4}' | compute-eer - 2>/dev/null)
   printf '% 5.2f' $eer
   
-  python local/olr/ivector/Compute_Cavg.py  exp/ivecotr/score/total/foo_lda data/${testdir}/utt2spk
-  rm $trials
+  python local/olr/ivector/Compute_Cavg.py  $exp/score/total/foo_lda data/${testdir}/utt2spk
 fi
 
-vectype=lda
-curve=$svmkernel
+# Evaluation with SVM
+vectype=lda-ivector # or just i-vector
+curve=linear
 maxit=-1
 
 mkdir -p svm
 mkdir -p exp/ivector_svm/$curve
 mkdir -p exp/ivector_svm/score
 
-if (( 1 == 1 ));then
-  ivector-normalize-length ark:$exp/ivectors_train_2048_400/lda_9.ark ark,t:exp/ivector_svm/$curve/train.dat
-  ivector-normalize-length ark:$exp/ivectors_${testdir}_2048_400/ldatest_9.ark ark,t:exp/ivector_svm/$curve/test.dat
-fi
-
-#If you don't need to execute the LDA  then 1==1
-if (( 1 == 0 ));then
+# using i-vector directly or i-vector with LDA for SVM
+if [ $vectype == "lda-ivector" ]; then
+  # using i-vector with LDA
+  ivector-normalize-length ark:$exp/ivectors_train_2048_400/lda_${clda}.ark ark,t:exp/ivector_svm/$curve/train.dat
+  ivector-normalize-length ark:$exp/ivectors_${testdir}_2048_400/lda_${clda}.ark ark,t:exp/ivector_svm/$curve/test.dat
+elif [ $vectype == "i-vector" ]; then
+  # using i-vecotr directly
   ivector-normalize-length scp:$exp/ivectors_train_2048_400/ivector.scp ark,t:exp/ivector_svm/$curve/train.dat
   ivector-normalize-length scp:$exp/ivectors_${testdir}_2048_400/ivector.scp ark,t:exp/ivector_svm/$curve/test.dat
+else
+  echo "Wrong feats!" && exit 1;
 fi
 
-if (( 2 == 2 ));then
+if [ $stage -le 6 ];then
   python local/olr/ivector/pre_svmd.py exp/ivector_svm/$curve/train.dat data/train/utt2spk exp/ivector_svm/$curve/svmtrain.dat
   python local/olr/ivector/pre_svmd.py exp/ivector_svm/$curve/test.dat data/${testdir}/utt2spk exp/ivector_svm/$curve/svmtest.dat
   
@@ -158,7 +166,7 @@ if (( 2 == 2 ));then
   python local/olr/ivector/ttt.py exp/ivector_svm/score/foo_${curve}_${maxit}.score exp/ivector_svm/score/foo_standard_${curve}_${maxit}.score
   
   
-  echo L-vector + svm
+  echo "$vectype + $curve SVM"
   echo
   
   python local/olr/ivector/pre_eerdata.py exp/ivector_svm/$curve/svmtest.dat exp/ivector_svm/score/foo_standard_${curve}_${maxit}.score exp/ivector_svm/$curve/foo_eerdata_${curve}_${maxit}.dat
