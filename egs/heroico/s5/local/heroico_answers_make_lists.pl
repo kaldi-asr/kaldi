6efe1bed826ea734e689ba0908b608b765329b0f
johnjosephmorgan <johnjosephmorgan@gmail.com>
2017-09-20 20:57:50 -0400

    added 	local/heroico_answers_make_lists.pl

---
+++ egs/heroico/s5/local/heroico_answers_make_lists.pl
@@ -0,0 +1,86 @@
+#!/usr/bin/perl -w
+
+# Copyright 2017 John Morgan
+# Apache 2.0.
+
+# heroico_answers_make_lists.pl - make acoustic model training lists
+
+use strict;
+use warnings;
+use Carp;
+
+use File::Spec;
+use File::Copy;
+use File::Basename;
+
+BEGIN {
+    @ARGV == 1 or croak "USAGE: $0 <PROMPTSLIST>";
+}
+
+my ($p) = @ARGV;
+
+my $tmpdir = "data/local/tmp/heroico";
+
+system "mkdir -p $tmpdir/answers";
+
+# input wav file list
+my $w = "$tmpdir/wav_list.txt";
+
+# output temporary wav.scp files
+my $o = "$tmpdir/answers/wav.scp";
+
+# output temporary utt2spk files
+my $u = "$tmpdir/answers/utt2spk";
+
+# output temporary text files
+my $t = "$tmpdir/answers/text";
+
+# initialize hash for prompts
+my %p = ();
+
+open my $P, '<', $p or croak "problem with $p $!";
+
+# store prompts in hash
+LINEA: while ( my $line = <$P> ) {
+    chomp $line;
+    my ($num,$sent) = split /\t/, $line, 2;
+    my ($volume,$directories,$file) = File::Spec->splitpath( $num );
+    my @dirs = split /\//, $directories;
+    # get the speaker number
+    my $s = $dirs[-1];
+    # the utterance name
+    my $i = $s . '_' . $file . '_a';
+    $p{$i} = $sent;
+}
+close $P;
+
+open my $W, '<', $w or croak "problem with $w $!";
+open my $O, '+>', $o or croak "problem with $o $!";
+open my $U, '+>', $u or croak "problem with $u $!";
+open my $T, '+>', $t or croak "problem with $t $!";
+
+ LINE: while ( my $line = <$W> ) {
+     chomp $line;
+     next LINE unless ( $line =~ /Answers/ );
+     next LINE if ( $line =~ /Recordings/ );
+     my ($volume,$directories,$file) = File::Spec->splitpath( $line );
+     my @dirs = split /\//, $directories;
+     my $r = basename $line, ".wav";
+     my $s = $dirs[-1];
+     my $rid = $s . '_' . $r . '_a';
+     if ( exists $p{$rid} ) {
+	 print $T "$rid\t$p{$rid}\n";
+     } elsif ( defined $rid ) {
+	 warn  "problem\t$rid";
+	 next LINE;
+     } else {
+	 croak "$line";
+     }
+
+	print $O "$rid\tsox -r 22050 -e signed -b 16 $line -r 16000 |\n";
+     print $U "$rid\t$s\n";
+}
+close $T;
+close $O;
+close $U;
+close $W;
