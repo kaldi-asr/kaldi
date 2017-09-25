9fa77d9f3618b1fca25ceb21d1cedb645ac44fb0
Jan Trmal <jtrmal@gmail.com>
2017-09-25 14:46:37 -0400

    validate_lang checks for incompatible UTF-8 whitespaces

--- egs/wsj/s5/utils/validate_lang.pl
+++ egs/wsj/s5/utils/validate_lang.pl
@@ -3,9 +3,88 @@
 # Apache 2.0.
 # Copyright  2012   Guoguo Chen
 #            2014   Neil Nelson
+#            2017   Johns Hopkins University (Jan "Yenda" Trmal <jtrmal@gmail.com>)
 #
 # Validation script for data/lang
 
+# this function reads the opened file (supplied as a first
+# parameter) into an array of lines. For each
+# line, it tests whether it's a valid utf-8 compatible
+# line. If all lines are valid utf-8, it returns the lines 
+# decoded as utf-8, otherwise it assumes the file's encoding
+# is one of those 1-byte encodings, such as ISO-8859-x
+# or Windows CP-X.
+# Please recall we do not really care about
+# the actually encoding, we just need to 
+# make sure the length of the (decoded) string 
+# is correct (to make the output formatting looking right).
+sub get_utf8_or_bytestream {
+  use Encode qw(decode encode);
+  my $is_utf_compatible = 1;
+  my @unicode_lines;
+  my @raw_lines;
+  my $raw_text;
+  my $lineno = 0;
+  my $file = shift;
+
+  while (<$file>) {
+    $raw_text = $_;
+    last unless $raw_text;
+    if ($is_utf_compatible) {
+      my $decoded_text = eval { decode("UTF-8", $raw_text, Encode::FB_CROAK) } ;
+      $is_utf_compatible = $is_utf_compatible && defined($decoded_text); 
+      push @unicode_lines, $decoded_text;
+    } else {
+      #print STDERR "WARNING: the line $raw_text cannot be interpreted as UTF-8: $decoded_text\n";
+      ;
+    }
+    push @raw_lines, $raw_text;
+    $lineno += 1;
+  }
+
+  if (!$is_utf_compatible) {
+    return (0, @raw_lines);
+  } else {
+    return (1, @unicode_lines);
+  }
+}
+
+# check if the given unicode string contain unicode whitespaces
+# other than the usual four: TAB, LF, CR and SPACE
+sub validate_utf8_whitespaces {
+  my $unicode_lines = shift;
+  use feature 'unicode_strings'; 
+  for (my $i = 0; $i < scalar @{$unicode_lines}; $i++) {
+    my $current_line = $unicode_lines->[$i];
+    # we replace TAB, LF, CR, and SPACE
+    # this is to simplify the test
+    $current_line =~ s/[\x{0009}\x{000a}\x{000d}\x{0020}]/./g; 
+    if ($current_line =~/\s/) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+sub perform_whitespace_checks {
+  my $file = shift;
+  my $pos = tell($file);
+  (my $is_utf, my @lines) = get_utf8_or_bytestream($file);
+  if ($is_utf) {
+    my $has_invalid_whitespaces = validate_utf8_whitespaces(\@lines);
+    print "--> text seems to be UTF-8 or ASCII, checking whitespaces\n";
+    if ($has_invalid_whitespaces) {
+      print "--> ERROR: the text containes disallowed UTF-8 whitespace character(s)\n"; 
+      return 0;
+    } else {
+      print "--> text contains only allowed whitespaces\n";
+    }
+  } else {
+    print "--> text doesn't seem to be UTF-8 or ASCII, won't check whitespaces\n";
+  }
+  seek($file, $pos, SEEK_SET);
+  return 1;
+}
 
 $skip_det_check = 0;
 $skip_disambig_check = 0;
@@ -44,6 +123,7 @@ if (!open(P, "<$lang/phones.txt")) {
 }
 $idx = 1;
 %psymtab = ();
+perform_whitespace_checks(\*P) or exit 1;
 while (<P>) {
   chomp;
   my @col = split(" ", $_);
@@ -77,6 +157,7 @@ if (!open(W, "<$lang/words.txt")) {
 }
 $idx = 1;
 %wsymtab = ();
+perform_whitespace_checks(\*W) or exit 1;
 while (<W>) {
   chomp;
   my @col = split(" ", $_);
