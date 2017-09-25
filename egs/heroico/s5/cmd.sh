aa40caefc02a9043740b68b8171c3c7dbe3719f5
jjm <johnjosephmorgan@gmail.com>
2017-09-25 14:25:33 -0400

    removed    cuda_cmd

--- egs/heroico/s5/cmd.sh
+++ egs/heroico/s5/cmd.sh
@@ -12,12 +12,8 @@
 
 export train_cmd=queue.pl
 export decode_cmd="queue.pl --mem 2G"
-# the use of cuda_cmd is deprecated, used only in 'nnet1',
-export cuda_cmd="queue.pl --gpu 1"
-
 if [ "$(hostname -d)" == "fit.vutbr.cz" ]; then
   queue_conf=$HOME/queue_conf/default.conf # see example /homes/kazi/iveselyk/queue_conf/default.conf,
   export train_cmd="queue.pl --config $queue_conf --mem 2G --matylda 0.2"
   export decode_cmd="queue.pl --config $queue_conf --mem 3G --matylda 0.1"
-  export cuda_cmd="queue.pl --config $queue_conf --gpu 1 --mem 10G --tmp 40G"
 fi
