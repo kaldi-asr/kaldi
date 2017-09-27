4140772c8604b0a27827ff687d2eae00cb035302
Jan Trmal <jtrmal@gmail.com>
2017-09-27 11:58:17 -0400

    better makefile parsing

--- src/makefile_to_cmake.pl
+++ src/makefile_to_cmake.pl
@@ -19,7 +19,7 @@
 use strict;
 use warnings;
 use utf8;
-
+use Data::Dumper;
 
 sub read_whole_file {
   my $file = shift;
@@ -49,21 +49,83 @@ sub extract_statement {
   return $content;
 }
 
+sub substitute_variables {
+  my @blacklist = ("CXX", "AR", "AS");
+  my $variables = shift;
+  my $value = shift;
+
+  while ($value =~ /\$\(([A-Z_]+)\)/) {
+    my $var = $1;
+    my $val = "";
+    
+    if (defined($variables->{$1})) {
+      $val = $variables->{$1};
+    } else {
+      print STDERR "WARNING: Flag/value $var not defined\n";
+      # lets convert it to CMake-style variable   
+      $val = "\${$1}";
+      $val = "";
+    }
+    $value =~ s/\$\(\Q$var\E\)/$val/g;
+  }
+  return $value;
+}
+
+sub extract_variables {
+  my $kaldimk = shift;
+  my @lines = split(/\n/, $kaldimk);
+  my %variables;
+
+  my $line;
+  my $inside_cond = 0;
+  for (my $i = 0; $i <= $#lines; $i++) {
+    $line = $lines[$i];
+    next if $line =~ /^ *$/;
+    next if (($inside_cond > 0) && ($line !~ /^endif/));
+    $line =~ s/ #.*/ /g;
+    $line =~ s/  +/ /g;
+    next if $line =~ /^ *$/;
+    
+    if ($line =~ /([A-Z_]+) *:?= *(.*)/) {
+      my $variable = $1;
+      my $value = $2; 
+      $value = substitute_variables(\%variables, $value);
+      $value =~ s/^\s+|\s+$//g;
+      $variables{$variable} = $value;
+    } elsif ($line =~ /([A-Z_]+) *:?\+= *(.*)/) {
+      my $variable = $1;
+      my $value = $2; 
+      $value = substitute_variables(\%variables, $value);
+      $variables{$variable} .= " $value ";
+      $variables{$variable} =~ s/^\s+|\s+$//g;
+    } elsif ($line =~ /^#/) {
+      next;
+    } elsif ( $line =~ /^ifeq/ || $line =~ /^ifneq/ ) {
+      $inside_cond += 1;
+    } elsif ($line =~ /^ifdef/ || $line =~ /^ifndef/ ) {
+      $inside_cond += 1;
+    } elsif ($line =~ /^endif/ ) {
+      $inside_cond -= 1;
+      die "Error parsing Makefile[$i], probably syntax error\n" if $inside_cond < 0;
+    } else {
+      print "Cannot parse Makefile[$i]: $line\n";
+    }
+  }
+  print Dumper(\%variables);
+  return %variables;
+}
 
 sub write_root_cmake {
   my $kaldimk = shift;
   my $directories = shift;
   my $out = shift;
-  
-  #print $kaldimk;
-  
-  my $openfstinc = extract_statement("OPENFSTINC", $kaldimk) or die "No OPENFSTINC";
-  my $openfstlib = extract_statement("OPENFSTLIBS", $kaldimk) or die "No OPENFSTLIBS";
-  my $openfstflags = extract_statement("OPENFSTLDFLAGS", $kaldimk) or die "No OPENFSTLDFLAGS";
+  my %variables;
 
-  my $cxxflags = extract_statement("CXXFLAGS", $kaldimk) or die "No CXXFLAGS!";
-  my $ldflags = extract_statement("LDFLAGS", $kaldimk) or die "No LDFLAGS!";
-  my $ldlibs = extract_statement("LDLIBS", $kaldimk) or die "No LDLIBS!";
+  #print $kaldimk;
+  %variables = extract_variables($kaldimk);
+  my $cxxflags = $variables{"CXXFLAGS"} or die "No CXXFLAGS!";
+  my $ldflags = $variables{"LDFLAGS"} or die "No LDFLAGS!";
+  my $ldlibs = $variables{"LDLIBS"} or die "No LDLIBS!";
 
   my @cxxflags_array = split / /, $cxxflags;
   my @defines_from_flags;
@@ -88,6 +150,7 @@ sub write_root_cmake {
   $cxxflags =~ s/\$[^\)]+\)//g;
   $cxxflags =~ s/ *$//g;
   $cxxflags =~ s/^ *//g;
+  $cxxflags =~ s/  +/ /g;
   $ldlibs =~ s/\$[^\)]+\)//g;
   $ldlibs =~ s/^ *//g;
   $ldlibs =~ s/ *$//g;
@@ -104,8 +167,10 @@ sub write_root_cmake {
   print $f "cmake_minimum_required (VERSION 2.8.11)\n";
   print $f "project(kaldi)\n";
   print $f "enable_testing()\n";
+  print $f "\n";
+  print $f "set(CMAKE_CXX_STANDARD 11)\n"; 
+  print $f "\n";
   print $f "include_directories(\${CMAKE_SOURCE_DIR})\n";
-  print $f "include_directories($openfstinc)\n";
   foreach my $param (@includes_from_flags) {
     print $f "include_directories($param)\n";
   }
@@ -114,19 +179,24 @@ sub write_root_cmake {
     print $f "include_directories(\"$mkl" . '\\\\' . "include\")\n";
   }
   #print $f "add_definitions(-DHAVE_CLAPACK)\n";
-  print $f "add_definitions(-DHAVE_EXECINFO=1)\n";
-  print $f "add_definitions(-DHAVE_CXXABI_H)\n";
-  print $f "add_definitions(-DKALDI_DOUBLEPRECISION=0)\n";
+  print $f "\n";
   foreach my $param (@defines_from_flags) {
     print $f "add_definitions(-D$param)\n";
   }
-  print $f "option (BUILD_SHARED_LIBS \"Build shared libraries.\" \"SHARED\")\n";
+  print $f "\n";
+  print $f "option (BUILD_SHARED_LIBS \"Build shared libraries.\" ON)\n";
   print $f "option (BUILD_USE_SOLUTION_FOLDERS \"Enable grouping of projects in VS\" ON)\n"; 
   print $f "set_property(GLOBAL PROPERTY USE_FOLDERS \${BUILD_USE_SOLUTION_FOLDERS})\n"; 
-  print $f "set(CMAKE_CXX_FLAGS $cxxflags)\n";
+  print $f "\n";
   print $f "set(KALDI_CXX_FLAGS \"$cxxflags\")\n";
-  print $f "set(KALDI_LINKER_FLAGS $openfstlib $ldflags $openfstflags $ldlibs)\n";
-  print $f "set(CMAKE_CXX_STANDARD 11)\n"; 
+  print $f "set(KALDI_LINKER_FLAGS \"$ldflags\")\n";
+  print $f "set(KALDI_LINKER_LIBS \"$ldlibs\")\n";
+  print $f "\n";
+  print $f "set(KALDI_CUDA_FLAGS \"$variables{CUDA_FLAGS}\")\n" if defined($variables{CUDA_FLAGS});
+  print $f "set(KALDI_CUDA_LDFLAGS \"$variables{CUDA_LDFLAGS}\")\n" if defined($variables{CUDA_LDFLAGS});
+  print $f "set(KALDI_CUDA_LDLIBS \"$variables{CUDA_LDLIBS}\")\n" if defined($variables{CUDA_LDLIBS});
+  print $f "set(KALDI_CUDA_ARCH \"$variables{CUDA_ARCH}\")\n" if defined($variables{CUDA_ARCH});
+  print $f "\n";
   foreach my $dir (@{$directories}) {
       print $f "add_subdirectory ($dir)\n";
   }
@@ -167,7 +237,7 @@ sub write_cmake {
     $addlibs = join(" ", split(" ", $addlibs));
     
     $ccfiles = join(" ", split(" ", $ccfiles));
-    print $f "add_library($libname STATIC $ccfiles)\n";
+    print $f "add_library($libname $ccfiles)\n";
     print $f "target_link_libraries($libname \${KALDI_LINKER_FLAGS} $addlibs)\n" if $addlibs;
     print $f "set_target_properties($libname PROPERTIES FOLDER $dir_basename)\n";
     #print $libname . "\n";
@@ -224,7 +294,7 @@ sub write_cmake {
 #          "./tree", "./util");
           
           
-my @dirs = ("./base", "./matrix");
+my @dirs = ("./base", "./matrix", "./util", "./tree", "./gmm");
              
 my $kaldimk = read_whole_file("kaldi.mk");
    
