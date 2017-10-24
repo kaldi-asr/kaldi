60e78a24b193ec99dff1843dbf4ef1b973cd15d1
Ewald Enzinger <ee@entn.at>
2017-10-24 14:34:05 -0700

    Pass in args.egs_command if not None, otherwise args.command in chain training script

--- egs/wsj/s5/steps/nnet3/chain/train.py
+++ egs/wsj/s5/steps/nnet3/chain/train.py
@@ -366,6 +366,7 @@ def train(args, run_opts):
             right_context=egs_right_context,
             left_context_initial=egs_left_context_initial,
             right_context_final=egs_right_context_final,
+            cmd=(args.egs_command if args.egs_command is not None else args.command),
             run_opts=run_opts,
             left_tolerance=args.left_tolerance,
             right_tolerance=args.right_tolerance,
