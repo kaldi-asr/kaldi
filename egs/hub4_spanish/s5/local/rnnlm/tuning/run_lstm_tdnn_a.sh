6127c327d2dbaddcdb4ef8e57373c3d670e7fd7e
Ke Li <kli26@jhu.edu>
2017-11-08 21:03:28 -0500

    [egs] Update RNNLM recipes with L2 regularization (#2005)

--- egs/hub4_spanish/s5/local/rnnlm/tuning/run_lstm_tdnn_a.sh
+++ egs/hub4_spanish/s5/local/rnnlm/tuning/run_lstm_tdnn_a.sh
@@ -4,27 +4,28 @@
 #           2017  Hainan Xu
 #           2017  Ke Li
 
-# rnnlm/train_rnnlm.sh: best iteration (out of 10) was 8, linking it to final iteration.
-# rnnlm/train_rnnlm.sh: train/dev perplexity was 78.4 / 147.8.
-# Train objf: -1556.00 -5.43 -5.15 -5.00 -4.90 -4.82 -4.75 -4.69 -4.63 -4.58
-# Dev objf:   -11.92 -5.70 -5.29 -5.16 -5.08 -5.04 -5.02 -5.00 -5.00 -5.00
+# rnnlm/train_rnnlm.sh: best iteration (out of 10) was 3, linking it to final iteration.
+# rnnlm/train_rnnlm.sh: train/dev perplexity was 44.7 / 152.8.
+# Train objf: -310.30 -4.70 -4.24 -3.89 -3.58 -3.30 -3.06 -2.86 -2.69 -2.56 
+# Dev objf:   -10.07 -5.28 -5.04 -5.03 -5.08 -5.14 -5.26 -5.34 -5.43 -5.52 
 
 # Begin configuration section.
 cmd=run.pl
 dir=exp/rnnlm_lstm_tdnn_a
 embedding_dim=800
-lstm_rpd=200
-lstm_nrpd=200
+embedding_l2=0.005 # embedding layer l2 regularize
+comp_l2=0.005 # component-level l2 regularize
+output_l2=0.005 # output-layer l2 regularize
+epochs=160
 stage=-10
 train_stage=-10
-epochs=20
 
 . utils/parse_options.sh
 
 
 text=data/train/text
 wordlist=data/lang/words.txt
-dev_sents=10000
+dev_sents=3000
 text_dir=data/rnnlm/text
 mkdir -p $dir/config
 set -e
@@ -37,7 +38,7 @@ done
 if [ $stage -le 0 ]; then
   mkdir -p $text_dir
   cat $text | cut -d ' ' -f2- | head -n $dev_sents> $text_dir/dev.txt
-  cat $text | cut -d ' ' -f2- | tail -n +$[$dev_sents+1] > $text_dir/ted.txt
+  cat $text | cut -d ' ' -f2- | tail -n +$[$dev_sents+1] > $text_dir/hub.txt
 fi
 
 if [ $stage -le 1 ]; then
@@ -50,7 +51,7 @@ if [ $stage -le 1 ]; then
   echo "<unk>" >$dir/config/oov.txt
 
   cat > $dir/config/data_weights.txt <<EOF
-ted   1   1.0
+hub   1   1.0
 EOF
 
   rnnlm/get_unigram_probs.py --vocab-file=$dir/config/words.txt \
@@ -61,19 +62,21 @@ EOF
   # choose features
   rnnlm/choose_features.py --unigram-probs=$dir/config/unigram_probs.txt \
                            --use-constant-feature=true \
-                           --top-word-features=10000 \
+                           --top-word-features 10000 \
                            --min-frequency 1.0e-03 \
                            --special-words='<s>,</s>,<brk>,<unk>' \
                            $dir/config/words.txt > $dir/config/features.txt
 
+lstm_opts="l2-regularize=$comp_l2"
+tdnn_opts="l2-regularize=$comp_l2"
+output_opts="l2-regularize=$output_l2"
+
   cat >$dir/config/xconfig <<EOF
 input dim=$embedding_dim name=input
-relu-renorm-layer name=tdnn1 dim=$embedding_dim input=Append(0, IfDefined(-1))
-fast-lstmp-layer name=lstm1 cell-dim=$embedding_dim recurrent-projection-dim=$lstm_rpd non-recurrent-projection-dim=$lstm_nrpd
-relu-renorm-layer name=tdnn2 dim=$embedding_dim input=Append(0, IfDefined(-2))
-fast-lstmp-layer name=lstm2 cell-dim=$embedding_dim recurrent-projection-dim=$lstm_rpd non-recurrent-projection-dim=$lstm_nrpd
-relu-renorm-layer name=tdnn3 dim=$embedding_dim input=Append(0, IfDefined(-1))
-output-layer name=output include-log-softmax=false dim=$embedding_dim
+lstm-layer name=lstm1 cell-dim=$embedding_dim $lstm_opts
+relu-renorm-layer name=tdnn dim=$embedding_dim $tdnn_opts input=Append(0, IfDefined(-1))
+lstm-layer name=lstm2 cell-dim=$embedding_dim $lstm_opts
+output-layer name=output $output_opts include-log-softmax=false dim=$embedding_dim
 EOF
   rnnlm/validate_config_dir.sh $text_dir $dir/config
 fi
@@ -82,14 +85,14 @@ if [ $stage -le 2 ]; then
   # the --unigram-factor option is set larger than the default (100)
   # in order to reduce the size of the sampling LM, because rnnlm-get-egs
   # was taking up too much CPU (as much as 10 cores).
-  rnnlm/prepare_rnnlm_dir.sh --unigram-factor 200.0 \
+  rnnlm/prepare_rnnlm_dir.sh --unigram-factor 100.0 \
                              $text_dir $dir/config $dir
 fi
-echo "rnnlm dir done"
 
 if [ $stage -le 3 ]; then
-  rnnlm/train_rnnlm.sh --num-jobs-initial 1 --num-jobs-final 1 \
-                       --stage $train_stage --num-epochs $epochs --cmd "queue.pl" $dir
+  rnnlm/train_rnnlm.sh --embedding_l2 $embedding_l2 \
+                       --stage $train_stage \
+                       --num-epochs $epochs --cmd "queue.pl" $dir
 fi
 
 exit 0
