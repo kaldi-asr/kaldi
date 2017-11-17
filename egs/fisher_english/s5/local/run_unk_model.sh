010bc4e1494eed60a95b018a91021a783a0b5df4
Vimal Manohar <vimal.manohar91@gmail.com>
2017-11-17 16:28:20 -0500

    semisup: Unk model on Fisher

--- egs/fisher_english/s5/local/run_unk_model.sh
+++ egs/fisher_english/s5/local/run_unk_model.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-utils/lang/make_unk_lm.sh data/local/dict exp/unk_lang_model
+# Copyright 2017  Vimal Manohar
+
+utils/lang/make_unk_lm.sh data/local/dict exp/unk_lang_model || exit 1
 
 utils/prepare_lang.sh --unk-fst exp/unk_lang_model/unk_fst.txt \
   data/local/dict "<unk>" data/local/lang data/lang_unk
@@ -10,7 +12,15 @@ utils/prepare_lang.sh --unk-fst exp/unk_lang_model/unk_fst.txt \
 # keeps the graph compact after adding the unk model (we only have to add one
 # copy of it).
 
-cp data/lang/G.fst data/lang_unk/G.fst
+mkdir -p data/lang_poco_test_unk
+cp -r data/lang_unk/* data/lang_poco_test_unk
+cp data/lang_poco_test/G.fst data/lang_poco_test_unk/G.fst
+
+mkdir -p data/lang_poco_test_ex250k_unk
+cp -r data/lang_unk/* data/lang_poco_test_ex250k_unk
+cp data/lang_poco_test_ex250k/G.fst data/lang_poco_test_ex250k_unk/G.fst
+
+exit 0
 
 utils/mkgraph.sh data/lang_unk exp/tri3 exp/tri3/graph_unk
 
