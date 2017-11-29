3cf8ae7f15b00f27b149f88d05517e73ddde2937
Sean Robertson <sdrobert@cs.toronto.edu>
2017-11-28 19:45:38 -0500

    MessageLogger sanitizes string before printing
    In #2044, StringToReadable was employed to ensure invalid buffer
    strings were actually printable on console. However, there are many
    more places where error messages could contain unprintable chars
    than just kaldi-matrix and kaldi-vector (e.g. keys, rxfilenames
    from corrupted scp files, etc.). As part of the debug process in
    I/O, these partial results are printed all over the place, which
    lead to segfaults.
    
    This commit pushes the work to the destructor of
    kaldi::MessageLogger. It uses the function StringToReadable to
    sanitize the strings before sending them off to callbacks or
    stderr.

--- src/base/kaldi-error.cc
+++ src/base/kaldi-error.cc
@@ -148,8 +148,8 @@ MessageLogger::MessageLogger(LogMessageEnvelope::Severity severity,
 
 
 MessageLogger::~MessageLogger() KALDI_NOEXCEPT(false) {
-  // remove trailing '\n',
-  std::string str = ss_.str();
+  // sanitize string and remove trailing '\n',
+  std::string str = StringToReadable(ss_.str());
   while (!str.empty() && str[str.length() - 1] == '\n')
     str.resize(str.length() - 1);
 
