cb9191136c7c4d5353bbf4a1cb2aceaa1ab7c59b
Christoph <christoph@i2x.ai>
2017-12-06 19:48:05 +0100

    Suboptimal working swig binding

--- src/online2/online2-nnet3-latgen-i2x-wrapper.h
+++ src/online2/online2-nnet3-latgen-i2x-wrapper.h
@@ -1,7 +1,8 @@
 // online2/online2-nnet3-latgen-i2x-wrapper.h
 #ifndef KALDI_ONLINE2_ONLINE_NNET3_LATGEN_I2X_WRAPPER_H_
 #define KALDI_ONLINE2_ONLINE_NNET3_LATGEN_I2X_WRAPPER_H_
-#include <stdint.h>
+#include <cstdint>
+
 namespace kaldi {
 class DecoderFactory;
 class Decoder; 
@@ -18,20 +19,20 @@ using kaldi::Decoder;
    which will create light-weighted decoder objects (one per session).
    Returns nullptr on failure.
 */
-DecoderFactory *InitDecoderFactory(const std::string &resource_dir);
+extern DecoderFactory *InitDecoderFactory(const std::string &resource_dir);
 
 // Creates a decoder object.
 // Returns nullptr on failure.
-Decoder *StartDecodingSession(const DecoderFactory *);
+extern Decoder *StartDecodingSession(const DecoderFactory *);
 
 // Feed PCM SI16 data into the decoder.
 // Returns 0 on success, error code otherwise.
 // If called with length == 0, the decoder is finalized and no further calls are allowed.
-int32_t FeedChunk(Decoder *, int16_t *data, size_t length);
+extern int32_t FeedChunk(Decoder *, int16_t *data, size_t length);
 
 // Puts the final recognition result in the string passed by pointer.
 // Frees the resources and destroys the recognition session.
 // Returns 0 on success, error code otherwise.
-int32_t GetResultAndFinalize(Decoder *decoder, std::string *result);
+extern int32_t GetResultAndFinalize(Decoder *decoder, std::string *result);
 #endif // KALDI_ONLINE2_ONLINE_NNET3_LATGEN_I2X_WRAPPER_H_
 
---
+++ src/online2/online2-nnet3-latgen-i2x-wrapper.i
@@ -0,0 +1,28 @@
+%module online2_nnet3_latgen_i2x_wrapper
+%{
+#include "online2-nnet3-latgen-i2x-wrapper.h"
+
+int16_t* Cast(intptr_t ptr) {
+  return (int16_t*)(ptr);
+}
+std::string* CreateString() {
+  return new std::string;
+}
+void FreeString(std::string* str) {
+  delete str;
+}
+std::string DereferenceStringPtr(std::string* str) {
+  return *str;
+}
+%}
+
+%include stdint.i
+%include cpointer.i
+%pointer_class(int16_t, intp);
+%include std_string.i
+using std::string;
+%include "online2-nnet3-latgen-i2x-wrapper.h"
+extern int16_t* Cast(int ptr);
+extern std::string* CreateString();
+extern void FreeString(std::string* str);
+extern std::string DereferenceStringPtr(std::string* str);
