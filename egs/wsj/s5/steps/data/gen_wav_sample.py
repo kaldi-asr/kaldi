63a7d680724eb5bdb5d010d0f01e5b23b0f51e99
Ye Bai <by1993@qq.com>
2017-12-10 03:13:13 -0500

    add interpreter env

--- egs/wsj/s5/steps/data/gen_wav_sample.py
+++ egs/wsj/s5/steps/data/gen_wav_sample.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 #encoding:utf8
 from __future__ import print_function
 import subprocess
@@ -10,35 +11,35 @@ def execute_command(command):
     if p.returncode is not 0:
         raise Exception("Command exited with status {0}: {1}".format(
                 p.returncode, command))
-                
+
 
 def get_args():
     parser = argparse.ArgumentParser(description="Generate wave samples based on wav.scp.\n"
                                                  "Usage: gen_wav_sample.py [options...] <wav.scp> <out-data-dir>\n"
                                                  "E.g. python gen_wav_sample.py wav.scp sample",
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)                
-                
-    parser.add_argument("--num", type=int, dest = "sample_num", default = 10, 
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("--num", type=int, dest = "sample_num", default = 10,
                                         help="Number of samples to generate.")
 
-    parser.add_argument("--prefix", type=str, dest = "prefix", default = "", 
+    parser.add_argument("--prefix", type=str, dest = "prefix", default = "",
                                         help="Prefix of generated samples.")
-                                        
-    parser.add_argument("--record_file", type=str, dest = "record_file", default = "", 
+
+    parser.add_argument("--record_file", type=str, dest = "record_file", default = "",
                                         help="A list of records to generate samples.\n"
                                             " The format is like this:"
                                             "rvb_noise_f001_000008.wav \n"
                                             "rvb_noise_f001_000009.wav \n"
                                             "...")
-                                        
+
     parser.add_argument("scp_file",
                         help="Input data")
     parser.add_argument("output_dir",
                         help="Output data directory")
-                                        
+
     args = parser.parse_args()
     return args
-    
+
 if __name__ == '__main__':
 
     args = get_args()
@@ -46,12 +47,12 @@ if __name__ == '__main__':
     prefix = args.prefix
     scp_file = args.scp_file
     output_dir = args.output_dir
-    
+
     execute_command("mkdir -p {}".format(output_dir))
-    
+
     if prefix:
         prefix += "_"
-    
+
     record_dic = {}
 
     if args.record_file:
@@ -60,8 +61,8 @@ if __name__ == '__main__':
                 id = line.strip()
                 if id not in record_dic:
                     record_dic[id] = 1
-    
-    with open(scp_file, "r") as f:   
+
+    with open(scp_file, "r") as f:
         for line in f:
             if sample_num == 0:
                 break
@@ -79,7 +80,7 @@ if __name__ == '__main__':
                         execute_command("wav-copy '{wav}' {outpath}/{out_name}".format(wav=reco,
                                     outpath=output_dir,out_name=prefix+id+".wav"))
                     sample_num -=1
-            else:                
+            else:
                 reco = " ".join(items[1:]).strip()
                 if reco[-1] == "|":
                     execute_command("{wav} > {outpath}/{out_name}".format(wav=reco[0:-1],
@@ -87,7 +88,7 @@ if __name__ == '__main__':
                 else:
                     execute_command("wav-copy '{wav}' {outpath}/{out_name}".format(wav=reco,
                                 outpath=output_dir,out_name=prefix+id+".wav"))
-                sample_num -=1          
+                sample_num -=1
     print("Done.")
 
 
