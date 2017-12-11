91e36418d05f56bd209e0c08c7d438779a1babf8
Ilya Edrenkin <ilya.edrenkin@gmail.com>
2017-12-11 11:41:05 +0100

    Add RTF measurement to the smoke test

--- src/online2_py/smoke_test.py
+++ src/online2_py/smoke_test.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python3
+
 import online2_nnet3_latgen_i2x_wrapper as m
+
 import numpy
+import random
+import timeit
+import wave
 
 wavefile = numpy.random.randint(low=-2**15, high=2**15, size=16000*10, dtype=numpy.int16)
 dfactory = m.DecoderFactory(b"../online2bin/resourcedir/")
@@ -10,13 +16,26 @@ rr = m.RecognitionResult()
 decoder.GetResultAndFinalize(rr.this)
 print(str(rr.transcript, 'utf8'))
 
-import wave
 f = open(b"/home/christoph/data/transcription/to_transcribe2/16khz/e9603e8f814dbe98d233b04af040ad3154f689c34128026a7b93bbd8924b8623.webm.wav", 'rb')
 w = wave.open(f)
 www = w.readframes(w.getnframes())
-decoder = dfactory.StartDecodingSession()
-decoder.FeedBytestring(www)
-decoder.FeedBytestring(b"")
+
+def run_decoding():
+    decoder = dfactory.StartDecodingSession()
+    i = 0
+    while i < len(www):
+        chunk_length = random.randint(1, 200)
+        decoder.FeedBytestring(www[i:i+chunk_length])
+        i += chunk_length
+    decoder.FeedBytestring(b"")
+    return decoder
+
+decoder = run_decoding()
 rr = m.RecognitionResult()
 decoder.GetResultAndFinalize(rr.this)
 print(str(rr.transcript, 'utf8'))
+
+NUM = 3
+elapsed = timeit.repeat("run_decoding()", setup="from __main__ import run_decoding", number=1, repeat=NUM)
+length = w.getnframes() / w.getframerate()
+print("Elapsed: {}, Sound length: {}, best-case RTF: {}".format(elapsed, length, min(elapsed) / length))
