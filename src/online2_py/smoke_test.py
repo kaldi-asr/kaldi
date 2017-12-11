05569c4fdadf1a752e11cfaef6a0978aa7786bd3
Christoph <christoph@i2x.ai>
2017-12-11 18:59:53 +0100

    multiprocess_test.py is a multi-process test for the library. It decodes all 10 wave-files in resourcedir/wav.scp in parallel. Pep8 compliance

--- src/online2_py/smoke_test.py
+++ src/online2_py/smoke_test.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 
-import online2_nnet3_latgen_i2x_wrapper as m
-
-import numpy
 import random
 import timeit
 import wave
+
+import numpy
+import online2_nnet3_latgen_i2x_wrapper as m
 
 
 def printRecoResult(rr):
     print("[ num frames = '{:5d}', average per-frame likelihood = {:02.3f} ], {}".format(
         rr.num_frames, rr.mean_frame_likelihood, str(rr.transcript, 'utf8')))
 
-wavefile = numpy.random.randint(low=-2**15, high=2**15, size=16000*10, dtype=numpy.int16)
+
+wavefile = numpy.random.randint(low=-2 ** 15, high=2 ** 15, size=16000 * 10, dtype=numpy.int16)
 dfactory = m.DecoderFactory(b"../online2bin/resourcedir/")
 decoder = dfactory.StartDecodingSession()
 decoder.FeedBytestring(wavefile.tostring())
@@ -20,22 +21,26 @@ decoder.Finalize()
 rr = decoder.GetResult()
 printRecoResult(rr)
 
-f = open(b"/home/christoph/data/transcription/to_transcribe2/16khz/e9603e8f814dbe98d233b04af040ad3154f689c34128026a7b93bbd8924b8623.webm.wav", 'rb')
+f = open(
+    b"/home/christoph/data/transcription/to_transcribe2/16khz/e9603e8f814dbe98d233b04af040ad3154f689c34128026a7b93bbd8924b8623.webm.wav",
+    'rb')
 w = wave.open(f)
 w_samples = w.readframes(w.getnframes())
+
 
 def run_decoding(partial, low=1, high=200):
     decoder = dfactory.StartDecodingSession()
     i = 0
     while i < len(w_samples):
         chunk_length = random.randint(low, high)
-        decoder.FeedBytestring(w_samples[i:i+chunk_length])
+        decoder.FeedBytestring(w_samples[i:i + chunk_length])
         if partial:
             result = decoder.GetResult()
             printRecoResult(result)
         i += chunk_length
     decoder.Finalize()
     return decoder
+
 
 decoder = run_decoding(partial=True, low=100, high=16000)
 decoder.Finalize()
