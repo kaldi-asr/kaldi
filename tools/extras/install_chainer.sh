7a814ed4bca47a7084da40c7b01c7af2733f6061
Aswin Shanmugam Subramanian <aswinphoenix@gmail.com>
2017-12-11 19:21:57 -0500

    added script to install chainer

---
+++ tools/extras/install_chainer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Installs chainer for nn-gev on the CLSP grid
+
+python3 -m pip install --user chainer==1.16.0
+python3 -m pip install --user tqdm
+python3 -m pip install --user scikit-learn
+python3 -m pip install --user librosa
