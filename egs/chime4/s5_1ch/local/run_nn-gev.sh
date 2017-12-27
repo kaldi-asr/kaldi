d3898b0c2c7d4c064294334bde1f6c61b00866fc
Aswin Shanmugam Subramanian <aswinphoenix@gmail.com>
2017-12-27 08:36:36 -0500

    Fixed bug in run_nn-gev.sh

--- egs/chime4/s5_1ch/local/run_nn-gev.sh
+++ egs/chime4/s5_1ch/local/run_nn-gev.sh
@@ -20,5 +20,5 @@ case $(hostname -f) in
   *.clsp.jhu.edu) gpu_id=`free-gpu` ;; # JHU,
 esac 
 
-$HOME/miniconda3/bin/python local/nn-gev/train.py --chime_dir=$sdir/data --gpu $gpu_id local/nn-gev/data local/nn-gev/beamform.sh $sdir/data local/nn-gev/data $odir local/nn-gev/data/BLSTM_model/best.nnet BLSTM --gpu $gpu_id --single $enhancement_type
+$HOME/miniconda3/bin/python local/nn-gev/train.py --chime_dir=$sdir/data --gpu $gpu_id local/nn-gev/data BLSTM
 local/nn-gev/beamform.sh $sdir/data local/nn-gev/data $odir local/nn-gev/data/BLSTM_model/best.nnet BLSTM --gpu $gpu_id --single $enhancement_type
