kaldi-asr/kaldi

commit df7a41978f2bc9ad57774411c947dd225bde3dc2

[scripts] Fix validation-script bugs found by Somogyi Z; make validate_text.pl quieter.

egs/wsj/s5/utils/validate_data_dir.sh
@@ -162,9 +162,10 @@ if [ -f $data/wav.scp ]; then
 
     segments_len=`cat $data/segments | wc -l`
     if [ -f $data/text ]; then
-      ! cmp -s $tmpdir/utts <(awk '{print $1}' <$data/text) && \
-        echo "$0: Utterance list differs between $data/text and $data/segments " && \
+      ! cmp -s $tmpdir/utts <(awk '{print $1}' <$data/segments) && \
+        echo "$0: Utterance list differs between $data/utt2spk and $data/segments " && \
         echo "$0: Lengths are $segments_len vs $num_utts";
+      exit 1
     fi
 
     cat $data/segments | awk '{print $2}' | sort | uniq > $tmpdir/recordings
@@ -298,7 +299,7 @@ fi
 if [ -f $data/utt2warp ]; then
   check_sorted_and_uniq $data/utt2warp
   ! cat $data/utt2warp | awk '{if (!((NF == 2 && ($2 > 0.5 && $2 < 1.5)))){ print; exit 1; }}' && \
-     echo "$0: Mal-formed spk2warp file" && exit 1;
+     echo "$0: Mal-formed utt2warp file" && exit 1;
   cat $data/utt2warp | awk '{print $1}' > $tmpdir/utts.utt2warp
   cat $data/utt2spk | awk '{print $1}' > $tmpdir/utts
   if ! cmp -s $tmpdir/utts{,.utt2warp}; then

egs/wsj/s5/utils/validate_text.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/env perl
+#
 #===============================================================================
-# Copyright 2017  (Author: Yenda Trmal <jtrmal@gmail.com>)
+# Copyright 2017  Johns Hopkins University (author: Yenda Trmal <jtrmal@gmail.com>)
+#                 Johns Hopkins University (author: Daniel Povey)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -88,23 +90,20 @@ sub validate_utf8_whitespaces {
 # reading the text.
 sub check_allowed_whitespace {
   my $file = shift;
+  my $filename = shift;
   my $pos = tell($file);
   (my $is_utf, my @lines) = get_utf8_or_bytestream($file);
   seek($file, $pos, SEEK_SET);
   if ($is_utf) {
     my $has_invalid_whitespaces = validate_utf8_whitespaces(\@lines);
-    print "--> text seems to be UTF-8 or ASCII, checking whitespaces\n";
     if ($has_invalid_whitespaces) {
-      print "--> ERROR: the text containes disallowed UTF-8 whitespace character(s)\n";
+      print STDERR "$0: ERROR: text file '$filename' contains disallowed UTF-8 whitespace character(s)\n";
       return 0;
-    } else {
-      print "--> text contains only allowed whitespaces\n";
     }
-  } else {
-    print "--> text doesn't seem to be UTF-8 or ASCII, won't check whitespaces\n";
   }
   return 1;
 }
+
 if(@ARGV != 1) {
   die "Usage: validate_text.pl <text-file>\n" .
       "e.g.: validate_text.pl data/train/text\n";
@@ -112,20 +111,15 @@ if(@ARGV != 1) {
 
 my $text = shift @ARGV;
 
-# Checking optional_silence.txt -------------------------------
-print "Checking $text ...\n";
-if(-z "$text") {
-  print "--> ERROR: $text is empty or not exists\n";
+if (-z "$text") {
+  print STDERR "$0: ERROR: file '$text' is empty or does not exist\n";
   exit 1;
 }
 
 if(!open(FILE, "<$text")) {
-  print "--> ERROR: fail to open $text\n";
+  print STDERR "$0: ERROR: failed to open $text\n";
   exit 1;
 }
 
-print "--> reading $text\n";
-check_allowed_whitespace(\*FILE) or exit 1;
+check_allowed_whitespace(\*FILE, $text) or exit 1;
 close(FILE);
-
-

commit 006e3c8cca0e7dd952542ad12834ea4a9d43de97

[scripts] Fig bug in validate_data_dir.sh introduced in df7a41978f2. Thx:@jcsilva

egs/wsj/s5/utils/validate_data_dir.sh
@@ -164,8 +164,8 @@ if [ -f $data/wav.scp ]; then
     if [ -f $data/text ]; then
       ! cmp -s $tmpdir/utts <(awk '{print $1}' <$data/segments) && \
         echo "$0: Utterance list differs between $data/utt2spk and $data/segments " && \
-        echo "$0: Lengths are $segments_len vs $num_utts";
-      exit 1
+        echo "$0: Lengths are $segments_len vs $num_utts" && \
+        exit 1
     fi
 
     cat $data/segments | awk '{print $2}' | sort | uniq > $tmpdir/recordings