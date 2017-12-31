414d33c5c9198276ac6b301c3f547b08a1bc91fa
Daniel Galvez <galv@users.noreply.github.com>
2017-12-31 12:03:33 -0800

    [src] Make faster ApplyFloor and ApplyCeiling variants for Matrix and CuMatrix. (#2115)
    ( breaks backwards compatibility, since the signatures of these methods change).

--- src/nnet3/nnet-general-component.cc
+++ src/nnet3/nnet-general-component.cc
@@ -1127,7 +1127,8 @@ void BackpropTruncationComponent::Backprop(const std::string &debug_info,
                               kNoTrans, 0.0);
   // now clipping_scales contains the squared (norm of each row divided by
   //  clipping_threshold)
-  int32 num_not_scaled = clipping_scales.ApplyFloor(1.0);
+  int32 num_not_scaled;
+  clipping_scales.ApplyFloor(1.0, &num_not_scaled);
   // now clipping_scales contains min(1, squared-(norm/clipping_threshold))
   clipping_scales.ApplyPow(-0.5);
   // now clipping_scales contains max(1, clipping_threshold/vector_norm)
