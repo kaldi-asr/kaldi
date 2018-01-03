30b464cd538da440a0c68d8d73627c5b91f044df
Shinji Watanabe <sw005320@gmail.com>
2018-01-03 18:09:08 -0500

    bug fix when compiling PESQ

--- egs/chime4/s5_1ch/local/download_se_eval_tool.sh
+++ egs/chime4/s5_1ch/local/download_se_eval_tool.sh
@@ -6,8 +6,8 @@ wget http://bass-db.gforge.inria.fr/bss_eval/bss_eval_sources.m -O local/bss_eva
 wget https://github.com/JacobD10/SoundZone_Tools/raw/master/stoi.m -O local/stoi.m
 wget https://github.com/JacobD10/SoundZone_Tools/raw/master/estoi.m -O local/estoi.m
 wget 'https://www.itu.int/rec/dologin_pub.asp?lang=e&id=T-REC-P.862-200102-I!!SOFT-ZST-E&type=items' -O PESQ.zip
-unzip PESQ.zip -d local/PESQ
-cd local/PESQ/P862/Software/source
-gcc -SQ *.c -lm
+unzip PESQ.zip -d local/PESQ_sources
+cd local/PESQ_sources/P862/Software/source
+gcc  *.c -lm -o PESQ
 cd ../../../../../
-mv local/P862/Software/source/PESQ local/
+mv local/PESQ_sources/P862/Software/source/PESQ local/
