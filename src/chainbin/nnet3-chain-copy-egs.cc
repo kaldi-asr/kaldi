db0bc5471550ada270cae7d08d1ccb2359bf4963
Vimal Manohar <vimal.manohar91@gmail.com>
2018-01-09 05:12:47 -0500

    Remove truncate-deriv-weights

--- src/chainbin/nnet3-chain-copy-egs.cc
+++ src/chainbin/nnet3-chain-copy-egs.cc
@@ -302,7 +302,6 @@ int main(int argc, char *argv[]) {
     bool random = false;
     int32 srand_seed = 0;
     int32 frame_shift = 0;
-    int32 truncate_deriv_weights = 0;
     int32 frame_subsampling_factor = -1;
     BaseFloat keep_proportion = 1.0;
     int32 left_context = -1, right_context = -1;
@@ -321,9 +320,6 @@ int main(int argc, char *argv[]) {
                 "in the supervision data (excluding iVector data) - useful in "
                 "augmenting data.  Note, the outputs will remain at the closest "
                 "exact multiples of the frame subsampling factor");
-    po.Register("truncate-deriv-weights", &truncate_deriv_weights,
-                "If nonzero, the number of initial/final subsample frames that "
-                "will have their derivatives' weights set to zero.");
     po.Register("left-context", &left_context, "Can be used to truncate the "
                 "feature left-context that we output.");
     po.Register("right-context", &right_context, "Can be used to truncate the "
@@ -398,8 +394,7 @@ int main(int argc, char *argv[]) {
           new_output_name = output_reader.Value(key);
         }
       }
-      if (frame_shift == 0 && truncate_deriv_weights == 0 &&
-          left_context == -1 && right_context == -1) {
+      if (frame_shift == 0 && left_context == -1 && right_context == -1) {
         for (int32 c = 0; c < count; c++) {
           int32 index = (random ? Rand() : num_written) % num_outputs;
           if (modify_eg_output) // Only for multilingual training
@@ -419,8 +414,6 @@ int main(int argc, char *argv[]) {
                                     frame_subsampling_factor, &eg_out);
         else
           eg_out.Swap(&eg);
-        if (truncate_deriv_weights != 0)
-          TruncateDerivWeights(truncate_deriv_weights, &eg_out);
         for (int32 c = 0; c < count; c++) {
           int32 index = (random ? Rand() : num_written) % num_outputs;
           if (modify_eg_output)
