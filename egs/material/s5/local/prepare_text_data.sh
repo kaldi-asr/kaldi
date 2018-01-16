c9b3e756eba2331b0a02db1374cd829c1c65e109
freewym <freewym@gmail.com>
2018-01-16 11:19:44 -0500

    reoriganized the scripts structures to allow to specifying language names instead of putting different languages into different s5* subdirs

--- egs/material/s5/local/prepare_text_data.sh
+++ egs/material/s5/local/prepare_text_data.sh
@@ -8,35 +8,36 @@ set -e -o pipefail
 set -o nounset                              # Treat unset variables as an error
 echo "$0 " "$@"
 
-if [ $# -ne 1 ] ; then
+if [ $# -ne 2 ] ; then
   echo "Invalid number of script parameters. "
-  echo "  $0 <path-to-material-corpus>"
+  echo "  $0 <path-to-material-corpus> <language-name>"
   echo "e.g."
-  echo "  $0 /export/corpora5/MATERIAL/IARPA_MATERIAL_BASE-1A-BUILD_v1.0/"
+  echo "  $0 /export/corpora5/MATERIAL/IARPA_MATERIAL_BASE-1A-BUILD_v1.0/ swahili"
   exit
 fi
 data=$1;
+language=$2
 conversational_train=$data/conversational/training/
-mkdir -p data/train/
+mkdir -p data/$language/train/
 for file in $conversational_train/transcription/*txt ; do
   ./local/parse_transcripts.pl $file
-done  > data/train/transcripts.txt
+done  > data/$language/train/transcripts.txt
 
 
 conversational_dev=$data/conversational/dev/
-mkdir -p data/dev
+mkdir -p data/$language/dev
 for file in $conversational_dev/transcription/*txt ; do
   ./local/parse_transcripts.pl $file
-done > data/dev/transcripts.txt
+done > data/$language/dev/transcripts.txt
 
 
-cat data/train/transcripts.txt | \
+cat data/$language/train/transcripts.txt | \
   local/cleanup_transcripts.pl | \
-  local/create_datafiles.pl data/train/
+  local/create_datafiles.pl data/$language/train/
 
-cat data/dev/transcripts.txt | \
+cat data/$language/dev/transcripts.txt | \
   local/cleanup_transcripts.pl | \
-  local/create_datafiles.pl data/dev/
+  local/create_datafiles.pl data/$language/dev/
 
 
 
