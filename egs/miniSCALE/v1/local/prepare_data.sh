49725d045a88f6c62cfd8046ba97f97970d983c7
Chun-Chieh Chang <cchunch1@jhu.edu>
2018-01-17 13:06:01 -0500

    created initial files from the iam_ocr files

---
+++ egs/miniSCALE/v1/local/prepare_data.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Copyright      2017  Chun Chieh Chang
+#                2017  Ashish Arora
+#                2017  Hossein Hadian
+# Apache 2.0
+
+# This script downloads the IAM handwriting database and prepares the training
+# and test data (i.e text, images.scp, utt2spk and spk2utt) by calling process_data.py.
+# It also downloads the LOB and Brown text corpora. It downloads the database files
+# only if they do not already exist in download directory.
+
+#  Eg. local/prepare_data.sh
+#  Eg. text file: 000_a01-000u-00 A MOVE to stop Mr. Gaitskell from
+#      utt2spk file: 000_a01-000u-00 000
+#      images.scp file: 000_a01-000u-00 data/local/lines/a01/a01-000u/a01-000u-00.png
+#      spk2utt file: 000 000_a01-000u-00 000_a01-000u-01 000_a01-000u-02 000_a01-000u-03
+
+stage=0
+download_dir=data/download/madcat_sample
+username=
+password=       # username and password for downloading the IAM database
+                # if you have not already downloaded the database, please
+                # register at http://www.fki.inf.unibe.ch/databases/iam-handwriting-database
+                # and provide this script with your username and password.
+
+. ./cmd.sh
+. ./path.sh
+. ./utils/parse_options.sh || exit 1;
+
+if [[ ! -d $download_dir ]]; then
+  echo "$0: Warning: Couldn't find $download_dir."
+  echo ""
+fi
+
+if [ $stage -le 0 ]; then
+  local/process_data.py $download_dir data/train --dataset train || exit 1
+  local/process_data.py $download_dir data/test --dataset test || exit 1
+  local/process_data.py $download_dir data/val --dataset validation || exit 1
+
+  utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
+  utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
+fi
---
+++ egs/miniSCALE/v1/local/process_data.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+# Copyright      2017  Chun Chieh Chang
+#                2017  Ashish Arora
+
+""" This script reads the extracted IAM database files and creates
+    the following files (for the data subset selected via --dataset):
+    text, utt2spk, images.scp.
+
+  Eg. local/process_data.py data/local data/train data --dataset train
+  Eg. text file: 000_a01-000u-00 A MOVE to stop Mr. Gaitskell from
+      utt2spk file: 000_a01-000u-00 000
+      images.scp file: 000_a01-000u-00 data/local/lines/a01/a01-000u/a01-000u-00.png
+"""
+
+import argparse
+import os
+import sys
+import xml.dom.minidom as minidom
+
+parser = argparse.ArgumentParser(description="""Creates text, utt2spk and images.scp files.""")
+parser.add_argument('database_path', type=str,
+                    help='Path to the downloaded (and extracted) IAM data')
+parser.add_argument('out_dir', type=str,
+                    help='Where to write output files.')
+parser.add_argument('--dataset', type=str, default='train',
+                    choices=['train', 'test','validation'],
+                    help='Subset of data to process.')
+args = parser.parse_args()
+
+text_file = os.path.join(args.out_dir + '/', 'text')
+text_fh = open(text_file, 'w')
+utt2spk_file = os.path.join(args.out_dir + '/', 'utt2spk')
+utt2spk_fh = open(utt2spk_file, 'w')
+image_file = os.path.join(args.out_dir + '/', 'images.scp')
+image_fh = open(image_file, 'w')
+
+for f in sorted(os.listdir(args.database_path)):
+    if f.endswith('.gedi.xml'):
+        gedi_xml_path = os.path.join(args.database_path, f)
+        base_name = os.path.splitext(os.path.splitext(gedi_xml_path)[0])[0]
+        madcat_xml_path = base_name + '.madcat.xml'
+        madcat_doc = minidom.parse(madcat_xml_path)
+        gedi_doc = minidom.parse(gedi_xml_path)
+
+        writer = madcat_doc.getElementsByTagName('writer')
+        writer_id = writer[0].getAttribute('id')
+        dl_page = gedi_doc.getElementsByTagName('DL_PAGE')
+        for page in dl_page:
+            image_file_path = base_name + '.tiff'
+            dl_zone = page.getElementsByTagName('DL_ZONE')
+            lines = []
+            for zone in dl_zone:
+                contents = zone.getAttribute('contents')
+                lineID = zone.getAttribute('lineID')
+                if lineID != '':
+                    lineID = int(lineID)
+                    while len(lines) < lineID:
+                        lines.append([])
+                    lines[lineID - 1].append(contents)
+            for lineID, line in enumerate(lines, start=1):
+                text = ''.join(line)
+                utt_id = writer_id + '_' + base_name + '_' + str(lineID)
+                text_fh.write(utt_id + ' ' + text + '\n')
+                utt2spk_fh.write(utt_id + ' ' + writer_id + '\n')
+                image_fh.write(utt_id + ' ' + image_file_path + '\n')
+
+#with open(dataset_path) as f:
+#  for line in f:
+#    line = line.strip()
+#    line_vect = line.split('-')
+#    xml_file = line_vect[0] + '-' + line_vect[1]
+#    xml_path = os.path.join(args.database_path, 'xml', xml_file + '.xml')
+#    img_num = line[-3:]
+#    doc = minidom.parse(xml_path)
+#
+#    form_elements = doc.getElementsByTagName('form')[0]
+#    writer_id = form_elements.getAttribute('writer-id')
+#    outerfolder = form_elements.getAttribute('id')[0:3]
+#    innerfolder = form_elements.getAttribute('id')
+#    lines_path = os.path.join(args.database_path, 'lines', outerfolder, innerfolder, innerfolder)
+#    image_file_path = lines_path + img_num + '.png'
+#    text =  text_dict[line]
+#    utt_id = writer_id + '_' + line
+#    text_fh.write(utt_id + ' ' + text + '\n')
+#    utt2spk_fh.write(utt_id + ' ' + writer_id + '\n')
+#    image_fh.write(utt_id + ' ' + image_file_path + '\n')
