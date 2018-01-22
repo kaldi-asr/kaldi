0c5663496a3c8fbc2044f7b24f1dd05cfe91a5c9
Chun-Chieh Chang <cchunch1@jhu.edu>
2018-01-22 16:35:25 -0500

    lm stuff

---
+++ egs/miniSCALE/v1/local/prepare_dict.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Copyright      2017  Hossein Hadian
+#                2017  Chun Chieh Chang
+#                2017  Ashish Arora
+
+# This script prepares the dictionary.
+
+set -e
+dir=data/local/dict
+segments=/home/kduh/proj/scale2018/data/madcat_datasplit/zh-en/madcat.train.tok.zh
+mkdir -p $dir
+
+cat $segments | tr ' ' '\n' | sort -u | \
+  LC_ALL=en_US.UTF-8 awk '{len=split($0,chars,""); printf($0); for (i=0;i<=len;i++){printf(chars[i]" ")}; printf("\n")};' | \
+  sed 's/.$//' >  $dir/lexicon.txt || exit 1;
+
+cut -d' ' -f2- $dir/lexicon.txt | tr ' ' '\n' | sort -u >$dir/nonsilence_phones.txt || exit 1;
+
+echo '<sil> SIL' >> $dir/lexicon.txt
+echo '<unk> SIL' >> $dir/lexicon.txt
+
+echo SIL > $dir/silence_phones.txt
+
+echo SIL >$dir/optional_silence.txt
+
+echo -n "" >$dir/extra_questions.txt
