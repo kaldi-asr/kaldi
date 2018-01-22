e16f9ae92ffdfa60998e1dc0aaf3085db016ef12
Aswin Shanmugam Subramanian <aswinphoenix@gmail.com>
2018-01-21 19:39:32 -0500

    bug fix -- check for directory before matlab simulation in run_beamform_blstm_gev_6ch_track.sh

--- egs/chime4/s5_1ch/local/run_beamform_blstm_gev_6ch_track.sh
+++ egs/chime4/s5_1ch/local/run_beamform_blstm_gev_6ch_track.sh
@@ -67,8 +67,14 @@ else
 fi
 
 mkdir -p $odir
-nIsolatedDirs=`ls local/nn-gev/data/audio/16kHz/isolated/ 2>/dev/null | wc -l`
-nIsolatedExtDirs=`ls local/nn-gev/data/audio/16kHz/isolated_ext/ 2>/dev/null | wc -l`
+
+if [[ -d local/nn-gev/data/audio/16kHz/isolated && -d local/nn-gev/data/audio/16kHz/isolated_ext ]];then
+   nIsolatedDirs=`ls local/nn-gev/data/audio/16kHz/isolated/ | wc -l`
+   nIsolatedExtDirs=`ls local/nn-gev/data/audio/16kHz/isolated_ext/ | wc -l`
+else
+   nIsolatedDirs=0
+   nIsolatedExtDirs=0
+fi
 if [[ "$nIsolatedDirs" -ne 12 || "$nIsolatedExtDirs" -ne 12 ]];then
    echo "generating simulation data and storing in local/nn-gev/data"
    $cmd $odir/simulation.log matlab -nodisplay -nosplash -r "addpath('local'); CHiME3_simulate_data_patched_parallel(1,$nj,'$sdir','$chime3_dir');exit"
