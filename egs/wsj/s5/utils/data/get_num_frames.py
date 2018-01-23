4a43b8af30c94acf99b756d9f25cdcf47d36713e
Gaofeng Cheng <770579626@qq.com>
2018-01-23 16:00:53 +0800

    Fix upper bound bug in get_num_frames.sh

---
+++ egs/wsj/s5/utils/data/get_num_frames.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+import argparse
+parser = argparse.ArgumentParser(description="""
+         Calculate the total frame number from utt2dur file.""")
+
+parser.add_argument("--frame-shift", type = float, default = 0.01)
+parser.add_argument("--utt2dur-dir", type = str, default = "")
+args = parser.parse_args()
+
+utt2dur_file = open(args.utt2dur_dir)
+wav_time = 0.0
+
+while 1:
+  line = utt2dur_file.readline()
+  if not line:
+    break
+  line.split()
+  wav_time += float(line.split()[1])
+
+num_frames = wav_time / args.frame_shift
+print(str(num_frames).split('.')[0])
+
