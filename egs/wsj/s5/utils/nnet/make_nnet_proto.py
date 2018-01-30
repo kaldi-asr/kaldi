39f56c6784823f025929a4bfce14bdbcf521d767
Blaise Potard <blaise@cereproc.com>
2018-01-30 11:57:20 +0000

    Added the possibility to append an activation layer at the end of the network

--- egs/wsj/s5/utils/nnet/make_nnet_proto.py
+++ egs/wsj/s5/utils/nnet/make_nnet_proto.py
@@ -37,6 +37,9 @@ parser.add_option('--block-softmax-dims', dest='block_softmax_dims',
                    help='Generate <BlockSoftmax> with dims D1:D2:D3 [default: %default]',
                    default="", type='string');
 # Activation related,
+parser.add_option('--activation-final', dest='activation_final',
+                   help='If set, outputs an activation layer as final layer [default: %default]',
+                   default=False, action='store_true');
 parser.add_option('--activation-type', dest='activation_type',
                    help='Select type of activation function : (<Sigmoid>|<Tanh>|<ParametricRelu>) [default: %default]',
                    default='<Sigmoid>', type='string');
@@ -230,6 +233,9 @@ if o.with_softmax:
     print "<Softmax> <InputDim> %d <OutputDim> %d" % (num_leaves, num_leaves)
   else:
     print "<BlockSoftmax> <InputDim> %d <OutputDim> %d <BlockDims> %s" % (num_leaves, num_leaves, o.block_softmax_dims)
+
+if o.activation_final:
+  print "%s <InputDim> %d <OutputDim> %d %s" % (o.activation_type, num_leaves, num_leaves, o.activation_opts)
 
 # We are done!
 sys.exit(0)
