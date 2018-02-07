d7cf604bf3b14b9eeb2a52be9bee07b208c37ec6
Mahsa Yarmohammadi <mahsay@login3.clsp.jhu.edu>
2018-02-07 00:18:33 -0500

    remove  in exp/ and data/

--- egs/material/s5/local/chain/decode_test.sh
+++ egs/material/s5/local/chain/decode_test.sh
@@ -4,20 +4,19 @@ export LD_LIBRARY_PATH=/home/dpovey/libs
 # Set -e here so that we catch if any executable fails immediately
 set -euo pipefail
 
-language=swahili
 . utils/parse_options.sh
 
-datadev="data/"$language"/analysis1"
+datadev="data/analysis1"
 label_delay=5
 tlstm_affix=1a   # affix for the TDNN-LSTM directory name
 test_sets="analysis1-segmented"
 nnet3_affix=
-tree_dir=exp/$language/chain${nnet3_affix}/tree_sp${tree_affix:+_$tree_affix}             
-dir=exp/$language/chain${nnet3_affix}/tdnn_lstm${tlstm_affix}_sp
+tree_dir=exp/chain${nnet3_affix}/tree_sp${tree_affix:+_$tree_affix}             
+dir=exp/chain${nnet3_affix}/tdnn_lstm${tlstm_affix}_sp
 if [ $label_delay -gt 0 ]; then dir=${dir}_ld$label_delay; fi
 train_set=train
-train_data_dir=data/$language/${train_set}_sp_hires                                       
-lores_train_data_dir=data/$language/${train_set}_sp
+train_data_dir=data/${train_set}_sp_hires                                       
+lores_train_data_dir=data/${train_set}_sp
 
 decode_nj=30
 gmm=tri3
@@ -52,7 +51,7 @@ EOF
 fi
 
 # audio segmentation
-local/preprocess_test.sh $datadev $language
+local/preprocess_test.sh $datadev
 
 nj=30
 gmm=tri3
@@ -61,14 +60,14 @@ test_sets="analysis1-segmented"
 # stage 3
 
 for datadir in $test_sets; do
-  utils/copy_data_dir.sh data/$language/$datadir data/$language/${datadir}_hires
+  utils/copy_data_dir.sh data/$datadir data/${datadir}_hires
 done
 
 for datadir in $test_sets; do
   steps/make_mfcc.sh --nj $nj --mfcc-config conf/mfcc_hires.conf \
-    --cmd "$train_cmd" data/$language/${datadir}_hires || exit 1;
-  steps/compute_cmvn_stats.sh data/$language/${datadir}_hires || exit 1;
-  utils/fix_data_dir.sh data/$language/${datadir}_hires || exit 1;
+    --cmd "$train_cmd" data/${datadir}_hires || exit 1;
+  steps/compute_cmvn_stats.sh data/${datadir}_hires || exit 1;
+  utils/fix_data_dir.sh data/${datadir}_hires || exit 1;
 done
 
 # stage 6
@@ -77,19 +76,19 @@ done
 # perturbation (sp).
 for data in $test_sets; do
   steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd" --nj $nj \
-    data/$language/${data}_hires exp/$language/nnet3${nnet3_affix}/extractor \
-    exp/$language/nnet3${nnet3_affix}/ivectors_${data}_hires
+    data/${data}_hires exp/nnet3${nnet3_affix}/extractor \
+    exp/nnet3${nnet3_affix}/ivectors_${data}_hires
 done
 
 
-gmm_dir=exp/$language/$gmm                                                                
-ali_dir=exp/$language/${gmm}_ali_${train_set}_sp                                          
-tree_dir=exp/$language/chain${nnet3_affix}/tree_sp${tree_affix:+_$tree_affix}             
-dir=exp/$language/chain${nnet3_affix}/tdnn_lstm${tlstm_affix}_sp                          
+gmm_dir=exp/$gmm                                                                
+ali_dir=exp/${gmm}_ali_${train_set}_sp                                          
+tree_dir=exp/chain${nnet3_affix}/tree_sp${tree_affix:+_$tree_affix}             
+dir=exp/chain${nnet3_affix}/tdnn_lstm${tlstm_affix}_sp                          
 if [ $label_delay -gt 0 ]; then dir=${dir}_ld$label_delay; fi                   
-train_data_dir=data/$language/${train_set}_sp_hires                                       
-lores_train_data_dir=data/$language/${train_set}_sp                                       
-train_ivector_dir=exp/$language/nnet3${nnet3_affix}/ivectors_${train_set}_sp_hires        
+train_data_dir=data/${train_set}_sp_hires                                       
+lores_train_data_dir=data/${train_set}_sp                                       
+train_ivector_dir=exp/nnet3${nnet3_affix}/ivectors_${train_set}_sp_hires        
                                                                                 
 for f in $gmm_dir/final.mdl $train_data_dir/feats.scp $train_ivector_dir/ivector_online.scp \
     $lores_train_data_dir/feats.scp $ali_dir/ali.1.gz; do                       
@@ -103,7 +102,7 @@ rm $dir/.error 2>/dev/null || true
 
 for data in $test_sets; do
   (
-    nspk=$(wc -l <data/$language/${data}_hires/spk2utt)
+    nspk=$(wc -l <data/${data}_hires/spk2utt)
     steps/nnet3/decode.sh \
         --acwt 1.0 --post-decode-acwt 10.0 \
         --extra-left-context $chunk_left_context \
@@ -112,13 +111,13 @@ for data in $test_sets; do
         --extra-right-context-final 0 \
         --frames-per-chunk $frames_per_chunk \
         --nj $nspk --cmd "$decode_cmd"  --num-threads 4 \
-        --online-ivector-dir exp/$language/nnet3${nnet3_affix}/ivectors_${data}_hires \
-        $tree_dir/graph data/$language/${data}_hires ${dir}/decode_${data} || exit 1
+        --online-ivector-dir exp/nnet3${nnet3_affix}/ivectors_${data}_hires \
+        $tree_dir/graph data/${data}_hires ${dir}/decode_${data} || exit 1
   ) || touch $dir/.error &
   
   # resolve ctm overlaping regions, and compute wer
-  cp $datadev/reftext data/$language/${data}_hires
-  local/postprocess_test.sh $test_sets $tree_dir $dir $language
+  cp $datadev/reftext data/${data}_hires
+  local/postprocess_test.sh $test_sets $tree_dir $dir
 done
 wait
 [ -f $dir/.error ] && echo "$0: there was a problem while decoding" && exit 1
--- egs/material/s5/local/postprocess_test.sh
+++ egs/material/s5/local/postprocess_test.sh
@@ -7,7 +7,6 @@ echo "$0 $@"
 test_sets=$1
 tree_dir=$2
 dir=$3
-language=$4
 
 ./cmd.sh                                                                        
 ./path.sh                                                                       
@@ -18,22 +17,22 @@ language=$4
 
 for data in $test_sets; do
   steps/cleanup/internal/get_ctm.sh --frame-shift 0.03 \
-    data/$language/${data}_hires/ $tree_dir/graph/ \
+    data/${data}_hires/ $tree_dir/graph/ \
     $dir/decode_$data/
 
   cat $dir/decode_$data/score_10/${data}_hires.ctm.* \
     > $dir/decode_$data/score_10/${data}_hires.ctm
 
-  awk '{print $2" "$2" 1"}' data/$language/${data}_hires/segments > \
-    data/$language/${data}_hires/reco2file_and_channel
+  awk '{print $2" "$2" 1"}' data/${data}_hires/segments > \
+    data/${data}_hires/reco2file_and_channel
 
-  utils/ctm/resolve_ctm_overlaps.py data/$language/${data}_hires/segments\
+  utils/ctm/resolve_ctm_overlaps.py data/${data}_hires/segments\
     $dir/decode_$data/score_10/${data}_hires.ctm \
-    - | utils/convert_ctm.pl data/$language/${data}_hires/segments \
-    data/$language/${data}_hires/reco2file_and_channel > \
+    - | utils/convert_ctm.pl data/${data}_hires/segments \
+    data/${data}_hires/reco2file_and_channel > \
    $dir/decode_$data/score_10/ctm_out
 
   # compute WER              
-  local/score_segments.sh data/$language/${data}_hires/ $dir/decode_$data
+  local/score_segments.sh data/${data}_hires/ $dir/decode_$data
 done
 
