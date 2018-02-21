1accdf913f37ec66cc7b97c3ada997282cfae2d3
Blaise Potard <blaise@cereproc.com>
2018-02-21 17:02:06 +0000

    Added mlsacheck for SPTK

--- egs/tts_tangle_arctic/s2/local/mlsa_synthesis_pitch_mlpg.sh
+++ egs/tts_tangle_arctic/s2/local/mlsa_synthesis_pitch_mlpg.sh
@@ -214,7 +214,7 @@ elif [ "$synth" = "WORLD" ]; then
     echo $world/synth $fftlen $srate $f0.double $mcep.sp.double $bap.double $out_wav
     $world/synth $fftlen $srate $f0.double $mcep.sp.double $bap.double $out_wav
 else
-    x2x +af $mcep > $mcep.float
+    x2x +af $mcep | mlsacheck -l $fftlen -c 2 -r 0 -P 5 -m $order -a $alpha > $mcep.float
     psize=`echo "$period * $srate / 1000" | bc`
     # We have to drop the first few F0 frames to match SPTK behaviour
     cat $f0 | awk -v srate=$srate '(NR > 2){if ($1 > 0) print srate / $1; else print 0.0}' | x2x +af \
