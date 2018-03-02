cd8cf14b7c64d88ccf8c8da08fd64c28cd14da6a
Aswin Shanmugam Subramanian <aswinphoenix@gmail.com>
2018-03-02 17:45:59 -0500

    added additional instructions on chainer/miniconda installation. added code to check dependency cuDNN

--- tools/extras/install_miniconda.sh
+++ tools/extras/install_miniconda.sh
@@ -1,5 +1,14 @@
  #!/bin/bash
 
 # Choose default settings for installation
+# Miniconda should be installed in the HOME directory. This is the default setting.
+# Please don't change it.
+# Also don't make miniconda's python as default.
+
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
+
+$HOME/miniconda3/bin/python -m pip install --user tqdm
+$HOME/miniconda3/bin/python -m pip install --user scikit-learn
+$HOME/miniconda3/bin/python -m pip install --user librosa
+$HOME/miniconda3/bin/python -m pip install --user h5py
