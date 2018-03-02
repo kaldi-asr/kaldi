340de0c9a2f5995b5e09381701dce5cf8aecc3bc
Chun-Chieh Chang <cchunch1@jhu.edu>
2018-03-02 17:22:31 -0500

    added unk and added wellington corpus. removed LOB because IAM and LOB overlap

--- egs/iam/v1/local/prepare_dict.sh
+++ egs/iam/v1/local/prepare_dict.sh
@@ -10,6 +10,39 @@ set -e
 dir=data/local/dict
 mkdir -p $dir
 
+# Combine Wellington corpora and replace some of their annotations
+cat /export/corpora5/Wellington/WWC/SectionA.txt \
+  /export/corpora5/Wellington/WWC/SectionB.txt \
+  /export/corpora5/Wellington/WWC/SectionC.txt \
+  /export/corpora5/Wellington/WWC/SectionD.txt \
+  /export/corpora5/Wellington/WWC/SectionE.txt \
+  /export/corpora5/Wellington/WWC/SectionF.txt \
+  /export/corpora5/Wellington/WWC/SectionG.txt \
+  /export/corpora5/Wellington/WWC/SectionH.txt \
+  /export/corpora5/Wellington/WWC/SectionJ.txt \
+  /export/corpora5/Wellington/WWC/SectionK.txt \
+  /export/corpora5/Wellington/WWC/SectionL.txt | \
+  cut -d' ' -f3- | sed "s/^[ \t]*//" > data/local/Wellington_tmp.txt
+
+cat data/local/Wellington_tmp.txt | python3 <(
+cat << EOF
+import sys, io, re;
+from collections import OrderedDict;
+sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding="utf8");
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf8");
+dict=OrderedDict([("^",""), ("|",""), ("_",""), ("*0",""), ("*1",""), ("*2",""), ("*3",""), ("*4",""),
+  ("*5",""), ("*6",""), ("*7",""), ("*8",""), ("*9",""), ("*@","°"), ("**=",""), ("*=",""),
+  ("*+$",""), ("$",""), ("*+","£"), ("*-","-"), ("*/","*"), ("*|",""), ("*{","{"), ("*}","}"),
+  ("**#",""), ("*#",""), ("*?",""), ("**\"","\""), ("*\"","\""), ("**'","'"), ("*'","'"),
+  ("*<",""), ("*>",""), ("**[",""), ("**]",""), ("**;",""), ("*;",""), ("**:",""), ("*:",""),
+  ("\\\0",""), ("\\\15",""), ("\\\1",""), ("\\\2",""), ("\\\3",""), ("\\\6",""), ("\\\",""),
+  ("{0",""), ("{15",""), ("{1",""), ("{2",""), ("{3",""), ("{6","")]);
+pattern = re.compile("|".join(re.escape(key) for key in dict.keys()) + "|[^\\*]\\}");
+dict["}"]="";
+[sys.stdout.write(pattern.sub(lambda x: dict[x.group()[1:]] if re.match('[^\\*]\\}', x.group()) else dict[x.group()], line)) for line in sys.stdin];
+EOF
+) > data/local/Wellington_tmp2.txt
+
 # First get the set of all letters that occur in data/train/text
 cat data/train/text | \
   perl -ne '@A = split; shift @A; for(@A) {print join("\n", split(//)), "\n";}' | \
@@ -22,8 +55,8 @@ cat data/train/text | \
 export letters=$(cat $dir/nonsilence_phones.txt | tr -d "\n")
 
 cut -d' ' -f2- data/train/text | \
-  cat data/local/lobcorpus/0167/download/LOB_COCOA/lob.txt \
-      data/local/browncorpus/brown.txt - | \
+  cat data/local/browncorpus/brown.txt - | \
+  cat data/local/Wellington_tmp2.txt - | \
   perl -e '$letters=$ENV{letters};
 while(<>){ @A = split;
   foreach(@A) {
@@ -36,8 +69,7 @@ while(<>){ @A = split;
   }
 }' | sort > $dir/lexicon.txt
 
-
-sed -i '' "s/#/<HASH>/" $dir/nonsilence_phones.txt
+sed -i "s/#/<HASH>/" $dir/nonsilence_phones.txt
 
 echo '<sil> SIL' >> $dir/lexicon.txt
 echo '<unk> SIL' >> $dir/lexicon.txt
--- egs/iam/v1/run.sh
+++ egs/iam/v1/run.sh
@@ -53,6 +53,11 @@ if [ $stage -le 3 ]; then
   local/train_lm.sh
   utils/format_lm.sh data/lang data/local/local_lm/data/arpa/3gram_big.arpa.gz \
                      data/local/dict/lexicon.txt data/lang_test
+
+  utils/lang/make_unk_lm.sh --ngram-order 4 --num-extra-ngrams 10000 data/local/dict exp/unk_lang_model
+  utils/prepare_lang.sh --num-sil-states 4 --num-nonsil-states 8 --sil-prob 0.95 \
+                        --unk-fst exp/unk_lang_model/unk_fst.txt data/local/dict "<unk>" data/lang/temp data/lang_unk
+  cp data/lang_test/G.fst data/lang_unk/G.fst
 fi
 
 if [ $stage -le 4 ]; then
@@ -60,12 +65,12 @@ if [ $stage -le 4 ]; then
     data/lang exp/mono
 fi
 
-if [ $stage -le 5 ]; then
-  utils/mkgraph.sh --mono data/lang_test exp/mono exp/mono/graph
-
-  steps/decode.sh --nj $nj --cmd $cmd exp/mono/graph data/test \
-    exp/mono/decode_test
-fi
+#if [ $stage -le 5 ]; then
+#  utils/mkgraph.sh --mono data/lang_test exp/mono exp/mono/graph
+#
+#  steps/decode.sh --nj $nj --cmd $cmd exp/mono/graph data/test \
+#    exp/mono/decode_test
+#fi
 
 if [ $stage -le 6 ]; then
   steps/align_si.sh --nj $nj --cmd $cmd data/train data/lang \
@@ -75,12 +80,12 @@ if [ $stage -le 6 ]; then
     exp/mono_ali exp/tri
 fi
 
-if [ $stage -le 7 ]; then
-  utils/mkgraph.sh data/lang_test exp/tri exp/tri/graph
-
-  steps/decode.sh --nj $nj --cmd $cmd exp/tri/graph data/test \
-    exp/tri/decode_test
-fi
+#if [ $stage -le 7 ]; then
+#  utils/mkgraph.sh data/lang_test exp/tri exp/tri/graph
+#
+#  steps/decode.sh --nj $nj --cmd $cmd exp/tri/graph data/test \
+#    exp/tri/decode_test
+#fi
 
 if [ $stage -le 8 ]; then
   steps/align_si.sh --nj $nj --cmd $cmd data/train data/lang \
@@ -91,12 +96,12 @@ if [ $stage -le 8 ]; then
     data/train data/lang exp/tri_ali exp/tri2
 fi
 
-if [ $stage -le 9 ]; then
-  utils/mkgraph.sh data/lang_test exp/tri2 exp/tri2/graph
-
-  steps/decode.sh --nj $nj --cmd $cmd exp/tri2/graph \
-    data/test exp/tri2/decode_test
-fi
+#if [ $stage -le 9 ]; then
+#  utils/mkgraph.sh data/lang_test exp/tri2 exp/tri2/graph
+#
+#  steps/decode.sh --nj $nj --cmd $cmd exp/tri2/graph \
+#    data/test exp/tri2/decode_test
+#fi
 
 if [ $stage -le 10 ]; then
   steps/align_fmllr.sh --nj $nj --cmd $cmd --use-graphs true \
@@ -106,12 +111,12 @@ if [ $stage -le 10 ]; then
     data/train data/lang exp/tri2_ali exp/tri3
 fi
 
-if [ $stage -le 11 ]; then
-  utils/mkgraph.sh data/lang_test exp/tri3 exp/tri3/graph
-
-  steps/decode_fmllr.sh --nj $nj --cmd $cmd exp/tri3/graph \
-    data/test exp/tri3/decode_test
-fi
+#if [ $stage -le 11 ]; then
+#  utils/mkgraph.sh data/lang_test exp/tri3 exp/tri3/graph
+#
+#  steps/decode_fmllr.sh --nj $nj --cmd $cmd exp/tri3/graph \
+#    data/test exp/tri3/decode_test
+#fi
 
 if [ $stage -le 12 ]; then
   steps/align_fmllr.sh --nj $nj --cmd $cmd --use-graphs true \
@@ -119,9 +124,9 @@ if [ $stage -le 12 ]; then
 fi
 
 if [ $stage -le 13 ]; then
-  local/chain/run_cnn_1a.sh
+  local/chain/run_cnn_1a.sh --lang-test lang_unk
 fi
 
 if [ $stage -le 14 ]; then
-  local/chain/run_cnn_chainali_1b.sh --chain-model-dir exp/chain/cnn_1a --stage 2
+  local/chain/run_cnn_chainali_1b.sh --lang-test lang_unk --chain-model-dir exp/chain/cnn_1a --stage 2
 fi
