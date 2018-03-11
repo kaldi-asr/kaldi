4fac11eb5e2eae612d0ac665c758e95de5b64c95
Aswin Shanmugam Subramanian <aswinphoenix@gmail.com>
2018-03-11 04:55:24 -0400

    bug fix: et05 dir error in compute_stoi_estoi_sdr.sh

--- egs/chime4/s5_1ch/local/compute_stoi_estoi_sdr.sh
+++ egs/chime4/s5_1ch/local/compute_stoi_estoi_sdr.sh
@@ -8,14 +8,14 @@ set -e
 set -u
 set -o pipefail
 
-njobs=20
+njobs=10
 cmd=run.pl
 
 . utils/parse_options.sh || exit 1;
 
 if [ $# != 3 ]; then
    echo "Wrong #arguments ($#, expected 3)"
-   echo "Usage: local/compute_stoi_estoi_sdr.sh [options] <enhancement-method> <enhancement-directory> <chime-RIR-directory>"
+   echo "Usage: local/compute_stoi_estoi_sdr.sh [options] <enhancement-method> <enhancement-directory> <chime-rir-directory>"
    echo "main options (for others, see top of script file)"
    echo "  --njobs <njobs>                                # number of parallel jobs"
    echo "  --cmd <cmd>                              # Command to run in parallel with"
@@ -24,13 +24,13 @@ fi
 
 enhancement_method=$1
 enhancement_directory=$2
-chime_RIR_directory=$3
+chime_rir_directory=$3
 
 expdir=exp/compute_stoi_estoi_sdr_${enhancement_method}
 mkdir -p $expdir
-ls $chime_RIR_directory/dt05_*/*CH5.Clean.wav > $expdir/original_list
+ls $chime_rir_directory/dt05_*/*CH5.Clean.wav > $expdir/original_list
 ls $enhancement_directory/dt05_*simu/*.wav > $expdir/enhanced_list
 $cmd $expdir/compute_stoi_estoi_sdr_dt05.log matlab -nodisplay -nosplash -r "addpath('local'); stoi_estoi_sdr($njobs,'$enhancement_method','$expdir','dt05');exit"
-ls $chime_RIR_directory/et05_*/*CH5.Clean.wav > original_list
-ls $enhancement_directory/et05_*simu/*.wav > enhanced_list
+ls $chime_rir_directory/et05_*/*CH5.Clean.wav > $expdir/original_list
+ls $enhancement_directory/et05_*simu/*.wav > $expdir/enhanced_list
 $cmd $expdir/compute_stoi_estoi_sdr_et05.log matlab -nodisplay -nosplash -r "addpath('local'); stoi_estoi_sdr($njobs,'$enhancement_method','$expdir','et05');exit"
