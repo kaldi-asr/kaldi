e541acfcc584972d2addd02ff746c4d55fb82f7d
Chen Szu-Jui <cszu@b03.clsp.jhu.edu>
2018-03-12 18:42:58 -0400

    bug fixed

--- egs/chime4/s5_1ch/local/write_se_results.sh
+++ egs/chime4/s5_1ch/local/write_se_results.sh
@@ -15,7 +15,7 @@ fi
 
 enhancement=$1
 
-echo -e "PESQ ($enhancement) \t dt05_simu=$(cat exp/compute_pesq_$enhancement/PESQ_dt05) \t et05_simu=$(cat exp/compute_pesq_$enhancement/PESQ_et05)"
+echo -e "PESQ ($enhancement) \t dt05_simu=$(cat exp/compute_pesq_$enhancement/pesq_dt05) \t et05_simu=$(cat exp/compute_pesq_$enhancement/pesq_et05)"
 echo -e "STOI ($enhancement) \t dt05_simu=$(cat exp/compute_stoi_estoi_sdr_$enhancement/${enhancement}_dt05_STOI) \t et05_simu=$(cat exp/compute_stoi_estoi_sdr_$enhancement/${enhancement}_et05_STOI)"
 echo -e "eSTOI ($enhancement) \t dt05_simu=$(cat exp/compute_stoi_estoi_sdr_$enhancement/${enhancement}_dt05_eSTOI) \t et05_simu=$(cat exp/compute_stoi_estoi_sdr_$enhancement/${enhancement}_et05_eSTOI)"
 echo -e "SDR ($enhancement) \t dt05_simu=$(cat exp/compute_stoi_estoi_sdr_$enhancement/${enhancement}_dt05_SDR) \t et05_simu=$(cat exp/compute_stoi_estoi_sdr_$enhancement/${enhancement}_et05_SDR)"
