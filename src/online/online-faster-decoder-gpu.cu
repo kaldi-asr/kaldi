f660604f008af6af29efee9d085f027dfd4d0675
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2018-03-23 02:10:05 +0700

    Added Data Structure For GPUArc and GPUToken

--- src/online/online-faster-decoder-gpu.cu
+++ src/online/online-faster-decoder-gpu.cu
@@ -2,7 +2,26 @@
 #include "iostream"
 #include "stdlib.h"
 
+typedef int int32;
+typedef long long int64;
+
+struct GPUToken{
+    typedef float BaseFloat;
+    
+    GPUToken* prev_best_tok;
+    BaseFloat cost;
+};
+
+struct GPUArc{ 
+    typedef int StateId;
+
+    StateId curstate;
+    Arc arc;
+};
+
 __global__ ProcessEmitting();
 __global__ ProcessNonEmitting();
 
-__global__ Decode(DecodableInterface* decodable) //ini gimana cara bikin supaya DecodableInterface diassign di CUDA
+//ini gimana cara bikin supaya DecodableInterface diassign di CUDA
+//EDIT : Pake kelasnya langsung aja, jadi pake OnlineDecodableDiagGmmScaled langsung
+__global__ Decode(OnlineDecodableDiagGmmScaled* decodable);
