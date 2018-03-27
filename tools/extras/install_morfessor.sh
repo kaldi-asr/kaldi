626d2c5063722fb9c48d40d3ae2d0f5be10b0bd9
lucasjo <jty016@gmail.com>
2018-03-27 16:36:14 +0900

    simplified installation process acc. to psmit's advice

--- tools/extras/install_morfessor.sh
+++ tools/extras/install_morfessor.sh
@@ -10,32 +10,13 @@ if [ ! -d ./$dirname ]; then
   mkdir -p ./$dirname
   git clone https://github.com/aalto-speech/morfessor.git morfessor ||
     {
-      if [ $? -ne 0 ]; then
-        echo  >&2 "$0: Error git clone operation "
-        echo  >&2 "  Failed in cloning the github repository (https://github.com/aalto-speech/morfessor.git)"
-        echo  >&2 "  Download morfessor-2.0.1.tar.gz instead"
-        wget http://morpho.aalto.fi/projects/morpho/morfessor-2.0.1.tar.gz
-        tar -zxvf morfessor-2.0.1.tar.gz
-        mv Morfessor-2.0.1/ $dirname
-      fi
+      echo  >&2 "$0: Error git clone operation "
+      echo  >&2 "  Failed in cloning the github repository (https://github.com/aalto-speech/morfessor.git)"
+      exit
     }
 fi
 
-# local installation
-site_packages_dir=$(python -m site --user-site | grep -oE "lib.*")
-wd=`pwd`
-wd=`readlink -f $wd || pwd`
-export MORFESSOR="$wd/morfessor"
-export PYTHONPATH="${PYTHONPATH:-}:$MORFESSOR/${site_packages_dir}"
-(
-cd $dirname
-mkdir -p $site_packages_dir
-python setup.py install --prefix `pwd` --record files.txt
-)
-wait
-
 # env.sh setup
-site_packages_dir=$(cd morfessor; find ./lib{,64} -type d -name site-packages | head -n 1)
 (
   set +u
   [ ! -z "${MORFESSOR}" ] && \
@@ -51,10 +32,9 @@ site_packages_dir=$(cd morfessor; find ./lib{,64} -type d -name site-packages | 
   wd=`readlink -f $wd || pwd`
 
   echo "export MORFESSOR=\"$wd/morfessor\""
-  echo "export PATH=\"\$PATH:\${MORFESSOR}/bin\""
-  echo "export PYTHONPATH=\"\${PYTHONPATH:-}:\$MORFESSOR/${site_packages_dir}\""
+  echo "export PATH=\"\$PATH:\${MORFESSOR}/scripts\""
+  echo "export PYTHONPATH=\"\${PYTHONPATH:-}:\$MORFESSOR\""
 ) >> env.sh
 
 echo >&2 "installation of MORFESSOR finished successfully"
 echo >&2 "please source tools/env.sh in your path.sh to enable it"
-echo >&2 'when uninstall it, try: sudo rm $(cat ./morfessor/files.txt)'
