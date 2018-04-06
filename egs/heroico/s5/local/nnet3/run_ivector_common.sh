db08d282732be6b5311575614fcef038ff1cd0a6
John Joseph Morgan <johnjosephmorgan@gmail.com>
2018-04-06 14:37:44 -0400

    mfcc to plp_pitch.

--- egs/heroico/s5/local/nnet3/run_ivector_common.sh
+++ egs/heroico/s5/local/nnet3/run_ivector_common.sh
@@ -37,8 +37,8 @@ if [ $stage -le 1 ]; then
     utils/data/perturb_data_dir_speed_3way.sh \
 	data/${train_set} \
 	data/${train_set}_sp
-    echo "$0: making MFCC features for low-resolution speed-perturbed data"
-    steps/make_mfcc.sh \
+    echo "$0: making PLP_PITCH features for low-resolution speed-perturbed data"
+    steps/make_mfcc_plp_pitch.sh \
 	--cmd "$train_cmd" \
 	--nj 10 \
 	data/${train_set}_sp || exit 1;
