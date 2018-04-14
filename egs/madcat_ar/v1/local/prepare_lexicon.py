909f3a2bba823d6d8f1ceeb59670c0103c710bb4
aarora8 <aarora8@jhu.edu>
2018-04-14 06:29:02 -0400

    minor fix

--- egs/madcat_ar/v1/local/prepare_lexicon.py
+++ egs/madcat_ar/v1/local/prepare_lexicon.py
@@ -22,28 +22,6 @@ for line in text_fh:
         if line_vect[i] =='#':
             lex[line_vect[i]] = "<HASH>"
 
-text_path = os.path.join('data', 'test', 'text')
-text_fh = open(text_path, 'r', encoding='utf-8')
-for line in text_fh:
-    line_vect = line.strip().split(' ')
-    for i in range(1,len(line_vect)):
-        characters = list(line_vect[i])
-        characters = " ".join(characters)
-        lex[line_vect[i]] = characters
-        if line_vect[i] =='#':
-            lex[line_vect[i]] = "<HASH>"
-
-text_path = os.path.join('data', 'dev', 'text')
-text_fh = open(text_path, 'r', encoding='utf-8')
-for line in text_fh:
-    line_vect = line.strip().split(' ')
-    for i in range(1,len(line_vect)):
-        characters = list(line_vect[i])
-        characters = " ".join(characters)
-        lex[line_vect[i]] = characters
-        if line_vect[i] =='#':
-            lex[line_vect[i]] = "<HASH>"
-
 lex_file = os.path.join(args.dir, 'lexicon.txt')
 lex_fh = open(lex_file, 'w+', encoding='utf-8')
 
