70263e802ed37c3e3ae9f91c8b2c8890294f3b90
aarora8 <aarora8@jhu.edu>
2018-04-24 04:22:54 -0400

    updating extract line for parallelism of create_line_image_from_page_image

--- egs/madcat_ar/v1/local/extract_lines.sh
+++ egs/madcat_ar/v1/local/extract_lines.sh
@@ -15,6 +15,7 @@ echo "$0 $@"
 . ./path.sh
 . ./utils/parse_options.sh || exit 1;
 
+data=$1
 mkdir -p $log_dir
 mkdir -p $lines_dir
 
@@ -24,15 +25,14 @@ lines_dir=`perl -e '($dir,$pwd)= @ARGV; if($dir!~m:^/:) { $dir = "$pwd/$dir"; } 
 for n in $(seq $nj); do
     split_scps="$split_scps $log_dir/lines.$n.scp"
 done
-
 utils/split_scp.pl $dataset_file $split_scps || exit 1;
-mkdir -p $lines_dir/{1..$nj}
 
+mkdir -p $lines_dir/{1..$nj}
 $cmd JOB=1:$nj $log_dir/extract_lines.JOB.log \
   local/create_line_image_from_page_image.py $download_dir1 $download_dir2 $download_dir3 $log_dir/lines.JOB.scp $lines_dir/JOB \
   || exit 1;
 
 ## concatenate the .scp files together.
-#for n in $(seq $nj); do
-#  cat $lines_dir/images.$n.scp || exit 1;
-#done > $data/feats.scp || exit 1
+for n in $(seq $nj); do
+  cat $lines_dir/$n/images.scp || exit 1;
+done > $data/images.scp || exit 1
