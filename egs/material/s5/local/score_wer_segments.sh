2016d8c500ba25b66165a509df34236aec91a222
Mahsa Yarmohammadi <mahsay@b02.clsp.jhu.edu>
2018-04-24 13:41:51 -0400

    added sentence segmentation

--- egs/material/s5/local/score_wer_segments.sh
+++ egs/material/s5/local/score_wer_segments.sh
@@ -23,7 +23,6 @@ hyp_filtering_cmd="cat"
 [ -x local/wer_output_filter ] && hyp_filtering_cmd="local/wer_output_filter"
 [ -x local/wer_hyp_filter ] && hyp_filtering_cmd="local/wer_hyp_filter"
 
-
 mkdir -p $dir/scoring_kaldi
 if [ -f $data/reftext ]; then
   cat $data/reftext | $ref_filtering_cmd > $dir/scoring_kaldi/test_filt.txt || exit 1;
@@ -36,21 +35,16 @@ if [ $stage -le 0 ]; then
   mkdir -p $dir/scoring_kaldi/log
   # begin building hypothesis hyp.txt
   # in the same format as $data/reftext
-  rm -rf tmpconcat
   awk '{a[$1]=a[$1]" "$5;}END{for(i in a)print i""a[i];}' \
     $dir/score_10/ctm_out > tmpconcat
   if [ -f $data/reftext ]; then
-    awk -F" " '{print $1}' $data/reftext > tmpreforder
-    rm -rf $dir/score_10/ctm_out.concat
-    while read LINE; do
-      grep "$LINE" "tmpconcat" >> "$dir/score_10/ctm_out.concat"
-    done < "tmpreforder"
-    rm -rf tmpconcat
-    rm -rf tmpreforder
+    awk -F" " '{print $1}' $data/reftext > tmporder
+    awk 'FNR==NR {x2[$1] = $0; next} $1 in x2 {print x2[$1]}' \
+      tmpconcat tmporder > "$dir/score_10/ctm_out.concat"
     $hyp_filtering_cmd $dir/score_10/ctm_out.concat > \
       $dir/scoring_kaldi/hyp.txt || exit 1;
-    #end building hypothesis hyp.txt
-    
+    # end building hypothesis hyp.txt
+
     $cmd $dir/scoring_kaldi/log/score.hyp.log \
       cat $dir/scoring_kaldi/hyp.txt \| \
       compute-wer --text --mode=present \
@@ -59,12 +53,28 @@ if [ $stage -le 0 ]; then
     cat $dir/wer
   else
     cat tmpconcat > "$dir/score_10/ctm_out.concat"
-    rm -rf tmpconcat
+    awk -F" " '{print $1}' $dir/score_10/ctm_out.concat > tmporder
     $hyp_filtering_cmd $dir/score_10/ctm_out.concat > \
       $dir/scoring_kaldi/hyp.txt || exit 1;
-    exit 0;
+    #exit 0;
     #end building hypothesis hyp.txt
+
   fi
+  
+  # building hyp.segmentedXms.txt
+  for dur in {700,800,900,1000}; do                                             
+    dursec=`echo $dur' / 1000' | bc -l`                                         
+    awk '{if ($4 < '$dursec') a[$1]=a[$1]" "$5; else a[$1]=a[$1]" "$5"\n"$1"";}END\
+      {for(i in a)print i""a[i];}' $dir/score_10/ctm_out > tmpconcat          
+    rm -rf $dir/score_10/ctm_out.concat.$dur                                    
+    while read LINE; do                                                         
+    grep "$LINE" "tmpconcat" >> "$dir/score_10/ctm_out.concat."$dur           
+    done < "tmporder"                                                        
+    
+    $hyp_filtering_cmd $dir/score_10/ctm_out.concat.$dur > $dir/scoring_kaldi/hyp.segmented${dur}ms.txt || exit 1;                   
+  done       
+  rm -rf tmpconcat                                                            
+  rm -rf tmporder 
 fi
 
 if [ $stage -le 1 ]; then
