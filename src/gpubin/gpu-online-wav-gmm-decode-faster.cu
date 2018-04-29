e4f5d7b2cddbc9ac718ff9ab0e3f3b7c79ee6e73
Alfonsus Raditya Arsadjaja <alfonsusradityaarsadjaja@s.itb.ac.id>
2018-04-29 16:37:52 +0700

    Update gpu-online-wav-gmm-decode-faster.cu
    Add GPU Function

--- src/gpubin/gpu-online-wav-gmm-decode-faster.cu
+++ src/gpubin/gpu-online-wav-gmm-decode-faster.cu
@@ -19,10 +19,198 @@
 #include "online/online-faster-decoder.h"
 #include "online/onlinebin-util.h"
 
+int ceildiv(int x, int y) { return (x-1)/y+1; }
+#define BLOCK_SIZE 192
+#define BEAM_SIZE 10
+
+struct PointerComparison
+{
+    __host__ __device__ bool operator()(const prob_ptr_t &x, const prob_ptr_t &y)
+    {
+        float prob_x = unpack_prob(x);
+        float prob_y = unpack_prob(y);
+        return prob_x > prob_y;
+    }
+};
+
+
+__global__ void compute_initial(int *from_states, int *to_states, float *probs,
+				int start_offset, int end_offset,
+				state_t initial_state,
+				prob_ptr_t *viterbi)
+{
+    int id = blockIdx.x*blockDim.x+threadIdx.x;
+    int offset = start_offset+id;
+
+    __shared__ int from_shared_states[BLOCK_SIZE];
+    __shared__ int to_shared_states[BLOCK_SIZE];
+    __shared__ float shared_probs[BLOCK_SIZE];
+
+    from_shared_states[threadIdx.x] = from_states[offset];
+    to_shared_states[threadIdx.x] = to_states[offset];
+    shared_probs[threadIdx.x] = probs[offset];
+
+    if (offset < end_offset && from_shared_states[threadIdx.x] == initial_state) {
+	    atomicMax(&viterbi[to_shared_states[threadIdx.x]], pack(shared_probs[threadIdx.x], offset));
+    }
+}
+
+__global__ void compute_transition(int *from_states, int *to_states, float *probs,
+				   int start_offset, int end_offset,
+				   prob_ptr_t *viterbi_prev, prob_ptr_t *viterbi) {
+  int id = blockIdx.x*blockDim.x+threadIdx.x;
+  int offset = start_offset+id;
+
+  __shared__ int from_shared_states[BLOCK_SIZE];
+  __shared__ int to_shared_states[BLOCK_SIZE];
+  __shared__ prob_ptr_t viterbi_from_shared_states[BLOCK_SIZE];
+  __shared__ float shared_probs[BLOCK_SIZE];
+
+  from_shared_states[threadIdx.x] = from_states[offset];
+  to_shared_states[threadIdx.x] = to_states[offset];
+  shared_probs[threadIdx.x] = probs[offset];
+  viterbi_from_shared_states[threadIdx.x] = viterbi_prev[from_shared_states[threadIdx.x]];
+
+  if (offset < end_offset) {
+    // TODO : GANTI assignment pp dibawah ini
+    prob_ptr_t pp = pack(unpack_prob(viterbi_from_shared_states[threadIdx.x]) + shared_probs[threadIdx.x], offset);
+    atomicMax(&viterbi[/*to_register_state*/to_shared_states[threadIdx.x]], pp);
+  }
+}
+
+__global__ void compute_final(int *final_states, float *final_probs,
+			      int num_finals,
+			      prob_ptr_t *viterbi_prev, prob_ptr_t *viterbi) {
+  // To do: this should be a reduction instead
+  int id = blockIdx.x*blockDim.x+threadIdx.x;
+  if (id < num_finals) {
+    prob_ptr_t pp = pack(unpack_prob(viterbi_prev[final_states[id]]) + final_probs[id], final_states[id]);
+    atomicMax(viterbi, pp);
+  }
+}
+
+__global__ void get_path(int *from_nodes,
+			 prob_ptr_t *viterbi,
+			 int input_length, int num_nodes,
+			 prob_ptr_t *path) {
+  int id = blockIdx.x*blockDim.x+threadIdx.x;
+  if (id == 0) {
+    int state = unpack_ptr(path[input_length]);
+    for (int t = input_length-1; t >= 0; t--) {
+      prob_ptr_t pp = viterbi[t*num_nodes+state];
+      path[t] = pp;
+      state = from_nodes[unpack_ptr(pp)];
+    }
+  }
+}
+
+__global__ void set_states_in_beam(prob_ptr_t *viterbi, prob_ptr_t *viterbi_beam,int *to_states){
+  int id = blockIdx.x*blockDim.x+threadIdx.x;
+  if(id < BEAM_SIZE){
+    int state_unpacked = unpack_ptr(viterbi_beam[id]);
+ 
+    viterbi[to_states[state_unpacked]] = viterbi_beam[id];
+  }
+}
+
+prob_t viterbi(gpu_fst &m, const vector<sym_t> &input_symbols, vector<sym_t> &output_symbols) {
+    int verbose=0;
+
+    static thrust::device_vector<prob_ptr_t> viterbi;
+    viterbi.resize(input_symbols.size() * m.num_states);
+    prob_ptr_t init_value = pack(-FLT_MAX, 0);
+    thrust::fill(viterbi.begin(), viterbi.end(), init_value);
+   
+    static thrust::device_vector<prob_ptr_t> viterbi_beam;
+    viterbi_beam.resize(BEAM_SIZE * input_symbols.size());
+    thrust::fill(viterbi_beam.begin(),viterbi_beam.end(),init_value);
+
+    thrust::device_vector<prob_ptr_t> path(input_symbols.size()+1);
+
+    sym_t a = input_symbols[0];
+    int start_offset = m.input_offsets[a];
+    int end_offset = m.input_offsets[a+1];
+
+    compute_initial <<<ceildiv(end_offset-start_offset, BLOCK_SIZE), BLOCK_SIZE>>> (
+      m.from_states.data().get(),
+      m.to_states.data().get(),
+      m.probs.data().get(),
+      start_offset, end_offset,
+      m.initial,
+      viterbi.data().get()
+    );
+
+    if (verbose) {
+      for (auto pp: viterbi)
+	      cout << unpack_prob(pp) << " ";
+      cout << endl;
+    }
+
+    for (int t=1; t<input_symbols.size(); t++) {
+      sym_t a = input_symbols[t];
+      int start_offset = m.input_offsets[a];
+      int end_offset = m.input_offsets[a+1];
+
+      if (verbose) {
+        cerr << start_offset << " to " << end_offset << endl;
+        for (int i=start_offset; i<end_offset; i++) {
+          cerr << m.from_states[i] << " " << m.to_states[i] << " " << m.probs[i] << endl;
+        }
+      }
+
+      compute_transition <<<ceildiv(end_offset-start_offset, BLOCK_SIZE), BLOCK_SIZE>>> (
+        m.from_states.data().get(), 
+	      m.to_states.data().get(),
+	      m.probs.data().get(),
+        start_offset, end_offset,
+        viterbi.data().get() + (t-1)*m.num_states,
+        viterbi.data().get() + t*m.num_states);
+      
+      if (verbose) {
+        for (auto pp: viterbi)
+          cout << unpack_prob(pp) << " ";
+        cout << endl;
+      }
+
+      thrust::sort(viterbi.begin()+(t*m.num_states),viterbi.begin()+((t+1)*m.num_states),PointerComparison());
+      thrust::copy(viterbi.begin() + (t*m.num_states), viterbi.begin() + ((t*m.num_states)+ BEAM_SIZE), viterbi_beam.begin() + (t*BEAM_SIZE));
+
+      thrust::fill(viterbi.begin()+(t*m.num_states), viterbi.begin()+((t+1)*m.num_states), init_value);
+      set_states_in_beam <<<ceildiv(BEAM_SIZE, BLOCK_SIZE), BLOCK_SIZE>>> (viterbi.data().get() + (t*m.num_states), viterbi_beam.data().get()+ (t*BEAM_SIZE),m.to_states.data().get());
+    }
+
+    compute_final <<<ceildiv(m.final_states.size(), 1024), 1024>>> (
+      m.final_states.data().get(),
+	    m.final_probs.data().get(),
+      m.final_states.size(),
+      viterbi.data().get() + (input_symbols.size()-1)*m.num_states,
+      (&path.back()).get());
+
+    get_path <<<1,1>>> (
+      m.from_states.data().get(),
+      viterbi.data().get(),
+      input_symbols.size(), m.num_states,
+      path.data().get()
+    );
+
+    cudaError_t e = cudaGetLastError();                                 
+    if (e != cudaSuccess) {                                              
+      cerr << "CUDA failure: " << cudaGetErrorString(e) << endl;
+      exit(1);
+    }
+
+    thrust::host_vector<prob_ptr_t> h_path(path);
+    output_symbols.resize(input_symbols.size());
+    for (int t=0; t<input_symbols.size(); t++) {
+      output_symbols[t] = m.outputs[unpack_ptr(h_path[t])];
+    }
+
+    return unpack_prob(h_path.back());
+}
+
 int main(int argc, char *argv[]) {
   try {
     using namespace kaldi;
-    using namespace fst;
 
     typedef kaldi::int32 int32;
     typedef OnlineFeInput<Mfcc> FeInput;
@@ -109,13 +297,9 @@ int main(int argc, char *argv[]) {
         am_gmm.Read(ki.Stream(), binary);
     }
 
-//     fst::SymbolTable *word_syms = NULL;
-//     if (!(word_syms = fst::SymbolTable::ReadText(word_syms_filename)))
-//         KALDI_ERR << "Could not read symbol table from file "
-//                     << word_syms_filename;
 
-//     fst::Fst<fst::StdArc> *decode_fst = ReadDecodeGraph(fst_rspecifier);
-
+    gpu_fst m = read_fst_noNumberizer(fst_rspecifier) // harus berupa file text
+    
     // We are not properly registering/exposing MFCC and frame extraction options,
     // because there are parts of the online decoding code, where some of these
     // options are hardwired(ToDo: we should fix this at some point)
@@ -126,11 +310,8 @@ int main(int argc, char *argv[]) {
 
     int32 window_size = right_context + left_context + 1;
     decoder_opts.batch_size = std::max(decoder_opts.batch_size, window_size);
-
-    OnlineFasterDecoder decoder(*decode_fst, decoder_opts,
-                                silence_phones, trans_model);
+    
     SequentialTableReader<WaveHolder> reader(wav_rspecifier);
-    VectorFst<LatticeArc> out_fst;
     for (; !reader.Done(); reader.Next()) {
       std::string wav_key = reader.Key();
       std::cerr << "File: " << wav_key << std::endl;
@@ -176,6 +357,9 @@ int main(int argc, char *argv[]) {
       OnlineFeatureMatrix feature_matrix(feature_reading_opts,
                                          feat_transform);
       
+      OnlineDecodableDiagGmmScaled decodable(am_gmm, trans_model, acoustic_scale,
+                                             &feature_matrix);
+      
       // Copy AmDiagGmm ke GPUAmDiagGmm
       GPUAmDiagGmm gpu_am_gmm;
       for(size_t i = 0; i < am_gmm.NumPdfs(); ++i){
@@ -187,8 +371,8 @@ int main(int argc, char *argv[]) {
       GPUTransitionModel gpu_trans_model = GPUTransitionModel(trans_model);
       
       // Create GPUOnlineDecodableDiagGmmScaled
-      GPUOnlineDecodableDiagGmmScaled(gpu_am_gmm, acoustic_scale, gpu_trans_model);
-     
+      GPUOnlineDecodableDiagGmmScaled gpu_decodable(gpu_am_gmm, gpu_trans_model, acoustic_scale);
+      
       // TODO : Buat Loop Decodenya
       
       delete feat_transform;
