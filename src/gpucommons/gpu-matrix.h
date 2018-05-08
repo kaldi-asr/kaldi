c6efd7e65579d07479fb3bf62a484f7060eda7cc
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2018-05-08 09:33:13 +0700

    Change template to .h

--- src/gpucommons/gpu-matrix.h
+++ src/gpucommons/gpu-matrix.h
@@ -37,6 +37,52 @@ struct GPUMatrix{
   GPUMatrix(const Matrix<Real> &M);
 };
 
+template<typename Real>
+GPUMatrix<Real>::GPUMatrix() {}
+
+template<typename Real>
+GPUMatrix<Real>::GPUMatrix(Matrix<Real> &M) :
+  numcols_(M.NumCols()),
+  numrows_(M.NumRows()),
+  stride_(M.Stride())
+{
+  const size_t m_dim = M.SizeInBytes() / sizeof(Real);
+  Real* m_data = M.Data();
+
+  thrust::copy(m_data, m_data + m_dim, data_.begin());
+  data = data_.data().get();
+}
+
+template<typename Real>
+GPUMatrix<Real>::GPUMatrix(const Matrix<Real> &M) :
+  numcols_(M.NumCols()),
+  numrows_(M.NumRows()),
+  stride_(M.Stride())
+{
+  const size_t m_dim = M.SizeInBytes() / sizeof(Real);
+  Real* m_data = M.Data();
+
+  thrust::copy(m_data, m_data + m_dim, data_.begin());
+  data = data_.data().get();
+}
+
+__host__ __device__
+template<typename Real>
+int32 GPUMatrix<Real>::NumRows() const { return numrows_; }
+
+__host__ __device__
+template<typename Real>
+int32 GPUMatrix<Real>::NumCols() const { return numcols_; }
+
+__host__ __device__
+template<typename Real>
+int32 GPUMatrix<Real>::Stride() const { return stride_; }
+
+__host__ __device__
+template<typename Real>
+int32 GPUMatrix<Real>::Index(int32 r, int32 c) const { return r * stride_ + c; }
+
+
 }
 
 #endif
