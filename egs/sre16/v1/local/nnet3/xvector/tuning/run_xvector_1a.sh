2ad8d7821867a199e435aa36bbd13af6ed937c94
Ewald Enzinger <entn-at@users.noreply.github.com>
2018-05-18 13:27:32 -0700

    [egs] Minor fixes to x-vector based recipes (#2426)
    Add missing data prep scripts for MUSAN for callhome_diarization; Copy vad.scp and segments to *_cmn data folders after prepare_feats; Fix check before create_split_dir

--- egs/sre16/v1/local/nnet3/xvector/tuning/run_xvector_1a.sh
+++ egs/sre16/v1/local/nnet3/xvector/tuning/run_xvector_1a.sh
@@ -53,7 +53,7 @@ num_pdfs=$(awk '{print $2}' $data/utt2spk | sort | uniq -c | wc -l)
 if [ $stage -le 4 ]; then
   echo "$0: Getting neural network training egs";
   # dump egs.
-  if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d $dir/egs/storage ]; then
+  if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d $egs_dir/storage ]; then
     utils/create_split_dir.pl \
      /export/b{03,04,05,06}/$USER/kaldi-data/egs/sre16/v2/xvector-$(date +'%m_%d_%H_%M')/$egs_dir/storage $egs_dir/storage
   fi
