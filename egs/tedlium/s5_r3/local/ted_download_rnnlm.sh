3855c7b112c564b533380f5f3ec857daeebeb76c
François Hernandez <francoish.hernandez.fh@gmail.com>
2018-05-23 09:40:47 +0200

    change rnnlm download link

--- egs/tedlium/s5_r3/local/ted_download_rnnlm.sh
+++ egs/tedlium/s5_r3/local/ted_download_rnnlm.sh
@@ -10,7 +10,7 @@
 set -e
 
 echo "$0: downloading Tedlium RNNLM models (it won't re-download if it was already downloaded.)"
-wget --continue http://kaldi-asr.org/models/6/tedlium_rnnlm.tgz -P exp/rnnlm_lstm_tdnn_a_averaged || exit 1
+wget --continue http://kaldi-asr.org/models/5/tedlium_rnnlm.tgz -P exp/rnnlm_lstm_tdnn_a_averaged || exit 1
 tar -xvzf exp/rnnlm_lstm_tdnn_a_averaged/tedlium_rnnlm.tgz || exit 1
 rm exp/rnnlm_lstm_tdnn_a_averaged/tedlium_rnnlm.tgz
 
