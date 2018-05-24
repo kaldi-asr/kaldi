b1ae952feb5ba873aa2c0498eeb6547c358aed06
hainan-xv <hainan.xv@gmail.com>
2018-05-24 15:48:04 -0400

    [scripts,egs] Support averaging forward and backward RNNLMs (#2436)

--- egs/swbd/s5c/local/rnnlm/tuning/run_tdnn_lstm_back_1e.sh
+++ egs/swbd/s5c/local/rnnlm/tuning/run_tdnn_lstm_back_1e.sh
@@ -5,24 +5,27 @@
 #           2017  Hainan Xu
 #           2017  Xiaohui Zhang
 
-# This script trains LMs on the swbd LM-training data.
+# This script trains a backward LMs on the swbd LM-training data, and use it
+# to rescore either decoded lattices, or lattices that are just rescored with
+# a forward RNNLM. In order to run this, you must first run the forward RNNLM
+# recipe at local/rnnlm/run_tdnn_lstm.sh
 
 # rnnlm/train_rnnlm.sh: best iteration (out of 35) was 34, linking it to final iteration.
-# rnnlm/train_rnnlm.sh: train/dev perplexity was 41.9 / 50.0.
-# Train objf: -5.07 -4.43 -4.25 -4.17 -4.12 -4.07 -4.04 -4.01 -3.99 -3.98 -3.96 -3.94 -3.92 -3.90 -3.88 -3.87 -3.86 -3.85 -3.84 -3.83 -3.82 -3.81 -3.80 -3.79 -3.78 -3.78 -3.77 -3.77 -3.76 -3.75 -3.74 -3.73 -3.73 -3.72 -3.71
-# Dev objf:   -10.32 -4.68 -4.43 -4.31 -4.24 -4.19 -4.15 -4.13 -4.10 -4.09 -4.05 -4.03 -4.02 -4.00 -3.99 -3.98 -3.98 -3.97 -3.96 -3.96 -3.95 -3.94 -3.94 -3.94 -3.93 -3.93 -3.93 -3.92 -3.92 -3.92 -3.92 -3.91 -3.91 -3.91 -3.91
+# rnnlm/train_rnnlm.sh: train/dev perplexity was 41.8 / 55.1.
+# Train objf: -5.18 -4.46 -4.26 -4.18 -4.12 -4.07 -4.04 -4.00 -3.99 -3.98 -3.95 -3.93 -3.91 -3.90 -3.88 -3.87 -3.86 -3.85 -3.83 -3.82 -3.82 -3.81 -3.79 -3.79 -3.78 -3.77 -3.76 -3.77 -3.75 -3.74 -3.74 -3.73 -3.72 -3.71 -3.71
+# Dev objf:   -10.32 -4.89 -4.57 -4.45 -4.37 -4.33 -4.29 -4.26 -4.24 -4.22 -4.18 -4.17 -4.15 -4.14 -4.13 -4.12 -4.11 -4.10 -4.09 -4.08 -4.07 -4.06 -4.06 -4.05 -4.05 -4.05 -4.04 -4.04 -4.03 -4.03 -4.02 -4.02 -4.02 -4.01 -4.01
 
 # %WER 11.1 | 1831 21395 | 89.9 6.4 3.7 1.0 11.1 46.3 | exp/nnet3/tdnn_lstm_1a_adversarial0.3_epochs12_ld5_sp/decode_eval2000_sw1_fsh_fg_looped/score_13_0.0/eval2000_hires.ctm.swbd.filt.sys
 # %WER 9.9 | 1831 21395 | 91.0 5.8 3.2 0.9 9.9 43.2 | exp/nnet3/tdnn_lstm_1a_adversarial0.3_epochs12_ld5_sp/decode_eval2000_sw1_fsh_fg_looped_rnnlm_1e/score_11_0.0/eval2000_hires.ctm.swbd.filt.sys
-# %WER 9.9 | 1831 21395 | 91.0 5.8 3.2 0.9 9.9 42.9 | exp/nnet3/tdnn_lstm_1a_adversarial0.3_epochs12_ld5_sp/decode_eval2000_sw1_fsh_fg_looped_rnnlm_1e_nbest/score_10_0.0/eval2000_hires.ctm.swbd.filt.sys
+# %WER 9.5 | 1831 21395 | 91.4 5.5 3.1 0.9 9.5 42.5 | exp/nnet3/tdnn_lstm_1a_adversarial0.3_epochs12_ld5_sp/decode_eval2000_sw1_fsh_fg_looped_rnnlm_1e_back/score_11_0.0/eval2000_hires.ctm.swbd.filt.sys
 
 # %WER 15.9 | 4459 42989 | 85.7 9.7 4.6 1.6 15.9 51.6 | exp/nnet3/tdnn_lstm_1a_adversarial0.3_epochs12_ld5_sp/decode_eval2000_sw1_fsh_fg_looped/score_10_0.0/eval2000_hires.ctm.filt.sys
 # %WER 14.4 | 4459 42989 | 87.0 8.7 4.3 1.5 14.4 49.4 | exp/nnet3/tdnn_lstm_1a_adversarial0.3_epochs12_ld5_sp/decode_eval2000_sw1_fsh_fg_looped_rnnlm_1e/score_11_0.0/eval2000_hires.ctm.filt.sys
-# %WER 14.4 | 4459 42989 | 87.1 8.7 4.2 1.5 14.4 49.0 | exp/nnet3/tdnn_lstm_1a_adversarial0.3_epochs12_ld5_sp/decode_eval2000_sw1_fsh_fg_looped_rnnlm_1e_nbest/score_10_0.0/eval2000_hires.ctm.filt.sys
+# %WER 13.9 | 4459 42989 | 87.6 8.4 4.0 1.5 13.9 48.6 | exp/nnet3/tdnn_lstm_1a_adversarial0.3_epochs12_ld5_sp/decode_eval2000_sw1_fsh_fg_looped_rnnlm_1e_back/score_10_0.0/eval2000_hires.ctm.filt.sys
 
 # Begin configuration section.
 
-dir=exp/rnnlm_lstm_1e
+dir=exp/rnnlm_lstm_1e_backward
 embedding_dim=1024
 lstm_rpd=256
 lstm_nrpd=256
@@ -31,16 +34,13 @@ train_stage=-10
 
 # variables for lattice rescoring
 run_lat_rescore=true
-run_nbest_rescore=true
-run_backward_rnnlm=false
-
 ac_model_dir=exp/nnet3/tdnn_lstm_1a_adversarial0.3_epochs12_ld5_sp
-decode_dir_suffix=rnnlm_1e
+decode_dir_suffix_forward=rnnlm_1e
+decode_dir_suffix_backward=rnnlm_1e_back
 ngram_order=4 # approximate the lattice-rescoring by limiting the max-ngram-order
               # if it's set, it merges histories in the lattice if they share
               # the same ngram history and this prevents the lattice from 
               # exploding exponentially
-pruned_rescore=true
 
 . ./cmd.sh
 . ./utils/parse_options.sh
@@ -48,7 +48,7 @@ pruned_rescore=true
 text=data/train_nodev/text
 fisher_text=data/local/lm/fisher/text1.gz
 lexicon=data/local/dict_nosp/lexiconp.txt
-text_dir=data/rnnlm/text_nosp_1e
+text_dir=data/rnnlm/text_nosp_1e_back
 mkdir -p $dir/config
 set -e
 
@@ -61,7 +61,7 @@ if [ $stage -le 0 ]; then
   mkdir -p $text_dir
   echo -n >$text_dir/dev.txt
   # hold out one in every 50 lines as dev data.
-  cat $text | cut -d ' ' -f2- | awk -v text_dir=$text_dir '{if(NR%50 == 0) { print >text_dir"/dev.txt"; } else {print;}}' >$text_dir/swbd.txt
+  cat $text | cut -d ' ' -f2- | awk '{for(i=NF;i>0;i--) printf("%s ", $i); print""}' | awk -v text_dir=$text_dir '{if(NR%50 == 0) { print >text_dir"/dev.txt"; } else {print;}}' >$text_dir/swbd.txt
   cat > $dir/config/hesitation_mapping.txt <<EOF
 hmm hum
 mmm um
@@ -69,7 +69,7 @@ mm um
 mhm um-hum 
 EOF
   gunzip -c $fisher_text | awk 'NR==FNR{a[$1]=$2;next}{for (n=1;n<=NF;n++) if ($n in a) $n=a[$n];print $0}' \
-    $dir/config/hesitation_mapping.txt - > $text_dir/fisher.txt
+    $dir/config/hesitation_mapping.txt - | awk '{for(i=NF;i>0;i--) printf("%s ", $i); print""}' > $text_dir/fisher.txt
 fi
 
 if [ $stage -le 1 ]; then
@@ -121,42 +121,22 @@ fi
 LM=sw1_fsh_fg # using the 4-gram const arpa file as old lm
 if [ $stage -le 4 ] && $run_lat_rescore; then
   echo "$0: Perform lattice-rescoring on $ac_model_dir"
-#  LM=sw1_tg # if using the original 3-gram G.fst as old lm
-  pruned=
-  if $pruned_rescore; then
-    pruned=_pruned
-  fi
+
   for decode_set in eval2000; do
     decode_dir=${ac_model_dir}/decode_${decode_set}_${LM}_looped
+    if [ ! -d ${decode_dir}_${decode_dir_suffix_forward} ]; then
+      echo "$0: Must run the forward recipe first at local/rnnlm/run_tdnn_lstm.sh"
+      exit 1
+    fi
 
     # Lattice rescoring
-    rnnlm/lmrescore$pruned.sh \
+    rnnlm/lmrescore_back.sh \
       --cmd "$decode_cmd --mem 4G" \
       --weight 0.45 --max-ngram-order $ngram_order \
       data/lang_$LM $dir \
-      data/${decode_set}_hires ${decode_dir} \
-      ${decode_dir}_${decode_dir_suffix}_0.45
+      data/${decode_set}_hires ${decode_dir}_${decode_dir_suffix_forward}_0.45 \
+      ${decode_dir}_${decode_dir_suffix_backward}_0.45
   done
-fi
-
-if [ $stage -le 5 ] && $run_nbest_rescore; then
-  echo "$0: Perform nbest-rescoring on $ac_model_dir"
-  for decode_set in eval2000; do
-    decode_dir=${ac_model_dir}/decode_${decode_set}_${LM}_looped
-
-    # Lattice rescoring
-    rnnlm/lmrescore_nbest.sh \
-      --cmd "$decode_cmd --mem 4G" --N 20 \
-      0.8 data/lang_$LM $dir \
-      data/${decode_set}_hires ${decode_dir} \
-      ${decode_dir}_${decode_dir_suffix}_nbest
-  done
-fi
-
-# running backward RNNLM, which further improves WERS by combining backward with
-# the forward RNNLM trained in this script.
-if [ $stage -le 6 ] && $run_backward_rnnlm; then
-  local/rnnlm/run_tdnn_lstm_back.sh
 fi
 
 exit 0
