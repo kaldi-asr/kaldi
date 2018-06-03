13c98d98cad5a4ed7ca2290cb0d00c9a9d41f283
babakrkb <babak73rekabdar@gmail.com>
2018-06-03 08:11:18 -0400

    Fixed reviews

--- egs/wsj/s5/utils/lang/bpe/add_final_optional_silence.sh
+++ egs/wsj/s5/utils/lang/bpe/add_final_optional_silence.sh
@@ -18,36 +18,33 @@ fi
 
 langdir=$1
 
-sil_zero=$(echo "$final_sil_prob == 0.0" | bc -l)
-sil_one=$(echo "$final_sil_prob == 1.0" | bc -l)
-sil_ge_zero=$(echo "$final_sil_prob >= 0.0" | bc -l)
-sil_le_one=$(echo "$final_sil_prob <= 1.0" | bc -l)
-
+sil_zero=$(echo $(perl -e "if ( $final_sil_prob == 0.0) {print 1;} else {print 0;}"))
+sil_one=$(echo $(perl -e "if ( $final_sil_prob == 1.0) {print 1;} else {print 0;}"))
+sil_ge_zero=$(echo $(perl -e "if ( $final_sil_prob >= 0.0) {print 1;} else {print 0;}"))
+sil_le_one=$(echo $(perl -e "if ( $final_sil_prob <= 1.0) {print 1;} else {print 0;}"))
 if [ $sil_ge_zero -eq 1 ] && [ $sil_le_one -eq 1 ]
 then
-	if [ $sil_zero -eq 1 ]
-	then
-		echo "final-sil-prob = 0 => Final silence was not added."
-	elif [ $sil_one -eq 1 ]
-	then
-		log=$(echo "l($final_sil_prob)" | bc -l)
-		mv $langdir/L.fst $langdir/preL.fst
-		mv $langdir/L_disambig.fst $langdir/preL_disambig.fst
-		echo -e "0\t1\t1\t0\n1" > $langdir/final_sil.fst.txt
-		fstcompile $langdir/final_sil.fst.txt > $langdir/final_sil.fst
-		fstconcat $langdir/preL.fst $langdir/final_sil.fst | fstarcsort --sort_type=olabel > $langdir/L.fst
-		fstconcat $langdir/preL_disambig.fst $langdir/final_sil.fst | fstarcsort --sort_type=olabel > $langdir/L_disambig.fst
-	else
-		log=$(echo "l($final_sil_prob)" | bc -l)
-		mv $langdir/L.fst $langdir/preL.fst
-		mv $langdir/L_disambig.fst $langdir/preL_disambig.fst
-		echo -e "0\t1\t1\t0\t$log\n0\t$log\n1\t0.0" > $langdir/final_sil.fst.txt
-		fstcompile $langdir/final_sil.fst.txt > $langdir/final_sil.fst
-		fstconcat $langdir/preL.fst $langdir/final_sil.fst | fstarcsort --sort_type=olabel > $langdir/L.fst
-		fstconcat $langdir/preL_disambig.fst $langdir/final_sil.fst | fstarcsort --sort_type=olabel > $langdir/L_disambig.fst
-	fi
+  if [ $sil_zero -eq 1 ]
+  then
+    echo "$0 final-sil-prob = 0 => Final silence was not added."
+  elif [ $sil_one -eq 1 ]
+  then
+    log=$(echo $(perl -e "print log $final_sil_prob"))
+    mv $langdir/L.fst $langdir/L.fst.orig
+    mv $langdir/L_disambig.fst $langdir/L_disambig.fst.orig
+    echo -e "0\t1\t1\t0\n1" | fstcompile > $langdir/final_sil.fst
+    fstconcat $langdir/L.fst.orig $langdir/final_sil.fst | fstarcsort --sort_type=olabel > $langdir/L.fst
+    fstconcat $langdir/L_disambig.fst.orig $langdir/final_sil.fst | fstarcsort --sort_type=olabel > $langdir/L_disambig.fst
+  else
+    log=$(echo $(perl -e "print log $final_sil_prob"))
+    mv $langdir/L.fst $langdir/L.fst.orig
+    mv $langdir/L_disambig.fst $langdir/L_disambig.fst.orig
+    echo -e "0\t1\t1\t0\t$log\n0\t$log\n1\t0.0" | fstcompile > $langdir/final_sil.fst
+    fstconcat $langdir/L.fst.orig $langdir/final_sil.fst | fstarcsort --sort_type=olabel > $langdir/L.fst
+    fstconcat $langdir/L_disambig.fst.orig $langdir/final_sil.fst | fstarcsort --sort_type=olabel > $langdir/L_disambig.fst
+  fi
 else
-	echo "final-sil-prob should be between 0.0 and 1.0. Final silence was not added."
-	exit 1;
+  echo "$0 final-sil-prob should be between 0.0 and 1.0. Final silence was not added."
+  exit 1;
 fi
 
