d9cb0ae64d7fc2f835b16b36984815be59fe20be
babakrkb <babak73rekabdar@gmail.com>
2018-06-03 17:59:39 -0400

    Reviews fixed

--- egs/wsj/s5/utils/lang/bpe/add_final_optional_silence.sh
+++ egs/wsj/s5/utils/lang/bpe/add_final_optional_silence.sh
@@ -11,40 +11,43 @@ if [ $# -ne 1 ]; then
   echo "Usage: $0  <lang>"
   echo " Add final optional silence to lexicon FSTs (L.fst and L_disambig.fst) in  "
   echo " lang/ directory <lang>."
+  echo "This can be useful in systems with byte-pair encoded (BPE) lexicons, in which"
+  echo "the word-initial silence is part of the lexicon, so we turn off the standard optional"
+  echo " silence in the lexicon"
   echo "options:"
   echo "   --final-sil-prob <final silence probability>		# default 0.5"
   exit 1;
 fi
 
-langdir=$1
+lang=$1
 
-sil_zero=$(echo $(perl -e "if ( $final_sil_prob == 0.0) {print 1;} else {print 0;}"))
-sil_one=$(echo $(perl -e "if ( $final_sil_prob == 1.0) {print 1;} else {print 0;}"))
-sil_ge_zero=$(echo $(perl -e "if ( $final_sil_prob >= 0.0) {print 1;} else {print 0;}"))
-sil_le_one=$(echo $(perl -e "if ( $final_sil_prob <= 1.0) {print 1;} else {print 0;}"))
-if [ $sil_ge_zero -eq 1 ] && [ $sil_le_one -eq 1 ]
-then
-  if [ $sil_zero -eq 1 ]
-  then
-    echo "$0 final-sil-prob = 0 => Final silence was not added."
-  elif [ $sil_one -eq 1 ]
-  then
-    log=$(echo $(perl -e "print log $final_sil_prob"))
-    mv $langdir/L.fst $langdir/L.fst.orig
-    mv $langdir/L_disambig.fst $langdir/L_disambig.fst.orig
-    echo -e "0\t1\t1\t0\n1" | fstcompile > $langdir/final_sil.fst
-    fstconcat $langdir/L.fst.orig $langdir/final_sil.fst | fstarcsort --sort_type=olabel > $langdir/L.fst
-    fstconcat $langdir/L_disambig.fst.orig $langdir/final_sil.fst | fstarcsort --sort_type=olabel > $langdir/L_disambig.fst
-  else
-    log=$(echo $(perl -e "print log $final_sil_prob"))
-    mv $langdir/L.fst $langdir/L.fst.orig
-    mv $langdir/L_disambig.fst $langdir/L_disambig.fst.orig
-    echo -e "0\t1\t1\t0\t$log\n0\t$log\n1\t0.0" | fstcompile > $langdir/final_sil.fst
-    fstconcat $langdir/L.fst.orig $langdir/final_sil.fst | fstarcsort --sort_type=olabel > $langdir/L.fst
-    fstconcat $langdir/L_disambig.fst.orig $langdir/final_sil.fst | fstarcsort --sort_type=olabel > $langdir/L_disambig.fst
-  fi
-else
+if [ $lang/phones/final_sil_prob -nt $lang/phones/nonsilence.txt ]; then
+  echo "$0 $lang/phones/final_sil_prob exists. Exiting..."
+  exit 1;
+fi
+
+sil_eq_zero=$(echo $(perl -e "if ( $final_sil_prob == 0.0) {print 'true';} else {print 'false';}"))
+sil_eq_one=$(echo $(perl -e "if ( $final_sil_prob == 1.0) {print 'true';} else {print 'false';}"))
+sil_lt_zero=$(echo $(perl -e "if ( $final_sil_prob < 0.0) {print 'true';} else {print 'false';}"))
+sil_gt_one=$(echo $(perl -e "if ( $final_sil_prob > 1.0) {print 'true';} else {print 'false';}"))
+
+if  $sil_lt_zero || $sil_gt_one; then
   echo "$0 final-sil-prob should be between 0.0 and 1.0. Final silence was not added."
   exit 1;
+else
+  if $sil_eq_zero; then
+    echo "$0 final-sil-prob = 0 => Final silence was not added."
+    exit 0;
+  elif $sil_eq_one; then
+    echo -e "0\t1\t1\t0\n1" | fstcompile > $lang/final_sil.fst
+  else
+    log_silprob=$(echo $(perl -e "print log $final_sil_prob"))
+    echo -e "0\t1\t1\t0\t$log_silprob\n0\t$log_silprob\n1\t0.0" | fstcompile > $lang/final_sil.fst
+  fi
+  mv $lang/L.fst $lang/L.fst.orig
+  mv $lang/L_disambig.fst $lang/L_disambig.fst.orig
+  fstconcat $lang/L.fst.orig $lang/final_sil.fst | fstarcsort --sort_type=olabel > $lang/L.fst
+  fstconcat $lang/L_disambig.fst.orig $lang/final_sil.fst | fstarcsort --sort_type=olabel > $lang/L_disambig.fst
+  echo "$final_sil_prob" > $lang/phones/final_sil_prob
 fi
 
