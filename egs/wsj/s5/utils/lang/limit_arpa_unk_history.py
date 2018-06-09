dbce6de8700afb73e15797a646519acd1fb70ce7
Armin <md.oliya@gmail.com>
2018-06-09 19:18:04 +0200

    Read and write to input/output streams. Added support for gzipped LMs via --gzip flag. Refactored code.

--- egs/wsj/s5/utils/lang/limit_arpa_unk_history.py
+++ egs/wsj/s5/utils/lang/limit_arpa_unk_history.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
-
 # Copyright 2018    Armin Oliya
 # Apache 2.0.
-
 
 # This script takes an existing ARPA lanugage model and limits the <unk> history 
 # to make it suitable for downstream <unk> modeling. This is for the case when
@@ -11,23 +9,23 @@
 # If you do, you can use pocolm with the option --limit-unk-history=true. This
 # keeps the graph compact after adding the unk model.
 
-
-
 import argparse
-import codecs 
+import io 
 import os
 import re 
+import sys 
+import gzip 
 from collections import defaultdict
 
 
+parser = argparse.ArgumentParser(
+    description = "This script takes an existing ARPA lanugage model and limits the <unk> history to make it suitable for downstream <unk> modeling.\nIt supports up to 5-grams.", 
+    usage = "utils/lang/limit_arpa_unk_history.py [--gzip] <oov_dict_entry> <input_arpa >output_arpa")
 
-parser = argparse.ArgumentParser(description='This script takes an existing ARPA lanugage model and limits the <unk> history to make it suitable for downstream <unk> modeling.')
-parser.add_argument('--old_lm',required=True, help='path to the old ARPA language model', type=str)
-parser.add_argument('--new_lm',required=True, help='path to the new ARPA model with limited unk history', type=str)
-
+parser.add_argument('oov_dict_entry', help='oov identifier, for example <unk>', type=str)
+parser.add_argument('--gzip', dest='compressed', action='store_true', help='whether the input/output streams are .gz compressed')
+parser.set_defaults(compressed=False)
 args = parser.parse_args()	
-
-
 
 
 def get_ngram_stats(old_lm_lines):
@@ -38,43 +36,35 @@ def get_ngram_stats(old_lm_lines):
         if g:
             ngram_counts[int(g.group(1))] = int(g.group(2))
     
+    if len(ngram_counts) == 0:
+        sys.exit("Couldn't get counts per ngram section. The input doesn't seem to be a valid ARPA language model.")
+
     max_ngrams = list(ngram_counts.keys())[-1]
     skip_rows = ngram_counts[1] 
+
+    if max_ngrams > 5: 
+        sys.exit("This script supports up to 5-gram language models.")
 
     return max_ngrams, skip_rows, ngram_counts
 
 
-def find_n_replace_unks(old_lm_lines,max_ngrams,skip_rows):
-
-    new_lm_lines = old_lm_lines[:skip_rows]
+def find_and_replace_unks(old_lm_lines,max_ngrams,skip_rows):
     ngram_diffs =  defaultdict(int)
-
-    unk_pattern = re.compile("[0-9.-]+(?:[\s\\t]\S+){1,3}[\s\\t]<unk>[\s\\t](?!-[0-9]+\.[0-9]+).*")
+    unk_pattern = re.compile("[0-9.-]+(?:[\s\\t]\S+){1,3}[\s\\t]"+args.oov_dict_entry+"[\s\\t](?!-[0-9]+\.[0-9]+).*")
     backoff_pattern = re.compile("[0-9.-]+(?:[\s\\t]\S+){1,3}[\s\\t]<unk>[\s\\t]-[0-9]+\.[0-9]+")
+    passed_2grams, last_ngram = False, False
+    unk_row_count, backoff_row_count = 0, 0
 
-    passed_2grams=False
-    last_ngram=False
-
-    unk_row_count = 0
-    backoff_row_count = 0
-
-
-
-
-    print "Upading the language model .. "
-
-
+    sys.stderr.write("Upadting the language model .. \n")
+    new_lm_lines = old_lm_lines[:skip_rows]
 
     for i in range(skip_rows, len(old_lm_lines)):
-
             l = old_lm_lines[i].strip()
 
-        
             if "\{}-grams:".format(3) in l:
                 passed_2grams = True
             if "\{}-grams:".format(max_ngrams) in l:
                 last_ngram = True
-                
                 
             # remove any n-gram states of the form: foo <unk> -> X 
             # that is, any n-grams of order > 2 where <unk> is the second-to-last word
@@ -87,7 +77,6 @@ def find_n_replace_unks(old_lm_lines,max_ngrams,skip_rows):
                     ngram_diffs[ngram] = ngram_diffs[ngram] - 1
                     unk_row_count += 1
                     continue 
-
             
             # remove backoff probability from the lines that end with <unk>
             # for example, the -0.64 in -4.09 every <unk> -0.64
@@ -104,17 +93,28 @@ def find_n_replace_unks(old_lm_lines,max_ngrams,skip_rows):
                 
             new_lm_lines.append(l+"\n")
 
-
-    print ("Removed {} lines including <unk> as second-to-last term".format(unk_row_count))
-    print ("Removed backoff probabilties from {} lines".format(backoff_row_count))
+    sys.stderr.write("Removed {} lines including {} as second-to-last term\n".format(unk_row_count, args.oov_dict_entry))
+    sys.stderr.write("Removed backoff probabilties from {} lines\n".format(backoff_row_count))
 
     return new_lm_lines, ngram_diffs
 
 
+def read_old_lm():
+    sys.stderr.write("Reading ARPA LM frome input stream .. \n")
+    
+    if args.compressed:
+        input_stream = gzip.open(sys.stdin.buffer, 'rt')
+    else:
+        input_stream = io.TextIOWrapper(sys.stdin.buffer) #encoding="latin-1"
+    
+    old_lm_lines = input_stream.readlines()
+    input_stream.close()
 
+    return old_lm_lines
+
+    
 def write_new_lm(new_lm_lines,ngram_counts,ngram_diffs):
-    # update n-gram counts that go in the header of the arpa lm
-
+    ''' Update n-gram counts that go in the header of the arpa lm '''
 
     for i in range(10):
         
@@ -122,29 +122,23 @@ def write_new_lm(new_lm_lines,ngram_counts,ngram_diffs):
         if g:
             n = int(g.group(1))
             if n in ngram_diffs:
-                new_num_ngrams = ngram_counts[n] - ngram_diffs[n]
+                new_num_ngrams = ngram_counts[n] + ngram_diffs[n] #ngram_diffs contains negative values
                 new_lm_lines[i]="ngram {}={}\n".format(n,new_num_ngrams)
     
-        
-    with codecs.open(os.path.expanduser(args.new_lm),"w", encoding="utf-8") as f: 
-        f.writelines(new_lm_lines)
+    if args.compressed:
+        output_stream = gzip.open(sys.stdout.buffer, 'wt')
+    else:
+        output_stream = io.TextIOWrapper(sys.stdout.buffer) 
+    
+    output_stream.writelines(new_lm_lines)
+    output_stream.close()
 
 
-
-
-def main(): 
-
-
-    with codecs.open(os.path.expanduser(args.old_lm),"r", encoding="utf-8") as f: 
-        old_lm_lines = f.readlines()
-
+def main():
+    old_lm_lines = read_old_lm()
     max_ngrams, skip_rows,  ngram_counts = get_ngram_stats(old_lm_lines)
-
-    new_lm_lines, ngram_diffs = find_n_replace_unks(old_lm_lines,max_ngrams,skip_rows)
-
+    new_lm_lines, ngram_diffs = find_and_replace_unks(old_lm_lines,max_ngrams,skip_rows)
     write_new_lm(new_lm_lines,ngram_counts,ngram_diffs)
-
-
 
 if __name__ == "__main__":
     main()
