dc57e9467064adce72969c762071ccbc91f01c94
Zhuohao Chen <zhuohaoc@usc.edu>
2018-06-13 14:45:47 -0700

    can be used with both py2 and py3

--- egs/wsj/s5/steps/online/nnet2/extract_ivectors.sh
+++ egs/wsj/s5/steps/online/nnet2/extract_ivectors.sh
@@ -212,7 +212,7 @@ for line_index in range(len(lines)):
     if ((current_count >= $sub_speaker_frames) and ((total_counts[spk] - covered_count) >= $sub_speaker_frames)) or (utt == parts[-1]):
       spk_partial = '{0}-{1:06x}'.format(spk, numeric_id)
       numeric_id += 1
-      print '{0} {1}'.format(spk_partial, ' '.join(current_utts))
+      print ('{0} {1}'.format(spk_partial, ' '.join(current_utts)))
       current_utts = []
       current_count = 0
 "> $dir/spk2utt || exit 1;
