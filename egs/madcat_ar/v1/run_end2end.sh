d86bae0b25f45c835ac49cfc2605887104a703a1
Hossein Hadian <hn.hadian@gmail.com>
2018-07-12 13:53:10 -0400

    Some cosmetic fixes in Madcat Arabic scripts

--- egs/madcat_ar/v1/run_end2end.sh
+++ egs/madcat_ar/v1/run_end2end.sh
@@ -80,10 +80,14 @@ fi
 
 if [ $stage -le 5 ]; then
   echo "$0: Preparing dictionary and lang..."
-  cut -d' ' -f2- data/train/text | python3 local/reverse.py | python3 local/prepend_words.py | python3 utils/lang/bpe/learn_bpe.py -s 700 > data/train/bpe.out
-  for set in test train dev ; do
+  cut -d' ' -f2- data/train/text | local/reverse.py | \
+    local/prepend_words.py | \
+    utils/lang/bpe/learn_bpe.py -s 700 > data/train/bpe.out
+  for set in test train dev; do
     cut -d' ' -f1 data/$set/text > data/$set/ids
-    cut -d' ' -f2- data/$set/text | python3 local/reverse.py | python3 local/prepend_words.py | python3 utils/lang/bpe/apply_bpe.py -c data/train/bpe.out | sed 's/@@//g' > data/$set/bpe_text
+    cut -d' ' -f2- data/$set/text | local/reverse.py | \
+      local/prepend_words.py | utils/lang/bpe/apply_bpe.py -c data/train/bpe.out \
+      | sed 's/@@//g' > data/$set/bpe_text
     mv data/$set/text data/$set/text.old
     paste -d' ' data/$set/ids data/$set/bpe_text > data/$set/text
   done
