1f8d5ce7b6d905a2bae971011ad919ea612933f4
aarora8 <aarora8@jhu.edu>
2018-08-13 17:45:35 -0400

    minor change to speedup find error

--- egs/yomdle_tamil/v1/run_end2end.sh
+++ egs/yomdle_tamil/v1/run_end2end.sh
@@ -14,6 +14,7 @@ nj=30
 . ./utils/parse_options.sh
 mkdir -p data/{train,test}/data
 mkdir -p data/local/backup
+mkdir -p data/backup
 
 if [ $stage -le -1 ]; then
   echo "$(date): creating line images for shared model and unsupervised training..."
@@ -22,6 +23,8 @@ if [ $stage -le -1 ]; then
   cat /export/corpora5/handwriting_ocr/corpus_data/ta/* > data/local/text/ta.txt
   head -20000 data/local/text/ta.txt > data/local/text/val.txt
   tail -n +20000 data/local/text/ta.txt > data/local/text/corpus.txt
+  cp -r data/download data/backup/
+  cp -r data/local data/backup/
 fi
 
 if [ $stage -le 0 ]; then
@@ -40,12 +43,13 @@ if [ $stage -le 1 ]; then
   done
   image/fix_data_dir.sh data/train
   rm -rf data/train/.backup
+  rm -rf data/test/.backup
 fi
 
 if [ $stage -le 2 ]; then
   echo "$(date) stage 2: BPE preparation"
-  cp -r data/train data/local/backup1/
-  cp -r data/test data/local/backup1/
+  cp -r data/train data/local/backup/
+  cp -r data/test data/local/backup/
 
   cut -d' ' -f2- data/train/text | \
     local/get_phones.py > data/local/text/cleaned/phones.txt
