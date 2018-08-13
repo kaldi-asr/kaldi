2dc5029fe8793fb6d2c1e7ada1c538be6fdcbb83
aarora8 <aarora8@jhu.edu>
2018-08-13 03:12:49 -0400

    cosmetic change

--- egs/yomdle_tamil/v1/run_end2end.sh
+++ egs/yomdle_tamil/v1/run_end2end.sh
@@ -25,7 +25,7 @@ if [ $stage -le -1 ]; then
 fi
 
 if [ $stage -le 0 ]; then
-  echo "$0: Processing train, train unsupervised and test data...$(date)"
+  echo "stage 0: Processing train, train unsupervised and test data...$(date)"
   local/prepare_data.sh --language tamil
 fi
 
@@ -46,32 +46,49 @@ if [ $stage -le 1 ]; then
 fi
 
 if [ $stage -le 2 ]; then
-  echo "$0: Processing corpus text and preparing BPE file $(date)"
+  echo "stage 2: BPE preparation  $(date)"
   cp -r data/train data/local/backup/
   cp -r data/test data/local/backup/
   cp -r data/train_unsup data/local/backup/
-  cut -d' ' -f2- data/train/text | python3 local/get_phones.py > data/local/text/cleaned/phones.txt
+
+  cut -d' ' -f2- data/train/text | \
+    python3 local/get_phones.py > data/local/text/cleaned/phones.txt
   cut -d' ' -f2- data/train/text > data/local/text/cleaned/train.txt
-  cat data/local/text/corpus.txt | python3 local/process_corpus.py > data/local/text/cleaned/corpus.txt
-  cat data/local/text/val.txt | python3 local/process_corpus.py > data/local/text/cleaned/val.txt
-  cat data/local/text/cleaned/phones.txt data/local/text/cleaned/train.txt | python3 local/prepend_words.py | python3 utils/lang/bpe/learn_bpe.py -s 700 > data/local/bpe.out
+
+  echo ": Processing corpus text"
+  cat data/local/text/corpus.txt | \
+    local/process_corpus.py > data/local/text/cleaned/corpus.txt
+  cat data/local/text/val.txt | \
+    local/process_corpus.py > data/local/text/cleaned/val.txt
+
+  echo ": learning BPE"
+  cat data/local/text/cleaned/phones.txt data/local/text/cleaned/train.txt | \
+    local/prepend_words.py | utils/lang/bpe/learn_bpe.py -s 700 > data/local/bpe.txt || exit 1;
 fi
 
 if [ $stage -le 3 ]; then
-  echo "$0: applying BPE on train test and corpus text... $(date)"
+  echo "stage 3: applying BPE...$(date)"
+  echo "applying BPE on train test text..."
   for set in test train; do
       cut -d' ' -f1 data/$set/text > data/$set/ids
-      cut -d' ' -f2- data/$set/text | local/prepend_words.py | python3 utils/lang/bpe/apply_bpe.py -c data/local/bpe.out | sed 's/@@//g' > data/$set/bpe_text
+      cut -d' ' -f2- data/$set/text | local/prepend_words.py | \
+        utils/lang/bpe/apply_bpe.py -c data/local/bpe.txt | \
+        sed 's/@@//g' > data/$set/bpe_text
       mv data/$set/text data/$set/text.old
       paste -d' ' data/$set/ids data/$set/bpe_text > data/$set/text
   done
-  echo "$0: Preparing BPE corpus $(date)"
-  cat data/local/text/cleaned/corpus.txt | python3 local/prepend_words.py | python3 utils/lang/bpe/apply_bpe.py -c data/train/bpe.out | sed 's/@@//g' > data/local/text/cleaned/bpe_corpus.txt
-  cat data/local/text/cleaned/val.txt | python3 localprepend_words.py | python3 utils/lang/bpe/apply_bpe.py -c data/train/bpe.out | sed 's/@@//g' > data/local/text/cleaned/bpe_val.txt
+
+  echo "applying BPE to corpus text"
+  cat data/local/text/cleaned/corpus.txt | local/prepend_words.py | \
+    utils/lang/bpe/apply_bpe.py -c data/local/bpe.txt | \
+    sed 's/@@//g' > data/local/text/cleaned/bpe_corpus.txt
+  cat data/local/text/cleaned/val.txt | local/prepend_words.py | \
+    utils/lang/bpe/apply_bpe.py -c data/local/bpe.txt | \
+    sed 's/@@//g' > data/local/text/cleaned/bpe_val.txt
 fi
 
 if [ $stage -le 4 ]; then
-  echo "$0: Preparing dictionary and lang... $(date)"
+  echo "stage 4: Preparing dictionary and lang... $(date)"
   local/prepare_dict.sh --dir data/local/dict
   utils/prepare_lang.sh --num-sil-states 4 --num-nonsil-states 8 --sil-prob 0.0 --position-dependent-phones false \
       data/local/dict "<sil>" data/lang/temp data/lang
@@ -79,7 +96,7 @@ if [ $stage -le 4 ]; then
 fi
 
 if [ $stage -le 5 ]; then
-  echo "$0: Estimating a language model for decoding...$(date)"
+  echo "stage 5: Estimating a language model for decoding...$(date)"
   local/train_lm.sh --dir data/local/local_lm --order 3
   utils/format_lm.sh data/lang data/local/local_lm/data/arpa/3gram_unpruned.arpa.gz \
       data/local/dict/lexicon.txt data/lang_test
@@ -90,18 +107,18 @@ if [ $stage -le 5 ]; then
 fi
 
 if [ $stage -le 6 ]; then
-  echo "$0: Calling the flat-start chain recipe...$(date)"
+  echo "stage 6: Calling the flat-start chain recipe...$(date)"
   local/chain/run_e2e_cnn.sh
 fi
 
 if [ $stage -le 7 ]; then
-  echo "$0: Aligning the training data using the e2e chain model... $(date)"
+  echo "stage 7: Aligning the training data using the e2e chain model... $(date)"
   steps/nnet3/align.sh --nj $nj --cmd "$cmd" \
       --scale-opts '--transition-scale=1.0 --acoustic-scale=1.0 --self-loop-scale=1.0' \
       data/train data/lang exp/chain/e2e_cnn_1a exp/chain/e2e_ali_train
 fi
 
 if [ $stage -le 8 ]; then
-  echo "$0: Building a tree and training a regular chain model using the e2e alignments...$(date)"
+  echo "stage 8: Building a tree and training a regular chain model using the e2e alignments...$(date)"
   local/chain/run_cnn_e2eali_1b.sh
 fi
