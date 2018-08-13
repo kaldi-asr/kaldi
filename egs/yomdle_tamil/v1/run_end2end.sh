70619e3f377f670714c9caf638e2e6da65449a15
aarora8 <aarora8@jhu.edu>
2018-08-13 16:23:32 -0400

    minor change changing comments

--- egs/yomdle_tamil/v1/run_end2end.sh
+++ egs/yomdle_tamil/v1/run_end2end.sh
@@ -16,16 +16,16 @@ mkdir -p data/{train,train_unsup,test}/data
 mkdir -p data/local/backup
 
 if [ $stage -le -1 ]; then
-  echo "$0: creating line images for shared model and unsupervised training...$(date)"
+  echo "$(date): creating line images for shared model and unsupervised training..."
   image/ocr/yomdle/create_download_dir.sh --language_main Tamil
-  echo "$0: getting text for language modelling...$(date)"
+  echo "$(date): getting text for language modelling..."
   cat /export/corpora5/handwriting_ocr/corpus_data/ta/* > data/local/text/ta.txt
   head -20000 data/local/text/ta.txt > data/local/text/val.txt
   tail -n +20000 data/local/text/ta.txt > data/local/text/corpus.txt
 fi
 
 if [ $stage -le 0 ]; then
-  echo "stage 0: Processing train and test data...$(date)"
+  echo "$(date) stage 0: Processing train and test data..."
   local/prepare_data.sh --language tamil
 fi
 
@@ -34,7 +34,7 @@ if [ $stage -le 1 ]; then
   image/get_image2num_frames.py --feat-dim 40 data/train
   image/get_allowed_lengths.py --frame-subsampling-factor 4 10 data/train
   for set in train test; do
-    echo "Extracting features and calling compute_cmvn_stats: $(date) "
+    echo "$(date) Extracting features and calling compute_cmvn_stats"
     local/extract_features.sh --nj $nj --cmd "$cmd" --feat-dim 40 data/${set}
     steps/compute_cmvn_stats.sh data/${set} || exit 1;
   done
@@ -42,7 +42,7 @@ if [ $stage -le 1 ]; then
 fi
 
 if [ $stage -le 2 ]; then
-  echo "stage 2: BPE preparation  $(date)"
+  echo "$(date) stage 2: BPE preparation"
   cp -r data/train data/local/backup1/
   cp -r data/test data/local/backup1/
   cp -r data/train_unsup data/local/backup1/
@@ -63,7 +63,7 @@ if [ $stage -le 2 ]; then
 fi
 
 if [ $stage -le 3 ]; then
-  echo "stage 3: applying BPE...$(date)"
+  echo "$(date) stage 3: applying BPE..."
   echo "applying BPE on train, test text..."
   for set in test train; do
     cut -d' ' -f1 data/$set/text > data/$set/ids
@@ -84,7 +84,7 @@ if [ $stage -le 3 ]; then
 fi
 
 if [ $stage -le 4 ]; then
-  echo "stage 4: Preparing dictionary and lang... $(date)"
+  echo "$(date) stage 4: Preparing dictionary and lang..."
   local/prepare_dict.sh --dir data/local/dict
   utils/prepare_lang.sh --num-sil-states 4 --num-nonsil-states 8 --sil-prob 0.0 --position-dependent-phones false \
       data/local/dict "<sil>" data/lang/temp data/lang
@@ -92,7 +92,7 @@ if [ $stage -le 4 ]; then
 fi
 
 if [ $stage -le 5 ]; then
-  echo "stage 5: Estimating a language model for decoding...$(date)"
+  echo "$(date) stage 5: Estimating a language model for decoding..."
   local/train_lm.sh --dir data/local/local_lm --order 3
   utils/format_lm.sh data/lang data/local/local_lm/data/arpa/3gram_unpruned.arpa.gz \
       data/local/dict/lexicon.txt data/lang_test
@@ -103,18 +103,18 @@ if [ $stage -le 5 ]; then
 fi
 
 if [ $stage -le 6 ]; then
-  echo "stage 6: Calling the flat-start chain recipe...$(date)"
+  echo "$(date) stage 6: Calling the flat-start chain recipe..."
   local/chain/run_e2e_cnn.sh
 fi
 
 if [ $stage -le 7 ]; then
-  echo "stage 7: Aligning the training data using the e2e chain model... $(date)"
+  echo "$(date) stage 7: Aligning the training data using the e2e chain model..."
   steps/nnet3/align.sh --nj $nj --cmd "$cmd" \
     --scale-opts '--transition-scale=1.0 --acoustic-scale=1.0 --self-loop-scale=1.0' \
     data/train data/lang exp/chain/e2e_cnn_1a exp/chain/e2e_ali_train
 fi
 
 if [ $stage -le 8 ]; then
-  echo "stage 8: Building a tree and training a regular chain model using the e2e alignments...$(date)"
+  echo "$(date) stage 8: Building a tree and training a regular chain model using the e2e alignments..."
   local/chain/run_cnn_e2eali_1b.sh
 fi
