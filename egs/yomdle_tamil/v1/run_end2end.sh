1799a7919559479673622fccbc30db8190f073c9
aarora8 <aarora8@jhu.edu>
2018-08-12 20:54:02 -0400

    minor change

--- egs/yomdle_tamil/v1/run_end2end.sh
+++ egs/yomdle_tamil/v1/run_end2end.sh
@@ -33,37 +33,44 @@ if [ $stage -le 1 ]; then
   image/get_image2num_frames.py --feat-dim 40 data/train
   image/get_allowed_lengths.py --frame-subsampling-factor 4 10 data/train
 
-  for dataset in test train train_unsup; do
-    echo "$0: Extracting features and calling compute_cmvn_stats for dataset:  $dataset. "
+  for set in test train train_unsup; do
+    echo "$0: Extracting features and calling compute_cmvn_stats for dataset:  $set. "
     echo "Date: $(date)."
     #image/ocr/extract_features.sh --nj $nj --cmd $cmd --feat-dim 40 data/$dataset
-    image/ocr/make_features.py data/$dataset/images.scp --feat-dim 40 \
-      --allowed_len_file_path data/$dataset/allowed_lengths.txt --no-augment | \
+    image/ocr/make_features.py data/$set/images.scp --feat-dim 40 \
+      --allowed_len_file_path data/$set/allowed_lengths.txt --no-augment | \
       copy-feats --compress=true --compression-method=7 \
-        ark:- ark,scp:data/$dataset/data/images.ark,data/$dataset/feats.scp
-    steps/compute_cmvn_stats.sh data/$dataset || exit 1;
+        ark:- ark,scp:data/$set/data/images.ark,data/$set/feats.scp
+    steps/compute_cmvn_stats.sh data/$set || exit 1;
   done
   image/fix_data_dir.sh data/train
 fi
 
 if [ $stage -le 2 ]; then
-  echo "$0: Processing corpus and preparing BPE file $(date)"
+  echo "$0: Processing corpus text and preparing BPE file $(date)"
   cp -r data/train data/local/backup/
   cp -r data/test data/local/backup/
   cut -d' ' -f2- data/train/text | python3 local/get_phones.py > data/local/text/cleaned/phones.txt
   cut -d' ' -f2- data/train/text > data/local/text/cleaned/train.txt
   cat data/local/text/corpus.txt | python3 local/process_corpus.py > data/local/text/cleaned/corpus.txt
   cat data/local/text/val.txt | python3 local/process_corpus.py > data/local/text/cleaned/val.txt
-  cat data/local/text/cleaned/phones.txt data/local/text/cleaned/train.txt | python3 image/ocr/prepend_words.py | python3 utils/lang/bpe/learn_bpe.py -s 700 > data/train/bpe.out
-  for datasplit in test train; do
-      cut -d' ' -f1 data/$datasplit/text > data/$datasplit/ids
-      cut -d' ' -f2- data/$datasplit/text | python3 image/ocr/prepend_words.py | python3 utils/lang/bpe/apply_bpe.py -c data/train/bpe.out | sed 's/@@//g' > data/$datasplit/bpe_text
-      mv data/$datasplit/text data/$datasplit/text.old
-      paste -d' ' data/$datasplit/ids data/$datasplit/bpe_text > data/$datasplit/text
+  cat data/local/text/cleaned/phones.txt data/local/text/cleaned/train.txt | python3 image/ocr/prepend_words.py | python3 utils/lang/bpe/learn_bpe.py -s 700 > data/local/bpe.out
+fi
+
+if [ $stage -le 3 ]; then
+  echo "$0: applying BPE on train test and corpus text... $(date)"
+  for set in test train; do
+      cut -d' ' -f1 data/$set/text > data/$set/ids
+      cut -d' ' -f2- data/$set/text | image/ocr/prepend_words.py | python3 utils/lang/bpe/apply_bpe.py -c data/local/bpe.out | sed 's/@@//g' > data/$set/bpe_text
+      mv data/$set/text data/$set/text.old
+      paste -d' ' data/$set/ids data/$set/bpe_text > data/$set/text
   done
   echo "$0: Preparing BPE corpus $(date)"
   cat data/local/text/cleaned/corpus.txt | python3 image/ocr/prepend_words.py | python3 utils/lang/bpe/apply_bpe.py -c data/train/bpe.out | sed 's/@@//g' > data/local/text/cleaned/bpe_corpus.txt
   cat data/local/text/cleaned/val.txt | python3 image/ocr/prepend_words.py | python3 utils/lang/bpe/apply_bpe.py -c data/train/bpe.out | sed 's/@@//g' > data/local/text/cleaned/bpe_val.txt
+fi
+
+if [ $stage -le 4 ]; then
   echo "$0: Preparing dictionary and lang... $(date)"
   local/prepare_dict.sh --dir data/local/dict
   utils/prepare_lang.sh --num-sil-states 4 --num-nonsil-states 8 --sil-prob 0.0 --position-dependent-phones false \
@@ -71,19 +78,19 @@ if [ $stage -le 2 ]; then
   utils/lang/bpe/add_final_optional_silence.sh --final-sil-prob 0.5 data/lang
 fi
 
-if [ $stage -le 3 ]; then
+if [ $stage -le 5 ]; then
   echo "$0: Estimating a language model for decoding...$(date)"
   local/train_lm.sh --dir data/local/local_lm --order 3
   utils/format_lm.sh data/lang data/local/local_lm/data/arpa/3gram_unpruned.arpa.gz \
       data/local/dict/lexicon.txt data/lang_test
 fi
 
-if [ $stage -le 4 ]; then
+if [ $stage -le 6 ]; then
   echo "$0: Calling the flat-start chain recipe...$(date)"
   local/chain/run_flatstart_cnn1a.sh
 fi
 
-if [ $stage -le 5 ]; then
+if [ $stage -le 7 ]; then
   echo "$0: Aligning the training data using the e2e chain model... $(date)"
   steps/nnet3/align.sh --nj $nj --cmd "$cmd" \
       --use-gpu false \
@@ -91,7 +98,7 @@ if [ $stage -le 5 ]; then
       data/train data/lang exp/chain/e2e_cnn_1a exp/chain/e2e_ali_train
 fi
 
-if [ $stage -le 6 ]; then
+if [ $stage -le 8 ]; then
   echo "$0: Building a tree and training a regular chain model using the e2e alignments...$(date)"
   local/chain/run_cnn_e2eali_1b.sh
 fi
