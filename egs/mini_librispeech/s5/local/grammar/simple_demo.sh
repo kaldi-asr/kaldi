f8f6581da87b6ce467bbca99b4d7085bf2ae4b51
Daniel Povey <dpovey@gmail.com>
2018-08-20 23:58:00 -0400

    [egs] Improvements to demo script, test more aspects.

--- egs/mini_librispeech/s5/local/grammar/simple_demo.sh
+++ egs/mini_librispeech/s5/local/grammar/simple_demo.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# This script demonstrates how to use the grammar-decoding framework to build
+# graphs made out of more than one part.  It demonstrates using `fstequivalent`
+# that the graph constructed this way is equivalent to what you would create if
+# you had the LM all as a single piece.  This uses the command line tools to
+# expand to a regular FST (--write-as-grammar=false) In practice you might not
+# want do to that, since the result might be large, and since writing the entire
+# thing might take too much time.  The code itself allows you to construct these
+# GrammarFst objects in lightweight way and decode using them.
 
 stage=0
 . ./path.sh
@@ -26,8 +34,6 @@ if [ $stage -le 0 ]; then
 
   utils/prepare_lang.sh data/local/dict_nosp_grammar1 \
        "<UNK>" data/local/lang_tmp_nosp data/lang_nosp_grammar1
-
-  rm -r data/lang_nosp_grammar1{a,b}
 fi
 
 
@@ -36,8 +42,9 @@ if [ $stage -le 1 ]; then
   # Most contents of these directories will be the same, only G.fst differs, but
   # it's our practice to make these things as directories combining G.fst with
   # everything else.
-  cp -r data/lang_nosp_grammar1 data/lang_nosp_grammar1a
-  cp -r data/lang_nosp_grammar1 data/lang_nosp_grammar1b
+  rm -r  data/lang_nosp_grammar2{a,b}
+  cp -r data/lang_nosp_grammar1 data/lang_nosp_grammar2a
+  cp -r data/lang_nosp_grammar1 data/lang_nosp_grammar2b
 fi
 
 if [ $stage -le 2 ]; then
@@ -59,14 +66,14 @@ fi
 
 
 if [ $stage -le 3 ]; then
-  # create the stop-level graph in data/lang_nosp_grammar1a
+  # create the stop-level graph in data/lang_nosp_grammar2a
 
 # you can of course choose to put what symbols you want on the output side, as
 # long as they are defined in words.txt.  #nonterm:contact_list, #nonterm_begin
 # and #nonterm_end would be defined in this example.  This might be useful in
 # situations where you want to keep track of the structure of calling
 # nonterminals.
-lang=data/lang_nosp_grammar1a
+lang=data/lang_nosp_grammar2a
 cat <<EOF | fstcompile --isymbols=$lang/words.txt --osymbols=$lang/words.txt > $lang/G.fst
 0    1    GROUP   GROUP
 1    2    #nonterm:contact_list  <eps>
@@ -74,14 +81,14 @@ cat <<EOF | fstcompile --isymbols=$lang/words.txt --osymbols=$lang/words.txt > $
 2  0.69314718055994
 3
 EOF
-utils/mkgraph.sh --self-loop-scale 1.0 $lang $tree_dir $tree_dir/grammar1a
+utils/mkgraph.sh --self-loop-scale 1.0 $lang $tree_dir $tree_dir/grammar2a
 fi
 
 if [ $stage -le 4 ]; then
-  # Create the graph for the nonterminal in data/lang_nosp_grammar1b
+  # Create the graph for the nonterminal in data/lang_nosp_grammar2b
   # Again, we don't choose to put these symbols on the output side, but it would
   # be possible to do so.
-  lang=data/lang_nosp_grammar1b
+  lang=data/lang_nosp_grammar2b
   cat <<EOF | fstcompile --isymbols=$lang/words.txt --osymbols=$lang/words.txt > $lang/G.fst
 0    1    #nonterm_begin <eps>
 1    2    ONE  ONE    0.69314718055994
@@ -90,23 +97,24 @@ if [ $stage -le 4 ]; then
 2    3    #nonterm_end <eps>
 3
 EOF
-  utils/mkgraph.sh --self-loop-scale 1.0 $lang $tree_dir $tree_dir/grammar1b
+  utils/mkgraph.sh --self-loop-scale 1.0 $lang $tree_dir $tree_dir/grammar2b
 fi
 
 if [ $stage -le 5 ]; then
   # combine the top-level graph and the sub-graph together using the command
   # line tools. (In practice you might want to do this from appliation code).
 
+  lang=data/lang_nosp_grammar2a
   offset=$(grep nonterm_bos $lang/phones.txt | awk '{print $2}') # 364
   clist=$(grep nonterm:contact_list $lang/phones.txt | awk '{print $2}') # 368
 
-  # the graph in $tree_dir/grammar1c/HCLG.fst will be a normal FST (ConstFst)
+  # the graph in $tree_dir/grammar2/HCLG.fst will be a normal FST (ConstFst)
   # that was expanded from the grammar.  (we use --write-as-grammar=false to
   # make it expand it).  This is to test equivalence to the one in
   # $tree_dir/grammar1/
-
-  make-grammar-fst --write-as-grammar=false --nonterm-phones-offset=$offset $tree_dir/grammar1a/HCLG.fst \
-                   $clist $tree_dir/grammar1b/HCLG.fst  $tree_dir/grammar1c/HCLG.fst
+  mkdir -p $tree_dir/grammar2
+  make-grammar-fst --write-as-grammar=false --nonterm-phones-offset=$offset $tree_dir/grammar2a/HCLG.fst \
+                   $clist $tree_dir/grammar2b/HCLG.fst  $tree_dir/grammar2/HCLG.fst
 fi
 
 if [ $stage -le 6 ]; then
@@ -114,8 +122,8 @@ if [ $stage -le 6 ]; then
   # fstequivalent fails.
   echo "$0: will print costs with the two FSTs, for one random path."
   fstrandgen $tree_dir/grammar1/HCLG.fst > path.fst
-  for x in "" "c"; do
-    fstproject --project_output=false path.fst | fstcompose - $tree_dir/grammar1${x}/HCLG.fst | fstcompose - <(fstproject --project_output=true path.fst) > composed.fst
+  for x in 1 2; do
+    fstproject --project_output=false path.fst | fstcompose - $tree_dir/grammar${x}/HCLG.fst | fstcompose - <(fstproject --project_output=true path.fst) > composed.fst
     start_state=$(fstprint composed.fst | head -n 1 | awk '{print $1}')
     fstshortestdistance --reverse=true composed.fst | awk -v s=$start_state '{if($1 == s) { print $2; }}'
   done
@@ -124,7 +132,7 @@ fi
 
 if [ $stage -le 7 ]; then
   echo "$0: will test equivalece using fstequivalent"
-  if fstequivalent --delta=0.01 --random=true --npath=100 $tree_dir/grammar1/HCLG.fst $tree_dir/grammar1c/HCLG.fst; then
+  if fstequivalent --delta=0.01 --random=true --npath=100 $tree_dir/grammar1/HCLG.fst $tree_dir/grammar2/HCLG.fst; then
     echo "$0: success: the two were equivalent"
   else
     echo "$0: failure: the two were equivalent"
