e274fadc416aceb988b96a31fbe35fa7f2c2fb61
Desh Raj <r.desh26@gmail.com>
2018-08-25 21:50:11 -0400

    minor fix

--- src/decoder/lattice-faster-online-decoder.cc
+++ src/decoder/lattice-faster-online-decoder.cc
@@ -991,7 +991,7 @@ void LatticeFasterOnlineDecoder::ProcessNonemitting(BaseFloat cutoff) {
   KALDI_ASSERT(queue_.empty());
   for (const Elem *e = toks_.GetList(); e != NULL;  e = e->tail) {
     StateId key = e->key;
-    if (fst_->NumInputEpsilons(key) != 0)
+    if (fst_.NumInputEpsilons(key) != 0)
       queue_.push_back(key);
   }
   if (queue_.empty()) {
