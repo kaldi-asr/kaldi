1242305c6133c7acc58de5accd0bacbcd759a543
Desh Raj <r.desh26@gmail.com>
2018-08-30 13:44:36 -0400

    [src] Decoder speedup: check NumInputEpsilons() in ProcessNonemitting() (#2641)
    Note: in one configuration I saw 25% speedup with this change (depends on beams, etc.)

--- src/decoder/lattice-faster-decoder.cc
+++ src/decoder/lattice-faster-decoder.cc
@@ -809,8 +809,11 @@ void LatticeFasterDecoder::ProcessNonemitting(BaseFloat cutoff) {
   // problem did not improve overall speed.
 
   KALDI_ASSERT(queue_.empty());
-  for (const Elem *e = toks_.GetList(); e != NULL;  e = e->tail)
-    queue_.push_back(e->key);
+  for (const Elem *e = toks_.GetList(); e != NULL;  e = e->tail) {
+    StateId state = e->key;
+    if (fst_.NumInputEpsilons(state) != 0)
+      queue_.push_back(state);
+  }
   if (queue_.empty()) {
     if (!warned_) {
       KALDI_WARN << "Error, no surviving tokens: frame is " << frame;
@@ -850,7 +853,8 @@ void LatticeFasterDecoder::ProcessNonemitting(BaseFloat cutoff) {
 
           // "changed" tells us whether the new token has a different
           // cost from before, or is new [if so, add into queue].
-          if (changed) queue_.push_back(arc.nextstate);
+          if (changed && fst_.NumInputEpsilons(arc.nextstate) != 0) 
+            queue_.push_back(arc.nextstate);
         }
       }
     } // for all arcs
