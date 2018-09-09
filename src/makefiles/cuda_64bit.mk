0a00e7337a511cc9b0d46d9c57ab73cc7b9ceae2
Daniel Galvez <dt.galvez@gmail.com>
2018-09-09 15:02:52 -0400

    Download CUDNN, get this to build on CLSP

--- src/makefiles/cuda_64bit.mk
+++ src/makefiles/cuda_64bit.mk
@@ -5,12 +5,22 @@ ifndef CUDATKDIR
 $(error CUDATKDIR not defined.)
 endif
 
-# TODO: Clean this up to make cudnn an optional dependency
-CUDA_INCLUDE= -I$(CUDATKDIR)/include
-CUDA_FLAGS = -g -Xcompiler -fPIC --verbose --machine 64 -DHAVE_CUDA=1
-						 -DHAVE_CUDNN=1 -ccbin $(CXX) \
+# Order matters here. We must tell the compiler to search
+# $(CUDNNDIR)/lib64 before $(CUDATKDIR)/lib64 because the CUDNN .deb
+# files install cudnn to /usr/local/cuda/lib64, which would overshadow
+# the user-specified $(CUDNNDIR)
+ifdef CUDNNDIR
+CUDA_INCLUDE += -I$(CUDNNDIR)/include
+CUDA_FLAGS += -DHAVE_CUDNN=1
+CXXFLAGS += -I$(CUDNNDIR)/include -DHAVE_CUDNN=1
+CUDA_LDFLAGS += -L$(CUDNNDIR)/lib64 -Wl,-rpath,$(CUDNNDIR)/lib64
+CUDA_LDLIBS += -lcudnn
+endif
+CUDA_INCLUDE += -I$(CUDATKDIR)/include
+CUDA_FLAGS += -g -Xcompiler -fPIC --verbose --machine 64 -DHAVE_CUDA=1 \
+						 -ccbin $(CXX) \
 						 -DKALDI_DOUBLEPRECISION=$(DOUBLE_PRECISION) \
              -DCUDA_API_PER_THREAD_DEFAULT_STREAM
-CXXFLAGS += -DHAVE_CUDA=1 -DHAVE_CUDNN=1 -I$(CUDATKDIR)/include
+CXXFLAGS += -DHAVE_CUDA=1 -I$(CUDATKDIR)/include
 CUDA_LDFLAGS += -L$(CUDATKDIR)/lib64 -Wl,-rpath,$(CUDATKDIR)/lib64
-CUDA_LDLIBS += -lcudnn -lcublas -lcusparse -lcudart -lcurand #LDLIBS : The libs are loaded later than static libs in implicit rule
+CUDA_LDLIBS += -lcublas -lcusparse -lcudart -lcurand #LDLIBS : The libs are loaded later than static libs in implicit rule
