6a865314e863b831e0a20709052b46fd49cf3173
aarora8 <aarora8@jhu.edu>
2018-09-11 23:43:29 -0400

    hardcoding for removing only remaining long utterence

--- egs/iam/v2/local/remove_test_utterances_from_lob.py
+++ egs/iam/v2/local/remove_test_utterances_from_lob.py
@@ -91,7 +91,7 @@ for line_id, line_to_find in utterance_dict.items():
     found_line = False
     # avoiding very small utterance, it causes removing
     # complete lob text
-    if len(line_to_find) < 13:
+    if len(line_to_find) < 10:
         remaining_utterances[line_id] = line_to_find
     else:
         for i in range(1, (len(corpus_text_lowercase_wo_sc) - 2)):
@@ -109,6 +109,25 @@ for line_id, line_to_find in utterance_dict.items():
     if not found_line:
         remaining_utterances[line_id] = line_to_find
 
+# removing long utterances not found above
+row_to_keep[87530] = False; row_to_keep[87531] = False; row_to_keep[87532] = False
+row_to_keep[31724] = False; row_to_keep[31725] = False; row_to_keep[31726] = False
+row_to_keep[16704] = False; row_to_keep[16705] = False; row_to_keep[16706] = False;
+row_to_keep[94181] = False; row_to_keep[94182] = False; row_to_keep[94183] = False;
+row_to_keep[20171] = False; row_to_keep[20172] = False; row_to_keep[20173] = False;
+row_to_keep[16734] = False; row_to_keep[16733] = False; row_to_keep[16732] = False;
+row_to_keep[20576] = False; row_to_keep[20577] = False; row_to_keep[20578] = False;
+row_to_keep[31715] = False; row_to_keep[31716] = False; row_to_keep[31717] = False;
+row_to_keep[31808] = False; row_to_keep[31809] = False; row_to_keep[31810] = False;
+row_to_keep[31822] = False; row_to_keep[31823] = False; row_to_keep[31824] = False;
+row_to_keep[88791] = False; row_to_keep[88792] = False; row_to_keep[88793] = False;
+row_to_keep[31745] = False; row_to_keep[31746] = False; row_to_keep[31825] = False;
+row_to_keep[94256] = False; row_to_keep[94257] = False; row_to_keep[88794] = False;
+row_to_keep[88665] = False; row_to_keep[17093] = False; row_to_keep[17094] = False;
+row_to_keep[20586] = False; row_to_keep[87228] = False; row_to_keep[87229] = False;
+row_to_keep[16744] = False; row_to_keep[87905] = False; row_to_keep[87906] = False;
+row_to_keep[16669] = False; row_to_keep[16670] = False; row_to_keep[16719] = False;
+row_to_keep[87515] = False; row_to_keep[20090] = False; row_to_keep[31748] = False;
 for i in range(len(original_corpus_text)):
     transcript = original_corpus_text[i].strip()
     if row_to_keep[i]:
