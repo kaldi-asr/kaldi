dadb232394ec7dc014c303332c2eaf95bba698b6
Chun-Chieh Chang <cchunch1@jhu.edu>
2018-09-13 13:43:04 -0400

    added some more comments

--- egs/yomdle_fa/README.txt
+++ egs/yomdle_fa/README.txt
@@ -1,4 +1,4 @@
 This directory contains example scripts for OCR on the Yomdle and Slam datasets.
 Training is done on the Yomdle dataset and testing is done on Slam.
 LM rescoring is also done with extra corpus data obtained from various newswires (e.g. Hamshahri)
-
+There is also an option for normalized scoring provided in the local/normalize_scoring/normalized_socring.sh
--- egs/yomdle_fa/v1/local/normalized_scoring/normalized_scoring.sh
+++ egs/yomdle_fa/v1/local/normalized_scoring/normalized_scoring.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# This script provides normalized scoring for the various slam dataset languages.
+# This is especially important for Farsi as this script scores using various normalizations
+# on things like Farsi vs Arabic yeh.
+# Usage e.g.:
+# local/normalized_scoring/normalized_scoring.sh output exp_yomdle_farsi/chain/cnn_e2eali_1b/decode_test/scoring_kaldi/penalty_1.0/7.txt farsi
+
 OUTDIR=$1
 HYP_FILE=$2
 LANG=$3
@@ -8,5 +15,3 @@ cat ${HYP_FILE} | cut -d' ' -f2- | python3 local/bidi.py > hyp
 paste -d' ' ids hyp | python3 local/normalized_scoring/convert2snor.py > hyp_file.txt
 
 local/normalized_scoring/score.sh ${OUTDIR} hyp_file.txt ${LANG}
-
-#rm ids hyp hyp_file.txt
