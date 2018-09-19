92a470da6c151e23c872e0760a99b8f730439132
aarora8 <aarora8@jhu.edu>
2018-09-19 14:12:27 -0400

    removing unnecessary files

--- egs/wsj/s5/utils/lang/bpe/prepend_words.py
+++ egs/wsj/s5/utils/lang/bpe/prepend_words.py
@@ -4,16 +4,11 @@
 # the beginning of the words for finding the initial-space of every word
 # after decoding.
 
-import argparse
 import sys, io
 
-parser = argparse.ArgumentParser(description="Prepends '|' to the beginning of every word")
-parser.add_argument('--encoding', type=str, default='latin-1',
-                    help='Type of encoding')
-args = parser.parse_args()
-
-infile = io.TextIOWrapper(sys.stdin.buffer, encoding=args.encoding)
-output = io.TextIOWrapper(sys.stdout.buffer, encoding=args.encoding)
+infile = io.TextIOWrapper(sys.stdin.buffer, encoding='latin-1')
+output = io.TextIOWrapper(sys.stdout.buffer, encoding='latin-1')
 for line in infile:
     output.write(' '.join([ "|"+word for word in line.split()]) + '\n')
+
 
