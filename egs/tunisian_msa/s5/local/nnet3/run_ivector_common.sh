fe7d648bcbb9d3f7b3bf6a0a9472512f312c08d3
John Josephmorgan Morgan <johnjosephmorgan@gmail.com>
2018-09-19 19:59:59 -0400

    mfcc instead of plp features.

--- egs/tunisian_msa/s5/local/nnet3/run_ivector_common.sh
+++ egs/tunisian_msa/s5/local/nnet3/run_ivector_common.sh
@@ -37,8 +37,8 @@ if [ $stage -le 1 ]; then
     utils/data/perturb_data_dir_speed_3way.sh \
 	data/${train_set} \
 	data/${train_set}_sp
-    echo "$0: making plp pitch features for low-resolution speed-perturbed data"
-    steps/make_plp_pitch.sh \
+    echo "$0: making mfcc features for low-resolution speed-perturbed data"
+    steps/make_mfcc.sh \
 	--cmd "$train_cmd" \
 	--nj 10 \
 	data/${train_set}_sp
