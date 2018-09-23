55227dfcc242cb49ef9cf46e6ff29a3f3f1f1109
huangruizhe <eraser567@163.com>
2018-09-23 03:07:26 -0400

    minor fixes for robustness of the script

--- egs/wsj/s5/steps/dict/apply_g2p_phonetisaurus.sh
+++ egs/wsj/s5/steps/dict/apply_g2p_phonetisaurus.sh
@@ -7,6 +7,9 @@
 # This script applies a trained Phonetisarus G2P model to
 # synthesize pronunciations for missing words (i.e., words in
 # transcripts but not the lexicon), and output the expanded lexicon.
+# The user could specify either nbest or pmass option 
+# to determine the number of output pronunciation variants, 
+# or use them together to get the intersection of two options.
 
 # Begin configuration section.  
 stage=0
@@ -27,12 +30,11 @@ if [ $# != 3 ]; then
   echo "... where <g2p-model> is the trained g2p model."
   echo "          <word-list> is a list of words whose pronunciation is to be generated."
   echo "          <lexicon-out> output lexicon, whose format is <word>\t<prob>\t<pronunciation> for each line."
-  echo "e.g.: $0 exp/g2p/model.fst exp/g2p/oov_words.txt data/local/dict_nosp/lexicon.txt"
+  echo "e.g.: $0 --nbest 1 exp/g2p/model.fst exp/g2p/oov_words.txt data/local/dict_nosp/lexicon.txt"
   echo ""
   echo "main options (for others, see top of script file)"
-  echo "  --nbest <int>    # Maximum number of hypotheses to produce. By default, nbest=1."
-  echo "  --pmass <float>  # Select the maximum number of hypotheses summing to a total mass of pmass amount, within [0, 1], for a word."
-  echo "  --nbest <int> --pmass <float>  # When specified together, we generate the intersection of these two options."
+  echo "  --nbest <int>    # Maximum number of hypotheses to produce. By default, nbest=20"
+  echo "  --pmass <float>  # Select the maximum number of hypotheses summing to a total mass of pmass amount, within [0, 1], for a word. By default, pmass=1.0"
   exit 1;
 fi
 
@@ -40,10 +42,8 @@ model=$1
 word_list=$2
 out_lexicon=$3
 
-[ ! -z $nbest ] && [[ ! $nbest =~ ^[0-9]+$ ]] && echo "$0: nbest should be a positive integer." && exit 1
-[ ! -z $pmass ] && ! { [[ $pmass =~ ^[0-9]+\.?[0-9]*$ ]] && [ $(bc <<< "$pmass >= 0") -eq 1 -a $(bc <<< "$pmass <= 1") -eq 1 ]; } \
-  && echo "$0: pmass should be within [0, 1]." && exit 1
-[ -z $pmass ] && [ -z $nbest ] && nbest=1
+[ ! -z $nbest ] && [[ ! $nbest =~ ^[1-9][0-9]*$ ]] && echo "$0: nbest should be a positive integer." && exit 1;
+[ -z $pmass ] && [ -z $nbest ] && echo "$0: nbest or/and pmass should be specified." && exit 1;
 
 if [ -z $pmass ]; then
   echo "Synthesizing pronunciations for words in $word_list based on nbest=$nbest"
@@ -57,6 +57,9 @@ else
 fi
 phonetisaurus-apply $options --model $model --thresh 5 --accumulate --verbose --prob --word_list $word_list 1>$out_lexicon
 
-echo "Finished. Synthesized lexicon for new words is in $out_lexicon"
+if [ $(tr -d [:space:] < $out_lexicon | wc -c) -eq 0 ]; then
+  echo "$0: Did not generate the lexicon $out_lexicon for new words succesfully, which has no content." && exit 1;
+else
+  echo "$0: Successful. The synthesized lexicon for new words is in $out_lexicon" && exit 0;
+fi
 
-exit 0
