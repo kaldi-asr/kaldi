ea64aca627994e986a085ed1c08c5149bf0cd34c
puneetbawa <bawapuneet994@gmail.com>
2018-10-15 12:14:46 +0530

    Update ami_download.sh

--- egs/ami/s5/local/ami_download.sh
+++ egs/ami/s5/local/ami_download.sh
@@ -54,11 +54,11 @@ wgetfile=$wdir/wget_$mic.sh
 
 # TODO fix this with Pawel, files don't exist anymore,
 manifest="wget --continue -O $adir/MANIFEST.TXT http://groups.inf.ed.ac.uk/ami/download/temp/amiBuild-0153-Tue-Oct-2-2018.manifest.txt"
-# license="wget --continue -O $adir/LICENCE.TXT http://groups.inf.ed.ac.uk/ami/download/temp/Creative-Commons-Attribution-NonCommercial-ShareAlike-2.5.txt"
+
 
 echo "#!/bin/bash" > $wgetfile
 echo $manifest >> $wgetfile
-echo $license >> $wgetfile
+
 while read line; do
    if [ "$mic" == "ihm" ]; then
      extra_headset= #some meetings have 5 sepakers (headsets)
@@ -100,7 +100,7 @@ else
   fi
 fi
 
-echo "Downloads of AMI corpus completed succesfully. License can be found under $adir/LICENCE.TXT"
+echo "Downloads of AMI corpus completed succesfully."
 exit 0;
 
 
