8ab15786aa0fc63380b9bd80c9003753de41c0fd
Brett Tiplitz <btiplitz.code@gmail.com>
2018-11-01 11:08:34 -0400

    Incorporate comments

--- src/cudamatrix/cu-allocator.h
+++ src/cudamatrix/cu-allocator.h
@@ -338,8 +338,8 @@ class CuMemoryAllocator {
 
   // Keep track of the memory usage from the cache to track the maximum memory used by
   //   the application
-  int max_allocated_memory_;
-  int allocated_memory_;
+  size_t max_allocated_memory_;
+  size_t allocated_memory_;
 };
 
 
