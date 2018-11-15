10d4713d03b0761b808250f03934989826a7cbcd
Aswin Shanmugam Subramanian <aswinphoenix@gmail.com>
2018-11-14 19:04:32 -0500

    Enabled SE computation by default and added flag to enable PESQ

--- egs/reverb/s5/local/compute_se_scores.sh
+++ egs/reverb/s5/local/compute_se_scores.sh
@@ -13,6 +13,7 @@ set -o pipefail
 
 cmd=run.pl
 nch=8
+enable_pesq=false
 
 . utils/parse_options.sh || exit 1;
 
@@ -22,6 +23,7 @@ if [ $# != 3 ]; then
    echo "options"
    echo "  --cmd <cmd>                              # Command to run in parallel with"
    echo "  --nch <nch>                              # nch of WPE to use for computing SE scores"
+   echo "  --enable_pesq <enable_pesq>              # Boolean flag to enable PESQ"
    exit 1;
 fi
 
@@ -30,10 +32,16 @@ enhancement_directory=$2
 pesqdir=$3
 enhancement_directory_sim=$enhancement_directory/WPE/${nch}ch/REVERB_WSJCAM0_dt/data/
 enhancement_directory_real=$enhancement_directory/WPE/${nch}ch/MC_WSJ_AV_Dev/
-
 expdir=${PWD}/exp/compute_se_${nch}ch
+if $enable_pesq; then
+   compute_pesq=1
+else
+   compute_pesq=0
+fi
+
 pushd local/REVERB_scores_source/REVERB-SPEENHA.Release04Oct/evaltools
 $cmd $expdir/compute_se_real.log matlab -nodisplay -nosplash -r "addpath('SRMRToolbox'); score_RealData('$reverb_data','$enhancement_directory_real');exit"
-$cmd $expdir/compute_se_sim.log matlab -nodisplay -nosplash -r "addpath('SRMRToolbox'); score_SimData('$reverb_data','$enhancement_directory_sim','$pesqdir');exit"
+$cmd $expdir/compute_se_sim.log matlab -nodisplay -nosplash -r "addpath('SRMRToolbox'); score_SimData('$reverb_data','$enhancement_directory_sim','$pesqdir',$compute_pesq);exit"
 popd
+rm -rf $expdir/scores
 mv local/REVERB_scores_source/REVERB-SPEENHA.Release04Oct/scores $expdir/
