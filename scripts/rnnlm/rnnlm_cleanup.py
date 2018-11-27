afe09ae0120a3be5d1526cf19d0d48ade918bb4a
Andris Vāravs <andris.varavs@Tilde.lv>
2018-11-27 11:48:07 +0200

    working version of both keep_latest and keep_best

--- scripts/rnnlm/rnnlm_cleanup.py
+++ scripts/rnnlm/rnnlm_cleanup.py
@@ -3,6 +3,7 @@
 import argparse
 import sys
 import os
+import re
 
 script_name = sys.argv[0]
 
@@ -15,9 +16,9 @@ parser = argparse.ArgumentParser(description="Removes models from past training 
 
 parser.add_argument("rnnlm_dir",
                     help="Directory where the RNNLM has been trained")
-parser.add_argument("last_iteration",
-                    help="Number of the last iteration",
-                    type=int)
+# parser.add_argument("last_iteration",
+#                     help="Number of the last iteration",
+#                     type=int)
 parser.add_argument("--iters_to_keep",
                     help="Max number of iterations to keep",
                     type=int,
@@ -40,12 +41,6 @@ if args.keep_latest and args.keep_best:
     sys.exit(script_name + ": can only use either 'keep_latest' or 'keep_best', but not both")
 elif not args.keep_latest and not args.keep_best:
     sys.exit(script_name + ": no cleanup strategy specified: use 'keep_latest' or 'keep_best'")
-
-# TODO now for some actual logic............
-# check exp dir for model files
-# list all files there, look for word_embedding.%d.mat and %d.raw files
-# if keep_best, check compute_prob logs for best eval scores or adapt/use get_best_model.py
-# if keep_latest, find the latest iteration that is not used or rely on last_iteration arg?
 
 
 def get_iteration_files(exp_dir):
@@ -82,11 +77,47 @@ def keep_latest(iteration_dict):
             os.remove(iter_files[1])
 
 
-# TODO just testing
+def keep_best(iteration_dict, exp_dir):
+    iters_to_keep = args.iters_to_keep
+    best = []
+    for iter, iter_files in iteration_dict.items():
+        # this is roughly taken from get_best_model.py
+        logfile = "{0}/log/compute_prob.{1}.log".format(exp_dir, iter)
+        try:
+            f = open(logfile, "r", encoding="latin-1")
+        except:
+            sys.exit(script_name + ": could not open log-file " + logfile)
+        objf = -2000
+        for line in f:
+            m = re.search('Overall objf .* (\S+)$', str(line))
+            if m is not None:
+                try:
+                    objf = float(m.group(1))
+                except Exception as e:
+                    sys.exit(script_name + ": line in file {0} could not be parsed: {1}, error is: {2}".format(
+                        logfile, line, str(e)))
+        if objf == -2000:
+            print(script_name + ": warning: could not parse objective function from " + logfile, file=sys.stderr)
+            continue
+        # add potential best, sort by objf, trim to iters_to_keep size
+        best.append((iter, objf))
+        best = sorted(best, key=lambda x: -x[1])
+        if len(best) > iters_to_keep:
+            throwaway = best[iters_to_keep:]
+            best = best[:iters_to_keep]
+            # remove iters that we know are not the best
+            for (iter, _) in throwaway:
+                iter_files = iteration_dict[iter]
+                os.remove(iter_files[0])
+                os.remove(iter_files[1])
+
+
+# grab all the iterations mapped to their word_embedding and .raw files
 iterations = get_iteration_files(args.rnnlm_dir)
 print(iterations)
-keep_latest(iterations)
+# apply chosen cleanup strategy
+if args.keep_latest:
+    keep_latest(iterations)
+else:
+    keep_best(iterations, args.rnnlm_dir)
 print(get_iteration_files(args.rnnlm_dir))
-
-
-# TODO implement rest of the bookkeeping
