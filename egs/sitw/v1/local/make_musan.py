kaldi-asr/kaldi

commit 5a720accdfafc28677c379cf7aac4683fef3f68d

[scripts,egs] Changes for Python 2/3 compatibility (#2925)

egs/sitw/v1/local/make_musan.py
@@ -47,9 +47,9 @@ def prepare_music(root_dir, use_vocals):
         utt2wav_str = utt2wav_str + utt + " " + utt2wav[utt] + "\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file {}".format(utt))
       num_bad_files += 1
-  print("In music directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print(("In music directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def prepare_speech(root_dir):
@@ -73,9 +73,9 @@ def prepare_speech(root_dir):
       utt2wav_str = utt2wav_str + utt + " " + utt2wav[utt] + "\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file {}".format(utt))
       num_bad_files += 1
-  print("In speech directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print(("In speech directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def prepare_noise(root_dir):
@@ -99,9 +99,9 @@ def prepare_noise(root_dir):
       utt2wav_str = utt2wav_str + utt + " " + utt2wav[utt] + "\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file {}".format(utt))
       num_bad_files += 1
-  print("In noise directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print(("In noise directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def main():

egs/bn_music_speech/v1/local/make_musan.py
@@ -43,9 +43,9 @@ def prepare_music(root_dir, use_vocals):
         utt2wav_str = utt2wav_str + utt + " " + utt2wav[utt] + "\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file {}".format(utt))
       num_bad_files += 1
-  print("In music directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print(("In music directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def prepare_speech(root_dir):
@@ -69,9 +69,9 @@ def prepare_speech(root_dir):
       utt2wav_str = utt2wav_str + utt + " " + utt2wav[utt] + "\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file {}".format(utt))
       num_bad_files += 1
-  print("In speech directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print(("In speech directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def prepare_noise(root_dir):
@@ -95,9 +95,9 @@ def prepare_noise(root_dir):
       utt2wav_str = utt2wav_str + utt + " " + utt2wav[utt] + "\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file {}".format(utt))
       num_bad_files += 1
-  print("In noise directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print(("In noise directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def main():

egs/callhome_diarization/v1/local/make_musan.py
@@ -43,9 +43,9 @@ def prepare_music(root_dir, use_vocals):
         utt2wav_str = utt2wav_str + utt + " sox -t wav " + utt2wav[utt] + " -r 8k -t wav - |\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file: {}".format(utt))
       num_bad_files += 1
-  print("In music directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print("In music directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files)
   return utt2spk_str, utt2wav_str
 
 def prepare_speech(root_dir):
@@ -69,9 +69,9 @@ def prepare_speech(root_dir):
       utt2wav_str = utt2wav_str + utt + " sox -t wav " + utt2wav[utt] + " -r 8k -t wav - |\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file: {}".format(utt))
       num_bad_files += 1
-  print("In speech directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print("In speech directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files)
   return utt2spk_str, utt2wav_str
 
 def prepare_noise(root_dir):
@@ -95,9 +95,9 @@ def prepare_noise(root_dir):
       utt2wav_str = utt2wav_str + utt + " sox -t wav " + utt2wav[utt] + " -r 8k -t wav - |\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file: {}".format(utt))
       num_bad_files += 1
-  print("In noise directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print("In noise directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files)
   return utt2spk_str, utt2wav_str
 
 def main():

egs/dihard_2018/v2/local/make_musan.py
@@ -47,9 +47,9 @@ def prepare_music(root_dir, use_vocals):
         utt2wav_str = utt2wav_str + utt + " " + utt2wav[utt] + "\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file {}".format(utt))
       num_bad_files += 1
-  print("In music directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print(("In music directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def prepare_speech(root_dir):
@@ -73,9 +73,9 @@ def prepare_speech(root_dir):
       utt2wav_str = utt2wav_str + utt + " " + utt2wav[utt] + "\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file {}".format(utt))
       num_bad_files += 1
-  print("In speech directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print(("In speech directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def prepare_noise(root_dir):
@@ -99,9 +99,9 @@ def prepare_noise(root_dir):
       utt2wav_str = utt2wav_str + utt + " " + utt2wav[utt] + "\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file {}".format(utt))
       num_bad_files += 1
-  print("In noise directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print(("In noise directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def main():

egs/sre16/v1/local/make_musan.py
@@ -43,9 +43,9 @@ def prepare_music(root_dir, use_vocals):
         utt2wav_str = utt2wav_str + utt + " sox -t wav " + utt2wav[utt] + " -r 8k -t wav - |\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file {}".format(utt))
       num_bad_files += 1
-  print("In music directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print("In music directory, processed {} files; {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def prepare_speech(root_dir):
@@ -69,9 +69,9 @@ def prepare_speech(root_dir):
       utt2wav_str = utt2wav_str + utt + " sox -t wav " + utt2wav[utt] + " -r 8k -t wav - |\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file {}".format(utt))
       num_bad_files += 1
-  print("In speech directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print("In speech directory, processed {} files; {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def prepare_noise(root_dir):
@@ -95,9 +95,9 @@ def prepare_noise(root_dir):
       utt2wav_str = utt2wav_str + utt + " sox -t wav " + utt2wav[utt] + " -r 8k -t wav - |\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file {}".format(utt))
       num_bad_files += 1
-  print("In noise directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print("In noise directory, processed {} files; {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def main():

egs/voxceleb/v1/local/make_musan.py
@@ -47,9 +47,9 @@ def prepare_music(root_dir, use_vocals):
         utt2wav_str = utt2wav_str + utt + " " + utt2wav[utt] + "\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file {}".format(utt))
       num_bad_files += 1
-  print("In music directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print("In music directory, processed {} files; {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def prepare_speech(root_dir):
@@ -73,9 +73,9 @@ def prepare_speech(root_dir):
       utt2wav_str = utt2wav_str + utt + " " + utt2wav[utt] + "\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file {}".format(utt))
       num_bad_files += 1
-  print("In speech directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print("In speech directory, processed {} files; {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def prepare_noise(root_dir):
@@ -99,9 +99,9 @@ def prepare_noise(root_dir):
       utt2wav_str = utt2wav_str + utt + " " + utt2wav[utt] + "\n"
       num_good_files += 1
     else:
-      print("Missing file", utt)
+      print("Missing file {}".format(utt))
       num_bad_files += 1
-  print("In noise directory, processed", num_good_files, "files;", num_bad_files, "had missing wav data")
+  print("In noise directory, processed {} files; {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def main():

commit 00cc52538f67804ecccf78826a3418c8ffb501b6

[egs] fixing more syntax errors introduced in PR #2925

egs/sitw/v1/local/make_musan.py
@@ -49,7 +49,7 @@ def prepare_music(root_dir, use_vocals):
     else:
       print("Missing file {}".format(utt))
       num_bad_files += 1
-  print(("In music directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files))
+  print("In music directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def prepare_speech(root_dir):
@@ -75,7 +75,7 @@ def prepare_speech(root_dir):
     else:
       print("Missing file {}".format(utt))
       num_bad_files += 1
-  print(("In speech directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files))
+  print("In speech directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def prepare_noise(root_dir):
@@ -101,7 +101,7 @@ def prepare_noise(root_dir):
     else:
       print("Missing file {}".format(utt))
       num_bad_files += 1
-  print(("In noise directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files))
+  print("In noise directory, processed {} files: {} had missing wav data".format(num_good_files, num_bad_files))
   return utt2spk_str, utt2wav_str
 
 def main():