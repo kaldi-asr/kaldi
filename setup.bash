8cd0c6e2f5ad7125454c139dc281c08508e9a26f
Arpit Aggarwal <ar13pit@gmail.com>
2019-02-02 14:27:20 +0100

    added gst-plugin to GST_PLUGIN_PATH

--- setup.bash
+++ setup.bash
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 #
-# Environment variables to use LASeR
+# Environment variables to use by speech_recognition repo
 
 export KALDI_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export GST_PLUGIN_PATH=$KALDI_ROOT/src/gst-plugin${GST_PLUGIN_PATH:+:${GST_PLUGIN_PATH}}
