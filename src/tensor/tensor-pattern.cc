ff56d26e136c8ed18f252ff54d8d99179cef9b04
Daniel Povey <dpovey@gmail.com>
2019-03-17 19:42:25 -0400

    [src] Add source file

---
+++ src/tensor/tensor-pattern.cc
@@ -0,0 +1,18 @@
+#include "tensor/tensor-pattern.h"
+
+/**
+   This is some notes on plans for kaldi10 tensor stuff, nothing is fully fleshed out.
+*/
+
+namespace kaldi {
+namespace tensor {
+
+
+
+
+}
+
+
+
+}
+}
