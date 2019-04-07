84b785b0b6c84ae2a803c3601e17039dfe351356
Arpit Aggarwal <ar13pit@gmail.com>
2019-04-07 12:33:31 +0200

    added kaldi tools to PATH

--- setup.bash
+++ setup.bash
@@ -4,3 +4,17 @@
 
 export KALDI_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export GST_PLUGIN_PATH=$KALDI_ROOT/src/gst-plugin${GST_PLUGIN_PATH:+:${GST_PLUGIN_PATH}}
+
+[ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
+
+export PATH=$KALDI_ROOT/egs/wsj/s5/utils/:$KALDI_ROOT/tools/openfst/bin:$PATH
+
+[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
+. $KALDI_ROOT/tools/config/common_path.sh
+
+# Make sure that MITLM shared libs are found by the dynamic linker/loader
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/tools/mitlm-svn/lib
+
+# Needed for "correct" sorting
+export LC_ALL=C
+
