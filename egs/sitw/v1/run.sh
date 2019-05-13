feb4ebb63656e62530647b69ca025bf398aec455
phanisankar-nidadavolu <phanisankar.nsv@gmail.com>
2019-05-13 19:20:54 -0400

    updated local/musan.sh to steps/data/make_musan.sh in speaker id scripts

--- egs/sitw/v1/run.sh
+++ egs/sitw/v1/run.sh
@@ -137,7 +137,7 @@ if [ $stage -le 4 ]; then
 
   # Prepare the MUSAN corpus, which consists of music, speech, and noise
   # suitable for augmentation.
-  local/make_musan.sh $musan_root data
+  steps/data/make_musan.sh --sampling-rate 16000 $musan_root data
 
   # Get the duration of the MUSAN recordings.  This will be used by the
   # script augment_data_dir.py.
