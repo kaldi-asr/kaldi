5cc7ce063aaa96800c38c25c6b9fa8bf455578c9
LvHang <hanglv@nwpu-aslp.org>
2019-06-25 23:55:43 -0400

    [src] Update Insert function of hashlist and decoders (#3402)
    makes interface of HashList more standard; slight speed improvement.

--- src/decoder/faster-decoder.cc
+++ src/decoder/faster-decoder.cc
@@ -277,18 +277,14 @@ double FasterDecoder::ProcessEmitting(DecodableInterface *decodable) {
           double new_weight = arc.weight.Value() + tok->cost_ + ac_cost;
           if (new_weight < next_weight_cutoff) {  // not pruned..
             Token *new_tok = new Token(arc, ac_cost, tok);
-            Elem *e_found = toks_.Find(arc.nextstate);
+            Elem *e_found = toks_.Insert(arc.nextstate, new_tok);
             if (new_weight + adaptive_beam < next_weight_cutoff)
               next_weight_cutoff = new_weight + adaptive_beam;
-            if (e_found == NULL) {
-              toks_.Insert(arc.nextstate, new_tok);
+            if ( *(e_found->val) < *new_tok ) {
+              Token::TokenDelete(e_found->val);
+              e_found->val = new_tok;
             } else {
-              if ( *(e_found->val) < *new_tok ) {
-                Token::TokenDelete(e_found->val);
-                e_found->val = new_tok;
-              } else {
-                Token::TokenDelete(new_tok);
-              }
+              Token::TokenDelete(new_tok);
             }
           }
         }
@@ -307,11 +303,12 @@ void FasterDecoder::ProcessNonemitting(double cutoff) {
   // Processes nonemitting arcs for one frame. 
   KALDI_ASSERT(queue_.empty());
   for (const Elem *e = toks_.GetList(); e != NULL;  e = e->tail)
-    queue_.push_back(e->key);
+    queue_.push_back(e);
   while (!queue_.empty()) {
-    StateId state = queue_.back();
+    const Elem* e = queue_.back();
     queue_.pop_back();
-    Token *tok = toks_.Find(state)->val;  // would segfault if state not
+    StateId state = e->key;
+    Token *tok = e->val;  // would segfault if state not
     // in toks_ but this can't happen.
     if (tok->cost_ > cutoff) { // Don't bother processing successors.
       continue;
@@ -326,15 +323,14 @@ void FasterDecoder::ProcessNonemitting(double cutoff) {
         if (new_tok->cost_ > cutoff) {  // prune
           Token::TokenDelete(new_tok);
         } else {
-          Elem *e_found = toks_.Find(arc.nextstate);
-          if (e_found == NULL) {
-            toks_.Insert(arc.nextstate, new_tok);
-            queue_.push_back(arc.nextstate);
+          Elem *e_found = toks_.Insert(arc.nextstate, new_tok);
+          if (e_found->val == new_tok) {
+            queue_.push_back(e_found);
           } else {
             if ( *(e_found->val) < *new_tok ) {
               Token::TokenDelete(e_found->val);
               e_found->val = new_tok;
-              queue_.push_back(arc.nextstate);
+              queue_.push_back(e_found);
             } else {
               Token::TokenDelete(new_tok);
             }
