05a50490fb76da126256a8ec9aab059a63a33db9
Srikanth MADIKERI <srikanth.madikeri@idiap.ch>
2019-06-27 10:51:53 +0200

    egs/wsj5/steps/nnet3/chain/get_model_context.sh: removed bottom nnet related variables

--- egs/wsj/s5/steps/nnet3/chain/get_model_context.sh
+++ egs/wsj/s5/steps/nnet3/chain/get_model_context.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Copyright   2019  Johns Hopkins University (Author: Daniel Povey).  Apache 2.0.
+#             2019  Idiap Research Institute (Author: Srikanth Madikeri)
 #
 # This script computes the total left and right context needed for example (eg)
-# creation from a set of 'chaina' models.
+# creation from a set of 'chain' models.
 # See the usage message for more information about input and output formats.
 
 # Begin configuration section.
@@ -12,10 +13,6 @@ frame_subsampling_factor=1   # The total frame subsampling factor of the bottom
                              # frame rate between the input of the bottom model
                              # and the output of the top model.  Would normally
                              # be 3.
-bottom_subsampling_factor=1  # The frame subsampling factor of the bottom
-                             # (feature-extracting) model only.  Must be a
-                             # divisor of frame_subsampling_factor.  Would
-                             # normally be 1 or 3.
 
 langs=default                # the list of languages.  This script checks that
                              # in the dir (first arg to the script), each
@@ -42,7 +39,7 @@ frame_subsampling_factor 3
 bottom_subsampling_factor 3
 model_left_context 22
 model_right_context 22
-  e.g.: $0 --frame-subsampling-factor 3 --bottom-subsampling-factor 3
+  e.g.: $0 --frame-subsampling-factor 3 
           --langs 'default' exp/chaina/tdnn1a_sp/0 exp/chaina/tdnn1a_sp/0/info.txt
  Options:
      --frame-subsampling-factor    # (default: 1)  Total frame subsampling factor of
@@ -78,27 +75,12 @@ if [ -z $langs ]; then
   exit 1
 fi
 
-if  ! [ $frame_subsampling_factor -ge 1 ] || \
-    ! [ $bottom_subsampling_factor -ge 1 ] || \
-    ! [ $[frame_subsampling_factor%bottom_subsampling_factor] -eq 0 ]; then
-  echo 1>&2 "$0: there was a problem with the options --frame-subsampling-factor=$frame_subsampling_factor --bottom-subsampling-factor=$bottom_subsampling_factor"
+if  ! [ $frame_subsampling_factor -ge 1 ]; then
+  echo 1>&2 "$0: there was a problem with the options --frame-subsampling-factor=$frame_subsampling_factor"
   exit 1
 fi
 
 mkdir -p $dir/temp
-
-if [ ! -s $dir/bottom.raw ]; then
-  echo 1>&2 "$0: expected file $dir/bottom.raw to exist and be nonempty"
-  exit 1
-fi
-
-nnet3-info $dir/bottom.raw > $dir/temp/bottom.info
-bottom_left_context=$(grep '^left-context:' $dir/temp/bottom.info | awk '{print $2}')
-bottom_right_context=$(grep '^right-context:' $dir/temp/bottom.info | awk '{print $2}')
-
-max_top_left_context=0
-max_top_right_context=0
-
 
 for lang in $langs; do
   if [ ! -s $dir/$lang.mdl ]; then
@@ -108,25 +90,18 @@ for lang in $langs; do
   nnet3-am-info $dir/$lang.mdl > $dir/temp/$lang.info
   this_left_context=$(grep '^left-context:' $dir/temp/$lang.info | awk '{print $2}')
   this_right_context=$(grep '^right-context:' $dir/temp/$lang.info | awk '{print $2}')
-  if [ $this_left_context -gt $max_top_left_context ]; then
-    max_top_left_context=$this_left_context
-  fi
-  if [ $this_right_context -gt $max_top_right_context ]; then
-    max_top_right_context=$this_right_context
-  fi
 done
 
-left_context=$[bottom_left_context+(max_top_left_context*bottom_subsampling_factor)]
-right_context=$[bottom_right_context+(max_top_right_context*bottom_subsampling_factor)]
+left_context=$this_left_context
+right_context=$this_right_context
 
 
 cat >$info_file <<EOF
 frame_subsampling_factor $frame_subsampling_factor
-bottom_subsampling_factor $bottom_subsampling_factor
 langs $langs
 model_left_context $left_context
 model_right_context $right_context
 EOF
 
 
-echo "$0: Finished randomizing egs"
+echo "$0: Finished getting model context"
