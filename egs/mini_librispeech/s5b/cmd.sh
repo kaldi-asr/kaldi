4060fa4316ed50e8bd6a30b196cc63efc1ec8ee4
Srikanth MADIKERI <srikanth.madikeri@idiap.ch>
2019-07-26 14:45:26 +0200

    Idiap related changes to files in egs/mini_librispeech/s5b: cmd.sh path.sh ../../wsj/s5/steps/diagnostic/analyze_alignments.sh ../../wsj/s5/steps/diagnostic/analyze_lats.sh ../../wsj/s5/steps/nnet3/chain/get_egs.sh
    NOTE: don't merge these changes to master

--- egs/mini_librispeech/s5b/cmd.sh
+++ egs/mini_librispeech/s5b/cmd.sh
@@ -10,6 +10,7 @@
 # conf/queue.conf in http://kaldi-asr.org/doc/queue.html for more information,
 # or search for the string 'default_config' in utils/queue.pl or utils/slurm.pl.
 
-export train_cmd="queue.pl --mem 2G"
-export decode_cmd="queue.pl --mem 4G"
-export mkgraph_cmd="queue.pl --mem 8G"
+export train_cmd="queue.pl -l q1d -l io_big -V"
+export decode_cmd="queue.pl -l q1d -l io_big -V"
+export mkgraph_cmd="queue.pl -l q1d -l io_big -V"
+export cuda_cmd="queue.pl -l q_gpu -V"
