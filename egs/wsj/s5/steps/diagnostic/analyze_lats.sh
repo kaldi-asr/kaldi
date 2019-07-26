147ec841114bc3da54f98f7e72a9d3b25fa0f1e8
Srikanth MADIKERI <srikanth.madikeri@idiap.ch>
2019-07-26 14:45:26 +0200

    Idiap related changes to files in egs/mini_librispeech/s5b: cmd.sh path.sh ../../wsj/s5/steps/diagnostic/analyze_alignments.sh ../../wsj/s5/steps/diagnostic/analyze_lats.sh ../../wsj/s5/steps/nnet3/chain/get_egs.sh
    NOTE: don't merge these changes to master

--- egs/wsj/s5/steps/diagnostic/analyze_lats.sh
+++ egs/wsj/s5/steps/diagnostic/analyze_lats.sh
@@ -14,7 +14,7 @@ acwt=0.1
 
 echo "$0 $@"  # Print the command line for logging
 
-[ -f ./path.sh ] && . ./path.sh
+#[ -f ./path.sh ] && . ./path.sh
 . parse_options.sh || exit 1;
 
 if [ $# -ne 2 ]; then
@@ -32,6 +32,7 @@ lang=$1
 dir=$2
 
 model=$dir/../${iter}.mdl
+exit 0
 
 for f in $lang/words.txt $model $dir/lat.1.gz $dir/num_jobs; do
   [ ! -f $f ] && echo "$0: expecting file $f to exist" && exit 1;
