kaldi-asr/kaldi

commit 317cad43b46c61ba698d0314afdc41dae991d7b3

[src] Add option to convert pdf-posteriors to phones (#3526)

src/bin/post-to-phone-post.cc
@@ -1,6 +1,7 @@
 // bin/post-to-phone-post.cc
 
 // Copyright 2012-2013  Johns Hopkins University (author: Daniel Povey)
+//                2019  Daniel Povey
 
 // See ../../COPYING for clarification regarding multiple authors
 //
@@ -26,24 +27,36 @@
 int main(int argc, char *argv[]) {
   try {
     using namespace kaldi;
-    typedef kaldi::int32 int32;  
+    typedef kaldi::int32 int32;
 
     const char *usage =
-        "Convert posteriors to phone-level posteriors\n"
+        "Convert posteriors (or pdf-level posteriors) to phone-level posteriors\n"
         "See also: post-to-pdf-post, post-to-weights, get-post-on-ali\n"
         "\n"
+        "First, the usage when your posteriors are on transition-ids (the normal case):\n"
         "Usage: post-to-phone-post [options] <model> <post-rspecifier> <phone-post-wspecifier>\n"
-        " e.g.: post-to-phone-post --binary=false 1.mdl \"ark:ali-to-post 1.ali|\" ark,t:-\n";
-    
+        " e.g.: post-to-phone-post --binary=false 1.mdl \"ark:ali-to-post 1.ali|\" ark,t:-\n"
+        "\n"
+        "Next, the usage when your posteriors are on pdfs (e.g. if they are neural-net\n"
+        "posteriors)\n"
+        "post-to-phone-post --transition-id-counts=final.tacc 1.mdl ark:pdf_post.ark ark,t:-\n"
+        "See documentation of --transition-id-counts option for more details.";
+
+    std::string tacc_rxfilename;
+
     ParseOptions po(usage);
 
     po.Read(argc, argv);
+
+    po.Register("transition-id-counts", &tacc_rxfilename, "Rxfilename where vector of counts\n"
+                "for transition-ids can be read (would normally come from training data\n"
+                "alignments, e.g. from ali-to-post and then post-to-tacc with --per-pdf=false\n");
 
     if (po.NumArgs() != 3) {
       po.PrintUsage();
       exit(1);
     }
-      
+
     std::string model_rxfilename = po.GetArg(1),
         post_rspecifier = po.GetArg(2),
         phone_post_wspecifier = po.GetArg(3);
@@ -51,20 +64,87 @@ int main(int argc, char *argv[]) {
     kaldi::SequentialPosteriorReader posterior_reader(post_rspecifier);
     kaldi::PosteriorWriter posterior_writer(phone_post_wspecifier);
 
-    TransitionModel trans_model;    
+    TransitionModel trans_model;
     {
       bool binary_in;
       Input ki(model_rxfilename, &binary_in);
       trans_model.Read(ki.Stream(), binary_in);
     }
-    int32 num_done = 0;      
-    
-    for (; !posterior_reader.Done(); posterior_reader.Next()) {
-      const kaldi::Posterior &posterior = posterior_reader.Value();
-      kaldi::Posterior phone_posterior;
-      ConvertPosteriorToPhones(trans_model, posterior, &phone_posterior);
-      posterior_writer.Write(posterior_reader.Key(), phone_posterior);
-      num_done++;
+    int32 num_done = 0;
+
+
+    if (tacc_rxfilename.empty()) {
+      // Input is transition-ids
+      for (; !posterior_reader.Done(); posterior_reader.Next()) {
+        const kaldi::Posterior &posterior = posterior_reader.Value();
+        kaldi::Posterior phone_posterior;
+        ConvertPosteriorToPhones(trans_model, posterior, &phone_posterior);
+        posterior_writer.Write(posterior_reader.Key(), phone_posterior);
+        num_done++;
+      }
+    } else {
+      Vector<BaseFloat> transition_counts;
+      ReadKaldiObject(tacc_rxfilename, &transition_counts);
+      int32 num_pdfs = trans_model.NumPdfs(),
+          num_tids = trans_model.NumTransitionIds();
+      if (transition_counts.Dim() != num_tids + 1) {
+        KALDI_ERR << "Wrong size for transition counts in " << tacc_rxfilename
+                  << ", expected " << num_tids << " + 1, got "
+                  << transition_counts.Dim();
+      }
+      // Maps from pdf-id to a map from phone -> count associated with that
+      // phone.
+      std::vector<std::unordered_map<int32, BaseFloat> > pdf_to_phones(num_pdfs);
+
+      for (int32 i = 1; i <= num_tids; i++) {
+        BaseFloat count = transition_counts(i);
+        int32 phone = trans_model.TransitionIdToPhone(i),
+            pdf_id = trans_model.TransitionIdToPdf(i);
+        // Relying on C++11 value-initialization thingies that should make the
+        // map's elements default to zero.
+        pdf_to_phones[pdf_id][phone] += count;
+      }
+
+      for (int32 i = 0; i < num_pdfs; i++) {
+        BaseFloat denominator = 0.0;
+        for (auto p: pdf_to_phones[i])
+          denominator += p.second;
+        for (auto q: pdf_to_phones[i]) {
+          if (denominator != 0.0)
+            q.second /= denominator;
+          else
+            q.second = 1.0 / pdf_to_phones[i].size();
+        }
+      }
+
+      // Input is transition-ids
+      for (; !posterior_reader.Done(); posterior_reader.Next()) {
+        const kaldi::Posterior &posterior = posterior_reader.Value();
+        int32 T = posterior.size();
+        kaldi::Posterior phone_posterior(T);
+        std::unordered_map<int32, BaseFloat> phone_to_count;
+        for (int32 t = 0; t < T; t++) {
+          phone_to_count.clear();
+          for (auto p : phone_posterior[t]) {
+            int32 pdf_id = p.first;
+            BaseFloat count = p.second;
+            if (pdf_id < 0 || pdf_id >= num_pdfs)
+              KALDI_ERR << "pdf-id on input out of range, expected [0.." << (num_pdfs-1)
+                        << ", got: " << pdf_id;
+            for (auto q: pdf_to_phones[pdf_id]) {
+              int32 phone = q.first;
+              BaseFloat prob = q.second;
+              phone_to_count[phone] += count * prob;
+            }
+          }
+          for (auto p : phone_to_count) {
+            phone_posterior[t].push_back(
+                std::pair<int32, BaseFloat>(p.first, p.second));
+          }
+        }
+        posterior_writer.Write(posterior_reader.Key(), phone_posterior);
+        num_done++;
+      }
     }
     KALDI_LOG << "Done converting posteriors to phone posteriors for "
               << num_done << " utterances.";

commit 0b7c9a95f0c5d6b42b91e5227460d20c8f5e70fd

[src] Small fix to post-to-phone-post.cc (problem introduced in from #3526) (#3534)

src/bin/post-to-phone-post.cc
@@ -46,11 +46,11 @@ int main(int argc, char *argv[]) {
 
     ParseOptions po(usage);
 
-    po.Read(argc, argv);
-
     po.Register("transition-id-counts", &tacc_rxfilename, "Rxfilename where vector of counts\n"
                 "for transition-ids can be read (would normally come from training data\n"
                 "alignments, e.g. from ali-to-post and then post-to-tacc with --per-pdf=false\n");
+
+    po.Read(argc, argv);
 
     if (po.NumArgs() != 3) {
       po.PrintUsage();