845eba6e6dc5353f7f421bb100e097cfb6c8292c
Cloud Han <cloudhan@outlook.com>
2019-10-21 23:49:02 +0800

    handle version.h with cmake

--- cmake/VersionHelper.cmake
+++ cmake/VersionHelper.cmake
@@ -1,8 +1,14 @@
 function(get_version)
+    file(READ ${CMAKE_CURRENT_SOURCE_DIR}/src/.version version)
+    string(STRIP ${version} version)
+    execute_process(COMMAND git log -n1 --format=%H src/.version
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                    OUTPUT_VARIABLE version_commit
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+    execute_process(COMMAND git rev-list --count "${version_commit}..HEAD"
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                    OUTPUT_VARIABLE patch_number)
 
-execute_process(COMMAND git --format='%H' -n1 OUTPUT_VARIABLE version_commit)
-execute_process(COMMAND git rev-list ${version_commit}..HEAD OUTPUT_VARIABLE patch_number)
-
-set(version "??")
-
+    set(KALDI_VERSION ${version} PARENT_SCOPE)
+    set(KALDI_PATCH_NUMBER ${patch_number} PARENT_SCOPE)
 endfunction()
