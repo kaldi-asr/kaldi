2a7b836163be62d98e36a10a8e133d6360932ce7
Cloud Han <cloudhan@outlook.com>
2019-10-25 01:02:04 +0800

    build successfully, install failed, back it up, we need to find a better method

--- cmake/third_party/openfst.cmake
+++ cmake/third_party/openfst.cmake
@@ -7,6 +7,7 @@ ExternalProject_Add(openfst
     GIT_TAG winport
     SOURCE_DIR "${CMAKE_BINARY_DIR}/openfst"
     BINARY_DIR ""
+    PATCH_COMMAND "git" "apply" "--cached" "${PROJECT_SOURCE_DIR}/cmake/third_party/openfst.patch"
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND ""
