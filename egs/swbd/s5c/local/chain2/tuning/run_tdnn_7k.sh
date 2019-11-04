fab61067e94262d8faef0d1fe2e1d3b6238c63c8
Srikanth MADIKERI <srikanth.madikeri@idiap.ch>
2019-11-04 16:14:29 +0100

    modifying local/chain2/tuning/run_tdnn_7k.sh to be identical to local/chain2/tuning/run_tdnn_7k.sh

--- egs/swbd/s5c/local/chain2/tuning/run_tdnn_7k.sh
+++ egs/swbd/s5c/local/chain2/tuning/run_tdnn_7k.sh
@@ -38,7 +38,6 @@ common_egs_dir=exp/chain/tdnn_8k_chaina_v2_sp/egs
 xent_regularize=0.1
 srand=0
 graph_dir=
-config=
 
 test_online_decoding=false  # if true, it will run the last decoding stage.
 
@@ -48,10 +47,6 @@ echo "$0 $@"  # Print the command line for logging
 . ./cmd.sh
 . ./path.sh
 . ./utils/parse_options.sh
-
-if [ ! -z $config ]; then
-    . $config
-fi
 
 if ! cuda-compiled; then
   cat <<EOF && exit 1
@@ -311,16 +306,11 @@ if [ $stage -le 20 ]; then
   # Note: it might appear that this $lang directory is mismatched, and it is as
   # far as the 'topo' is concerned, but this script doesn't read the 'topo' from
   # the lang directory.
-  if [ -z $graph_dir -o ! -d $graph_dir ]; then
-    utils/mkgraph.sh --self-loop-scale 1.0 data/lang_sw1_tg $dir $dir/graph_sw1_tg
-    graph_dir=$dir/graph_sw1_tg
-  fi
-  if [ -z $graph_dir ]; then
-    graph_dir=$dir/graph_sw1_tg
-  fi
+  utils/mkgraph.sh --self-loop-scale 1.0 data/lang_sw1_tg $dir $dir/graph_sw1_tg
 fi
 
 
+graph_dir=$dir/graph_sw1_tg
 iter_opts=
 if [ ! -z $decode_iter ]; then
   iter_opts=" --iter $decode_iter "
