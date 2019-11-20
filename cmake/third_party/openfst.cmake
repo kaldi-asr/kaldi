f36e036d0ef8896763814060ce45341f38182c1c
Cloud Han <cloudhan@outlook.com>
2019-11-20 22:54:51 +0800

    original branch does not work on windows, use win branch, use hash

--- cmake/third_party/openfst.cmake
+++ cmake/third_party/openfst.cmake
@@ -4,7 +4,7 @@ project(openfst-download NONE)
 include(ExternalProject)
 ExternalProject_Add(openfst
     GIT_REPOSITORY https://github.com/kkm000/openfst
-    GIT_TAG win/1.7.2.1
+    GIT_TAG 0bca6e76d24647427356dc242b0adbf3b5f1a8d9 # tag win/1.7.2.1
     SOURCE_DIR "${CMAKE_BINARY_DIR}/openfst"
     BINARY_DIR ""
     CONFIGURE_COMMAND ""
