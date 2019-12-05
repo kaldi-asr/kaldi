1b625dbef40eb7d3274a8d35bb82c7005937b571
Brett Tiplitz <26090422+btiplitz@users.noreply.github.com>
2019-12-05 17:44:50 -0500

    output needs to be CuMatrix to match

--- src/nnet2/nnet-compute.h
+++ src/nnet2/nnet-compute.h
@@ -58,7 +58,7 @@ void NnetComputation(const Nnet &nnet,
 void NnetComputationChunked(const Nnet &nnet,
                      const CuMatrixBase<BaseFloat> &input,  // features
                      int32 chunk_size,
-                     Matrix<BaseFloat> *output); // posteriors.
+                     CuMatrixBase<BaseFloat> *output); // posteriors.
 
 /** Does the neural net computation and backprop, given input and labels.
     Note: if pad_input==true the number of rows of input should be the
