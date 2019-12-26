38544c5e452381b832f963198c45dbe9dbbab5f5
Fangjun Kuang <csukuangfj@gmail.com>
2019-12-26 16:01:45 +0800

    add chain loss operator for PyTorch.

--- egs/aishell/s10/chain/chain_loss.py
+++ egs/aishell/s10/chain/chain_loss.py
@@ -3,66 +3,91 @@
 # Copyright 2019 Mobvoi AI Lab, Beijing, China (author: Fangjun Kuang)
 # Apache 2.0
 
-import kaldi  # import kaldi before torch!
-import kaldi_pybind.fst as fst
+import torch
+from torch.autograd import Function
+
 import kaldi_pybind.chain as chain
 
-from dataset import read_nnet_chain_example
+g_nnet_output_deriv_tensor = None
+g_xent_output_deriv_tensor = None
 
-import torch
-from torch.utils.dlpack import to_dlpack
 
-device_id = 7
+class KaldiChainObjfFunction(Function):
 
-kaldi.SelectGpuDevice(device_id)
-device = torch.device('cuda', device_id)
+    @staticmethod
+    def forward(ctx, opts, den_graph, supervision, nnet_output_tensor,
+                xent_out_unused_tensor):
+        '''
+        opts:          Struct containing options
 
-opts = chain.ChainTrainingOptions()
-opts.l2_regularize = 5e-4
-opts.leaky_hmm_coefficient = 0.1
-opts.xent_regularize = 0
+        den_graph:     The denominator graph, derived from denominator fst.
 
-den_fst_filename = '/cache/fangjun/chain/aishell_kaldi_pybind/test/den.fst'
-num_pdfs = 4336
+        supervision:   The supervision object, containing the supervision
+                       paths and constraints on the alignment as an FST.
 
-den_fst = fst.StdVectorFst.Read(filename=den_fst_filename)
+        nnet_output_tensor:   The output of the neural net; dimension must
+                              equal ((supervision.num_sequences * supervision.frames_per_sequence)
+                              by den_graph.NumPdfs()).  The rows are ordered
+                              as: all sequences for frame 0; all sequences for
+                              frame 1; etc.
 
-den_graph = chain.DenominatorGraph(fst=den_fst, num_pdfs=num_pdfs)
+        xent_out_unused_tensor: It MUST have the same structure as `nnet_output_tensor`;
+                                it is passed but NOT used directly in this
+                                function; it is still needed because we need
+                                to do back propagation for it.
+        '''
+        global g_nnet_output_deriv_tensor
+        global g_xent_output_deriv_tensor
 
-egs_scp_rxfilename = '/cache/fangjun/chain/aishell_kaldi_pybind/cegs.1.ark:13'
-eg = read_nnet_chain_example(egs_scp_rxfilename)
-supervision = eg.outputs[0].supervision
-print('weight', supervision.weight)
-print('num_sequences', supervision.num_sequences)
-print('frames_per_sequence', supervision.frames_per_sequence)
-print('label_dim', supervision.label_dim)
+        if g_nnet_output_deriv_tensor is None or g_nnet_output_deriv_tensor.shape != nnet_output_tensor.shape:
+            # preallocate GPU memory in PyTorch and it will be shared with Kaldi
+            g_nnet_output_deriv_tensor = torch.zeros_like(nnet_output_tensor)
+            g_xent_output_deriv_tensor = torch.zeros_like(
+                nnet_output_tensor).contiguous()
+            # (fangjun): Note that g_xent_output_deriv needs to be contiguous,
+            # i.e., stride == cols; otherwise, it will be re-allocated in Kaldi
+        else:
+            # (fangjun): we have to zero them out manually!
+            g_nnet_output_deriv_tensor.zero_()
+            g_xent_output_deriv_tensor.zero_()
 
-nnet_output_tensor = torch.rand(
-    supervision.num_sequences * supervision.frames_per_sequence,
-    num_pdfs).to(device)
-nnet_output = kaldi.CuSubMatrixFromDLPack(to_dlpack(nnet_output_tensor))
+        # it contains [objf, l2_term, weight] and will be returned to the caller
+        objf_l2_term_weight_tensor = torch.zeros(3)
 
-objf_l2_term_weight_tensor = torch.zeros(3)
-objf_l2_term_weight = kaldi.SubVectorFromDLPack(
-    to_dlpack(objf_l2_term_weight_tensor))
+        nnet_output = kaldi.CuSubMatrixFromDLPack(to_dlpack(nnet_output_tensor))
 
-nnet_output_deriv_tensor = torch.zeros_like(nnet_output_tensor)
-nnet_output_deriv = kaldi.CuSubMatrixFromDLPack(
-    to_dlpack(nnet_output_deriv_tensor))
+        nnet_output_deriv = kaldi.CuSubMatrixFromDLPack(
+            to_dlpack(g_nnet_output_deriv_tensor))
 
-xent_output_deriv_tensor = torch.zeros_like(nnet_output_tensor).contiguous()
-assert xent_output_deriv_tensor.stride(0) == xent_output_deriv_tensor.shape[1]
+        xent_output_deriv = kaldi.CuSubMatrixFromDLPack(
+            to_dlpack(g_xent_output_deriv_tensor))
 
-xent_output_deriv = kaldi.CuSubMatrixFromDLPack(
-    to_dlpack(xent_output_deriv_tensor))
+        objf_l2_term_weight = kaldi.SubVectorFromDLPack(
+            to_dlpack(objf_l2_term_weight_tensor))
 
-chain.ComputeChainObjfAndDeriv(opts=opts,
-                               den_graph=den_graph,
-                               supervision=supervision,
-                               nnet_output=nnet_output,
-                               objf_l2_term_weight=objf_l2_term_weight,
-                               nnet_output_deriv=nnet_output_deriv,
-                               xent_output_deriv=xent_output_deriv)
-print(objf_l2_term_weight_tensor)
-print(nnet_output_deriv_tensor[0, 0])
-print(xent_output_deriv_tensor[0, 0])
+        chain.ComputeChainObjfAndDeriv(opts=opts,
+                                       den_graph=den_graph,
+                                       supervision=supervision,
+                                       nnet_output=nnet_output,
+                                       objf_l2_term_weight=objf_l2_term_weight,
+                                       nnet_output_deriv=nnet_output_deriv,
+                                       xent_output_deriv=xent_output_deriv)
+
+        ctx.save_for_backward(g_nnet_output_deriv_tensor,
+                              g_xent_output_deriv_tensor)
+
+        objf_l2_term_weight_tensor = objf_l2_term_weight_tensor.to(
+            nnet_output_tensor.device())
+        return objf_l2_term_weight_tensor
+
+    @staticmethod
+    def backward(ctx, unused):
+        nnet_output_deriv, xent_output_deriv = ctx.saved_tensors
+        # Multiply by a negative number as we want to do
+        # gradient **descent** and not **ascent**
+        nnet_output_deriv *= -1
+        xent_output_deriv *= -0.1
+
+        # return the derivative for the input parametes:
+        # (opts, den_graph, supervision, nnet_output_tensor, xent_out_unused_tensor)
+        return None, None, None, nnet_output_deriv, xent_output_deriv
