b40d43e4ecb51c6aae1b36de4e410b14ebfa516f
Fangjun Kuang <csukuangfj@gmail.com>
2020-01-12 12:50:22 +0800

    [pybind] support ParseOptions::Register.

--- src/pybind/util/parse_options_pybind.cc
+++ src/pybind/util/parse_options_pybind.cc
@@ -22,50 +22,109 @@
 
 using namespace kaldi;
 
-void pybind_parse_options(py::module& m) {
-  using PyClass = ParseOptions;
-  py::class_<PyClass, OptionsItf>(m, "ParseOptions")
-      .def(py::init<const char*>(), py::arg("usage"))
-      .def("Read",
-           [](PyClass* opts, const std::vector<std::string>& args) {
-             int argc = static_cast<int>(args.size());
-             const char** argv = new const char*[argc + 1];
-             for (int i = 0; i < argc; ++i) {
-               argv[i] = args[i].c_str();
-             }
-             argv[argc] = nullptr;
-             opts->Read(argc, argv);
-             delete[] argv;
-           },
-           "Parses the command line options and fills the "
-           "ParseOptions-registered variables. This must be called after all "
-           "the variables were registered!!!"
-           "\n"
-           "Initially the variables have implicit values, then the config file "
-           "values are set-up, finally the command line values given. Returns "
-           "the first position in argv that was not used. [typically not "
-           "useful: use NumParams() and GetParam(). ]",
-           py::arg("args"))
-      .def("PrintUsage", &PyClass::PrintUsage,
-           "Prints the usage documentation [provided in the constructor].",
-           py::arg("print_command_line") = false)
-      .def("ReadConfigFile", &PyClass::ReadConfigFile,
-           "Reads the options values from a config file.  Must be called after "
-           "registering all options.  This is usually used internally after "
-           "the standard --config option is used, but it may also be called "
-           "from a program.",
-           py::arg("filename"))
-      .def("NumArgs", &PyClass::NumArgs,
-           "Number of positional parameters (c.f. argc-1).")
-      .def("GetArg", &PyClass::GetArg,
-           "Returns one of the positional parameters; 1-based indexing for "
-           "argc/argv compatibility. Will crash if param is not >=1 and "
-           "<=NumArgs().",
-           py::arg("param"))
-      .def("GetOptArg", &PyClass::GetOptArg, py::arg("param"))
-      .def("__str__", [](PyClass* opts) {
+namespace {
+
+template <typename Type>
+struct ArgName;
+
+#define DEFINE_ARG_NAME(type, name)             \
+  template <>                                   \
+  struct ArgName<type> {                        \
+    static constexpr const char* value = #name; \
+  }
+
+DEFINE_ARG_NAME(bool, BoolArg);
+DEFINE_ARG_NAME(int32, Int32Arg);
+DEFINE_ARG_NAME(uint32, UInt32Arg);
+DEFINE_ARG_NAME(float, FloatArg);
+DEFINE_ARG_NAME(double, DoubleArg);
+DEFINE_ARG_NAME(std::string, StringArg);
+
+#undef DEFINE_ARG_NAME
+
+template <typename Type>
+struct Arg {
+  Type value{};
+};
+
+template <typename Type, typename Opt>
+void pybind_arg(py::module& m, Opt& opt) {
+  using PyClass = Arg<Type>;
+  py::class_<PyClass>(m, ArgName<Type>::value)
+      .def(py::init<>())
+      .def_readwrite("value", &PyClass::value)
+      .def("__str__", [](const PyClass& arg) {
         std::ostringstream os;
-        opts->PrintConfig(os);
+        os << arg.value;
         return os.str();
       });
+
+  opt.def("Register",
+          [](typename Opt::type* o, const std::string& name, PyClass* arg,
+             const std::string& doc) { o->Register(name, &arg->value, doc); },
+          py::arg("name"), py::arg("arg"), py::arg("doc"));
+}
+
+}  // namespace
+
+void pybind_parse_options(py::module& m) {
+  using PyClass = ParseOptions;
+
+  auto opt =
+      py::class_<PyClass, OptionsItf>(m, "ParseOptions")
+          .def(py::init<const char*>(), py::arg("usage"))
+          .def("Read",
+               [](PyClass* opts, const std::vector<std::string>& args) {
+                 int argc = static_cast<int>(args.size());
+                 const char** argv = new const char*[argc + 1];
+                 for (int i = 0; i < argc; ++i) {
+                   argv[i] = args[i].c_str();
+                 }
+                 argv[argc] = nullptr;
+                 opts->Read(argc, argv);
+                 delete[] argv;
+               },
+               "Parses the command line options and fills the "
+               "ParseOptions-registered variables. This must be called after "
+               "all "
+               "the variables were registered!!!"
+               "\n"
+               "Initially the variables have implicit values, then the config "
+               "file "
+               "values are set-up, finally the command line values given. "
+               "Returns "
+               "the first position in argv that was not used. [typically not "
+               "useful: use NumParams() and GetParam(). ]",
+               py::arg("args"))
+          .def("PrintUsage", &PyClass::PrintUsage,
+               "Prints the usage documentation [provided in the constructor].",
+               py::arg("print_command_line") = false)
+          .def(
+              "ReadConfigFile", &PyClass::ReadConfigFile,
+              "Reads the options values from a config file.  Must be called "
+              "after "
+              "registering all options.  This is usually used internally after "
+              "the standard --config option is used, but it may also be called "
+              "from a program.",
+              py::arg("filename"))
+          .def("NumArgs", &PyClass::NumArgs,
+               "Number of positional parameters (c.f. argc-1).")
+          .def("GetArg", &PyClass::GetArg,
+               "Returns one of the positional parameters; 1-based indexing for "
+               "argc/argv compatibility. Will crash if param is not >=1 and "
+               "<=NumArgs().",
+               py::arg("param"))
+          .def("GetOptArg", &PyClass::GetOptArg, py::arg("param"))
+          .def("__str__", [](PyClass* opts) {
+            std::ostringstream os;
+            opts->PrintConfig(os);
+            return os.str();
+          });
+
+  pybind_arg<bool>(m, opt);
+  pybind_arg<int32>(m, opt);
+  pybind_arg<uint32>(m, opt);
+  pybind_arg<float>(m, opt);
+  pybind_arg<double>(m, opt);
+  pybind_arg<std::string>(m, opt);
 }
