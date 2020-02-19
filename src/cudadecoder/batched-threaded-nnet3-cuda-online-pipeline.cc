ed54dc1b39f09fa99bd0cc88cf90d89db3004dce
Hugo Braun <hbraun@nvidia.com>
2020-02-19 12:05:16 -0800

    Switch to GetSamplingFrequency

--- src/cudadecoder/batched-threaded-nnet3-cuda-online-pipeline.cc
+++ src/cudadecoder/batched-threaded-nnet3-cuda-online-pipeline.cc
@@ -393,7 +393,7 @@ void BatchedThreadedNnet3CudaOnlinePipeline::ReadParametersFromModel() {
   use_ivectors_ = (feature.IvectorFeature() != NULL);
   input_dim_ = feature.InputFeature()->Dim();
   if (use_ivectors_) ivector_dim_ = feature.IvectorFeature()->Dim();
-  model_frequency_ = feature_info_->GetModelFrequency();
+  model_frequency_ = feature_info_->GetSamplingFrequency();
   BaseFloat frame_shift = feature_info_->FrameShiftInSeconds();
   input_frames_per_chunk_ = config_.compute_opts.frames_per_chunk;
   seconds_per_chunk_ = input_frames_per_chunk_ * frame_shift;
