747f668402f8b74bd7c60e5bab551c1e58067f71
aarora8 <aarora8@jhu.edu>
2020-02-26 12:28:08 -0500

    minor fix

--- egs/chime6/s5_track2/local/install_dscore.sh
+++ egs/chime6/s5_track2/local/install_dscore.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-# Installs pb_chime5
-
+# Installs dscore
 git clone https://github.com/nryant/dscore.git
-#pip3 install intervaltree --user
-#pip3 install tabulate --user
-#pip3 install munkres
+pip3 install intervaltree --user
+pip3 install tabulate --user
+pip3 install munkres --user
 pip3 install pytest --user
-# Download submodule dependencies  # https://stackoverflow.com/a/3796947/5766934
-#git submodule init  
-#git submodule update
