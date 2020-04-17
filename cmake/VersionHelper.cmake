28e2bed59e1673f83472edde1d908ca0a717e4bf
AlexHTHung <AlexHT_Hung@asus.com>
2020-04-17 12:01:48 +0800

    [build] Fix bugs in CMake build (#4044)

--- cmake/VersionHelper.cmake
+++ cmake/VersionHelper.cmake
@@ -8,6 +8,7 @@ function(get_version)
     execute_process(COMMAND git rev-list --count "${version_commit}..HEAD"
                     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
                     OUTPUT_VARIABLE patch_number)
+    string(STRIP ${patch_number} patch_number)
 
     set(KALDI_VERSION ${version} PARENT_SCOPE)
     set(KALDI_PATCH_NUMBER ${patch_number} PARENT_SCOPE)
