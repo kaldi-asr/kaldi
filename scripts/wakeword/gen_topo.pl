e9326e01f17b64ad637f63599eb0a6f338c8811f
Yiming Wang <freewym@gmail.com>
2020-04-19 01:15:28 -0400

    [src,scripts,egs] Add examples for wake-word detection (#3467)

--- scripts/wakeword/gen_topo.pl
+++ scripts/wakeword/gen_topo.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/env perl
 
-# Copyright 2012  Johns Hopkins University (author: Daniel Povey)
+# Copyright 2012-2020  Daniel Povey
+#           2018-2020  Yiming Wang
 
-# Generate a topology file.  This allows control of the number of states in the
-# non-silence HMMs, and in the silence HMMs.
+# This script is modified from utils/gen_topo.pl. It is used for wake word detection
+# chain training, where the transotion probs are even. The topo also contains separate
+# ForwardPdfClass and SelfLoopPdfClass.
 
 if (@ARGV != 4) {
   print STDERR "Usage: utils/gen_topo.pl <num-nonsilence-states> <num-silence-states> <colon-separated-nonsilence-phones> <colon-separated-silence-phones>\n";
@@ -30,7 +32,7 @@ print "$nonsil_phones\n";
 print "</ForPhones>\n";
 for ($state = 0; $state < $num_nonsil_states; $state++) {
   $statep1 = $state+1;
-  print "<State> $state <PdfClass> $state <Transition> $state 0.75 <Transition> $statep1 0.25 </State>\n";
+  print "<State> $state <ForwardPdfClass> @{[2*$state]} <SelfLoopPdfClass> @{[2*$state+1]} <Transition> $state 0.5 <Transition> $statep1 0.5 </State>\n";
 }
 print "<State> $num_nonsil_states </State>\n"; # non-emitting final state.
 print "</TopologyEntry>\n";
@@ -43,7 +45,7 @@ if ($num_sil_states > 1) {
   print "<ForPhones>\n";
   print "$sil_phones\n";
   print "</ForPhones>\n";
-  print "<State> 0 <PdfClass> 0 ";
+  print "<State> 0 <PdfClass> <ForwardPdfClass> 0 <SelfLoopPdfClass> 1 ";
   for ($nextstate = 0; $nextstate < $num_sil_states-1; $nextstate++) { # Transitions to all but last
     # emitting state.
     print "<Transition> $nextstate $transp ";
@@ -51,7 +53,7 @@ if ($num_sil_states > 1) {
   print "</State>\n";
   for ($state = 1; $state < $num_sil_states-1; $state++) { # the central states all have transitions to
     # themselves and to the last emitting state.
-    print "<State> $state <PdfClass> $state ";
+    print "<State> $state <ForwardPdfClass> @{[2*$state]} <SelfLoopPdfClass> @{[2*$state+1]} ";
     for ($nextstate = 1; $nextstate < $num_sil_states; $nextstate++) {
       print "<Transition> $nextstate $transp ";
     }
@@ -59,7 +61,7 @@ if ($num_sil_states > 1) {
   }
   # Final emitting state (non-skippable).
   $state = $num_sil_states-1;
-  print "<State> $state <PdfClass> $state <Transition> $state 0.75 <Transition> $num_sil_states 0.25 </State>\n";
+  print "<State> $state <ForwardPdfClass> @{[2*$state]} <SelfLoopPdfClass> @{[2*$state+1]} <Transition> $state 0.5 <Transition> $num_sil_states 0.5 </State>\n";
   # Final nonemitting state:
   print "<State> $num_sil_states </State>\n";
   print "</TopologyEntry>\n";
@@ -68,9 +70,9 @@ if ($num_sil_states > 1) {
   print "<ForPhones>\n";
   print "$sil_phones\n";
   print "</ForPhones>\n";
-  print "<State> 0 <PdfClass> 0 ";
-  print "<Transition> 0 0.75 ";
-  print "<Transition> 1 0.25 ";
+  print "<State> 0 <ForwardPdfClass> 0 <SelfLoopPdfClass> 1 ";
+  print "<Transition> 0 0.5 ";
+  print "<Transition> 1 0.5 ";
   print "</State>\n";
   print "<State> $num_sil_states </State>\n"; # non-emitting final state.
   print "</TopologyEntry>\n";
