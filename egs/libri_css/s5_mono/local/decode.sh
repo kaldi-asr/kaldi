0ab242e5b95e981cde9f7900a8bffb370565a453
Desh Raj <r.desh26@gmail.com>
2020-06-16 13:28:34 -0400

    removed file from PR

--- egs/libri_css/s5_mono/local/decode.sh
+++ egs/libri_css/s5_mono/local/decode.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # This script decodes raw utterances through the entire pipeline:
-# VAD -> Feature extraction -> Diarization -> ASR
+# Feature extraction -> SAD -> Diarization -> ASR
 #
 # Copyright  2017  Johns Hopkins University (Author: Shinji Watanabe and Yenda Trmal)
 #            2019  Desh Raj, David Snyder, Ashish Arora, Zhaoheng Ni
@@ -11,25 +11,15 @@
 nj=8
 stage=0
 score_sad=true
-diarizer_stage=1
+diarizer_stage=0
 decode_diarize_stage=0
-decode_oracle_stage=0
+decode_oracle_stage=1
 score_stage=0
-nnet3_affix=_cleaned # affix for the chain directory name
-affix=1d_ft   # affix for the TDNN directory name
+affix=1d # This should be the affix of the tdnn model you want to decode with 
 
 # If the following is set to true, we use the oracle speaker and segment
 # information instead of performing SAD and diarization.
 use_oracle_segments=
-rnnlm_rescore=true
-
-# RNNLM rescore options
-ngram_order=4 # approximate the lattice-rescoring by limiting the max-ngram-order
-              # if it's set, it merges histories in the lattice if they share
-              # the same ngram history and this prevents the lattice from 
-              # exploding exponentially
-pruned_rescore=true
-rnnlm_dir=exp/rnnlm_lstm_1a
 
 test_sets="dev eval"
 
@@ -38,11 +28,7 @@ test_sets="dev eval"
 . ./cmd.sh
 . ./path.sh
 
-# Get dev and eval set names from the test_sets
-dev_set=$( echo $test_sets | cut -d " " -f1 )
-eval_set=$( echo $test_sets | cut -d " " -f2 )
-
-$use_oracle_segments && [ $stage -le 8 ] && stage=8
+$use_oracle_segments && [ $stage -le 6 ] && stage=6
 
 #######################################################################
 # Perform SAD on the dev/eval data using py-webrtcvad package
@@ -107,11 +93,9 @@ if [ $stage -le 3 ]; then
     ref_rttm=data/${datadir}/ref_rttm
     steps/segmentation/convert_utt2spk_and_segments_to_rttm.py data/${datadir}/utt2spk.bak \
       data/${datadir}/segments.bak $ref_rttm
-    diar_nj=$(wc -l < "data/$datadir/wav.scp") # This is important especially for VB-HMM
+    diar_nj=$(wc -l < "data/$datadir/wav.scp")
 
-    [ ! -d exp/xvector_nnet_1a ] && ./local/download_diarizer.sh
-
-    local/diarize_spectral.sh --nj $diar_nj --cmd "$train_cmd" --stage $diarizer_stage \
+    local/diarize.sh --nj $diar_nj --cmd "$train_cmd" --stage $diarizer_stage \
       --ref-rttm $ref_rttm \
       exp/xvector_nnet_1a \
       data/${datadir} \
@@ -127,8 +111,8 @@ if [ $stage -le 4 ]; then
     asr_nj=$(wc -l < "data/$datadir/wav.scp")
     local/decode_diarized.sh --nj $asr_nj --cmd "$decode_cmd" --stage $decode_diarize_stage \
       --lm-suffix "_tgsmall" \
-      exp/${datadir}_diarization/rttm data/$datadir data/lang_test_tgsmall \
-      exp/chain${nnet3_affix}/tdnn_${affix} exp/nnet3${nnet3_affix} \
+      exp/${datadir}_diarization data/$datadir data/lang_nosp_test_tgsmall \
+      exp/chain_cleaned/tdnn_${affix}_sp exp/nnet3_cleaned \
       data/${datadir}_diarized || exit 1
   done
 fi
@@ -137,46 +121,14 @@ fi
 # Score decoded dev/eval sets
 #######################################################################
 if [ $stage -le 5 ]; then
+  # final scoring to get the challenge result
   # please specify both dev and eval set directories so that the search parameters
   # (insertion penalty and language model weight) will be tuned using the dev set
   local/score_reco_diarized.sh --stage $score_stage \
-      --dev_decodedir exp/chain${nnet3_affix}/tdnn_${affix}/decode_${dev_set}_diarized_2stage \
-      --dev_datadir ${dev_set}_diarized_hires \
-      --eval_decodedir exp/chain${nnet3_affix}/tdnn_${affix}/decode_${eval_set}_diarized_2stage \
-      --eval_datadir ${eval_set}_diarized_hires
-fi
-
-############################################################################
-# RNNLM rescoring
-############################################################################
-if $rnnlm_rescore; then
-  if [ $stage -le 6 ]; then
-    echo "$0: Perform RNNLM lattice-rescoring"
-    pruned=
-    ac_model_dir=exp/chain${nnet3_affix}/tdnn_${affix}
-    if $pruned_rescore; then
-      pruned=_pruned
-    fi
-    for decode_set in $test_sets; do
-      decode_dir=${ac_model_dir}/decode_${decode_set}_diarized_2stage
-      # Lattice rescoring
-      rnnlm/lmrescore$pruned.sh \
-          --cmd "$decode_cmd --mem 8G" \
-          --weight 0.45 --max-ngram-order $ngram_order \
-          data/lang_test_tgsmall $rnnlm_dir \
-          data/${decode_set}_diarized_hires ${decode_dir} \
-          ${ac_model_dir}/decode_${decode_set}_diarized_2stage_rescore
-    done
-  fi
-  
-  if [ $stage -le 7 ]; then
-    echo "$0: WERs after rescoring with $rnnlm_dir"
-    local/score_reco_diarized.sh --stage 4 \
-        --dev_decodedir exp/chain${nnet3_affix}/tdnn_${affix}/decode_${dev_set}_diarized_2stage_rescore \
-        --dev_datadir ${dev_set}_diarized_hires \
-        --eval_decodedir exp/chain${nnet3_affix}/tdnn_${affix}/decode_${eval_set}_diarized_2stage_rescore \
-        --eval_datadir ${eval_set}_diarized_hires
-  fi
+      --dev_decodedir exp/chain_cleaned/tdnn_${affix}_sp/decode_dev_diarized_2stage \
+      --dev_datadir dev_diarized_hires \
+      --eval_decodedir exp/chain_cleaned/tdnn_${affix}_sp/decode_eval_diarized_2stage \
+      --eval_datadir eval_diarized_hires
 fi
 
 $use_oracle_segments || exit 0
@@ -184,7 +136,7 @@ $use_oracle_segments || exit 0
 ######################################################################
 # Here we decode using oracle speaker and segment information
 ######################################################################
-if [ $stage -le 8 ]; then
+if [ $stage -le 6 ]; then
   # mfccdir should be some place with a largish disk where you
   # want to store MFCC features.
   mfccdir=mfcc
@@ -204,12 +156,11 @@ if [ $stage -le 8 ]; then
   done
 fi
 
-if [ $stage -le 9 ]; then
+if [ $stage -le 7 ]; then
   local/decode_oracle.sh --stage $decode_oracle_stage \
     --affix $affix \
-    --lang-dir data/lang_test_tgsmall \
+    --lang-dir data/lang_nosp_test_tgsmall \
     --lm-suffix "_tgsmall" \
-    --rnnlm-rescore $rnnlm_rescore \
     --test_sets "$test_sets"
 fi
 
