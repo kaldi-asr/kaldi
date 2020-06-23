66779226b67b2a4dd0e88ee3c0e9f1092cd38e99
kk-yy <66395208+kk-yy@users.noreply.github.com>
2020-06-23 09:35:01 +0800

    Update prepare_data.sh
    wrong path

--- egs/swahili/s5/local/prepare_data.sh
+++ egs/swahili/s5/local/prepare_data.sh
@@ -12,29 +12,29 @@ if [[ $1 && $2 ]]; then
 
   cd asr_swahili/data
 
-        pushd $1
-  cp wav.scp ../../../data/$1/wav.scp
+  pushd $1
+  cp wav.scp $local/../data/$1/wav.scp
   popd
 
   pushd $2/wav5
   ls */*.wav |  sed 's/^/asr_swahili\/data\/test\/wav5\//g' > tutu1
   cat tutu1 | sed "s/\//#/g" | awk 'BEGIN{FS="#"} {print $6}' | sed "s/\.wav//g" > tutu2
-  paste tutu2 tutu1 > ../../../../data/$2/wav.scp
+  paste tutu2 tutu1 > $local/../data/$2/wav.scp
   rm tutu1 tutu2
   popd
 
   echo "copy spk2utt, utt2spk, text for $1 $2"
 
   for x in $1 $2; do
-    cp $x/spk2utt ../../data/$x/.
-    cp $x/utt2spk ../../data/$x/.
-    cp $x/text ../../data/$x/.
+    cp $x/spk2utt $local/../data/$x/.
+    cp $x/utt2spk $local/../data/$x/.
+    cp $x/text $local/../data/$x/.
   done
 
-  pushd ../../data/local
+  pushd $local/../data/local
   if [ ! -f  "swahili.arpa" ]; then
-    cd ../../asr_swahili/LM
-    unzip swahili.arpa.zip -d ../../data/local/
+    cd $local/../asr_swahili/LM
+    unzip swahili.arpa.zip -d $local/../data/local/
   fi
   popd
 
