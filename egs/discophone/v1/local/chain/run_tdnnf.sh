beaf75dee949dc59f09f0f1735af3d01a9525847
Piotr Żelasko <petezor@gmail.com>
2020-07-10 12:43:06 -0400

    Start creating a nnet3 discophone recipe

---
+++ egs/discophone/v1/local/chain/run_tdnnf.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+
+# TODO:
+# [x] Combine data dirs
+# [ ] Combine alignment dirs
+# [ ] I-vector extraction
+# [ ] Extract hi-res MFCC
+# [ ] Specify network architecture
+# [ ] Create egs
+# [ ] Training
+# [ ] Decoding
+
