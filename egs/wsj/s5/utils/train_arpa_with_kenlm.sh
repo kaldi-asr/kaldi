2bd51052368bb3c6657cd9e05b312889e045a95c
dophist <jerry.jiayu.du@gmail.com>
2020-08-30 11:38:11 +0800

    add train_arpa_with_kenlm usage description

--- egs/wsj/s5/utils/train_arpa_with_kenlm.sh
+++ egs/wsj/s5/utils/train_arpa_with_kenlm.sh
@@ -1,10 +1,18 @@
+#!/bin/bash
+
+# This script uses kenlm to estimate an arpa model out of training text
+# it is a resort when you hit memory limit dealing with large scale training corpus
+# kenlm estimate arpa using on-disk structure,
+# as long as you have big enough hard disk, memory shouldn't be a problem.
+# by default, kenlm use up to 50% of your local memory, you can control this through -S option
+
 kenlm_opts="" # e.g. "-o 4 -S 50% --prune 0 5 7 7"
 
 # TODO: arg parsing refinement and check
 text=$1
 symbol_table=$2
 dir=$3
-arpa=$4
+arpa_name=$4
 
 # TODO: KenLM installation check
 if [ ! "kenlm is properly installed && can find the training binary: lmplz "] && echo "cannot find training tool *lmplz*, please check your kenlm installation and try again"
@@ -14,7 +22,7 @@ if [ ! "kenlm is properly installed && can find the training binary: lmplz "] &&
 # get rid off irrelavent symbols, the rest of symbols are used as LM training vocabulary. 
 cat $symbol_table | grep -v '<eps>' | grep -v '#0' | grep -v '<unk>' | grep -v '<UNK>' | grep -v '<s>' | grep -v '</s>' | awk '{print $1}' > $dir/ngram.vocab
 
-# KenLM's vocabulary control is trick
+# KenLM's vocabulary control is tricky
 # the behavior of option --limit_vocab_file is not as SRILM's -limit-vocab.
 # --limit_vocab_file actually spcified a vocabulary containing all valid words,
 # but a valid word, unseen in training text, won't appear in final arpa.
@@ -24,4 +32,6 @@ cat $symbol_table | grep -v '<eps>' | grep -v '#0' | grep -v '<unk>' | grep -v '
 # we explicitly append kaldi's vocab to kenlm's training text, and feed kaldi vocab to --limit_vocab_file
 # so we will always get an arpa that has strictly the same vocabulary as kaldi,
 # and the effect of this trick is just as add-one smoothing, shouldn't have any significant impact in practice.
-cat $dir/ngram.vocab $text | lmplz $kenlm_opts --limit_vocab_file $dir/ngram.vocab > $arpa
+cat $dir/ngram.vocab $text | lmplz $kenlm_opts --limit_vocab_file $dir/ngram.vocab > $dir/${arpa_name}.arpa
+
+echo "$0: Done training arpa to: $dir/${arpa_name}.arpa"
