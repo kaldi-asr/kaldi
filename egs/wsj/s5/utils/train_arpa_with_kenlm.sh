f4a82cb8aa243761e16c310dbede287b8082a880
dophist <jerry.jiayu.du@gmail.com>
2020-08-30 11:06:40 +0800

    fix typo

--- egs/wsj/s5/utils/train_arpa_with_kenlm.sh
+++ egs/wsj/s5/utils/train_arpa_with_kenlm.sh
@@ -1,7 +1,10 @@
+kenlm_opts="" # e.g. "-o 4 -S 50% --prune 0 5 7 7"
+
 # TODO: arg parsing refinement and check
 text=$1
 symbol_table=$2
-arpa=$3
+dir=$3
+arpa=$4
 
 # TODO: KenLM installation check
 if [ ! "kenlm is properly installed && can find the training binary: lmplz "] && echo "cannot find training tool *lmplz*, please check your kenlm installation and try again"
@@ -11,7 +14,7 @@ if [ ! "kenlm is properly installed && can find the training binary: lmplz "] &&
 # get rid off irrelavent symbols, the rest of symbols are used as LM training vocabulary. 
 cat $symbol_table | grep -v '<eps>' | grep -v '#0' | grep -v '<unk>' | grep -v '<UNK>' | grep -v '<s>' | grep -v '</s>' | awk '{print $1}' > $dir/ngram.vocab
 
-# KenLM's vocabulary contoll is trick
+# KenLM's vocabulary control is trick
 # the behavior of option --limit_vocab_file is not as SRILM's -limit-vocab.
 # --limit_vocab_file actually spcified a vocabulary containing all valid words,
 # but a valid word, unseen in training text, won't appear in final arpa.
@@ -21,4 +24,4 @@ cat $symbol_table | grep -v '<eps>' | grep -v '#0' | grep -v '<unk>' | grep -v '
 # we explicitly append kaldi's vocab to kenlm's training text, and feed kaldi vocab to --limit_vocab_file
 # so we will always get an arpa that has strictly the same vocabulary as kaldi,
 # and the effect of this trick is just as add-one smoothing, shouldn't have any significant impact in practice.
-cat $dir/ngram.vocab $processed_text | lmplz $kenlm_opts --limit_vocab_file $dir/ngram.vocab > $arpa
+cat $dir/ngram.vocab $text | lmplz $kenlm_opts --limit_vocab_file $dir/ngram.vocab > $arpa
