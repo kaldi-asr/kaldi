c5982a5f3f955e86654f85862015716611065caf
dophist <jerry.jiayu.du@gmail.com>
2020-08-30 11:00:46 +0800

    clearify the trick to explicitly enforce strictly the same vocab between kaldi and kenlm

--- egs/wsj/s5/utils/train_arpa_with_kenlm.sh
+++ egs/wsj/s5/utils/train_arpa_with_kenlm.sh
@@ -11,5 +11,14 @@ if [ ! "kenlm is properly installed && can find the training binary: lmplz "] &&
 # get rid off irrelavent symbols, the rest of symbols are used as LM training vocabulary. 
 cat $symbol_table | grep -v '<eps>' | grep -v '#0' | grep -v '<unk>' | grep -v '<UNK>' | grep -v '<s>' | grep -v '</s>' | awk '{print $1}' > $dir/ngram.vocab
 
-# KenLM training: cat vocab & text together to make sure kenlm has strictly the same vocabulary as kaldi symbol table
+# KenLM's vocabulary contoll is trick
+# the behavior of option --limit_vocab_file is not as SRILM's -limit-vocab.
+# --limit_vocab_file actually spcified a vocabulary containing all valid words,
+# but a valid word, unseen in training text, won't appear in final arpa.
+# that means, kenlm arpa's actual vocab is partly and implicitly defined by the training text
+# this will bring inconsistency between kenlm and kaldi system
+# so the trick is, exploiting the fact that kenlm will never prune unigram,
+# we explicitly append kaldi's vocab to kenlm's training text, and feed kaldi vocab to --limit_vocab_file
+# so we will always get an arpa that has strictly the same vocabulary as kaldi,
+# and the effect of this trick is just as add-one smoothing, shouldn't have any significant impact in practice.
 cat $dir/ngram.vocab $processed_text | lmplz $kenlm_opts --limit_vocab_file $dir/ngram.vocab > $arpa
