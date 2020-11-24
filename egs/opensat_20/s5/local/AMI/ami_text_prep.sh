bc802e1f5b797b12e364471eedfc788b2874d783
aarora8 <aarora8@jhu.edu>
2020-11-24 02:29:23 -0500

    minor fix

--- egs/opensat_20/s5/local/AMI/ami_text_prep.sh
+++ egs/opensat_20/s5/local/AMI/ami_text_prep.sh
@@ -32,7 +32,7 @@ fi
 
 
 # extract text from AMI XML annotations,
-local/ami_xml2text.sh $dir
+local/AMI/ami_xml2text.sh $dir
 
 wdir=data/local/AMI_annotations
 [ ! -f $wdir/transcripts1 ] && echo "$0: File $wdir/transcripts1 not found." && exit 1;
