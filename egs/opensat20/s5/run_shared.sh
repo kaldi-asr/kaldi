ccc767e7f260e28743158f1e6fc94543500ca72a
aarora8 <aarora8@jhu.edu>
2020-11-26 02:23:27 -0500

    cosmetic changes

--- egs/opensat20/s5/run_shared.sh
+++ egs/opensat20/s5/run_shared.sh
@@ -24,6 +24,10 @@ ICSI_DIR=/export/corpora5/LDC/LDC2004S02/meeting_speech/speech
 AMI_DIR=/export/corpora5/amicorpus/
 
 if [ $stage -le 0 ]; then
+  echo ============================================================================
+  echo "              Prepare SAFE-T data"
+  echo ============================================================================
+
   local/safet_data_prep.sh ${SAFE_T_AUDIO_R11} ${SAFE_T_TEXTS_R11} data/safe_t_r11
   local/safet_data_prep.sh ${SAFE_T_AUDIO_R20} ${SAFE_T_TEXTS_R20} data/safe_t_r20
   local/safet_data_prep.sh ${SAFE_T_AUDIO_DEV1} ${SAFE_T_TEXTS_DEV1} data/safe_t_dev1
@@ -37,6 +41,9 @@ if [ $stage -le 1 ]; then
 fi
 
 if [ $stage -le 2 ]; then
+  echo ============================================================================
+  echo "              Prepare ICSI data"
+  echo ============================================================================
   #prepare annotations, note: dict is assumed to exist when this is called
   local/ICSI/icsi_run_prepare_shared.sh
   local/ICSI/icsi_ihm_data_prep.sh $ICSI_DIR
@@ -45,6 +52,9 @@ if [ $stage -le 2 ]; then
 fi
 
 if [ $stage -le 3 ]; then
+  echo ============================================================================
+  echo "              Prepare AMI data"
+  echo ============================================================================
   local/AMI/ami_text_prep.sh data/local/download
   local/AMI/ami_ihm_data_prep.sh $AMI_DIR
   local/AMI/ami_ihm_scoring_data_prep.sh $AMI_DIR dev
@@ -94,6 +104,9 @@ if [ $stage -le 6 ]; then
 fi
 
 if [ $stage -le 7 ] ; then
+  echo ============================================================================
+  echo "              Obtain LM from SAFE-T train text"
+  echo ============================================================================
   local/safet_train_lms_srilm.sh \
     --train_text data/train_safet/text --dev_text data/safe_t_dev1/text  \
     data/ data/local/srilm
@@ -102,6 +115,9 @@ if [ $stage -le 7 ] ; then
 fi
 
 if [ $stage -le 8 ] ; then
+   echo ============================================================================
+  echo "              Extract features"
+  echo ============================================================================
   utils/data/combine_data.sh data/train_icsiami data/ICSI/train data/AMI/train
   for dset in train_icsiami train_safet; do
     steps/make_mfcc.sh --nj 75 --cmd "$train_cmd" data/$dset
@@ -114,6 +130,9 @@ fi
 suffix=all
 # monophone training
 if [ $stage -le 9 ]; then
+  echo ============================================================================
+  echo "        GMM-HMM training : mono, delta, LDA + MLLT + SAT"
+  echo ============================================================================
   utils/subset_data_dir.sh data/train_${suffix} 15000 data/train_15k
   steps/train_mono.sh --nj $nj --cmd "$train_cmd" \
     data/train_15k data/lang_nosp_test exp/mono_train_${suffix}
@@ -145,7 +164,13 @@ if [ $stage -le 12 ]; then
 fi
 
 if [ $stage -le 13 ]; then
+  echo ============================================================================
+  echo "              augmentation, i-vector extraction, and chain model training"
+  echo ============================================================================
   local/chain/run_cnn_tdnn_shared.sh
 fi
 
+echo ===========================================================================
+echo "Finished Successfully"
+echo ===========================================================================
 exit 0
