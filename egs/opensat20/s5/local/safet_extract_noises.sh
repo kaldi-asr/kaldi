c9361c4f5df04302e3f6f4964061f1726e004304
aarora8 <aarora8@jhu.edu>
2020-12-05 15:20:02 -0500

    adding comments

--- egs/opensat20/s5/local/safet_extract_noises.sh
+++ egs/opensat20/s5/local/safet_extract_noises.sh
@@ -13,16 +13,29 @@ if [ $# != 1 ]; then
    echo "Wrong #arguments ($#, expected 1)"
    echo "Usage: local/safet_extract_noises.sh [options] data/train_safet"
    echo "This script extract noises and use it for augmentation"
-   exit 1;
+   echo "Data, files created = data/train_safet/time_stamp/, data/train_safet/wav_files/ "
+   echo "data/local/audio_list  data/safe_t_noise_wavfiles data/safe_t_noise"
+   echo "data/safe_t_noise_filtered  " 
+  exit 1;
 fi
 
 indir=$1
-#outdir=$2
 
 # it creates a text file which have all the time stamps belonging to that wav file
 # which have transcription. The $indir/time_stamp/ directory will contain list of
 # time segments (where the transcription is present) for wavfile
+
 if [ $stage -le 0 ]; then
+  echo "$0: remove old files if they already exist"
+  rm -r $indir/time_stamp/ $indir/wav_files/
+  rm data/local/audio_list
+  rm -r data/safe_t_noise_wavfiles data/safe_t_noise_filtered
+  rm -r data/safe_t_noise
+fi
+
+if [ $stage -le 0 ]; then
+  echo "$0: extracting list of speech segments for each wavfile."
+  rm -r $indir/time_stamp/
   mkdir -p $indir/time_stamp/
   while read -r line;
     do
@@ -33,6 +46,7 @@ fi
 
 ## it reads a flac file and converts into into wav file
 if [ $stage -le 1 ]; then
+  echo "$0: converting .flac to .wav"
   mkdir -p $indir/wav_files/
   while read -r line;
     do
@@ -46,6 +60,7 @@ fi
 
 ## it reads a $indir/wav.scp file and creates audio_list
 if [ $stage -le 2 ]; then
+  echo "$0: store audiolist (recording id)"
   rm data/local/audio_list
   while read -r line;
     do
@@ -56,21 +71,16 @@ fi
 
 ## it reads a audio wav files, wav time stamps and audio list file and creates noises
 if [ $stage -le 3 ]; then
+  echo "$0: extract noise from the safet wav files"
   local/safet_extract_noises.py $indir/wav_files $indir/time_stamp data/local/audio_list data/safe_t_noise_wavfiles
 fi
-
-# it will give to 10050 noise wav files, its total duration is 55hrs
-#if [ $stage -le 4 ]; then
-#  for wav_name in distant_noises/*.wav; do
-#    soxi -D $wav_name >> data/local/audio_list/
-#  done
-#fi
 
 # it will give to 10050 noise wav files, its total duration is 55hrs
 # utt2spk: <utterance-id> <speaker-id>: noise1 noise1
 # wav.scp <recording-id> <wav-path> : noise1 data/safe_t_noise_wavfiles/noise1.wav
 # segments:  <utterance-id> <recording-id> <segment-begin> <segment-end> segments: noise1 noise1 0 20
 if [ $stage -le 5 ]; then
+  echo "$0: create kaldi style data directory for the noise data"
   mkdir -p data/safe_t_noise
   for wav_name in data/safe_t_noise_wavfiles/*.wav; do
     recording_id=$(echo "$wav_name" | cut -d"/" -f 3)
@@ -86,6 +96,7 @@ fi
 
 # # it will apply VAD to noise to find those noise wavfiles which have silence
 if [ $stage -le 6 ]; then
+  echo "$0: extract features for the safet noise data"
   steps/make_mfcc.sh --cmd "$train_cmd" --nj 60 data/safe_t_noise
   steps/compute_cmvn_stats.sh data/safe_t_noise
   sid/compute_vad_decision.sh --nj 40 --cmd "$train_cmd" data/safe_t_noise
@@ -94,6 +105,7 @@ fi
 
 
 if [ $stage -le 7 ]; then
+  echo "$0: filter noises using energy based VAD"
   mkdir -p data/safe_t_noise_filtered
   local/get_percent_overlap.py data/safe_t_noise > data/safe_t_noise/percentage_speech
   while read -r line;
