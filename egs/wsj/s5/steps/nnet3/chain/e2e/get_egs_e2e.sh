1453c922e0886c078d810cd5ae61fdc6fe3f9e43
Daniel Povey <dpovey@gmail.com>
2020-12-15 10:58:24 +0800

    [scripts] Change num combine examples in e2e setup (#4389)

--- egs/wsj/s5/steps/nnet3/chain/e2e/get_egs_e2e.sh
+++ egs/wsj/s5/steps/nnet3/chain/e2e/get_egs_e2e.sh
@@ -27,8 +27,8 @@ compress=true   # set this to false to disable compression (e.g. if you want to 
 num_utts_subset=1400     # number of utterances in validation and training
                         # subsets used for shrinkage and diagnostics.
 num_valid_egs_combine=0  # #validation examples for combination weights at the very end.
-num_train_egs_combine=250 # number of train examples for the above.
-num_egs_diagnostic=300 # number of frames for "compute_prob" jobs
+num_train_egs_combine=1000 # number of train examples for the above.
+num_egs_diagnostic=700 # number of examples for "compute_prob" jobs
 frames_per_iter=400000 # each iteration of training, see this many frames per
                        # job, measured at the sampling rate of the features
                        # used.  This is just a guideline; it will pick a number
