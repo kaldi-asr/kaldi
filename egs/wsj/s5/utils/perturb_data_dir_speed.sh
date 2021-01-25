dbff09ed7995d70a4da0805e5b24863f52f7f4f3
Xuechen Liu <553473428@qq.com>
2021-01-25 03:50:44 +0100

    [scripts] Add option to not modify speaker labels when doing speed augmentation (#4432)

--- egs/wsj/s5/utils/perturb_data_dir_speed.sh
+++ egs/wsj/s5/utils/perturb_data_dir_speed.sh
@@ -16,6 +16,8 @@
 #
 # It generates the files which are used for perturbing the speed of the original data.
 
+include_spk_prefix=true
+
 . utils/parse_options.sh
 
 if [ $# != 3 ]; then
@@ -31,8 +33,15 @@ factor=$1
 srcdir=$2
 destdir=$3
 label="sp"
-spk_prefix=$label$factor"-"
-utt_prefix=$label$factor"-"
+if $include_spk_prefix; then
+  spk_prefix=$label$factor"-"
+  utt_prefix=$label$factor"-"
+  utt_postfix=""
+else
+  spk_prefix=""
+  utt_prefix=""
+  utt_postfix="-"$label$factor
+fi
 
 #check is sox on the path
 which sox &>/dev/null
@@ -53,15 +62,27 @@ set -o pipefail
 
 mkdir -p $destdir
 
-cat $srcdir/utt2spk | awk -v p=$utt_prefix '{printf("%s %s%s\n", $1, p, $1);}' > $destdir/utt_map
 cat $srcdir/spk2utt | awk -v p=$spk_prefix '{printf("%s %s%s\n", $1, p, $1);}' > $destdir/spk_map
-cat $srcdir/wav.scp | awk -v p=$spk_prefix '{printf("%s %s%s\n", $1, p, $1);}' > $destdir/reco_map
-if [ ! -f $srcdir/utt2uniq ]; then
-  cat $srcdir/utt2spk | awk -v p=$utt_prefix '{printf("%s%s %s\n", p, $1, $1);}' > $destdir/utt2uniq
-else
-  cat $srcdir/utt2uniq | awk -v p=$utt_prefix '{printf("%s%s %s\n", p, $1, $2);}' > $destdir/utt2uniq
-fi
 
+# for utterance-level mapping files (utts and recos), if we do not perform
+# speaker augmentation, we put utterance affix as postfix
+if $include_spk_prefix; then
+  cat $srcdir/utt2spk | awk -v p=$utt_prefix '{printf("%s %s%s\n", $1, p, $1);}' > $destdir/utt_map
+  cat $srcdir/wav.scp | awk -v p=$spk_prefix '{printf("%s %s%s\n", $1, p, $1);}' > $destdir/reco_map
+  if [ ! -f $srcdir/utt2uniq ]; then
+    cat $srcdir/utt2spk | awk -v p=$utt_prefix '{printf("%s%s %s\n", p, $1, $1);}' > $destdir/utt2uniq
+  else
+    cat $srcdir/utt2uniq | awk -v p=$utt_prefix '{printf("%s%s %s\n", p, $1, $2);}' > $destdir/utt2uniq
+  fi
+else
+  cat $srcdir/utt2spk | awk -v p=$utt_postfix '{printf("%s %s%s\n", $1, $1, p);}' > $destdir/utt_map
+  cat $srcdir/wav.scp | awk -v p=$utt_postfix '{printf("%s %s%s\n", $1, $1, p);}' > $destdir/reco_map
+  if [ ! -f $srcdir/utt2uniq ]; then
+    cat $srcdir/utt2spk | awk -v p=$utt_postfix '{printf("%s%s %s\n", $1, p, $1);}' > $destdir/utt2uniq
+  else
+    cat $srcdir/utt2uniq | awk -v p=$utt_postfix '{printf("%s%s %s\n", $1, p, $2);}' > $destdir/utt2uniq
+  fi
+fi
 
 cat $srcdir/utt2spk | utils/apply_map.pl -f 1 $destdir/utt_map  | \
   utils/apply_map.pl -f 2 $destdir/spk_map >$destdir/utt2spk
