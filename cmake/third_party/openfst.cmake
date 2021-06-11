ca6d133262aa183b23f6daba48995bd7576fb572
Fang Huangqiang <fhq981014@gmail.com>
2021-06-12 06:43:47 +0800

    [cmake] Fetch OpenFST at configure time. (#4558)

--- cmake/third_party/openfst.cmake
+++ cmake/third_party/openfst.cmake
@@ -1,14 +1,25 @@
-cmake_minimum_required(VERSION 2.8.2)
-project(openfst-download NONE)
+cmake_minimum_required(VERSION 3.14)
+include(FetchContent)
 
-include(ExternalProject)
-ExternalProject_Add(openfst
-    GIT_REPOSITORY https://github.com/kkm000/openfst
-    GIT_TAG 0bca6e76d24647427356dc242b0adbf3b5f1a8d9 # tag win/1.7.2.1
-    SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/openfst"
-    BINARY_DIR ""
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
-    TEST_COMMAND ""
+FetchContent_Declare(
+        openfst
+        GIT_REPOSITORY  https://github.com/kkm000/openfst
+        GIT_TAG         338225416178ac36b8002d70387f5556e44c8d05 # tag win/1.7.2.1
 )
+
+FetchContent_GetProperties(openfst)
+if(NOT openfst_POPULATED)
+    FetchContent_Populate(openfst)
+    include_directories(${openfst_SOURCE_DIR}/src/include)
+
+    add_subdirectory(${openfst_SOURCE_DIR} ${openfst_BINARY_DIR})
+
+    install(DIRECTORY ${openfst_SOURCE_DIR}/src/include/ DESTINATION include/
+            FILES_MATCHING PATTERN "*.h")
+
+    install(TARGETS fst
+            EXPORT kaldi-targets
+            ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+            LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+            RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
+endif()
