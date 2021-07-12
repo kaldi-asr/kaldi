360528ee79f86b3a0a19f85f02335d9bd943903f
Anton Mitrofanov <mitrofanov-aa@speechpro.com>
2021-07-12 11:41:51 +0300

    2.55 epoch 103 ( 34 epoch per loop)

---
+++ egs/librispeech/s5/fairseq_ltlm/recipes/tuning/train_small_ltlm.lr1e-4.ddp2.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+#results 
+### ltlm.tasks.rescoring_task | Epoch 103. Valid wer is %WER 2.55 [ 1387 / 54402, 193 ins, 109 del, 1085 sub ]
+
+set -e
+. ./path.sh
+. ./fairseq_ltlm/path.sh
+
+stage=0
+
+# Exp options
+cmd_gpu=
+exp_dir=
+
+model_dir=
+graph=
+
+#Scales
+lmwt=12
+transformer_weight=12
+filter=
+
+# LTLM training params
+epoch=6 # REAL all data epoch
+lr=0.0001 # learning rate
+wd=1e-6 # weight decay
+c=1.25  # gradient clipping
+
+valid_data_dir=data/dev_clean_hires
+btz=22
+uf=8
+split_per_epoch=8
+
+. ./utils/parse_options.sh
+
+
+# out dir
+base_exp_dir=$exp_dir/lt_small_ddp_new_dc/2nodes
+ltlm_dir=$base_exp_dir/lr${lr}_wd${wd}_cl${c}_btz${btz}_uf${uf}_spe${split_per_epoch}
+
+# Data
+train_decoded_lats=$(cut -d' ' -f2 $exp_dir/train.decoded)
+train_generated_lats=$(cut -d' ' -f2 $exp_dir/train.generated)
+train_egs=$(for lat in $train_generated_lats ; do echo $lat/lt_egs_$training_type; done)
+num_egs=$(echo $train_egs | wc -w )
+#echo $num_egs
+
+# generated data epoch + real decode data epoch
+part_epochs=$(($epoch * (num_egs + $split_per_epoch - 1) / $split_per_epoch + $epoch))
+
+#echo "$part_epochs"
+#exit
+
+
+
+valid_egs=$model_dir/decode_$(basename $valid_data_dir)_graph_tgsmall_tglarge/lt_egs_$training_type
+
+###
+
+
+if [ $stage -le 0 ] ; then 
+	echo "Generate data_config.json"
+	python fairseq_ltlm/ltlm/pyscripts/get_data_config.py \
+			--train $train_decoded_lats/lt_egs_$training_type,'',0 ${train_egs}  \
+			--split_per_epoch ${split_per_epoch} \
+			--valid $valid_egs,$valid_data_dir/text \
+			--out $ltlm_dir/data_config.json
+fi
+
+if [ $stage -le 1 ] ; then
+	echo "Training model. "
+	sbatch  --output $ltlm_dir/full_log.out --error $ltlm_dir/full_log.err --nodes=2 \
+	fairseq_ltlm/recipes/tuning/sbatch_ddp_train.sh \
+				--user-dir fairseq_ltlm/ltlm/ \
+				--criterion bce_loss \
+				--task rescoring_task \
+				--arch lt_small \
+				--tokenizer_fn $graph/words.txt \
+				--data_json $ltlm_dir/data_config.json \
+				--lmwt $lmwt \
+				--model_weight $transformer_weight \
+				--hyp_filter $filter \
+				--max_len 600 \
+				--max-sentences $btz \
+				--optimizer adam \
+				--lr $lr \
+				--curriculum 2 \
+				--weight-decay $wd \
+				--clip-norm $c \
+				--max-epoch $part_epochs \
+				--num-workers 1 \
+				--no-epoch-checkpoints \
+				--warmup-updates 4000 \
+				--log-interval 100 \
+				--fp16 \
+				--dropout 0.2 \
+				--attention-dropout 0.05 \
+				--update-freq $uf \
+				--save-dir $ltlm_dir \
+				#--validate-interval 10 \
+
+				#--grad-checkpointing \
+fi
+
+exit 
+
+if [ $stage -le 2 ] ; then 
+	echo "Evaluating model. Logging in $ltlm_dir/eval.log"
+	$cmd_gpu --num_threads 12 --gpu 1 $exp_dir/eval.bce.log python fairseq_ltlm/ltlm/eval.py \
+			--device cuda \
+			--strategy base \
+			--max_len 600 \
+			--lmwt $lmwt \
+			--tokenizer_fn $graph/words.txt \
+			--model_weight $transformer_weight \
+			--data "$valid_egs_dir" \
+			$ltlm_dir/checkpoint_best.pt \
+			$valid_data_dir/text
+fi
+
+echo "Done"
+
