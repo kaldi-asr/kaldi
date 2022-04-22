a17776c0fcba8a3340eb76febddd91744c7f0372
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2022-04-21 17:48:55 -0700

    Remaining Conda-forge changes

--- src/cudadecoder/batched-threaded-nnet3-cuda-online-pipeline.cc
+++ src/cudadecoder/batched-threaded-nnet3-cuda-online-pipeline.cc
@@ -32,6 +32,23 @@
 #include "lat/lattice-functions.h"
 #include "nnet3/nnet-utils.h"
 
+// Necessary for supporting online2 libary on Windows
+#ifdef _WIN32
+  #include <windows.h>
+
+  void usleep(__int64 usec)
+  {
+      HANDLE timer;
+      LARGE_INTEGER ft;
+
+      ft.QuadPart = -(10*usec); // Convert to 100 nanosecond interval, negative value indicates relative time
+
+      timer = CreateWaitableTimer(NULL, TRUE, NULL);
+      SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
+      WaitForSingleObject(timer, INFINITE);
+      CloseHandle(timer);
+  }
+#endif
 namespace kaldi {
 namespace cuda_decoder {
 
