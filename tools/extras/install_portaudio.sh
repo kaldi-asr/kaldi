c680ca469f9d0681576dde9eb48c1807cfe7dd16
Jan "yenda" Trmal <jtrmal@gmail.com>
2022-08-12 11:37:31 +0200

    fix portaudio install script, closes #4755

--- tools/extras/install_portaudio.sh
+++ tools/extras/install_portaudio.sh
@@ -43,31 +43,32 @@ portaudio_tarball="pa_stable_${VERSION}.tgz"
 portaudio_github_tarball="pa_stable_${VERSION}_r1788.tar.gz"
 
 if [ ! -e $portaudio_tarball ]; then
-    echo "Could not find portaudio tarball $portaudio_tarball locally, downloading it..."
+  echo "Could not find portaudio tarball $portaudio_tarball locally, downloading it..."
 
-    if [ -d "$DOWNLOAD_DIR" ]; then
-        cp -p "$DOWNLOAD_DIR/$portaudio_tarball" .
-    else
-        if ! $WGET --version >&/dev/null; then
-            echo "This script requires you to first install wget"
-            echo "You can also just download pa_stable_$VERSION.tgz from"
-            echo "http://www.portaudio.com/download.html)"
-            exit 1;
-        fi
-	$WGET -nv -T 10 -t 3 -O $portaudio_tarball https://github.com/PortAudio/portaudio/archive/refs/tags/${portaudio_github_tarball} || \
-	$WGET -nv -T 10 -t 3 -O $portaudio_tarball http://www.portaudio.com/archives/$portaudio_tarball || \
-	rm ${portaudio_tarball}
-
+  if [ -d "$DOWNLOAD_DIR" ]; then
+    cp -p "$DOWNLOAD_DIR/$portaudio_tarball" .
+  else
+    if ! $WGET --version >&/dev/null; then
+      echo "This script requires you to first install wget"
+      echo "You can also just download pa_stable_$VERSION.tgz from"
+      echo "http://www.portaudio.com/download.html)"
+      exit 1;
     fi
 
-    if [ ! -e $portaudio_tarball ]; then
-        echo "Download of $portaudio_tarball - failed."
-        echo "Aborting script. Please download and install port audio manually."
-        exit 1;
-    fi
+    $WGET --no-check-certificate -nv -T 10 -t 3 -O $portaudio_tarball https://github.com/PortAudio/portaudio/archive/refs/tags/${portaudio_github_tarball} || \
+      $WGET --no-check-certificate -nv -T 10 -t 3 -O $portaudio_tarball http://files.portaudio.com/archives/$portaudio_tarball || \
+      rm ${portaudio_tarball}
+
+  fi
+
+  if [ ! -e $portaudio_tarball ]; then
+    echo "Download of $portaudio_tarball - failed."
+    echo "Aborting script. Please download and install port audio manually."
+    exit 1;
+  fi
 fi
 
-mkdir portaudio && tar -xzf $portaudio_tarball -C portaudio --strip-components 1 || exit 1
+mkdir -p  portaudio && tar -xzf $portaudio_tarball -C portaudio --strip-components 1 || exit 1
 
 read -d '' pa_patch << "EOF"
 --- portaudio/Makefile.in	2012-08-05 10:42:05.000000000 +0300
